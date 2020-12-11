14ce70c8163dd191cc62fb943517408e0f3dc2a3
Vikram Singh <vikram@devtron.ai>
2020-12-11 13:36:41 +0530

    db migration added for chart repo

---
+++ scripts/sql/3_alter_down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "public"."chart_repo" DROP COLUMN "user_name";
+ALTER TABLE "public"."chart_repo" DROP COLUMN "password";
+ALTER TABLE "public"."chart_repo" DROP COLUMN "ssh_key";
+ALTER TABLE "public"."chart_repo" DROP COLUMN "access_token";
+ALTER TABLE "public"."chart_repo" DROP COLUMN "auth_mode";
