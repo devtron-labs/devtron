323bbebede9ce21ecf650da5293c1688de0b610d
Vikram Singh <vikram@devtron.ai>
2020-12-16 14:04:40 +0530

    sso_login_config sql

---
+++ scripts/sql/3_create.up.sql
@@ -0,0 +1,16 @@
+CREATE SEQUENCE IF NOT EXISTS "public"."id_seq_sso_login_config";
+
+CREATE TABLE "public"."sso_login_config"
+  (
+     "id"         INT4 NOT NULL DEFAULT NEXTVAL('id_seq_sso_login_config'::
+     regclass),
+     "name"       VARCHAR(250),
+     "url"        VARCHAR(250),
+     "config"     TEXT,
+     "created_on" TIMESTAMPTZ,
+     "created_by" INT4,
+     "updated_on" TIMESTAMPTZ,
+     "updated_by" INT4,
+     "active"     BOOL,
+     PRIMARY KEY ("id")
+  );
