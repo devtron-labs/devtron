5320719c3ad164e2c7a29e9efd7c2e0fc20cf638
nishant <nishant@devtron.ai>
2021-01-04 23:35:07 +0530

    resource type name correction:

--- internal/util/K8sUtil.go
+++ internal/util/K8sUtil.go
@@ -100,7 +100,7 @@ func (impl K8sUtil) CreateArgoApplication(namespace string, application string, 
 	}
 	res, err := client.RESTClient().
 		Post().
-		Resource("Application").
+		Resource("application").
 		Namespace(namespace).
 		Body([]byte(application)).
 		Do().
