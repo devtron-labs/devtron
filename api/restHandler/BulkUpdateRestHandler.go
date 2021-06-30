234c8eb2a5702d1fc140f95ec27c59334664dd3c
kartik-579 <kartik@devtron.ai>
2021-06-30 12:09:47 +0530

    new router & restHandler for bulk update

--- api/restHandler/BulkUpdateRestHandler.go
+++ api/restHandler/BulkUpdateRestHandler.go
@@ -1,1 +1,159 @@
 package restHandler
+
+import (
+	"encoding/json"
+	"github.com/devtron-labs/devtron/client/argocdServer/application"
+	"github.com/devtron-labs/devtron/client/gitSensor"
+	"github.com/devtron-labs/devtron/internal/sql/repository/pipelineConfig"
+	"github.com/devtron-labs/devtron/internal/sql/repository/security"
+	"github.com/devtron-labs/devtron/pkg/appClone"
+	"github.com/devtron-labs/devtron/pkg/appWorkflow"
+	request "github.com/devtron-labs/devtron/pkg/cluster"
+	"github.com/devtron-labs/devtron/pkg/pipeline"
+	security2 "github.com/devtron-labs/devtron/pkg/security"
+	"github.com/devtron-labs/devtron/pkg/team"
+	"github.com/devtron-labs/devtron/pkg/user"
+	"github.com/devtron-labs/devtron/util/rbac"
+	"go.uber.org/zap"
+	"gopkg.in/go-playground/validator.v9"
+	"net/http"
+)
+
+type BulkUpdateRestHandler interface {
+	GetExampleInputBulkUpdate(w http.ResponseWriter, r *http.Request)
+	GetAppNameDeploymentTemplate(w http.ResponseWriter, r *http.Request)
+	BulkUpdateDeploymentTemplate(w http.ResponseWriter, r *http.Request)
+}
+type BulkUpdateRestHandlerImpl struct {
+	pipelineBuilder         pipeline.PipelineBuilder
+	ciPipelineRepository    pipelineConfig.CiPipelineRepository
+	ciHandler               pipeline.CiHandler
+	Logger                  *zap.SugaredLogger
+	chartService            pipeline.ChartService
+	propertiesConfigService pipeline.PropertiesConfigService
+	dbMigrationService      pipeline.DbMigrationService
+	application             application.ServiceClient
+	userAuthService         user.UserService
+	validator               *validator.Validate
+	teamService             team.TeamService
+	enforcer                rbac.Enforcer
+	gitSensorClient         gitSensor.GitSensorClient
+	pipelineRepository      pipelineConfig.PipelineRepository
+	appWorkflowService      appWorkflow.AppWorkflowService
+	enforcerUtil            rbac.EnforcerUtil
+	envService              request.EnvironmentService
+	gitRegistryConfig       pipeline.GitRegistryConfig
+	dockerRegistryConfig    pipeline.DockerRegistryConfig
+	cdHandelr               pipeline.CdHandler
+	appCloneService         appClone.AppCloneService
+	materialRepository      pipelineConfig.MaterialRepository
+	policyService           security2.PolicyService
+	scanResultRepository    security.ImageScanResultRepository
+}
+
+func NewBulkUpdateRestHandlerImpl(pipelineBuilder pipeline.PipelineBuilder, Logger *zap.SugaredLogger,
+	chartService pipeline.ChartService,
+	propertiesConfigService pipeline.PropertiesConfigService,
+	dbMigrationService pipeline.DbMigrationService,
+	application application.ServiceClient,
+	userAuthService user.UserService,
+	teamService team.TeamService,
+	enforcer rbac.Enforcer,
+	ciHandler pipeline.CiHandler,
+	validator *validator.Validate,
+	gitSensorClient gitSensor.GitSensorClient,
+	ciPipelineRepository pipelineConfig.CiPipelineRepository, pipelineRepository pipelineConfig.PipelineRepository,
+	enforcerUtil rbac.EnforcerUtil, envService request.EnvironmentService,
+	gitRegistryConfig pipeline.GitRegistryConfig, dockerRegistryConfig pipeline.DockerRegistryConfig,
+	cdHandelr pipeline.CdHandler,
+	appCloneService appClone.AppCloneService,
+	appWorkflowService appWorkflow.AppWorkflowService,
+	materialRepository pipelineConfig.MaterialRepository, policyService security2.PolicyService,
+	scanResultRepository security.ImageScanResultRepository) *BulkUpdateRestHandlerImpl {
+	return &BulkUpdateRestHandlerImpl{
+		pipelineBuilder:         pipelineBuilder,
+		Logger:                  Logger,
+		chartService:            chartService,
+		propertiesConfigService: propertiesConfigService,
+		dbMigrationService:      dbMigrationService,
+		application:             application,
+		userAuthService:         userAuthService,
+		validator:               validator,
+		teamService:             teamService,
+		enforcer:                enforcer,
+		ciHandler:               ciHandler,
+		gitSensorClient:         gitSensorClient,
+		ciPipelineRepository:    ciPipelineRepository,
+		pipelineRepository:      pipelineRepository,
+		enforcerUtil:            enforcerUtil,
+		envService:              envService,
+		gitRegistryConfig:       gitRegistryConfig,
+		dockerRegistryConfig:    dockerRegistryConfig,
+		cdHandelr:               cdHandelr,
+		appCloneService:         appCloneService,
+		appWorkflowService:      appWorkflowService,
+		materialRepository:      materialRepository,
+		policyService:           policyService,
+		scanResultRepository:    scanResultRepository,
+	}
+}
+
+func (handler BulkUpdateRestHandlerImpl) GetExampleInputBulkUpdate(w http.ResponseWriter, r *http.Request) {
+	includes := pipeline.NameIncludesExcludes{Name: "abc"}
+	excludes := pipeline.NameIncludesExcludes{Name: "abc"}
+	spec := pipeline.Specs{
+		PatchJson: "Enter Patch String"}
+	task := pipeline.Tasks{
+		Spec: spec,
+	}
+	payload := pipeline.BulkUpdateInput{
+		Includes:           includes,
+		Excludes:           excludes,
+		EnvIds:             make([]int, 0),
+		Global:             true,
+		DeploymentTemplate: task}
+
+	exampleInput := pipeline.BulkUpdatePayload{
+		ApiVersion: "core/v1beta1",
+		Kind:       "Application",
+		Payload:    payload,
+	}
+	getResponse := pipeline.BulkUpdateGet{
+		Task: "deployment-template",
+		Payload: exampleInput,
+		Readme: " ",
+	}
+	var response []pipeline.BulkUpdateGet
+	response = append(response,getResponse)
+	writeJsonResp(w, nil, response, http.StatusOK)
+}
+func (handler BulkUpdateRestHandlerImpl) GetAppNameDeploymentTemplate(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var bulkUpdatePayload pipeline.BulkUpdatePayload
+	err := decoder.Decode(&bulkUpdatePayload)
+	if err != nil {
+		writeJsonResp(w, err, nil, http.StatusBadRequest)
+		return
+	}
+	AppName, err := handler.chartService.GetBulkAppName(bulkUpdatePayload.Payload)
+	if err != nil {
+		writeJsonResp(w, err, nil, http.StatusInternalServerError)
+		return
+	}
+	writeJsonResp(w, err, AppName, http.StatusOK)
+}
+func (handler BulkUpdateRestHandlerImpl) BulkUpdateDeploymentTemplate(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var bulkUpdatePayload pipeline.BulkUpdatePayload
+	err := decoder.Decode(&bulkUpdatePayload)
+	if err != nil {
+		writeJsonResp(w, err, nil, http.StatusBadRequest)
+		return
+	}
+	response, err := handler.chartService.BulkUpdateDeploymentTemplate(bulkUpdatePayload.Payload)
+	if err != nil {
+		writeJsonResp(w, err, response, http.StatusInternalServerError)
+		return
+	}
+	writeJsonResp(w, err, response, http.StatusOK)
+}
--- api/router/BulkUpdateRouter.go
+++ api/router/BulkUpdateRouter.go
@@ -1,1 +1,26 @@
 package router
+
+import (
+	"github.com/devtron-labs/devtron/api/restHandler"
+	"github.com/gorilla/mux"
+)
+
+type BulkUpdateRouter interface {
+	initBulkUpdateRouter(bulkRouter *mux.Router)
+}
+
+type BulkUpdateRouterImpl struct {
+restHandler   restHandler.BulkUpdateRestHandler
+}
+
+func NewBulkUpdateRouterImpl(handler restHandler.BulkUpdateRestHandler) *BulkUpdateRouterImpl {
+	router := &BulkUpdateRouterImpl{
+		restHandler: handler,
+	}
+	return router
+}
+func (router BulkUpdateRouterImpl) initBulkUpdateRouter(bulkRouter *mux.Router) {
+	bulkRouter.Path("/application/see-example").HandlerFunc(router.restHandler.GetExampleInputBulkUpdate).Methods("GET")
+	bulkRouter.Path("/application/{dryrun}").HandlerFunc(router.restHandler.GetAppNameDeploymentTemplate).Methods("POST")
+	bulkRouter.Path("/application").HandlerFunc(router.restHandler.BulkUpdateDeploymentTemplate).Methods("POST")
+}
