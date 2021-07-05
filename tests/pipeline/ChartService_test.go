3b10b1e86cec95da1974b712f36d843b10828240
Vikram Singh <vikram@devtron.ai>
2021-07-05 10:56:34 +0530

    integration testing setup for batch operation

--- tests/pipeline/ChartService_test.go
+++ tests/pipeline/ChartService_test.go
@@ -4,7 +4,11 @@ import (
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
-	Pipeline "github.com/devtron-labs/devtron/pkg/pipeline"
+	"github.com/devtron-labs/devtron/internal/sql/models"
+	"github.com/devtron-labs/devtron/internal/sql/repository/chartConfig"
+	"github.com/devtron-labs/devtron/internal/util"
+	"github.com/devtron-labs/devtron/pkg/pipeline"
+	"go.uber.org/zap"
 	"io"
 	"log"
 	"os"
@@ -12,11 +16,23 @@ import (
 	"testing"
 )
 
+var chartService *pipeline.ChartServiceImpl
+var chartRepository chartConfig.ChartRepositoryImpl
+
+func setup() {
+	config, _ := models.GetConfig()
+	dbConnection, _ := models.NewDbConnection(config, &zap.SugaredLogger{})
+	chartRepository := chartConfig.NewChartRepository(dbConnection)
+	chartService = pipeline.NewChartServiceImpl(chartRepository, nil, nil, nil, nil, "",
+		pipeline.DefaultChart(""), util.MergeUtil{}, nil, nil, nil, nil, nil,
+		nil, nil, nil, nil)
+}
 func TestBulkUpdateDeploymentTemplate(t *testing.T) {
+	setup()
 	type test struct {
 		ApiVersion string
 		Kind       string
-		Payload    Pipeline.BulkUpdatePayload
+		Payload    pipeline.BulkUpdatePayload
 		want       string
 	}
 	TestsCsvFile, err := os.Open("ChartService_test.csv")
@@ -43,14 +59,14 @@ func TestBulkUpdateDeploymentTemplate(t *testing.T) {
 		if err != nil {
 			panic(err)
 		}
-		includes := Pipeline.NameIncludesExcludes{Name: record[2]}
-		excludes := Pipeline.NameIncludesExcludes{Name: record[3]}
-		spec := Pipeline.Specs{
+		includes := pipeline.NameIncludesExcludes{Name: record[2]}
+		excludes := pipeline.NameIncludesExcludes{Name: record[3]}
+		spec := pipeline.Specs{
 			PatchJson: record[6]}
-		task := Pipeline.Tasks{
+		task := pipeline.Tasks{
 			Spec: spec,
 		}
-		payload := Pipeline.BulkUpdatePayload{
+		payload := pipeline.BulkUpdatePayload{
 			Includes:           includes,
 			Excludes:           excludes,
 			EnvIds:             envId,
@@ -70,7 +86,7 @@ func TestBulkUpdateDeploymentTemplate(t *testing.T) {
 	for _, tt := range tests {
 		testname := fmt.Sprintf("%s,%s", tt.Payload.Includes, tt.Payload.Excludes)
 		t.Run(testname, func(t *testing.T) {
-			got, _ := Pipeline.ChartService.BulkUpdateDeploymentTemplate(Pipeline.ChartServiceImpl{}, tt.Payload)
+			got, _ := chartService.BulkUpdateDeploymentTemplate(tt.Payload)
 			if got != tt.want {
 				t.Errorf("got %s, want %s", got, tt.want)
 			}
