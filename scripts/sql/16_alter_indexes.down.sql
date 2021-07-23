80a0c4304365e9cc75852000eeb54d0735f774c7
Vikram Singh <vikram@devtron.ai>
2021-07-23 11:33:50 +0530

    db migration updated added indexs for app listing query optimization

--- scripts/sql/16_alter_indexes.down.sql
+++ scripts/sql/16_alter_indexes.down.sql
@@ -2,3 +2,8 @@ DROP INDEX "public"."environment_id_idx";
 DROP INDEX "public"."app_id_idx";
 DROP INDEX "public"."app_name_idx";
 DROP INDEX "public"."ci_artifact_id_idx";
+DROP INDEX "public"."cdwf_pipeline_id_idx";
+DROP INDEX "public"."cdwf_workflow_status_idx";
+DROP INDEX "public"."pco_pipeline_id_idx";
+DROP INDEX "public"."pco_deployment_type_idx";
+DROP INDEX "public"."cdwfr_cd_workflow_id_idx";
