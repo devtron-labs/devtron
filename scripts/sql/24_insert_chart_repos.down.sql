2b4822b2c6c7971530e1d1edc6131887236d811b
Arushi Shukla <arushishukla@Arushis-MacBook-Pro.local>
2021-09-21 15:37:52 +0530

    added sql migrations

---
+++ scripts/sql/24_insert_chart_repos.down.sql
@@ -0,0 +1,1 @@
+DELETE FROM "public"."chart_repo" WHERE ("id" >=5 AND "id" <=9);
---
+++ scripts/sql/24_insert_chart_repos.up.sql
@@ -0,0 +1,6 @@
+INSERT INTO "public"."chart_repo" ("id", "name", "url", "is_default", "active", "created_on", "created_by", "updated_on", "updated_by", "external") VALUES
+('5', 'nginx-ingress', 'https://kubernetes.github.io/ingress-nginx', 'f', 't', 'now()', '1', 'now()', '1', 't');
+('6', 'elastic', 'https://helm.elastic.co', 'f', 't', 'now()', '1', 'now()', '1', 't');
+('7', 'bitnami', 'https://charts.bitnami.com/bitnami', 'f', 't', 'now()', '1', 'now()', '1', 't');
+('8', 'prometheus-community', 'https://prometheus-community.github.io/helm-charts', 'f', 't', 'now()', '1', 'now()', '1', 't');
+('9', 'jetstack', 'https://charts.jetstack.io', 'f', 't', 'now()', '1', 'now()', '1', 't');
