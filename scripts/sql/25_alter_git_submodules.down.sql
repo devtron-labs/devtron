df0d2a70722186afd6e5a1d3783dd8467137f093
kartik-579 <kartik@devtron.ai>
2021-10-01 15:41:58 +0530

    corrected sql scripts for git submodule feature

--- scripts/sql/25_alter_git_submodules.down.sql
+++ scripts/sql/25_alter_git_submodules.down.sql
@@ -1,3 +1,11 @@
-ALTER TABLE "public"."git_provider" DROP COLUMN "ssh_key";
+---- ALTER TABLE git_provider - modify type
+ALTER TABLE git_provider
+ALTER COLUMN ssh_private_key TYPE varchar(250);
 
-ALTER TABLE "public"."git_material" DROP COLUMN column_name;
+---- ALTER TABLE git_provider - rename column
+ALTER TABLE git_provider
+RENAME COLUMN ssh_private_key TO ssh_key;
+
+---- ALTER TABLE git_material - drop column
+ALTER TABLE git_material
+DROP COLUMN IF EXISTS fetch_submodules
--- scripts/sql/25_alter_git_submodules.up.sql
+++ scripts/sql/25_alter_git_submodules.up.sql
@@ -1,3 +1,11 @@
-ALTER TABLE "public"."git_provider" ADD COLUMN "ssh_private_key" TEXT;
+---- ALTER TABLE git_provider - modify type
+ALTER TABLE git_provider
+ALTER COLUMN ssh_key TYPE text;
 
-ALTER TABLE "public"."git_material" ADD COLUMN fetch_submodules BOOLEAN;
+---- ALTER TABLE git_provider - rename column
+ALTER TABLE git_provider
+RENAME COLUMN ssh_key TO ssh_private_key;
+
+---- ALTER TABLE git_material - add column
+ALTER TABLE git_material
+ADD COLUMN fetch_submodules bool NOT NULL DEFAULT FALSE;
