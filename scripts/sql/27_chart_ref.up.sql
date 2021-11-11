10ec98e504891b21b1a41a47727755b453cd0482
Abhinav-26 <abhinavdubey616@gmail.com>
2021-11-11 12:25:58 +0530

    bug fixed & version updated

--- scripts/sql/27_chart_ref.up.sql
+++ scripts/sql/27_chart_ref.up.sql
@@ -1,3 +1,3 @@
 UPDATE chart_ref SET is_default=false;
 INSERT INTO "public"."chart_ref" ("location", "version", "is_default", "active", "created_on", "created_by", "updated_on", "updated_by") VALUES
-('cronjob-chart_1-0-0', '1.0.0', 't', 't', 'now()', 1, 'now()', 1);
+('cronjob-chart_1-2-0', '1.2.0', 't', 't', 'now()', 1, 'now()', 1);
