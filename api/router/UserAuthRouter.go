0c2ea05b0361ea4173619def62eb25827c0c34b6
nishant <nishant@devtron.ai>
2021-12-02 13:54:44 +0530

    path prefix url compatible

--- api/router/UserAuthRouter.go
+++ api/router/UserAuthRouter.go
@@ -77,7 +77,7 @@ func (router UserAuthRouterImpl) initUserAuthRouter(userAuthRouter *mux.Router) 
 	userAuthRouter.PathPrefix("/api/dex").HandlerFunc(router.dexProxy)
 	userAuthRouter.Path("/login").HandlerFunc(router.clientApp.HandleLogin)
 	userAuthRouter.Path("/auth/login").HandlerFunc(router.clientApp.HandleLogin)
-	userAuthRouter.PathPrefix("/auth/callback").HandlerFunc(router.clientApp.HandleCallback)
+	userAuthRouter.Path("/api/dex/callback").HandlerFunc(router.clientApp.HandleCallback)
 
 	userAuthRouter.Path("/api/v1/session").HandlerFunc(router.userAuthHandler.LoginHandler)
 	userAuthRouter.Path("/refresh").HandlerFunc(router.userAuthHandler.RefreshTokenHandler)
