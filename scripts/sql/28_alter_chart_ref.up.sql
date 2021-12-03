3658a1dc01b04f401de1f01d440ec45c2e5a55ab
Aviral Srivastava <aviralsrivastava@Avirals-MacBook-Pro.local>
2021-12-03 13:18:56 +0530

    sql query change

--- scripts/sql/28_alter_chart_ref.up.sql
+++ scripts/sql/28_alter_chart_ref.up.sql
@@ -1,3 +1,3 @@
 ALTER TABLE "public"."chart_ref" ADD COLUMN "name" varchar(250);
 
-ALTER TABLE "public"."chart_ref" ADD COLUMN "chart_data" BLOB;
+ALTER TABLE "public"."chart_ref" ADD COLUMN "chart_data" text;
