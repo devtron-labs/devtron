624e63ca6168324b9994d5c795cdbd0c4abcb82a
nishant <nishant@devtron.ai>
2021-12-09 20:48:52 +0530

    wire refactor

--- api/user/wire_user.go
+++ api/user/wire_user.go
@@ -1,40 +1,38 @@
 package user
 
 import (
-	user2 "github.com/devtron-labs/devtron/pkg/user"
+	"github.com/devtron-labs/devtron/pkg/user"
 	"github.com/devtron-labs/devtron/pkg/user/casbin"
-	repository4 "github.com/devtron-labs/devtron/pkg/user/repository"
+	"github.com/devtron-labs/devtron/pkg/user/repository"
 	"github.com/google/wire"
 )
 
-//depends on sql,casbin,validate,logger
+//depends on sql,validate,logger
 
 var UserWireSet = wire.NewSet(
 	NewUserAuthRouterImpl,
 	wire.Bind(new(UserAuthRouter), new(*UserAuthRouterImpl)),
 	NewUserAuthHandlerImpl,
 	wire.Bind(new(UserAuthHandler), new(*UserAuthHandlerImpl)),
-	user2.NewUserAuthServiceImpl,
-	wire.Bind(new(user2.UserAuthService), new(*user2.UserAuthServiceImpl)),
-	repository4.NewUserAuthRepositoryImpl,
-	wire.Bind(new(repository4.UserAuthRepository), new(*repository4.UserAuthRepositoryImpl)),
+	user.NewUserAuthServiceImpl,
+	wire.Bind(new(user.UserAuthService), new(*user.UserAuthServiceImpl)),
+	repository.NewUserAuthRepositoryImpl,
+	wire.Bind(new(repository.UserAuthRepository), new(*repository.UserAuthRepositoryImpl)),
 
 	NewUserRouterImpl,
 	wire.Bind(new(UserRouter), new(*UserRouterImpl)),
 	NewUserRestHandlerImpl,
 	wire.Bind(new(UserRestHandler), new(*UserRestHandlerImpl)),
-	user2.NewUserServiceImpl,
-	wire.Bind(new(user2.UserService), new(*user2.UserServiceImpl)),
-	repository4.NewUserRepositoryImpl,
-	wire.Bind(new(repository4.UserRepository), new(*repository4.UserRepositoryImpl)),
-	user2.NewRoleGroupServiceImpl,
-	wire.Bind(new(user2.RoleGroupService), new(*user2.RoleGroupServiceImpl)),
-	repository4.NewRoleGroupRepositoryImpl,
-	wire.Bind(new(repository4.RoleGroupRepository), new(*repository4.RoleGroupRepositoryImpl)),
-
+	user.NewUserServiceImpl,
+	wire.Bind(new(user.UserService), new(*user.UserServiceImpl)),
+	repository.NewUserRepositoryImpl,
+	wire.Bind(new(repository.UserRepository), new(*repository.UserRepositoryImpl)),
+	user.NewRoleGroupServiceImpl,
+	wire.Bind(new(user.RoleGroupService), new(*user.RoleGroupServiceImpl)),
+	repository.NewRoleGroupRepositoryImpl,
+	wire.Bind(new(repository.RoleGroupRepository), new(*repository.RoleGroupRepositoryImpl)),
 
 	casbin.NewEnforcerImpl,
 	wire.Bind(new(casbin.Enforcer), new(*casbin.EnforcerImpl)),
 	casbin.Create,
-
 )
