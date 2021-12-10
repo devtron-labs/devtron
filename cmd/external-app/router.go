898e4130c064575635b41a41b37a73c7a5abefef
nishant <nishant@devtron.ai>
2021-12-10 16:51:35 +0530

    sso conf ea integrated

---
+++ cmd/external-app/router.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/devtron-labs/devtron/api/restHandler/common"
+	"github.com/devtron-labs/devtron/api/sso"
+	"github.com/gorilla/mux"
+	"go.uber.org/zap"
+	"net/http"
+)
+
+type MuxRouter struct {
+	Router         *mux.Router
+	logger         *zap.SugaredLogger
+	ssoLoginRouter sso.SsoLoginRouter
+	//teamRouter     team.TeamRouter
+}
+
+func NewMuxRouter(
+	logger *zap.SugaredLogger, ssoLoginRouter sso.SsoLoginRouter,
+	//teamRouter team.TeamRouter,
+) *MuxRouter {
+	r := &MuxRouter{
+		Router:         mux.NewRouter(),
+		logger:         logger,
+		ssoLoginRouter: ssoLoginRouter,
+		//teamRouter:     teamRouter,
+	}
+	return r
+}
+func (r *MuxRouter) Init() {
+	r.Router.StrictSlash(true)
+	r.Router.Path("/health").HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(200)
+		response := common.Response{}
+		response.Code = 200
+		response.Result = "OK"
+		b, err := json.Marshal(response)
+		if err != nil {
+			b = []byte("OK")
+			r.logger.Errorw("Unexpected error in apiError", "err", err)
+		}
+		_, _ = writer.Write(b)
+	})
+
+	ssoLoginRouter := r.Router.PathPrefix("/orchestrator/sso").Subrouter()
+	r.ssoLoginRouter.InitSsoLoginRouter(ssoLoginRouter)
+/*	teamRouter := r.Router.PathPrefix("/orchestrator/team").Subrouter()
+	r.teamRouter.InitTeamRouter(teamRouter)*/
+}
--- cmd/external-app/wire.go
+++ cmd/external-app/wire.go
@@ -4,6 +4,8 @@
 package main
 
 import (
+	"github.com/devtron-labs/devtron/api/sso"
+	"github.com/devtron-labs/devtron/api/user"
 	"github.com/devtron-labs/devtron/internal/util"
 	"github.com/devtron-labs/devtron/pkg/sql"
 	"github.com/google/wire"
@@ -12,10 +14,19 @@ import (
 func InitializeApp() (*App, error) {
 	wire.Build(
 		sql.PgSqlWireSet,
-		//	team.TeamsWireSet,
+		user.UserWireSet,
+		sso.SsoConfigWireSet,
 		AuthWireSet,
+		//team.TeamsWireSet,
+
+		//	team.TeamsWireSet,
 		NewApp,
+		NewMuxRouter,
+
+
 		util.NewSugardLogger,
+		util.NewK8sUtil,
+		util.IntValidator,
 	)
 	return &App{}, nil
 }
