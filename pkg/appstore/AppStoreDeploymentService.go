dd14c44eccf79cb72ec4f704649351331595f653
kartik-579 <kartik@devtron.ai>
2021-12-20 21:30:08 +0530

    updated helm list update response

--- pkg/appstore/AppStoreDeploymentService.go
+++ pkg/appstore/AppStoreDeploymentService.go
@@ -20,6 +20,7 @@ package appstore
 import (
 	"bytes"
 	"context"
+	openapi "github.com/devtron-labs/devtron/api/helm-app/openapiClient"
 	"github.com/devtron-labs/devtron/client/argocdServer"
 	"github.com/devtron-labs/devtron/internal/sql/repository/app"
 	repository5 "github.com/devtron-labs/devtron/pkg/cluster/repository"
@@ -65,14 +66,16 @@ import (
 	"k8s.io/helm/pkg/proto/hapi/chart"
 )
 
-const DEFAULT_ENVIRONMENT_OR_NAMESPACE_OR_PROJECT string = "devtron"
-const CLUSTER_COMPONENT_DIR_PATH string = "/cluster/component"
+const (
+	DEFAULT_ENVIRONMENT_OR_NAMESPACE_OR_PROJECT = "devtron"
+	CLUSTER_COMPONENT_DIR_PATH                  = "/cluster/component"
+)
 
 type InstalledAppService interface {
 	UpdateInstalledApp(ctx context.Context, installAppVersionRequest *InstallAppVersionDTO) (*InstallAppVersionDTO, error)
 	GetInstalledApp(id int) (*InstallAppVersionDTO, error)
 	GetInstalledAppVersion(id int) (*InstallAppVersionDTO, error)
-	GetAll(filter *appstore.AppStoreFilter) ([]InstalledAppsResponse, error)
+	GetAll(filter *appstore.AppStoreFilter) (openapi.AppList, error)
 	GetAllInstalledAppsByAppStoreId(w http.ResponseWriter, r *http.Request, token string, appStoreId int) ([]InstalledAppsResponse, error)
 	DeleteInstalledApp(ctx context.Context, installAppVersionRequest *InstallAppVersionDTO) (*InstallAppVersionDTO, error)
 
@@ -493,29 +496,37 @@ func (impl InstalledAppServiceImpl) GetInstalledAppVersion(id int) (*InstallAppV
 	return installAppVersion, err
 }
 
-func (impl InstalledAppServiceImpl) GetAll(filter *appstore.AppStoreFilter) ([]InstalledAppsResponse, error) {
+func (impl InstalledAppServiceImpl) GetAll(filter *appstore.AppStoreFilter) (openapi.AppList, error) {
+	applicationType := "DEVTRON-CHART-STORE"
+	installedAppsResponse := openapi.AppList{
+		ApplicationType: &applicationType,
+	}
 	installedApps, err := impl.installedAppRepository.GetAllInstalledApps(filter)
 	if err != nil && !util.IsErrNoRows(err) {
 		impl.logger.Error(err)
-		return nil, err
+		return installedAppsResponse, err
 	}
-	var installedAppsResponse []InstalledAppsResponse
+	var helmAppsResponse []openapi.HelmApp
 	for _, a := range installedApps {
-		installedAppRes := InstalledAppsResponse{
-			AppStoreApplicationName:      a.AppStoreApplicationName,
-			ChartName:                    a.ChartRepoName,
-			EnvironmentName:              a.EnvironmentName,
-			Icon:                         a.Icon,
-			AppName:                      a.AppName,
-			DeployedAt:                   a.UpdatedOn,
-			EnvironmentId:                a.EnvironmentId,
-			InstalledAppVersionId:        a.InstalledAppVersionId,
-			AppStoreApplicationVersionId: a.AppStoreApplicationVersionId,
-			InstalledAppsId:              a.Id,
-			Deprecated:                   a.Deprecated,
+		appId := strconv.Itoa(a.Id)
+		projectId := int32(a.TeamId)
+		envId := int32(a.EnvironmentId)
+		environmentDetails := openapi.AppEnvironmentDetail{
+			EnvironmentName: &a.EnvironmentName,
+			EnvironmentId:   &envId,
+			Namespace:       &a.Namespace,
+			ClusterName:     &a.ClusterName,
 		}
-		installedAppsResponse = append(installedAppsResponse, installedAppRes)
+		helmAppResp := openapi.HelmApp{
+			AppName:           &a.AppName,
+			ChartName:         &a.AppStoreApplicationName,
+			AppId:             &appId,
+			ProjectId:         &projectId,
+			EnvironmentDetail: &environmentDetails,
+		}
+		helmAppsResponse = append(helmAppsResponse, helmAppResp)
 	}
+	installedAppsResponse.HelmApps = &helmAppsResponse
 	return installedAppsResponse, nil
 }
 
