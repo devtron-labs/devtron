21d51b7e1f7da2c0c47175d3f684f9e292a6a87f
kartik-579 <kartik@devtron.ai>
2022-02-04 14:42:29 +0530

    updated sql query for chart group

--- scripts/sql/34_alter_delete_options.down.sql
+++ scripts/sql/34_alter_delete_options.down.sql
@@ -11,3 +11,5 @@ ALTER TABLE team ADD CONSTRAINT team_name_key UNIQUE (name);
 ALTER TABLE git_provider ADD CONSTRAINT git_provider_name_key UNIQUE (name);
 
 ALTER TABLE git_provider ADD CONSTRAINT git_provider_url_key UNIQUE (url);
+
+ALTER TABLE chart_group ADD CONSTRAINT chart_group_name_key UNIQUE (name);
