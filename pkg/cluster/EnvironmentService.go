e36446ba2e510e02ede350906e27c87848b524b5
kartik-579 <84493919+kartik-579@users.noreply.github.com>
2022-02-21 17:20:22 +0530

    FIX : delete options auth roles delete bug fix (issue #1247) (#1248)
    * updated env roles fetch query and delete roles bug fix
    
    * updated queries for fetching app and chart group roles
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * added handling for deleting role group

--- pkg/cluster/EnvironmentService.go
+++ pkg/cluster/EnvironmentService.go
@@ -602,7 +602,7 @@ func (impl EnvironmentServiceImpl) Delete(deleteReq *EnvironmentBean, userId int
 		return err
 	}
 	//deleting auth roles entries for this environment
-	err = impl.userAuthService.DeleteRoles(repository2.ENV_TYPE, deleteRequest.Name, tx)
+	err = impl.userAuthService.DeleteRoles(repository2.ENV_TYPE, deleteRequest.Name, tx, existingEnv.EnvironmentIdentifier)
 	if err != nil {
 		impl.logger.Errorw("error in deleting auth roles", "err", err)
 		return err
