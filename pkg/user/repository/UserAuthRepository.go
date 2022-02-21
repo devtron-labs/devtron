1950dcff2754c0781f4ca5a0517d6450df104eb3
kartik-579 <kartik@devtron.ai>
2022-02-21 11:30:58 +0530

    wip

--- pkg/user/repository/UserAuthRepository.go
+++ pkg/user/repository/UserAuthRepository.go
@@ -1201,7 +1201,7 @@ func (impl UserAuthRepositoryImpl) GetRolesForProject(teamName string) ([]*RoleM
 
 func (impl UserAuthRepositoryImpl) GetRolesForApp(appName string) ([]*RoleModel, error) {
 	var roles []*RoleModel
-	err := impl.dbConnection.Model(&roles).Where("entity IS ?", nil).
+	err := impl.dbConnection.Model(&roles).Where("entity is NULL").
 		Where("entity_name = ?", appName).Select()
 	if err != nil {
 		impl.Logger.Errorw("error in getting roles for app", "err", err, "appName", appName)
