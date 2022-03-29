c0e913c5d31ead61de3448b92c3145d865346fbf
Aviral Srivastava <78467614+aviralsrivastava01@users.noreply.github.com>
2022-03-29 11:00:57 +0530

    feat: Multi charts (#1013)
    * sql query
    
    * sql query
    
    * sql query
    
    * sql query change
    
    * add name
    
    * add name
    
    * chart-sync
    
    * multi-charts
    
    * save-Configmaps
    
    * configmaps
    
    * configmap-charts
    
    * schema-validate
    
    * schema-fix
    
    * copy dir
    
    * minor changes
    
    * Delete .helmignore
    
    * Delete 580
    
    * Delete .image_descriptor_template.json
    
    * Delete Chart.yaml
    
    * Delete values.yaml
    
    * Delete kn-service.yaml
    
    * Delete generic.yaml
    
    * Delete release-values.yaml
    
    * Delete env-values.yaml
    
    * Delete pipeline-values.yaml
    
    * Delete app-values.yaml
    
    * fixes
    
    * bug fix
    
    * err fix
    
    * err fix
    
    * callback function
    
    * watcher changes
    
    * ExtractChartIfMissing
    
    * logger
    
    * watcher fix
    
    * watcher error
    
    * CreateChartFromEnvOverride update
    
    * review changes
    
    * minor changes - logs, local testing changes revert, fixed err handling
    
    * cleanup
    
    * removed executable binary, merged main
    
    * API to Create charts [1325]
    
    * API to Create charts [1325]
    
    * API to Create charts [1325]
    
    * API to Create charts [1325]
    
    * [1325] Unit test cases
    
    * Refactoring changes [1325]
    
    * Refactoring changes [1325]
    
    * [1325] wire changes
    
    * Refactoring changes [1325]
    
    * Updating DB check conditions [1325]
    
    * Adding API documentation [1325]
    
    * Updating wire for chart service in hyperion [1325]
    
    * merge conflicts
    
    * Adding return for failure and custom error message when data exists [1325]
    
    * Updating changes as per PR comments, deleting temporary folder after use [1325]
    
    * Refactoring deleting temporary folder logic for every error[1325][EA2]
    
    * Refactoring api arguments [1325][EA2]
    
    * Refactoring code [1325]
    
    * Refactoring code [1325]
    
    * Refactoring code [1325]
    
    * Hiding user message in error for security reasons[1325]
    
    * Adding empty value checks for chart values [1325]
    
    * Updating as per PR comments[1325]
    
    * Updating as per PR comments[1325]
    
    Co-authored-by: Aviral Srivastava <aviralsrivastava@Avirals-MacBook-Pro.local>
    Co-authored-by: kartik-579 <kartik@devtron.ai>
    Co-authored-by: snehithbikumandla <snehithb57624@gmail.com>

---
+++ pkg/chartRepo/ChartRepositoryService_test.go
@@ -0,0 +1,63 @@
+package chartRepo
+
+import (
+	"github.com/devtron-labs/devtron/internal/util"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+type ChartRepositoryServiceMock struct {
+	mock.Mock
+}
+
+func TestChartRepositoryServiceImpl_ValidateChartDetails(t *testing.T) {
+	sugaredLogger := util.NewSugardLogger()
+	impl := &ChartRepositoryServiceImpl{
+		logger:         sugaredLogger,
+		repoRepository: new(ChartRepoRepositoryImplMock),
+		K8sUtil:        nil,
+		clusterService: new(ClusterServiceImplMock),
+		aCDAuthConfig:  nil,
+		client:         nil,
+	}
+
+	type args struct {
+		FileName     string
+		chartVersion string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "Test file format",
+			args: struct {
+				FileName     string
+				chartVersion string
+			}{FileName: "test.tar.gz", chartVersion: "1.0.0"},
+			want: "test_1-0-0",
+		},
+		{
+			name: "Test file format",
+			args: struct {
+				FileName     string
+				chartVersion string
+			}{FileName: "test.pdf", chartVersion: "1.0.0"},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := impl.ValidateChartDetails(tt.args.FileName, tt.args.chartVersion)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ValidateChartDetails() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("ValidateChartDetails() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- wire_gen.go
+++ wire_gen.go
@@ -15,6 +15,7 @@ import (
 	chartRepo2 "github.com/devtron-labs/devtron/api/chartRepo"
 	cluster3 "github.com/devtron-labs/devtron/api/cluster"
 	"github.com/devtron-labs/devtron/api/connector"
+	"github.com/devtron-labs/devtron/api/deployment"
 	client4 "github.com/devtron-labs/devtron/api/helm-app"
 	"github.com/devtron-labs/devtron/api/restHandler"
 	app3 "github.com/devtron-labs/devtron/api/restHandler/app"
@@ -437,7 +438,7 @@ func InitializeApp() (*App, error) {
 	appStoreDeploymentRestHandlerImpl := appStoreDeployment2.NewAppStoreDeploymentRestHandlerImpl(sugaredLogger, userServiceImpl, enforcerImpl, enforcerUtilImpl, enforcerUtilHelmImpl, appStoreDeploymentServiceImpl, validate, helmAppServiceImpl, appStoreDeploymentCommonServiceImpl)
 	appStoreDeploymentRouterImpl := appStoreDeployment2.NewAppStoreDeploymentRouterImpl(appStoreDeploymentRestHandlerImpl)
 	appStoreRouterImpl := appStore2.NewAppStoreRouterImpl(installedAppRestHandlerImpl, appStoreValuesRouterImpl, appStoreDiscoverRouterImpl, appStoreDeploymentRouterImpl)
-	chartRepositoryRestHandlerImpl := chartRepo2.NewChartRepositoryRestHandlerImpl(sugaredLogger, userServiceImpl, chartRepositoryServiceImpl, enforcerImpl, validate, deleteServiceExtendedImpl)
+	chartRepositoryRestHandlerImpl := chartRepo2.NewChartRepositoryRestHandlerImpl(sugaredLogger, userServiceImpl, chartRepositoryServiceImpl, enforcerImpl, validate, deleteServiceExtendedImpl, chartRefRepositoryImpl, refChartDir)
 	chartRepositoryRouterImpl := chartRepo2.NewChartRepositoryRouterImpl(chartRepositoryRestHandlerImpl)
 	lensConfig, err := lens.GetLensConfig()
 	if err != nil {
@@ -522,7 +523,9 @@ func InitializeApp() (*App, error) {
 	k8sApplicationRouterImpl := k8s.NewK8sApplicationRouterImpl(k8sApplicationRestHandlerImpl)
 	pProfRestHandlerImpl := restHandler.NewPProfRestHandler(userServiceImpl)
 	pProfRouterImpl := router.NewPProfRouter(sugaredLogger, pProfRestHandlerImpl)
-	muxRouter := router.NewMuxRouter(sugaredLogger, helmRouterImpl, pipelineConfigRouterImpl, migrateDbRouterImpl, appListingRouterImpl, environmentRouterImpl, clusterRouterImpl, webhookRouterImpl, userAuthRouterImpl, applicationRouterImpl, cdRouterImpl, projectManagementRouterImpl, gitProviderRouterImpl, gitHostRouterImpl, dockerRegRouterImpl, notificationRouterImpl, teamRouterImpl, gitWebhookHandlerImpl, workflowStatusUpdateHandlerImpl, applicationStatusUpdateHandlerImpl, ciEventHandlerImpl, pubSubClient, userRouterImpl, cronBasedEventReceiverImpl, chartRefRouterImpl, configMapRouterImpl, appStoreRouterImpl, chartRepositoryRouterImpl, releaseMetricsRouterImpl, deploymentGroupRouterImpl, batchOperationRouterImpl, chartGroupRouterImpl, testSuitRouterImpl, imageScanRouterImpl, policyRouterImpl, gitOpsConfigRouterImpl, dashboardRouterImpl, attributesRouterImpl, commonRouterImpl, grafanaRouterImpl, ssoLoginRouterImpl, telemetryRouterImpl, telemetryEventClientImpl, bulkUpdateRouterImpl, webhookListenerRouterImpl, appLabelRouterImpl, coreAppRouterImpl, helmAppRouterImpl, k8sApplicationRouterImpl, pProfRouterImpl)
+	deploymentConfigRestHandlerImpl := deployment.NewDeploymentConfigRestHandlerImpl(sugaredLogger, userServiceImpl, enforcerImpl, validate, refChartDir, chartServiceImpl, chartRefRepositoryImpl)
+	deploymentConfigRouterImpl := deployment.NewDeploymentRouterImpl(deploymentConfigRestHandlerImpl)
+	muxRouter := router.NewMuxRouter(sugaredLogger, helmRouterImpl, pipelineConfigRouterImpl, migrateDbRouterImpl, appListingRouterImpl, environmentRouterImpl, clusterRouterImpl, webhookRouterImpl, userAuthRouterImpl, applicationRouterImpl, cdRouterImpl, projectManagementRouterImpl, gitProviderRouterImpl, gitHostRouterImpl, dockerRegRouterImpl, notificationRouterImpl, teamRouterImpl, gitWebhookHandlerImpl, workflowStatusUpdateHandlerImpl, applicationStatusUpdateHandlerImpl, ciEventHandlerImpl, pubSubClient, userRouterImpl, cronBasedEventReceiverImpl, chartRefRouterImpl, configMapRouterImpl, appStoreRouterImpl, chartRepositoryRouterImpl, releaseMetricsRouterImpl, deploymentGroupRouterImpl, batchOperationRouterImpl, chartGroupRouterImpl, testSuitRouterImpl, imageScanRouterImpl, policyRouterImpl, gitOpsConfigRouterImpl, dashboardRouterImpl, attributesRouterImpl, commonRouterImpl, grafanaRouterImpl, ssoLoginRouterImpl, telemetryRouterImpl, telemetryEventClientImpl, bulkUpdateRouterImpl, webhookListenerRouterImpl, appLabelRouterImpl, coreAppRouterImpl, helmAppRouterImpl, k8sApplicationRouterImpl, pProfRouterImpl, deploymentConfigRouterImpl)
 	mainApp := NewApp(muxRouter, sugaredLogger, sseSSE, versionServiceImpl, enforcer, db, pubSubClient, sessionManager)
 	return mainApp, nil
 }
