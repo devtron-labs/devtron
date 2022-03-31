c8da3c6c1d7ac7b3faef07a2e9c218f3819f14c8
Abhinav-26 <abhinavdubey616@gmail.com>
2022-03-31 23:48:50 +0530

    added default chart as rollout-4.11.0

--- scripts/sql/40_chart_ref.up.sql
+++ scripts/sql/40_chart_ref.up.sql
@@ -2,4 +2,5 @@ UPDATE chart_ref SET is_default=false;
 INSERT INTO "public"."chart_ref" ("location", "version", "is_default", "active", "created_on", "created_by", "updated_on", "updated_by", "name") VALUES
 ('cronjob-chart_1-3-0', '1.3.0', 'f', 't', 'now()', 1, 'now()', 1, 'Cron Job & Job');
 
+UPDATE "public"."chart_ref" SET "is_default" = 't' WHERE "location" = 'reference-chart_4-11-0' AND "version" = '4.11.0';
 UPDATE chart_ref SET name = replace(name, 'Cron Job & Job', 'CronJob & Job');
