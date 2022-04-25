12b05589387b923ac1858c40e7a7843f3906ccb3
pawan-59 <pawan@devtron.ai>
2022-04-25 23:46:52 +0530

    updated 43 chart_refs sql queries to 4-12-0

--- scripts/sql/43_chart_ref.down.sql
+++ scripts/sql/43_chart_ref.down.sql
@@ -1,3 +1,3 @@
-DELETE FROM "public"."chart_ref" WHERE ("location" = 'reference-chart_4-11-1' AND "version" = '4.11.1');
+DELETE FROM "public"."chart_ref" WHERE ("location" = 'reference-chart_4-12-0' AND "version" = '4.12.0');
 
 UPDATE "public"."chart_ref" SET "is_default" = 't' WHERE "location" = 'reference-chart_4-11-0' AND "version" = '4.11.0';
--- scripts/sql/43_chart_ref.up.sql
+++ scripts/sql/43_chart_ref.up.sql
@@ -1,3 +1,3 @@
 UPDATE chart_ref SET is_default=false;
 INSERT INTO "public"."chart_ref" ("location", "version", "is_default", "active", "created_on", "created_by", "updated_on", "updated_by") VALUES
-('reference-chart_4-11-1', '4.11.1', 't', 't', 'now()', 1, 'now()', 1);
+('reference-chart_4-12-0', '4.12.0', 't', 't', 'now()', 1, 'now()', 1);
