e0879ed5bd7d526cf610aef9e8ad11e68dc3e0da
Manish Agrawal <manish@devtron.ai>
2022-04-25 21:30:50 +0530

    fix

--- scripts/sql/43_modularisation_v1.down.sql
+++ scripts/sql/43_modularisation_v1.down.sql
@@ -3,5 +3,3 @@ DROP TABLE "public"."server_action_audit_log" CASCADE;
 DROP TABLE "public"."module_action_audit_log" CASCADE;
 
 DROP TABLE "public"."module" CASCADE;
-
-DROP TABLE "public"."server_info" CASCADE;
