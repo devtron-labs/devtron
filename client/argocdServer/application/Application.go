a97607c4cd5191ff6e65ded3cb8cb9f8ca9d1344
snehith57624 <snehithb57624@gmail.com>
2022-04-28 09:26:31 +0530

    Update Application.go

--- client/argocdServer/application/Application.go
+++ client/argocdServer/application/Application.go
@@ -22,7 +22,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"log"
 	"strings"
 	"time"
 
@@ -378,15 +377,12 @@ func (c ServiceClientImpl) ResourceTree(ctxt context.Context, query *application
 	defer util.Close(conn, c.logger)
 	asc := application.NewApplicationServiceClient(conn)
 	c.logger.Debugw("GRPC_GET_RESOURCETREE", "req", query)
-	c.logger.Infow("test ResourceTree 1", "query", query)
 	resp, err := asc.ResourceTree(ctx, query)
-	c.logger.Infow("test ResourceTree 2", "resp", resp)
 	if err != nil {
 		c.logger.Errorw("GRPC_GET_RESOURCETREE", "req", query, "err", err)
 		return nil, err
 	}
 	responses := parseResult(resp, query, ctx, asc, err, c)
-	c.logger.Infow("test ResourceTree 3", "responses", responses, "query", query)
 	podMetadata, newReplicaSet := c.buildPodMetadata(resp, responses)
 
 	appQuery := application.ApplicationQuery{Name: query.ApplicationName}
@@ -472,8 +468,6 @@ func (c ServiceClientImpl) buildPodMetadata(resp *v1alpha1.ApplicationTree, resp
 	}
 	newPodNames := make(map[string]bool, 0)
 	// for rollout we compare pod hash
-	c.logger.Infow("buildPodMetadata 1", "rolloutManifest", rolloutManifest, "replicaSetManifests", replicaSetManifests)
-	c.logger.Infow("buildPodMetadata 2", "statefulSetManifest", statefulSetManifest, "podManifests", podManifests)
 	if _, ok := rolloutManifest["kind"]; ok {
 		newReplicaSet = c.getRolloutNewReplicaSetName(rolloutManifest, replicaSetManifests)
 	}
@@ -494,31 +488,23 @@ func (c ServiceClientImpl) buildPodMetadata(resp *v1alpha1.ApplicationTree, resp
 		newPodNames = c.getJobsNewPods(jobsManifest, podManifests)
 	}
 
-	c.logger.Infow("buildPodMetadata 3", "newReplicaSet", newReplicaSet)
 	//podMetaData := make([]*PodMetadata, 0)
 	duplicateCheck := make(map[string]bool)
 	if newReplicaSet != "" {
 		results := buildPodMetadataFromReplicaSet(resp, newReplicaSet, replicaSetManifests)
-		c.logger.Infow("buildPodMetadata 4", "results", results)
 		for _, meta := range results {
-			c.logger.Infow("buildPodMetadata 5", "meta", meta)
 			duplicateCheck[meta.Name] = true
 			podMetaData = append(podMetaData, meta)
 		}
 	}
-	c.logger.Infow("buildPodMetadata 6", "newPodNames", newPodNames)
 	if newPodNames != nil {
-		c.logger.Infow("buildPodMetadata 7", "resp", resp, "podManifests", podManifests)
 		results := buildPodMetadataFromPod(resp, podManifests, newPodNames)
-		c.logger.Infow("buildPodMetadata 8", "results", results)
 		for _, meta := range results {
-			c.logger.Infow("buildPodMetadata 9", "meta", meta)
 			if _, ok := duplicateCheck[meta.Name]; !ok {
 				podMetaData = append(podMetaData, meta)
 			}
 		}
 	}
-	c.logger.Infow("buildPodMetadata 10", "podMetaData", podMetaData, "duplicateCheck", duplicateCheck)
 	return
 }
 
@@ -537,7 +523,6 @@ func parseResult(resp *v1alpha1.ApplicationTree, query *application.ResourcesQue
 			for _, pr := range node.ParentRefs {
 				podParents = append(podParents, pr.Name)
 			}
-			c.logger.Infow("parseResult 1 ", "Podparents", podParents, "node", node)
 		}
 	}
 	for _, node := range resp.Nodes {
@@ -562,7 +547,7 @@ func parseResult(resp *v1alpha1.ApplicationTree, query *application.ResourcesQue
 		}
 	}
 
-	c.logger.Infow("needPods", "pods", needPods)
+	c.logger.Debugw("needPods", "pods", needPods)
 
 	if needPods {
 		for _, node := range resp.Nodes {
@@ -570,7 +555,6 @@ func parseResult(resp *v1alpha1.ApplicationTree, query *application.ResourcesQue
 				queryNodes = append(queryNodes, node)
 			}
 		}
-		c.logger.Infow("parseResult 3 ", "queryNodes", queryNodes)
 	}
 
 	relevantCR := make(map[string]bool)
@@ -607,11 +591,10 @@ func parseResult(resp *v1alpha1.ApplicationTree, query *application.ResourcesQue
 			if err != nil {
 				c.logger.Errorw("GRPC_GET_RESOURCE", "data", request, "timeTaken", time.Since(startTime), "err", err)
 			} else {
-				c.logger.Infow("GRPC_GET_RESOURCE", "data", request, "timeTaken", time.Since(startTime))
+				c.logger.Debugw("GRPC_GET_RESOURCE", "data", request, "timeTaken", time.Since(startTime))
 			}
 			if res != nil || err != nil {
 				response <- Result{Response: res, Error: err, Request: &request}
-				c.logger.Infow("parseResult 4 ", "response", response)
 			} else {
 				response <- Result{Response: nil, Error: fmt.Errorf("connection closed by client"), Request: &request}
 			}
@@ -772,9 +755,7 @@ func (c ServiceClientImpl) getRolloutNewReplicaSetName(rManifest map[string]inte
 		if podHash == rPodHash {
 			newReplicaSet = getResourceName(rs)
 		}
-		c.logger.Infow("onexit1 testing", "podhash", podHash) //testing purpose
 	}
-	c.logger.Infow("onexit2 testing", "rPodHash", rPodHash, "newReplicaSet", newReplicaSet) //testing purpose
 	return newReplicaSet
 }
 
@@ -812,7 +793,6 @@ func buildPodMetadataFromPod(resp *v1alpha1.ApplicationTree, podManifests []map[
 	containerMapping := make(map[string][]*string)
 	initContainerMapping := make(map[string][]*string)
 	var parentWorkflow []string
-	log.Println("buildPodMetadataFromPod ", "podManifests", podManifests, "newPodNames", newPodNames)
 	for _, pod := range podManifests {
 		containerMapping[getResourceName(pod)] = getPodContainers(pod)
 	}
@@ -828,13 +808,9 @@ func buildPodMetadataFromPod(resp *v1alpha1.ApplicationTree, podManifests []map[
 	}
 
 	for _, node := range resp.Nodes {
-		log.Println("buildPodMetadataFromPod 1", "node", node, "kind", node.Kind, "parentWorkflows", parentWorkflow)
 		if node.Kind == "Pod" {
-			log.Println("check contains 1", "value", contains(parentWorkflow, node.Name))
 			isNew := newPodNames[node.Name] || contains(parentWorkflow, node.Name)
-			log.Println("test isNew 1", "isNew", isNew)
 			metadata := PodMetadata{Name: node.Name, UID: node.UID, Containers: containerMapping[node.Name], InitContainers: initContainerMapping[node.Name], IsNew: isNew}
-			log.Println("test metadata", "Pod metadata", metadata)
 			podMetadata = append(podMetadata, &metadata)
 		}
 	}
@@ -895,11 +871,9 @@ func getPodInitContainers(resource map[string]interface{}) []*string {
 
 func buildPodMetadataFromReplicaSet(resp *v1alpha1.ApplicationTree, newReplicaSet string, replicaSetManifests []map[string]interface{}) (podMetadata []*PodMetadata) {
 	replicaSets := make(map[string]map[string]interface{})
-	log.Println("buildPodMetadataFromReplicaSet 1", "newReplicaSet", newReplicaSet, "replicaSetManifests", replicaSetManifests)
 	for _, replicaSet := range replicaSetManifests {
 		replicaSets[getResourceName(replicaSet)] = replicaSet
 	}
-	log.Println("buildPodMetadataFromReplicaSet 2", "replicaSets", replicaSets, "resp", resp)
 	for _, node := range resp.Nodes {
 		if node.Kind == "Pod" {
 			parentName := ""
@@ -908,7 +882,6 @@ func buildPodMetadataFromReplicaSet(resp *v1alpha1.ApplicationTree, newReplicaSe
 					parentName = p.Name
 				}
 			}
-			log.Println("buildPodMetadataFromReplicaSet 3", "parentName", parentName, "newReplicaSet", newReplicaSet)
 			if parentName != "" {
 				isNew := parentName == newReplicaSet
 				replicaSet := replicaSets[parentName]
