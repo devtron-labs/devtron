537338688633339e091b08041b3692744c5cc789
Manish Agrawal <manish@devtron.ai>
2022-05-04 10:37:59 +0530

    log level made configurable

--- internal/util/BasicProviders.go
+++ internal/util/BasicProviders.go
@@ -18,7 +18,6 @@
 package util
 
 import (
-	"fmt"
 	"github.com/caarlos0/env"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -38,15 +37,14 @@ func GetLogger() *zap.SugaredLogger {
 }
 
 type LogConfig struct {
-	Level int `env:"LOG_LEVEL" envDefault:"0"`
+	Level int `env:"LOG_LEVEL" envDefault:"0"` // default info
 }
 
-func init() {
+func InitLogger() *zap.SugaredLogger{
 	cfg := &LogConfig{}
 	err := env.Parse(cfg)
 	if err != nil {
-		fmt.Println(err)
-		return
+		panic("failed to parse logger env config: " + err.Error())
 	}
 
 	config := zap.NewProductionConfig()
@@ -56,10 +54,11 @@ func init() {
 		panic("failed to create the default logger: " + err.Error())
 	}
 	logger = l.Sugar()
+	return logger
 }
 
 func NewSugardLogger() *zap.SugaredLogger {
-	return logger
+	return InitLogger()
 }
 
 func NewHttpClient() *http.Client {
