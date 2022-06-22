da50b129ae948a3bd81543cd74a946f19a38ae25
snehithbikumandla <snehithb57624@gmail.com>
2022-06-22 09:47:26 +0530

    logs

--- pkg/pipeline/CiHandler.go
+++ pkg/pipeline/CiHandler.go
@@ -258,6 +258,7 @@ func (impl *CiHandlerImpl) RefreshMaterialByCiPipelineMaterialId(gitMaterialId i
 
 func (impl *CiHandlerImpl) FetchMaterialsByPipelineId(pipelineId int) ([]CiPipelineMaterialResponse, error) {
 	ciMaterials, err := impl.ciPipelineMaterialRepository.GetByPipelineId(pipelineId)
+	impl.Logger.Infow("Testing Fetch Ci materials by pipeline Id ", "ci materials", ciMaterials)
 	if err != nil {
 		impl.Logger.Errorw("ciMaterials fetch failed", "err", err)
 	}
