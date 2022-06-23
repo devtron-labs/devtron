ff3ffae37f6a34ab4e5346a44969c1705c2b46b1
kripanshdevtron <kripansh@devtron.ai>
2022-06-23 13:08:26 +0530

    log msg improved

--- client/pubsub/NatsClient.go
+++ client/pubsub/NatsClient.go
@@ -49,13 +49,13 @@ func NewPubSubClient(logger *zap.SugaredLogger) (*PubSubClient, error) {
 	nc, err := nats.Connect(cfg.NatsServerHost,
 		nats.ReconnectWait(10*time.Second), nats.MaxReconnects(100),
 		nats.DisconnectErrHandler(func(nc *nats.Conn, err error) {
-			logger.Errorw("Got disconnected!", "Reason", err)
+			logger.Errorw("Nats Connection got disconnected!", "Reason", err)
 		}),
 		nats.ReconnectHandler(func(nc *nats.Conn) {
-			logger.Infow("Got reconnected", "url", nc.ConnectedUrl())
+			logger.Infow("Nats Connection got reconnected", "url", nc.ConnectedUrl())
 		}),
 		nats.ClosedHandler(func(nc *nats.Conn) {
-			logger.Errorw("Connection closed!", "Reason", nc.LastError())
+			logger.Errorw("Nats Client Connection closed!", "Reason", nc.LastError())
 		}))
 	if err != nil {
 		logger.Error("err", err)
