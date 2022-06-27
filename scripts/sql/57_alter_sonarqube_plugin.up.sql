b231c41e6c8f36380472ad21ec45c6ca05a51e1c
kartik-579 <kartik@devtron.ai>
2022-06-27 13:25:52 +0530

    fixed var name

--- scripts/sql/57_alter_sonarqube_plugin.up.sql
+++ scripts/sql/57_alter_sonarqube_plugin.up.sql
@@ -18,4 +18,4 @@ WHERE plugin_step.script_id = plugin_pipeline_script.id and plugin_metadata.name
 
 
 INSERT INTO "public"."plugin_step_variable" ("id", "plugin_step_id", "name", "format", "description", "is_exposed", "allow_empty_value", "variable_type", "value_type", "default_value", "variable_step_index", "deleted", "created_on", "created_by", "updated_on", "updated_by") VALUES
-((nextval('id_seq_plugin_step_variable')),(SELECT id from plugin_metadata WHERE name='Sonarqube'),'$UsePropertiesFileFromProject','BOOL','Boolean value - true or false. For knowing if sonar-project.properties file is to be used from the project.','t','f','INPUT','NEW','false', '1', 'f','now()', '1', 'now()', '1');
+((nextval('id_seq_plugin_step_variable')),(SELECT id from plugin_metadata WHERE name='Sonarqube'),'UsePropertiesFileFromProject','BOOL','Boolean value - true or false. For knowing if sonar-project.properties file is to be used from the project.','t','f','INPUT','NEW','false', '1', 'f','now()', '1', 'now()', '1');
