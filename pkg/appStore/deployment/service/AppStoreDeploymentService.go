8042db25ffebc5d7f10de4c1d5a3126edc96bb18
vikramdevtron <vikram@devtron.ai>
2022-07-07 15:25:37 +0530

    revert last commit to main

--- pkg/appStore/deployment/service/AppStoreDeploymentService.go
+++ pkg/appStore/deployment/service/AppStoreDeploymentService.go
@@ -411,12 +411,10 @@ func (impl AppStoreDeploymentServiceImpl) GetAllInstalledAppsByAppStoreId(w http
 		impl.logger.Error(err)
 		return nil, err
 	}
-
-
 	var installedAppsEnvResponse []appStoreBean.InstalledAppsResponse
 	for _, a := range installedApps {
 		var status string
-		if util2.GetDevtronVersion().ServerMode == util2.SERVER_MODE_HYPERION || a.AppOfferingMode == util2.SERVER_MODE_HYPERION || a.DeploymentAppType == "helm" {
+		if util2.GetDevtronVersion().ServerMode == util2.SERVER_MODE_HYPERION || a.AppOfferingMode == util2.SERVER_MODE_HYPERION {
 			status, err = impl.appStoreDeploymentHelmService.GetAppStatus(a, w, r, token)
 		} else {
 			status, err = impl.appStoreDeploymentArgoCdService.GetAppStatus(a, w, r, token)
