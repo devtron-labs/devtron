b0bcefaf8f271601319790e1d5a3360553a7efcf
Prakarsh <71125043+prakarsh-dt@users.noreply.github.com>
2022-08-18 14:42:01 +0530

    Update AppService.go

--- pkg/app/AppService.go
+++ pkg/app/AppService.go
@@ -1564,7 +1564,7 @@ func (impl AppServiceImpl) updateArgoPipeline(appId int, pipelineName string, en
 	if appStatus.Code() == codes.OK {
 		impl.logger.Debugw("argo app exists", "app", argoAppName, "pipeline", pipelineName)
 		if application.Spec.Source.Path != envOverride.Chart.ChartLocation {
-			patchReq := v1alpha1.Application{Spec: v1alpha1.ApplicationSpec{Source: v1alpha1.ApplicationSource{Path: envOverride.Chart.ChartLocation, RepoURL: envOverride.Chart.GitRepoUrl, TargetRevision: "master"}}}
+			patchReq := v1alpha1.Application{Spec: v1alpha1.ApplicationSpec{Source: v1alpha1.ApplicationSource{Path: envOverride.Chart.ChartLocation, RepoURL: envOverride.Chart.GitRepoUrl, TargetRevision: "HEAD"}}}
 			reqbyte, err := json.Marshal(patchReq)
 			if err != nil {
 				impl.logger.Errorw("error in creating patch", "err", err)
