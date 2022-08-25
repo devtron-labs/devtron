9c34ecebd06aef2f11b54446fa60f3fdc34313a6
kripanshdevtron <kripansh@devtron.ai>
2022-08-25 11:16:40 +0530

    refactoring

--- client/pubsub/NatsClient.go
+++ client/pubsub/NatsClient.go
@@ -36,37 +36,6 @@ type PubSubConfig struct {
 /* #nosec */
 func NewPubSubClient(puSubClientServiceImpl *pubsub_lib.PubSubClientServiceImpl) (*PubSubClient, error) {
 
-	//cfg := &PubSubConfig{}
-	//err := env.Parse(cfg)
-	//if err != nil {
-	//	logger.Error("err", err)
-	//	return &PubSubClient{}, err
-	//}
-	//
-	////Connect to NATS
-	//nc, err := nats.Connect(cfg.NatsServerHost,
-	//	nats.ReconnectWait(10*time.Second), nats.MaxReconnects(100),
-	//	nats.DisconnectErrHandler(func(nc *nats.Conn, err error) {
-	//		logger.Errorw("Nats Connection got disconnected!", "Reason", err)
-	//	}),
-	//	nats.ReconnectHandler(func(nc *nats.Conn) {
-	//		logger.Infow("Nats Connection got reconnected", "url", nc.ConnectedUrl())
-	//	}),
-	//	nats.ClosedHandler(func(nc *nats.Conn) {
-	//		logger.Errorw("Nats Client Connection closed!", "Reason", nc.LastError())
-	//	}))
-	//if err != nil {
-	//	logger.Error("err", err)
-	//	return &PubSubClient{}, err
-	//}
-	//
-	////Create a jetstream context
-	//js, err := nc.JetStream()
-	//
-	//if err != nil {
-	//	logger.Errorw("Error while creating jetstream context", "error", err)
-	//}
-
 	natsClient := &PubSubClient{
 		logger:     puSubClientServiceImpl.Logger,
 		JetStrCtxt: puSubClientServiceImpl.NatsClient.JetStrCtxt,
