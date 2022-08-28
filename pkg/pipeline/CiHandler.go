77a44160b0d9ca0cbb2af994c16a1620fc89f02f
nishant <nishant@devtron.ai>
2022-08-28 15:29:59 +0530

    fetch pod logs with region

--- pkg/pipeline/CiHandler.go
+++ pkg/pipeline/CiHandler.go
@@ -506,6 +506,7 @@ func (impl *CiHandlerImpl) getLogsFromRepository(pipelineId int, ciWorkflow *pip
 		ciLogRequest.MinioEndpoint = impl.ciConfig.MinioEndpoint
 		ciLogRequest.AccessKey = impl.ciConfig.MinioAccessKey
 		ciLogRequest.SecretKet = impl.ciConfig.MinioSecretKey
+		ciLogRequest.Region = impl.ciConfig.MinioRegion
 	}
 	oldLogsStream, cleanUp, err := impl.ciLogService.FetchLogs(ciLogRequest)
 	if err != nil {
@@ -608,6 +609,7 @@ func (impl *CiHandlerImpl) GetHistoricBuildLogs(pipelineId int, workflowId int, 
 		ciLogRequest.MinioEndpoint = impl.ciConfig.MinioEndpoint
 		ciLogRequest.AccessKey = impl.ciConfig.MinioAccessKey
 		ciLogRequest.SecretKet = impl.ciConfig.MinioSecretKey
+		ciLogRequest.Region = impl.ciConfig.MinioRegion
 	}
 	logsFile, cleanUp, err := impl.ciLogService.FetchLogs(ciLogRequest)
 	logs, err := ioutil.ReadFile(logsFile.Name())
