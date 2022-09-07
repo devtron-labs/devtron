e5d4aa622e7e5b369c507dbcb6535d9231cfbb3a
Manish Agrawal <manish@devtron.ai>
2022-09-07 11:44:36 +0530

    generic handling for module status in future scope

--- pkg/module/ModuleService.go
+++ pkg/module/ModuleService.go
@@ -109,7 +109,9 @@ func (impl ModuleServiceImpl) handleModuleNotFoundStatus(moduleName string) (Mod
 		impl.logger.Errorw("Error in getting module metadata", "moduleName", moduleName, "err", err)
 		return ModuleStatusNotInstalled, err
 	}
-	isLegacyModule := gjson.Get(string(moduleMetaData), "result.isIncludedInLegacyFullPackage").Bool()
+	moduleMetaDataStr := string(moduleMetaData)
+	isLegacyModule := gjson.Get(moduleMetaDataStr, "result.isIncludedInLegacyFullPackage").Bool()
+	baseMinVersionSupported := gjson.Get(moduleMetaDataStr, "result.baseMinVersionSupported").String()
 
 	// for enterprise user
 	if impl.serverEnvConfig.DevtronInstallationType == serverBean.DevtronInstallationTypeEnterprise {
@@ -143,7 +145,7 @@ func (impl ModuleServiceImpl) handleModuleNotFoundStatus(moduleName string) (Mod
 			}
 		}
 		cicdVersion := cicdModule.Version
-		if cicdVersion <= LegacyModuleSupportAssumptionCicdModuleVersion {
+		if cicdVersion <= LegacyModuleSupportAssumptionCicdModuleVersion || (len(baseMinVersionSupported) > 0 && cicdVersion <= baseMinVersionSupported) {
 			return impl.saveModuleAsInstalled(moduleName)
 		}
 	}
