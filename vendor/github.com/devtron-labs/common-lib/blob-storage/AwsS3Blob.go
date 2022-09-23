f80bea7ec43c64383016d8bf15032d7010527af8
kripanshdevtron <kripansh@devtron.ai>
2022-09-23 12:44:31 +0530

    minio versioning support

--- vendor/github.com/devtron-labs/common-lib/blob-storage/AwsS3Blob.go
+++ vendor/github.com/devtron-labs/common-lib/blob-storage/AwsS3Blob.go
@@ -55,48 +55,52 @@ func (impl *AwsS3Blob) DownloadBlob(request *BlobStorageRequest, downloadSuccess
 func downLoadFromS3(file *os.File, request *BlobStorageRequest, sess *session.Session) (success bool, bytesSize int64, err error) {
 	svc := s3.New(sess)
 	s3BaseConfig := request.AwsS3BaseConfig
-	input := &s3.ListObjectVersionsInput{
-		Bucket: aws.String(s3BaseConfig.BucketName),
-		Prefix: aws.String(request.SourceKey),
-	}
-	result, err := svc.ListObjectVersions(input)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			default:
-				log.Println(aerr.Error())
-			}
-		} else {
-			log.Println(err.Error())
-		}
-		return false, 0, err
-	}
-
 	var version *string
 	var size int64
-	for _, v := range result.Versions {
-		if *v.IsLatest && *v.Key == request.SourceKey {
-			version = v.VersionId
-			log.Println("selected version ", v.VersionId, " last modified ", v.LastModified)
-			size = *v.Size
-			break
+	if s3BaseConfig.VersioningEnabled {
+		input := &s3.ListObjectVersionsInput{
+			Bucket: aws.String(s3BaseConfig.BucketName),
+			Prefix: aws.String(request.SourceKey),
+		}
+		result, err := svc.ListObjectVersions(input)
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+				default:
+					log.Println(aerr.Error())
+				}
+			} else {
+				log.Println(err.Error())
+			}
+			return false, 0, err
+		}
+
+		for _, v := range result.Versions {
+			if *v.IsLatest && *v.Key == request.SourceKey {
+				version = v.VersionId
+				log.Println("selected version ", v.VersionId, " last modified ", v.LastModified)
+				size = *v.Size
+				break
+			}
 		}
 	}
 
 	downloader := s3manager.NewDownloader(sess)
-	numBytes, err := downloader.Download(file,
-		&s3.GetObjectInput{
-			Bucket:    aws.String(s3BaseConfig.BucketName),
-			Key:       aws.String(request.SourceKey),
-			VersionId: version,
-		})
+	input := &s3.GetObjectInput{
+		Bucket: aws.String(s3BaseConfig.BucketName),
+		Key:    aws.String(request.SourceKey),
+	}
+	if version != nil {
+		input.VersionId = version
+	}
+	numBytes, err := downloader.Download(file, input)
 	if err != nil {
 		log.Println("Couldn't download cache file")
 		return false, 0, nil
 	}
 	log.Println("downloaded ", file.Name(), numBytes, " bytes ")
 
-	if numBytes != size {
+	if version != nil && numBytes != size {
 		log.Println("cache sizes don't match, skipping step ", " version cache size ", size, " downloaded size ", numBytes)
 		return false, 0, nil
 	}
