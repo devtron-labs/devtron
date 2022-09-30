431711cd06f6725ee96f2fb68925484227dbc2d5
Kartik singhal <kartikdevtron.ai@Kartiks-MacBook-Pro.local>
2022-09-30 13:55:19 +0530

    fix

--- internal/sql/repository/pipelineConfig/CiWorkflowRepository.go
+++ internal/sql/repository/pipelineConfig/CiWorkflowRepository.go
@@ -67,6 +67,7 @@ type CiWorkflow struct {
 type WorkflowWithArtifact struct {
 	Id                 int               `json:"id"`
 	Name               string            `json:"name"`
+	PodName            string            `json:"podName"`
 	Status             string            `json:"status"`
 	PodStatus          string            `json:"pod_status"`
 	Message            string            `json:"message"`
