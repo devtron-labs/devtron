2f1545d8151aae0e49f2d7edbbf9b7267d803987
Kartik singhal <kartikdevtron.ai@Kartiks-MacBook-Pro.local>
2022-10-06 18:42:03 +0530

    updated query

--- scripts/sql/87_alter_pipeline_config_override.up.sql
+++ scripts/sql/87_alter_pipeline_config_override.up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE pipeline_config_override ADD COLUMN commit_time timestamptz,
-Update pipeline_config_override SET commit_time = updated_on;
+ALTER TABLE pipeline_config_override ADD COLUMN commit_time timestamptz;
+UPDATE pipeline_config_override SET commit_time = updated_on;
