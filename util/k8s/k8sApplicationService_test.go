b475e84a5c0f1d6c64d007116bb988af86061f91
Gireesh Naidu <gireesh@devtron.ai>
2022-10-07 11:31:57 +0530

    unit tests for batch processing

--- util/k8s/k8sApplicationService_test.go
+++ util/k8s/k8sApplicationService_test.go
@@ -5,7 +5,11 @@ import (
 	"fmt"
 	"github.com/devtron-labs/devtron/client/k8s/application"
 	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/mock"
+	"io"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/client-go/rest"
+	"math/rand"
 	"testing"
 )
 
@@ -69,23 +73,83 @@ var manifest = `{
 		}
 	  }`
 
+type NewK8sApplicationServiceImplMock struct {
+	mock.Mock
+}
+
+var testInput = make([]ResourceRequestAndGroupVersionKind, 0)
+var ptr = 0
+
+func (n NewK8sApplicationServiceImplMock) GetResource(restConfig *rest.Config, request *application.K8sRequestBean) (resp *application.ManifestResponse, err error) {
+
+}
+
+func (n NewK8sApplicationServiceImplMock) CreateResource(restConfig *rest.Config, request *application.K8sRequestBean, manifest string) (resp *application.ManifestResponse, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (n NewK8sApplicationServiceImplMock) UpdateResource(restConfig *rest.Config, request *application.K8sRequestBean) (resp *application.ManifestResponse, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (n NewK8sApplicationServiceImplMock) DeleteResource(restConfig *rest.Config, request *application.K8sRequestBean) (resp *application.ManifestResponse, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (n NewK8sApplicationServiceImplMock) ListEvents(restConfig *rest.Config, request *application.K8sRequestBean) (*application.EventsResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (n NewK8sApplicationServiceImplMock) GetPodLogs(restConfig *rest.Config, request *application.K8sRequestBean) (io.ReadCloser, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+var (
+	k8sASI = NewK8sApplicationServiceImplMock{}
+	impl   = NewK8sApplicationServiceImpl(
+		nil, nil, nil, k8sASI, nil,
+		nil, nil)
+)
+
+func Test_GetManifestsInBatch(t *testing.T) {
+	n := 10
+	kinds := []string{"Service", "Ingress", "Random", "Invalid"}
+	ExpectedTestOutputs := make([]BatchResourceResponse, 0)
+	for i := 0; i < n; i++ {
+		idx := rand.Int31n(int32(len(kinds)))
+		inp := generateTestResourceRequestAndGroupVersionKind(kinds[idx])
+		testInput = append(testInput, inp)
+	}
+	for i := 0; i < n; i++ {
+		man := generateTestManifest(testInput[i].Kind)
+		bRR := BatchResourceResponse{
+			ManifestResponse: &man,
+			Err:              nil,
+		}
+		ExpectedTestOutputs = append(ExpectedTestOutputs, bRR)
+	}
+
+}
+func generateTestResourceRequestAndGroupVersionKind(kind string) ResourceRequestAndGroupVersionKind {
+	return ResourceRequestAndGroupVersionKind{
+		Kind: kind,
+	}
+}
+
 type test struct {
 	inp application.ManifestResponse
 	out Response
 }
 
 func Test_getUrls(t *testing.T) {
-	impl := NewK8sApplicationServiceImpl(
-		nil, nil, nil, nil, nil,
-		nil, nil)
-
 	tests := make([]test, 3)
 	tests[0] = test{
-		inp: application.ManifestResponse{
-			Manifest: unstructured.Unstructured{
-				Object: getObj("Service"),
-			},
-		},
+		inp: generateTestManifest("Service"),
 		out: Response{
 			Kind:     "Service",
 			Name:     "test-service",
@@ -94,11 +158,7 @@ func Test_getUrls(t *testing.T) {
 		},
 	}
 	tests[1] = test{
-		inp: application.ManifestResponse{
-			Manifest: unstructured.Unstructured{
-				Object: getObj("Ingress"),
-			},
-		},
+		inp: generateTestManifest("Ingress"),
 		out: Response{
 			Kind:     "Ingress",
 			Name:     "test-service",
@@ -107,11 +167,7 @@ func Test_getUrls(t *testing.T) {
 		},
 	}
 	tests[2] = test{
-		inp: application.ManifestResponse{
-			Manifest: unstructured.Unstructured{
-				Object: getObj("Invalid"),
-			},
-		},
+		inp: generateTestManifest("Invalid"),
 		out: Response{
 			Kind:     "",
 			Name:     "",
@@ -129,9 +185,17 @@ func Test_getUrls(t *testing.T) {
 	}
 }
 
+func generateTestManifest(kind string) application.ManifestResponse {
+	return application.ManifestResponse{
+		Manifest: unstructured.Unstructured{
+			Object: getObj(kind),
+		},
+	}
+}
+
 func getObj(kind string) map[string]interface{} {
 	var obj map[string]interface{}
-	if kind == "Invalid" {
+	if (kind != "Service") && (kind != "Ingress") {
 		manifest = `{"invalid":{}}`
 	}
 	err := json.Unmarshal([]byte(manifest), &obj)
