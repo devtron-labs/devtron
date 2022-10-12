b191cc5fde0301f03a62e10dd1a32778794cc759
kartik-579 <kartik@devtron.ai>
2022-10-12 17:43:33 +0530

    added integration test cases

--- pkg/cluster/ClusterService_test.go
+++ pkg/cluster/ClusterService_test.go
@@ -1,35 +1,70 @@
 package cluster
 
 import (
-	"context"
-	"fmt"
-	"github.com/stretchr/testify/assert"
-	"k8s.io/apimachinery/pkg/api/errors"
-	testclient "k8s.io/client-go/kubernetes/fake"
-	"net/url"
+	"github.com/devtron-labs/devtron/client/k8s/informer"
+	"github.com/devtron-labs/devtron/internal/util"
+	"github.com/devtron-labs/devtron/pkg/cluster/repository"
+	"go.uber.org/zap"
 	"testing"
 )
 
-func TestClusterService(t *testing.T) {
-	t.Run("CheckIfConfigIsValid", func(t *testing.T) {
-		clientSet := testclient.NewSimpleClientset()
-		response, err := clientSet.Discovery().RESTClient().Get().AbsPath("/livez").DoRaw(context.Background())
-		var responseErr error
-		if err != nil {
-			if _, ok := err.(*url.Error); ok {
-				responseErr = fmt.Errorf("Incorrect server url : %v", err)
-			} else if statusError, ok := err.(*errors.StatusError); ok {
-				if statusError != nil {
-					responseErr = fmt.Errorf("%s : %s", statusError.ErrStatus.Reason, statusError.ErrStatus.Message)
-				} else {
-					responseErr = fmt.Errorf("Validation failed : %v", err)
-				}
-			} else {
-				responseErr = fmt.Errorf("Validation failed : %v", err)
+func TestClusterServiceImpl_CheckIfConfigIsValid(t *testing.T) {
+	t.SkipNow()
+	type fields struct {
+		clusterRepository  repository.ClusterRepository
+		logger             *zap.SugaredLogger
+		K8sUtil            *util.K8sUtil
+		K8sInformerFactory informer.K8sInformerFactory
+	}
+	type args struct {
+		cluster *ClusterBean
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			//incorrect server config
+			args: args{
+				cluster: &ClusterBean{
+					Id:        4,
+					ServerUrl: "",
+					Config: map[string]string{
+						"bearer_token": "",
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			//correct server config
+			args: args{
+				cluster: &ClusterBean{
+					Id:        5,
+					ServerUrl: "",
+					Config: map[string]string{
+						"bearer_token": "",
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+
+	logger, _ := util.NewSugardLogger()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			impl := ClusterServiceImpl{
+				clusterRepository:  nil,
+				logger:             logger,
+				K8sUtil:            nil,
+				K8sInformerFactory: nil,
 			}
-		} else if err == nil && string(response) != "ok" {
-			responseErr = fmt.Errorf("Validation failed with response : %s", string(response))
-		}
-		assert.NotNil(t, responseErr)
-	})
+			if err := impl.CheckIfConfigIsValid(tt.args.cluster); (err != nil) != tt.wantErr {
+				t.Errorf("ClusterServiceImpl.CheckIfConfigIsValid() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
