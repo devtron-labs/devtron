b551bcc566591dc14ca9829efa6a1d7226602e6a
Kripansh <kripansh@devtron.ai>
2022-10-25 14:01:27 +0530

    indentation fix

--- scripts/sql/89_ci_build_config.up.sql
+++ scripts/sql/89_ci_build_config.up.sql
@@ -1,26 +1,31 @@
 CREATE SEQUENCE IF NOT EXISTS id_seq_ci_build_config;
 
 -- Table Definition
-CREATE TABLE "public"."ci_build_config" (
-   "id" integer NOT NULL DEFAULT nextval('id_seq_ci_build_config'::regclass),
-   "type" varchar(100),
-   "ci_template_id" integer,
-   "ci_template_override_id" integer,
-   "build_metadata"   text,
-   "created_on" timestamptz,
-   "created_by" integer,
-   "updated_on" timestamptz,
-   "updated_by" integer,
-   PRIMARY KEY ("id")
+CREATE TABLE public.ci_build_config
+(
+    "id"                      integer NOT NULL DEFAULT nextval('id_seq_ci_build_config'::regclass),
+    "type"                    varchar(100),
+    "ci_template_id"          integer,
+    "ci_template_override_id" integer,
+    "build_metadata"          text,
+    "created_on"              timestamptz,
+    "created_by"              integer,
+    "updated_on"              timestamptz,
+    "updated_by"              integer,
+    PRIMARY KEY ("id")
 );
 
-ALTER TABLE ci_template ADD COLUMN ci_build_config_id integer;
+
+
+ALTER TABLE ci_template
+    ADD COLUMN ci_build_config_id integer;
 
 ALTER TABLE ONLY public.ci_template
     ADD CONSTRAINT ci_template_ci_build_config_id_fkey FOREIGN KEY (ci_build_config_id) REFERENCES public.ci_build_config(id);
 
 
-ALTER TABLE ci_template_override ADD COLUMN ci_build_config_id integer;
+ALTER TABLE ci_template_override
+    ADD COLUMN ci_build_config_id integer;
 
 ALTER TABLE ONLY public.ci_template_override
     ADD CONSTRAINT ci_template_override_ci_build_config_id_fkey FOREIGN KEY (ci_build_config_id) REFERENCES public.ci_build_config(id);
