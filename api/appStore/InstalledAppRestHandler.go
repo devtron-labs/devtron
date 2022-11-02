0eef6ff93bb79e51cca5c138dc4bb4309d9a3e4a
ayushmaheshwari <ayush@devtron.ai>
2022-11-02 10:28:40 +0530

    wip

--- api/appStore/InstalledAppRestHandler.go
+++ api/appStore/InstalledAppRestHandler.go
@@ -336,6 +336,7 @@ func (handler *InstalledAppRestHandlerImpl) FetchAppDetailsForInstalledApp(w htt
 	}
 	//rback block ends here
 
+	// saving no of time app is saved
 	err = handler.installedAppService.SaveExternalHelmAppTelemetry()
 
 	if err != nil {
