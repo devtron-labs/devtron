90c1f12551c9e1a715e4bde66afd231b7ffc602b
Gireesh Naidu <gireesh@devtron.ai>
2022-11-08 17:09:03 +0530

    scripts updated

--- scripts/sql/88_alter_external_links.down.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "public"."external_link" DROP COLUMN is_editable;
-ALTER TABLE "public"."external_link" DROP COLUMN description;
-
-ALTER TABLE "public"."external_link_monitoring_tool" DROP COLUMN category;
-
-DROP TABLE "public"."external_link_identifier_mapping";
