97a7ded101ccf8073e8c50794efc1d82f101727a
Gireesh Naidu <gireesh@devtron.ai>
2022-11-08 18:53:41 +0530

    query fix

--- scripts/sql/93_alter_external_links.up.sql
+++ scripts/sql/93_alter_external_links.up.sql
@@ -5,7 +5,7 @@ ALTER TABLE "public"."external_link" ADD COLUMN description text;
 --ADD column category for external_link_monitoring_tool
 ALTER TABLE "public"."external_link_monitoring_tool" ADD COLUMN category int4;
 ALTER TABLE IF EXISTS "public"."external_link_cluster_mapping" ADD COLUMN "type" int4 NOT NULL DEFAULT 0;
-ALTER TABLE IF EXISTS "public"."external_link_cluster_mapping" ADD COLUMN "identifier" varchar(255) NOT NULL;
+ALTER TABLE IF EXISTS "public"."external_link_cluster_mapping" ADD COLUMN "identifier" varchar(255) NOT NULL DEFAULT;
 ALTER TABLE IF EXISTS "public"."external_link_cluster_mapping" ADD COLUMN "env_id" int4 NOT NULL DEFAULT 0;
 ALTER TABLE IF EXISTS "public"."external_link_cluster_mapping" ADD COLUMN "app_id" int4 NOT NULL DEFAULT 0;
 
