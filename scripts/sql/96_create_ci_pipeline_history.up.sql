8c0d19954deb76a02e3f5553fea06c78fc92ead0
ayushmaheshwari <ayush@devtron.ai>
2022-11-10 17:30:54 +0530

    removing sql script of git and docker audit logs

--- scripts/sql/96_create_ci_pipeline_history.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE SEQUENCE IF NOT EXISTS id_seq_ci_pipeline_history;
-
-CREATE TABLE public.ci_pipeline_history(
-    id integer NOT NULL default nextval('id_seq_ci_pipeline_history'::regclass),
-    ci_pipeline_id integer,
-    ci_template_override_history text,
-    ci_pipeline_material_history text
-);
-
