a83f0525d534e61796e5aad6253c053eb8683f52
vikramdevtron <vikram@devtron.ai>
2022-11-10 19:23:20 +0530

    db migration fix for webhook

--- scripts/sql/92_alter_external_ci.up.sql
+++ scripts/sql/92_alter_external_ci.up.sql
@@ -1,4 +1,5 @@
 ALTER TABLE "public"."external_ci_pipeline" ALTER COLUMN "access_token" DROP NOT NULL;
+ALTER TABLE "public"."external_ci_pipeline" ALTER COLUMN "ci_pipeline_id" DROP NOT NULL;
 
 ALTER TABLE "public"."pipeline" ALTER COLUMN "ci_pipeline_id" DROP NOT NULL;
 
