49deea3ff5aaf8038fbac0beef117d3275aab8e3
Kripansh <kripansh@devtron.ai>
2022-11-11 12:05:17 +0530

    dockerfile checkout path

--- pkg/pipeline/CiService.go
+++ pkg/pipeline/CiService.go
@@ -436,7 +436,7 @@ func (impl *CiServiceImpl) buildWfRequestForCiPipeline(pipeline *pipelineConfig.
 	}
 	if ciBuildConfigBean.CiBuildType == bean2.SELF_DOCKERFILE_BUILD_TYPE || ciBuildConfigBean.CiBuildType == bean2.MANAGED_DOCKERFILE_BUILD_TYPE {
 		dockerBuildConfig := ciBuildConfigBean.DockerBuildConfig
-		dockerfilePath = filepath.Join(checkoutPath, dockerBuildConfig.DockerfilePath)
+		//dockerfilePath = filepath.Join(checkoutPath, dockerBuildConfig.DockerfilePath)
 		dockerBuildConfig.DockerfilePath = dockerfilePath
 		checkoutPath = dockerfilePath[:strings.LastIndex(dockerfilePath, "/")+1]
 	} else if ciBuildConfigBean.CiBuildType == bean2.BUILDPACK_BUILD_TYPE {
