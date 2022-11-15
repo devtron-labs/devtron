b423ae5d462daedbb23c81bde100b50ee4bcfe67
Prakash Kumar <prakash.kumar@devtron.ai>
2022-11-15 17:59:48 +0530

    Update 94_update_cronjobjob_to_jobcronjob.down.sql

--- scripts/sql/94_update_cronjobjob_to_jobcronjob.down.sql
+++ scripts/sql/94_update_cronjobjob_to_jobcronjob.down.sql
@@ -1,2 +1,2 @@
 UPDATE chart_ref_metadata SET "chart_name" = replace("chart_name", 'Job & CronJob', 'CronJob & Job');
-UPDATE chart_ref SET "name" = replace("name", 'Job & CronJob', 'CronJob & Job');
+UPDATE chart_ref SET "name" = 'Job & CronJob' WHERE "name" = 'CronJob & Job' and "user_uploaded" = false;
