7b454783c1becb532c403455b7215daaf53fbd7e
ayushmaheshwari <ayush@devtron.ai>
2022-11-23 12:21:31 +0530

    wip: adding more test cases

--- pkg/pipeline/GitopsOrHelmOption_test.go
+++ pkg/pipeline/GitopsOrHelmOption_test.go
@@ -1,6 +1,7 @@
 package pipeline
 
 import (
+	"github.com/devtron-labs/devtron/internal/sql/repository/pipelineConfig/mocks"
 	"github.com/devtron-labs/devtron/internal/util"
 	"github.com/devtron-labs/devtron/pkg/bean"
 	"github.com/stretchr/testify/assert"
@@ -27,7 +28,7 @@ func TestGitopsOrHelmOption(t *testing.T) {
 
 		pipelineCreateRequest := &bean.CdPipelines{
 			Pipelines: []*bean.CDPipelineConfigObject{
-				&bean.CDPipelineConfigObject{
+				{
 					Id:                            0,
 					EnvironmentId:                 1,
 					EnvironmentName:               "",
@@ -80,7 +81,7 @@ func TestGitopsOrHelmOption(t *testing.T) {
 
 		pipelineCreateRequest := &bean.CdPipelines{
 			Pipelines: []*bean.CDPipelineConfigObject{
-				&bean.CDPipelineConfigObject{
+				{
 					Id:                            0,
 					EnvironmentId:                 1,
 					EnvironmentName:               "",
@@ -198,6 +199,122 @@ func TestGitopsOrHelmOption(t *testing.T) {
 
 		for _, pipeline := range pipelineCreateRequestGitOps.Pipelines {
 			assert.Equal(t, pipeline.DeploymentAppType, "argo_cd")
+		}
+
+	})
+
+	t.Run("TestValidateCDPipelineRequestExternalUse", func(t *testing.T) {
+
+		sugaredLogger, err := util.NewSugardLogger()
+		assert.Nil(t, err)
+
+		mockedPipelineRepository := mocks.NewPipelineRepository(t)
+
+		mockedPipelineRepository.On("FindActiveByAppIdAndEnvironmentId", 1, 1).Return(nil, nil)
+
+		pipelineBuilderService := NewPipelineBuilderImpl(sugaredLogger, nil, nil, nil, nil,
+			mockedPipelineRepository, nil, nil,
+			nil, nil, nil, nil, nil,
+			nil, nil, nil, util.MergeUtil{Logger: sugaredLogger}, nil,
+			nil, nil, nil, nil, nil,
+			nil, nil, nil, nil, nil,
+			nil, nil, nil,
+			nil, nil, nil, nil,
+			nil, nil, nil, nil,
+			nil, nil, nil, &DeploymentServiceTypeConfig{IsInternalUse: false})
+
+		pipelineCreateRequest := &bean.CdPipelines{
+			Pipelines: []*bean.CDPipelineConfigObject{
+				&bean.CDPipelineConfigObject{
+					Id:                            0,
+					EnvironmentId:                 1,
+					EnvironmentName:               "",
+					CiPipelineId:                  1,
+					TriggerType:                   "AUTOMATIC",
+					Name:                          "cd-1-vo8q",
+					Strategies:                    nil,
+					Namespace:                     "devtron-demo",
+					AppWorkflowId:                 1,
+					DeploymentTemplate:            "",
+					PreStage:                      bean.CdStage{},
+					PostStage:                     bean.CdStage{},
+					PreStageConfigMapSecretNames:  bean.PreStageConfigMapSecretNames{},
+					PostStageConfigMapSecretNames: bean.PostStageConfigMapSecretNames{},
+					RunPreStageInEnv:              false,
+					RunPostStageInEnv:             false,
+					CdArgoSetup:                   false,
+					ParentPipelineId:              1,
+					ParentPipelineType:            "CI_PIPELINE",
+					DeploymentAppType:             "",
+				},
+			},
+			AppId:  1,
+			UserId: 0,
+		}
+		isGitOpsConfigured := false
+		isGitOpsRequired := pipelineBuilderService.IsGitOpsRequiredForCD(pipelineCreateRequest)
+
+		_, err = pipelineBuilderService.ValidateCDPipelineRequest(pipelineCreateRequest, isGitOpsConfigured, isGitOpsRequired)
+
+		assert.Nil(t, err)
+
+	})
+
+	t.Run("TestValidateCDPipelineRequestInternalUse", func(t *testing.T) {
+
+		sugaredLogger, err := util.NewSugardLogger()
+		assert.Nil(t, err)
+
+		mockedPipelineRepository := mocks.NewPipelineRepository(t)
+
+		//mockedPipelineRepository.On("FindActiveByAppIdAndEnvironmentId", 1, 1).Return(nil, nil)
+
+		pipelineBuilderService := NewPipelineBuilderImpl(sugaredLogger, nil, nil, nil, nil,
+			mockedPipelineRepository, nil, nil,
+			nil, nil, nil, nil, nil,
+			nil, nil, nil, util.MergeUtil{Logger: sugaredLogger}, nil,
+			nil, nil, nil, nil, nil,
+			nil, nil, nil, nil, nil,
+			nil, nil, nil,
+			nil, nil, nil, nil,
+			nil, nil, nil, nil,
+			nil, nil, nil, &DeploymentServiceTypeConfig{IsInternalUse: true})
+
+		pipelineCreateRequest := &bean.CdPipelines{
+			Pipelines: []*bean.CDPipelineConfigObject{
+				&bean.CDPipelineConfigObject{
+					Id:                            0,
+					EnvironmentId:                 1,
+					EnvironmentName:               "",
+					CiPipelineId:                  1,
+					TriggerType:                   "AUTOMATIC",
+					Name:                          "cd-1-vo8q",
+					Strategies:                    nil,
+					Namespace:                     "devtron-demo",
+					AppWorkflowId:                 1,
+					DeploymentTemplate:            "",
+					PreStage:                      bean.CdStage{},
+					PostStage:                     bean.CdStage{},
+					PreStageConfigMapSecretNames:  bean.PreStageConfigMapSecretNames{},
+					PostStageConfigMapSecretNames: bean.PostStageConfigMapSecretNames{},
+					RunPreStageInEnv:              false,
+					RunPostStageInEnv:             false,
+					CdArgoSetup:                   false,
+					ParentPipelineId:              1,
+					ParentPipelineType:            "CI_PIPELINE",
+					DeploymentAppType:             "argo_cd",
+				},
+			},
+			AppId:  1,
+			UserId: 0,
+		}
+		isGitOpsConfigured := false
+		isGitOpsRequired := pipelineBuilderService.IsGitOpsRequiredForCD(pipelineCreateRequest)
+
+		InvalidRequest, err := pipelineBuilderService.ValidateCDPipelineRequest(pipelineCreateRequest, isGitOpsConfigured, isGitOpsRequired)
+
+		if err != nil {
+			assert.Equal(t, InvalidRequest, false)
 		}
 
 	})
