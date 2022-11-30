deaef58d3a155aeb95ec134a13dfc65ee8316a26
kartik-579 <kartik@devtron.ai>
2022-11-30 17:27:17 +0530

    updated chart no

--- scripts/sql/95_alter_chart_ref.down.sql
+++ scripts/sql/95_alter_chart_ref.down.sql
@@ -1,4 +1,4 @@
-DELETE FROM "public"."chart_ref" WHERE ("location" = 'reference-chart_4-16-0' AND "version" = '4.16.0');
+DELETE FROM "public"."chart_ref" WHERE ("location" = 'reference-chart_4-17-0' AND "version" = '4.16.0');
 
 UPDATE "public"."chart_ref" SET "is_default" = 't' WHERE "location" = 'reference-chart_4-15-0' AND "version" = '4.15.0';
 
