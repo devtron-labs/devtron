345e6a358068e908c087633438da9fd54722723c
kartik-579 <kartik@devtron.ai>
2022-12-02 12:13:57 +0530

    fix sql

--- scripts/sql/99_alter_chart_ref.down.sql
+++ scripts/sql/99_alter_chart_ref.down.sql
@@ -1,6 +1,6 @@
-DELETE FROM "public"."chart_ref" WHERE ("location" = 'reference-chart_4-17-0' AND "version" = '4.16.0');
+DELETE FROM "public"."chart_ref" WHERE ("location" = 'reference-chart_4-17-0' AND "version" = '4.17.0');
 
-UPDATE "public"."chart_ref" SET "is_default" = 't' WHERE "location" = 'reference-chart_4-15-0' AND "version" = '4.15.0';
+UPDATE "public"."chart_ref" SET "is_default" = 't' WHERE "location" = 'reference-chart_4-16-0' AND "version" = '4.16.0';
 
 ALTER TABLE "chart_ref" DROP COLUMN "file_path_containing_strategy";
 ALTER TABLE "chart_ref" DROP COLUMN "json_path_for_strategy";
