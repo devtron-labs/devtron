30621371acac97677fa7d11f2a8c55f1cb43b17a
kartik-579 <kartik@devtron.ai>
2022-12-02 13:00:16 +0530

    updated down sql

--- scripts/sql/99_alter_chart_ref.down.sql
+++ scripts/sql/99_alter_chart_ref.down.sql
@@ -2,7 +2,7 @@ DELETE FROM "public"."chart_ref" WHERE ("location" = 'reference-chart_4-17-0' AN
 
 UPDATE "public"."chart_ref" SET "is_default" = 't' WHERE "location" = 'reference-chart_4-16-0' AND "version" = '4.16.0';
 
-ALTER TABLE "chart_ref" DROP COLUMN "file_path_containing_strategy";
+ALTER TABLE "chart_ref" DROP COLUMN "deployment_strategy_path";
 ALTER TABLE "chart_ref" DROP COLUMN "json_path_for_strategy";
 ALTER TABLE "chart_ref" DROP COLUMN "is_app_metrics_supported";
 
