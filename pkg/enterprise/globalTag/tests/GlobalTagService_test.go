88192af7bc26553cb5c836876a490990b4968989
Manish Agrawal <manish@devtron.ai>
2022-12-02 12:15:42 +0530

    updated testcase

--- pkg/enterprise/globalTag/tests/GlobalTagService_test.go
+++ pkg/enterprise/globalTag/tests/GlobalTagService_test.go
@@ -4,13 +4,11 @@ import (
 	"errors"
 	"github.com/devtron-labs/devtron/internal/util"
 	"github.com/devtron-labs/devtron/pkg/enterprise/globalTag"
-	"github.com/devtron-labs/devtron/pkg/sql"
-	"time"
-
-	//"github.com/devtron-labs/devtron/internal/util"
 	"github.com/devtron-labs/devtron/pkg/enterprise/globalTag/mocks"
+	"github.com/devtron-labs/devtron/pkg/sql"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestGlobalTagService(t *testing.T) {
@@ -89,7 +87,7 @@ func TestGlobalTagService(t *testing.T) {
 		assert.Equal(t, 0, len(globalTagsForProject))
 	})
 
-	t.Run("GetAllActiveTagsForProjectWithEmptyList", func(t *testing.T) {
+	t.Run("GetAllActiveTagsForProjectWithData", func(t *testing.T) {
 		sugaredLogger, err := util.NewSugardLogger()
 		assert.Nil(t, err)
 		globalTagRepositoryMocked := mocks.NewGlobalTagRepository(t)
@@ -150,6 +148,221 @@ func TestGlobalTagService(t *testing.T) {
 		}
 		assert.Equal(t, expectedMandatoryKeys, actualMandatoryKeys)
 		assert.Equal(t, expectedOptionalKeys, actualOptionalKeys)
+	})
+
+	t.Run("ValidateLabelsWithVError", func(t *testing.T) {
+		sugaredLogger, err := util.NewSugardLogger()
+		assert.Nil(t, err)
+		globalTagRepositoryMocked := mocks.NewGlobalTagRepository(t)
+		globalTagRepositoryMocked.On("FindAllActive").Return(nil, errors.New("some error occured"))
+		globalTagServiceImpl := globalTag.NewGlobalTagServiceImpl(sugaredLogger, globalTagRepositoryMocked)
+		err = globalTagServiceImpl.ValidateLabels(1, nil)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("ValidateLabelsWithInValid1", func(t *testing.T) {
+		sugaredLogger, err := util.NewSugardLogger()
+		assert.Nil(t, err)
+		globalTagRepositoryMocked := mocks.NewGlobalTagRepository(t)
+		var globalTagsFromDb []*globalTag.GlobalTag
+		globalTagsFromDb = append(globalTagsFromDb, &globalTag.GlobalTag{
+			Id:                     1,
+			Key:                    "key1",
+			MandatoryProjectIdsCsv: "1",
+			Description:            "someDescription1",
+			AuditLog:               sql.AuditLog{CreatedOn: time.Now(), CreatedBy: 1},
+		})
+		globalTagsFromDb = append(globalTagsFromDb, &globalTag.GlobalTag{
+			Id:                     2,
+			Key:                    "key2",
+			MandatoryProjectIdsCsv: "-1",
+			Description:            "someDescription2",
+			AuditLog:               sql.AuditLog{CreatedOn: time.Now(), CreatedBy: 2},
+		})
+		globalTagRepositoryMocked.On("FindAllActive").Return(globalTagsFromDb, nil)
+		globalTagServiceImpl := globalTag.NewGlobalTagServiceImpl(sugaredLogger, globalTagRepositoryMocked)
+		err = globalTagServiceImpl.ValidateLabels(1, nil)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("ValidateLabelsWithInValid2", func(t *testing.T) {
+		sugaredLogger, err := util.NewSugardLogger()
+		assert.Nil(t, err)
+		globalTagRepositoryMocked := mocks.NewGlobalTagRepository(t)
+		var globalTagsFromDb []*globalTag.GlobalTag
+		globalTagsFromDb = append(globalTagsFromDb, &globalTag.GlobalTag{
+			Id:                     1,
+			Key:                    "key1",
+			MandatoryProjectIdsCsv: "1",
+			Description:            "someDescription1",
+			AuditLog:               sql.AuditLog{CreatedOn: time.Now(), CreatedBy: 1},
+		})
+		globalTagsFromDb = append(globalTagsFromDb, &globalTag.GlobalTag{
+			Id:                     2,
+			Key:                    "key2",
+			MandatoryProjectIdsCsv: "-1",
+			Description:            "someDescription2",
+			AuditLog:               sql.AuditLog{CreatedOn: time.Now(), CreatedBy: 2},
+		})
+		globalTagRepositoryMocked.On("FindAllActive").Return(globalTagsFromDb, nil)
+		globalTagServiceImpl := globalTag.NewGlobalTagServiceImpl(sugaredLogger, globalTagRepositoryMocked)
+		labels := make(map[string]string)
+		labels["key1"] = "hello"
+		err = globalTagServiceImpl.ValidateLabels(1, labels)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("ValidateLabelsWithInValid", func(t *testing.T) {
+		sugaredLogger, err := util.NewSugardLogger()
+		assert.Nil(t, err)
+		globalTagRepositoryMocked := mocks.NewGlobalTagRepository(t)
+		var globalTagsFromDb []*globalTag.GlobalTag
+		globalTagsFromDb = append(globalTagsFromDb, &globalTag.GlobalTag{
+			Id:                     1,
+			Key:                    "key1",
+			MandatoryProjectIdsCsv: "1",
+			Description:            "someDescription1",
+			AuditLog:               sql.AuditLog{CreatedOn: time.Now(), CreatedBy: 1},
+		})
+		globalTagsFromDb = append(globalTagsFromDb, &globalTag.GlobalTag{
+			Id:                     2,
+			Key:                    "key2",
+			MandatoryProjectIdsCsv: "-1",
+			Description:            "someDescription2",
+			AuditLog:               sql.AuditLog{CreatedOn: time.Now(), CreatedBy: 2},
+		})
+		globalTagsFromDb = append(globalTagsFromDb, &globalTag.GlobalTag{
+			Id:                     3,
+			Key:                    "key2",
+			MandatoryProjectIdsCsv: "",
+			Description:            "someDescription2",
+			AuditLog:               sql.AuditLog{CreatedOn: time.Now(), CreatedBy: 2},
+		})
+
+		globalTagRepositoryMocked.On("FindAllActive").Return(globalTagsFromDb, nil)
+		globalTagServiceImpl := globalTag.NewGlobalTagServiceImpl(sugaredLogger, globalTagRepositoryMocked)
+		labels := make(map[string]string)
+		labels["key1"] = "hello1"
+		labels["key2"] = "hello2"
+		err = globalTagServiceImpl.ValidateLabels(1, labels)
+		assert.Nil(t, err)
+	})
+
+	t.Run("CreateTagsWithEmptyKey", func(t *testing.T) {
+		sugaredLogger, err := util.NewSugardLogger()
+		assert.Nil(t, err)
+		globalTagRepositoryMocked := mocks.NewGlobalTagRepository(t)
+		globalTagServiceImpl := globalTag.NewGlobalTagServiceImpl(sugaredLogger, globalTagRepositoryMocked)
+		var createTagRequestTags []*globalTag.CreateGlobalTagDto
+		createTagRequestTags = append(createTagRequestTags, &globalTag.CreateGlobalTagDto{
+			Key:                    "",
+			Description:            "some description",
+			MandatoryProjectIdsCsv: "",
+		})
+		createTagRequest := &globalTag.CreateGlobalTagsRequest{
+			Tags: createTagRequestTags,
+		}
+		err = globalTagServiceImpl.CreateTags(createTagRequest, 1)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("CreateTagsWithDuplicateKey", func(t *testing.T) {
+		sugaredLogger, err := util.NewSugardLogger()
+		assert.Nil(t, err)
+		globalTagRepositoryMocked := mocks.NewGlobalTagRepository(t)
+		globalTagRepositoryMocked.On("CheckKeyExistsForAnyActiveTag", "key1").Return(false, nil)
+		globalTagServiceImpl := globalTag.NewGlobalTagServiceImpl(sugaredLogger, globalTagRepositoryMocked)
+		var createTagRequestTags []*globalTag.CreateGlobalTagDto
+		createTagRequestTags = append(createTagRequestTags, &globalTag.CreateGlobalTagDto{
+			Key:                    "key1",
+			Description:            "some description",
+			MandatoryProjectIdsCsv: "",
+		})
+		createTagRequestTags = append(createTagRequestTags, &globalTag.CreateGlobalTagDto{
+			Key:                    "key1",
+			Description:            "some description",
+			MandatoryProjectIdsCsv: "",
+		})
+		createTagRequest := &globalTag.CreateGlobalTagsRequest{
+			Tags: createTagRequestTags,
+		}
+		err = globalTagServiceImpl.CreateTags(createTagRequest, 1)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("CreateTagsWithInvalidKey", func(t *testing.T) {
+		sugaredLogger, err := util.NewSugardLogger()
+		assert.Nil(t, err)
+		globalTagRepositoryMocked := mocks.NewGlobalTagRepository(t)
+		globalTagServiceImpl := globalTag.NewGlobalTagServiceImpl(sugaredLogger, globalTagRepositoryMocked)
+		var createTagRequestTags []*globalTag.CreateGlobalTagDto
+		createTagRequestTags = append(createTagRequestTags, &globalTag.CreateGlobalTagDto{
+			Key:                    "name/abcd/efgh",
+			Description:            "some description",
+			MandatoryProjectIdsCsv: "",
+		})
+		createTagRequest := &globalTag.CreateGlobalTagsRequest{
+			Tags: createTagRequestTags,
+		}
+		err = globalTagServiceImpl.CreateTags(createTagRequest, 1)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("CreateTagsWithKeyAlreadyExistsError", func(t *testing.T) {
+		sugaredLogger, err := util.NewSugardLogger()
+		assert.Nil(t, err)
+		globalTagRepositoryMocked := mocks.NewGlobalTagRepository(t)
+		globalTagRepositoryMocked.On("CheckKeyExistsForAnyActiveTag", "key1").Return(false, errors.New("some error occurred"))
+		globalTagServiceImpl := globalTag.NewGlobalTagServiceImpl(sugaredLogger, globalTagRepositoryMocked)
+		var createTagRequestTags []*globalTag.CreateGlobalTagDto
+		createTagRequestTags = append(createTagRequestTags, &globalTag.CreateGlobalTagDto{
+			Key:                    "key1",
+			Description:            "some description",
+			MandatoryProjectIdsCsv: "",
+		})
+		createTagRequest := &globalTag.CreateGlobalTagsRequest{
+			Tags: createTagRequestTags,
+		}
+		err = globalTagServiceImpl.CreateTags(createTagRequest, 1)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("CreateTagsWithKeyAlreadyExists", func(t *testing.T) {
+		sugaredLogger, err := util.NewSugardLogger()
+		assert.Nil(t, err)
+		globalTagRepositoryMocked := mocks.NewGlobalTagRepository(t)
+		globalTagRepositoryMocked.On("CheckKeyExistsForAnyActiveTag", "key1").Return(true, nil)
+		globalTagServiceImpl := globalTag.NewGlobalTagServiceImpl(sugaredLogger, globalTagRepositoryMocked)
+		var createTagRequestTags []*globalTag.CreateGlobalTagDto
+		createTagRequestTags = append(createTagRequestTags, &globalTag.CreateGlobalTagDto{
+			Key:                    "key1",
+			Description:            "some description",
+			MandatoryProjectIdsCsv: "",
+		})
+		createTagRequest := &globalTag.CreateGlobalTagsRequest{
+			Tags: createTagRequestTags,
+		}
+		err = globalTagServiceImpl.CreateTags(createTagRequest, 1)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("CreateTagsWithKeyAlreadyExists", func(t *testing.T) {
+		sugaredLogger, err := util.NewSugardLogger()
+		assert.Nil(t, err)
+		globalTagRepositoryMocked := mocks.NewGlobalTagRepository(t)
+		globalTagRepositoryMocked.On("CheckKeyExistsForAnyActiveTag", "key1").Return(true, nil)
+		globalTagServiceImpl := globalTag.NewGlobalTagServiceImpl(sugaredLogger, globalTagRepositoryMocked)
+		var createTagRequestTags []*globalTag.CreateGlobalTagDto
+		createTagRequestTags = append(createTagRequestTags, &globalTag.CreateGlobalTagDto{
+			Key:                    "key1",
+			Description:            "some description",
+			MandatoryProjectIdsCsv: "",
+		})
+		createTagRequest := &globalTag.CreateGlobalTagsRequest{
+			Tags: createTagRequestTags,
+		}
+		err = globalTagServiceImpl.CreateTags(createTagRequest, 1)
+		assert.NotNil(t, err)
 	})
 
 }
