27bd036fbf6d417f0113117962c6711a1366c05c
Gireesh Naidu <gireesh@devtron.ai>
2022-12-05 13:41:24 +0530

    error message fix

--- api/appStore/InstalledAppRestHandler.go
+++ api/appStore/InstalledAppRestHandler.go
@@ -375,7 +375,7 @@ func (handler *InstalledAppRestHandlerImpl) fetchResourceTree(w http.ResponseWri
 	if err != nil {
 		errCode := grpc_logging.DefaultErrorToCode(err)
 		if errCode == util.ErrorGrpcNotFound || errCode == util.ErrorGrpcUnKnown {
-			userMessage := "Deleted From " + appDetail.DeploymentAppType
+			userMessage := "release deleted From " + appDetail.DeploymentAppType
 			appDetail.AppDeleteError = userMessage
 		}
 		handler.Logger.Errorw("error occurred while fetching resource tree", "appName", appDetail.AppName, "envName", appDetail.EnvironmentName, "err", err)
