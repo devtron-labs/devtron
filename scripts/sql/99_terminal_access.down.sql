b5141c162426038fc59dcb2d52c6ff1eb1dd46f6
Kripansh <kripansh@devtron.ai>
2022-12-06 14:10:40 +0530

    drop sequence cmd added

--- scripts/sql/99_terminal_access.down.sql
+++ scripts/sql/99_terminal_access.down.sql
@@ -1,2 +1,5 @@
 DROP TABLE IF EXISTS "public"."terminal_access_templates";
 DROP TABLE IF EXISTS "public"."user_terminal_access_data";
+
+DROP SEQUENCE IF EXISTS public.id_seq_terminal_access_templates;
+DROP SEQUENCE IF EXISTS public.id_seq_user_terminal_access_data;
