bac86dc5f3c160965fcfa81fec88b2591dc29ef4
Kripansh <kripansh@devtron.ai>
2022-12-07 00:49:35 +0530

    attributes data cleanup

--- scripts/sql/100_terminal_access.down.sql
+++ scripts/sql/100_terminal_access.down.sql
@@ -5,3 +5,5 @@ DROP TABLE IF EXISTS "public"."user_terminal_access_data";
 
 DROP SEQUENCE IF EXISTS public.id_seq_terminal_access_templates;
 DROP SEQUENCE IF EXISTS public.id_seq_user_terminal_access_data;
+
+delete from attributes where key = 'DEFAULT_TERMINAL_IMAGE_LIST';
