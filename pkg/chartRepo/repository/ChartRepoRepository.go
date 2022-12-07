c9bd2dc9355fb10cad69e623d463a2154076b402
Gireesh Naidu <gireesh@devtron.ai>
2022-12-07 10:34:58 +0530

    code review changes in repository layer

--- pkg/chartRepo/repository/ChartRepoRepository.go
+++ pkg/chartRepo/repository/ChartRepoRepository.go
@@ -229,8 +229,7 @@ func (repositoryImpl ChartRepositoryImpl) FindNumberOfAppsWithDeploymentTemplate
 
 //---------------------------chart repository------------------
 
-type ChartRepo struct {
-	tableName   struct{}            `sql:"chart_repo"`
+type ChartRepoFields struct {
 	Id          int                 `sql:"id,pk"`
 	Name        string              `sql:"name"`
 	Url         string              `sql:"url"`
@@ -243,21 +242,14 @@ type ChartRepo struct {
 	AuthMode    repository.AuthMode `sql:"auth_mode,notnull"`
 	External    bool                `sql:"external,notnull"`
 	Deleted     bool                `sql:"deleted,notnull"`
+}
+type ChartRepo struct {
+	tableName struct{} `sql:"chart_repo"`
+	ChartRepoFields
 	sql.AuditLog
 }
 type ChartRepoWithDeploymentCount struct {
-	Id          int                 `sql:"id,pk"`
-	Name        string              `sql:"name"`
-	Url         string              `sql:"url"`
-	Active      bool                `sql:"active,notnull"`
-	Default     bool                `sql:"is_default,notnull"`
-	UserName    string              `sql:"user_name"`
-	Password    string              `sql:"password"`
-	SshKey      string              `sql:"ssh_key"`
-	AccessToken string              `sql:"access_token"`
-	AuthMode    repository.AuthMode `sql:"auth_mode,notnull"`
-	External    bool                `sql:"external,notnull"`
-	Deleted     bool                `sql:"deleted,notnull"`
+	ChartRepoFields
 	sql.AuditLog
 	ActiveDeploymentCount int `sql:"deployment_count,notnull"`
 }
