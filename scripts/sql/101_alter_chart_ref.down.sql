264cac938848187e041e057122320761f6c57d60
kartik-579 <kartik@devtron.ai>
2022-12-14 17:52:28 +0530

    removed deployment type chart as default

--- scripts/sql/101_alter_chart_ref.down.sql
+++ scripts/sql/101_alter_chart_ref.down.sql
@@ -1,7 +1,5 @@
 DELETE FROM "public"."chart_ref" WHERE ("location" = 'deployment-chart_1-0-0' AND "version" = '1.0.0');
 
-UPDATE "public"."chart_ref" SET "is_default" = 't' WHERE "location" = 'reference-chart_4-16-0' AND "version" = '4.16.0';
-
 ALTER TABLE "chart_ref" DROP COLUMN "deployment_strategy_path";
 ALTER TABLE "chart_ref" DROP COLUMN "json_path_for_strategy";
 ALTER TABLE "chart_ref" DROP COLUMN "is_app_metrics_supported";
