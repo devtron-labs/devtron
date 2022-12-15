cb3e713e138cabdbdbe53a97150a2acd2a75d071
kartik-579 <kartik@devtron.ai>
2022-12-15 12:18:13 +0530

    wip

--- tests/integrationTesting/create-test-env.sh
+++ tests/integrationTesting/create-test-env.sh
@@ -12,4 +12,12 @@ wget https://raw.githubusercontent.com/devtron-labs/devtron/main/tests/integrati
 yq '(select(.metadata.name == "postgresql-migrate-devtron") | .spec.template.spec.containers[0].env[0].value) = env(TEST_BRANCH)' migrator.yaml -i
 yq '(select(.metadata.name == "postgresql-migrate-devtron") | .spec.template.spec.containers[0].env[9].value) = env(LATEST_HASH)' migrator.yaml -i
 kubectl apply -f migrator.yaml
+# shellcheck disable=SC2046
+while [ ! $(kubectl -n devtroncd get $(kubectl -n devtroncd get job -l job=postgresql-migrate-devtron -o name) -o jsonpath="{.status.succeeded}")  ]; do sleep 10; done
+# shellcheck disable=SC2046
+while [ ! $(kubectl -n devtroncd get $(kubectl -n devtroncd get job -l job=postgresql-migrate-casbin -o name) -o jsonpath="{.status.succeeded}")  ]; do sleep 10; done
+# shellcheck disable=SC2046
+while [ ! $(kubectl -n devtroncd get $(kubectl -n devtroncd get job -l job=postgresql-migrate-lens -o name) -o jsonpath="{.status.succeeded}")  ]; do sleep 10; done
+# shellcheck disable=SC2046
+while [ ! $(kubectl -n devtroncd get $(kubectl -n devtroncd get job -l job=postgresql-migrate-gitsensor -o name) -o jsonpath="{.status.succeeded}")  ]; do sleep 10; done
 exit #to get out of container
