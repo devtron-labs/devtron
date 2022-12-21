a8a7261753458406ebbb20457171ac5f505df997
Gireesh Naidu <gireesh@devtron.ai>
2022-12-21 17:42:27 +0530

    getting app_name and env_name in repo layer for rbac optimisation

--- internal/sql/repository/argoAppStatus/ArgoAppStatusRepository.go
+++ internal/sql/repository/argoAppStatus/ArgoAppStatusRepository.go
@@ -6,9 +6,11 @@ import (
 	"time"
 )
 
-type ArgoAppStatusContainer struct {
+type AppStatusContainer struct {
 	TableName      struct{}  `sql:"argo_app_status" pg:",discard_unknown_columns"`
 	AppId          int       `sql:"app_id"`
+	AppName        string    `sql:"app_name"`
+	EnvName        string    `sql:"env_name"`
 	InstalledAppId int       `sql:"installed_app_id"` //unknown
 	EnvId          int       `sql:"env_id"`
 	Status         int       `sql:"status"`
@@ -17,48 +19,53 @@ type ArgoAppStatusContainer struct {
 	Active         bool      `sql:"active"`
 }
 
-type ArgoAppStatusRepository interface {
-	Create(container ArgoAppStatusContainer) error
-	Update(container ArgoAppStatusContainer) error
-	GetAllDevtronAppStatuses(appIds []int) ([]ArgoAppStatusContainer, error)
-	GetAllInstalledAppStatuses(installedAppIds []int) ([]ArgoAppStatusContainer, error)
-	Delete(container ArgoAppStatusContainer) error
+type AppStatusRepository interface {
+	Create(container AppStatusContainer) error
+	Update(container AppStatusContainer) error
+	GetAllDevtronAppStatuses(appIds []int) ([]AppStatusContainer, error)
+	GetAllInstalledAppStatuses(installedAppIds []int) ([]AppStatusContainer, error)
+	Delete(container AppStatusContainer) error
 }
 
-type ArgoAppStatusRepositoryImpl struct {
+type AppStatusRepositoryImpl struct {
 	dbConnection *pg.DB
 	logger       *zap.SugaredLogger
 }
 
-func NewArgoAppStatusRepositoryImpl(dbConnection *pg.DB, logger *zap.SugaredLogger) *ArgoAppStatusRepositoryImpl {
-	return &ArgoAppStatusRepositoryImpl{
+func NewArgoAppStatusRepositoryImpl(dbConnection *pg.DB, logger *zap.SugaredLogger) *AppStatusRepositoryImpl {
+	return &AppStatusRepositoryImpl{
 		dbConnection: dbConnection,
 		logger:       logger,
 	}
 }
 
-func (repo *ArgoAppStatusRepositoryImpl) Create(container ArgoAppStatusContainer) error {
+func (repo *AppStatusRepositoryImpl) Create(container AppStatusContainer) error {
 	return nil
 }
 
-func (repo *ArgoAppStatusRepositoryImpl) Update(container ArgoAppStatusContainer) error {
+func (repo *AppStatusRepositoryImpl) Update(container AppStatusContainer) error {
 	return nil
 }
-func (repo *ArgoAppStatusRepositoryImpl) GetAllDevtronAppStatuses(appIds []int) ([]ArgoAppStatusContainer, error) {
-	appStatusContainers := make([]ArgoAppStatusContainer, 0)
-	query := "select * " +
-		"from argo_app_status where app_id in ?;"
+func (repo *AppStatusRepositoryImpl) GetAllDevtronAppStatuses(appIds []int) ([]AppStatusContainer, error) {
+	appStatusContainers := make([]AppStatusContainer, 0)
+	query := "SELECT aas.*,app.app_name,env.environment_name as env_name ( SELECT * " +
+		"FROM argo_app_status WHERE app_id IN ? AND argo_app_status.active = true) aas " +
+		"INNER JOIN app ON app.id = aas.app_id AND app.active=true " +
+		"INNER JOIN environment env ON environment.id = aas.env_id AND env.active=true;"
 	_, err := repo.dbConnection.Query(&appStatusContainers, query, pg.In(appIds))
 	return appStatusContainers, err
 }
 
-func (repo *ArgoAppStatusRepositoryImpl) GetAllInstalledAppStatuses(installedAppIds []int) ([]ArgoAppStatusContainer, error) {
-	appStatusContainers := make([]ArgoAppStatusContainer, 0)
-	query := "SELECT aas.*,ia.id AS installed_app_id " +
-		"FROM argo_app_status aas INNER JOIN (SELECT id,app_id WHERE id IN ?) ia ON aas.app_id = ia.app_id;"
+func (repo *AppStatusRepositoryImpl) GetAllInstalledAppStatuses(installedAppIds []int) ([]AppStatusContainer, error) {
+	appStatusContainers := make([]AppStatusContainer, 0)
+	query := "SELECT aas.*,ia.id AS installed_app_id,app.app_name,env.environment_name " +
+		"FROM argo_app_status aas INNER JOIN ( SELECT id,app_id FROM installed_apps WHERE id IN ? AND active = true ) ia " +
+		"ON aas.app_id = ia.app_id AND aas.active=true " +
+		"INNER JOIN app ON app.id = aas.app_id AND app.active=true " +
+		"INNER JOIN environment env ON environment.id = aas.env_id AND env.active=true;"
 	_, err := repo.dbConnection.Query(&appStatusContainers, query, pg.In(installedAppIds))
 	return appStatusContainers, err
 }
-func (repo *ArgoAppStatusRepositoryImpl) Delete(container ArgoAppStatusContainer) error {
+func (repo *AppStatusRepositoryImpl) Delete(container AppStatusContainer) error {
 	return nil
 }
