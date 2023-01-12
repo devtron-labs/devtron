62ae48281c7ada2a02cb5e8fade406f6666fa6b3
Vikram <73224103+vikramdevtron@users.noreply.github.com>
2023-01-12 16:35:58 +0530

    fix: External Ci Trigger - Won't show images for manual Cd pipelines (#2802)
    * fix for ext ci webhook, manual child node artifact listing
    
    * changes on auth for external ci trigger

--- pkg/pipeline/WebhookService.go
+++ pkg/pipeline/WebhookService.go
@@ -267,20 +267,15 @@ func (impl WebhookServiceImpl) HandleExternalCiWebhook(externalCiId int, request
 		return 0, err
 	}
 
-	isAnyTriggered, err := impl.workflowDagExecutor.HandleWebhookExternalCiEvent(artifact, request.UserId, externalCiId, auth)
-	statusError, ok := err.(*util2.ApiError)
-	if err != nil && !ok {
-		impl.logger.Errorw("error on handle ci success event", "err", err)
-		return 0, err
-	}
-	if !isAnyTriggered {
-		if err1 := impl.ciArtifactRepository.Delete(artifact); err1 != nil {
-			impl.logger.Errorw("error in rollback artifact", "err", err1)
-			return 0, err1
-		}
-		if ok && statusError.Code == "401" {
-			impl.logger.Errorw("error on handle ci success event", "err", err)
-			return 0, err
+	hasAnyTriggered, err := impl.workflowDagExecutor.HandleWebhookExternalCiEvent(artifact, request.UserId, externalCiId, auth)
+	if err != nil {
+		impl.logger.Errorw("error on handle ext ci webhook", "err", err)
+		// if none of the child node has been triggered
+		if !hasAnyTriggered {
+			if err1 := impl.ciArtifactRepository.Delete(artifact); err1 != nil {
+				impl.logger.Errorw("error in rollback artifact", "err", err1)
+				return 0, err1
+			}
 		}
 	}
 	return artifact.Id, err
