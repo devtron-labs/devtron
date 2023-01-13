7e0c5635546f1c412cf976b837fe22e70a25e47d
Pawan Kumar <85476803+pawan-59@users.noreply.github.com>
2023-01-13 11:03:05 +0530

    updated sonarqube plugin script (#2855)

--- scripts/sql/106_sonarqube_plugin_polling.up.sql
+++ scripts/sql/106_sonarqube_plugin_polling.up.sql
@@ -16,39 +16,30 @@ VALUES(nextval('id_seq_plugin_step_variable'),2,'AbortPipelineOnPolicyCheckFaile
 
 UPDATE plugin_pipeline_script SET script=E'PathToCodeDir=/devtroncd$CheckoutPath
 cd $PathToCodeDir
-if [[ -z "$UsePropertiesFileFromProject" ]]
+if [[ -z "$UsePropertiesFileFromProject" || $UsePropertiesFileFromProject == false ]]
 then
   echo "sonar.projectKey=$SonarqubeProjectKey" > sonar-project.properties
-  docker run \\
-  --rm \\
-  -e SONAR_HOST_URL=$SonarqubeEndpoint \\
-  -e SONAR_LOGIN=$SonarqubeApiKey \\
-  -v "/$PWD:/usr/src" \\
-  sonarsource/sonar-scanner-cli
-elif [[ $UsePropertiesFileFromProject == false ]]
- then
- echo "sonar.projectKey=$SonarqubeProjectKey" > sonar-project.properties
- docker run \\
- --rm \\
- -e SONAR_HOST_URL=$SonarqubeEndpoint \\
- -e SONAR_LOGIN=$SonarqubeApiKey \\
- -v "/$PWD:/usr/src" \\
- sonarsource/sonar-scanner-cli
+fi
+docker run \\
+--rm \\
+-e SONAR_HOST_URL=$SonarqubeEndpoint \\
+-e SONAR_LOGIN=$SonarqubeApiKey \\
+-v "/$PWD:/usr/src" \\
+sonarsource/sonar-scanner-cli
 
- if [[ $CheckForSonarAnalysisReport == true && ! -z "$CheckForSonarAnalysisReport" ]]
+if [[ $CheckForSonarAnalysisReport == true && ! -z "$CheckForSonarAnalysisReport" ]]
+then
+ status=$(curl -u ${SonarqubeApiKey}:  -sS ${SonarqubeEndpoint}/api/qualitygates/project_status?projectKey=${SonarqubeProjectKey}&branch=master)
+ project_status=$(echo $status | jq -r  ".projectStatus.status")
+ echo "*********  SonarQube Policy Report  *********"
+ echo $status
+ if [[ $AbortPipelineOnPolicyCheckFailed == true && $project_status == "ERROR" ]]
  then
-   status=$(curl -u ${SonarqubeApiKey}:  -sS ${SonarqubeEndpoint}/api/qualitygates/project_status?projectKey=${SonarqubeProjectKey}&branch=master)
-   project_status=$(echo $status | jq -r  ".projectStatus.status")
-   echo "*********  SonarQube Policy Report  *********"
-   echo $status
-   if [[ $AbortPipelineOnPolicyCheckFailed == true && $project_status == "ERROR" ]]
-   then
-    echo "*********  SonarQube Policy Violated *********"
-    echo "*********  Exiting Build *********"
-    exit
-   elif [[ $AbortPipelineOnPolicyCheckFailed == true && $project_status == "OK" ]]
-   then
-    echo "*********  SonarQube Policy Passed *********"
-   fi
+  echo "*********  SonarQube Policy Violated *********"
+  echo "*********  Exiting Build *********"
+  exit
+ elif [[ $AbortPipelineOnPolicyCheckFailed == true && $project_status == "OK" ]]
+ then
+  echo "*********  SonarQube Policy Passed *********"
  fi
 fi' WHERE id=2;
