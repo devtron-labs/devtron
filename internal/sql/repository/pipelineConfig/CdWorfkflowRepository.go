4aaa9579a0d85d4edb6b6fc12159d0bcd2f3f666
kartik-579 <kartik@devtron.ai>
2023-01-24 13:31:17 +0530

    wip

--- internal/sql/repository/pipelineConfig/CdWorfkflowRepository.go
+++ internal/sql/repository/pipelineConfig/CdWorfkflowRepository.go
@@ -541,12 +541,15 @@ func (impl *CdWorkflowRepositoryImpl) GetLatestTriggersOfHelmPipelinesStuckInNon
 	err := impl.dbConnection.
 		Model(&wfrList).
 		Column("cd_workflow_runner.*", "CdWorkflow", "CdWorkflow.Pipeline", "CdWorkflow.Pipeline.Environment").
-		Where("workflow_type=?", bean.CD_WORKFLOW_TYPE_DEPLOY).
-		Where("started_on < NOW() - INTERVAL '? minutes'", deployedBeforeMinutes).
-		Where("status not in (?)", pg.In([]string{WorkflowAborted, WorkflowFailed, WorkflowSucceeded, application.HIBERNATING, string(health.HealthStatusHealthy), string(health.HealthStatusDegraded)})).
-		Where("cd_workflow_id in (select DISTINCT ON (pipeline_id) max(id) as id from cd_workflow group by pipeline_id, id order by pipeline_id, id desc)").
-		Where("pipeline.deployment_app_type = ?", util.PIPELINE_DEPLOYMENT_TYPE_HELM).
-		Where("pipeline.deleted=?", false).
+		Join("inner join cd_workflow wf on wf.id = cd_workflow_runner.cd_workflow_id").
+		Join("inner join pipeline p on p.id = wf.pipeline_id").
+		Join("inner join environment e on e.id = p.environment_id").
+		Where("cd_workflow_runner.workflow_type=?", bean.CD_WORKFLOW_TYPE_DEPLOY).
+		Where("cd_workflow_runner.started_on < NOW() - INTERVAL '? minutes'", deployedBeforeMinutes).
+		Where("cd_workflow_runner.status not in (?)", pg.In([]string{WorkflowAborted, WorkflowFailed, WorkflowSucceeded, application.HIBERNATING, string(health.HealthStatusHealthy), string(health.HealthStatusDegraded)})).
+		Where("cd_workflow_runner.cd_workflow_id in (select DISTINCT ON (pipeline_id) max(id) as id from cd_workflow group by pipeline_id, id order by pipeline_id, id desc)").
+		Where("p.deployment_app_type = ?", util.PIPELINE_DEPLOYMENT_TYPE_HELM).
+		Where("p.deleted=?", false).
 		Order("cd_workflow_runner.id DESC").
 		Select()
 	if err != nil {
