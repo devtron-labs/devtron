9b167994431f51fdb8c5f79146e7b13cb7fcc9ee
vishal bihani <vishal@devtron.ai>
2023-02-20 16:03:07 +0530

    register app in acd when migrating from helm to argocd

--- api/restHandler/AppListingRestHandler.go
+++ api/restHandler/AppListingRestHandler.go
@@ -29,7 +29,6 @@ import (
 	"github.com/devtron-labs/devtron/api/restHandler/common"
 	"github.com/devtron-labs/devtron/client/argocdServer/application"
 	"github.com/devtron-labs/devtron/client/cron"
-	"github.com/devtron-labs/devtron/internal/constants"
 	"github.com/devtron-labs/devtron/internal/sql/repository/pipelineConfig"
 	"github.com/devtron-labs/devtron/internal/util"
 	"github.com/devtron-labs/devtron/pkg/app"
@@ -776,13 +775,11 @@ func (handler AppListingRestHandlerImpl) fetchResourceTree(w http.ResponseWriter
 		resp, err := handler.application.ResourceTree(ctx, query)
 		elapsed := time.Since(start)
 		if err != nil {
-			handler.logger.Errorw("service err, FetchAppDetails, resource tree", "err", err, "app", appId, "env", envId)
-			err = &util.ApiError{
-				Code:            constants.AppDetailResourceTreeNotFound,
-				InternalMessage: "app detail fetched, failed to get resource tree from acd",
-				UserMessage:     "Error fetching detail, if you have recently created this deployment pipeline please try after sometime.",
-			}
-			common.WriteJsonResp(w, err, "", http.StatusInternalServerError)
+			handler.logger.Errorw("service err, FetchAppDetails, resource tree",
+				"err", err,
+				"app", appId,
+				"env", envId)
+
 			return appDetail
 		}
 		if resp.Status == string(health.HealthStatusHealthy) {
