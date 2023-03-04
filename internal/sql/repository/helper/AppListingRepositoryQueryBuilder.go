7367ba8a72e522140f2932fd079a12defe6149c5
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2023-03-05 00:01:58 +0530

    Added the check of deleted pipeline.

--- internal/sql/repository/helper/AppListingRepositoryQueryBuilder.go
+++ internal/sql/repository/helper/AppListingRepositoryQueryBuilder.go
@@ -67,7 +67,7 @@ func (impl AppListingRepositoryQueryBuilder) BuildJobListingQuery(appIDs []int, 
 		"inner join (SELECT  ci_pipeline_id, MAX(started_on) max_started_on FROM ci_workflow GROUP BY ci_pipeline_id) cws " +
 		"on cw.ci_pipeline_id = cws.ci_pipeline_id and cw.started_on = cws.max_started_on order by cw.ci_pipeline_id) cwr" +
 		" on cwr.ci_pipeline_id = ci_pipeline.id and ci_pipeline.active = true  " +
-		"right join app on app.id = ci_pipeline.app_id where app.active = true and app.app_store = 2 "
+		"right join app on app.id = ci_pipeline.app_id where app.active = true and app.app_store = 2 and ci_pipeline.active = true "
 	if len(appIDs) > 0 {
 		query += "and app.id IN (" + GetCommaSepratedString(appIDs) + ") "
 	}
