2e8fd9e294bd9fc9caaa0377f985ba4504552eb5
vishal bihani <vishal@devtron.ai>
2023-03-07 14:20:52 +0530

    add unit test and remove api client mock

--- client/gitSensor/GitSensorClient_test.go
+++ client/gitSensor/GitSensorClient_test.go
@@ -6,11 +6,24 @@ import (
 	"testing"
 )
 
-func TestNewGitSensorClientWithValidConfig(t *testing.T) {
+func TestNewGitSensorClientWithValidConfigAndGrpcEnabled(t *testing.T) {
 
 	config := &ClientConfig{
 		Url:     "127.0.0.1:7070",
 		UseGrpc: true,
+	}
+
+	logger, err := util.NewSugardLogger()
+	_, err = NewGitSensorClient(logger, config)
+
+	assert.Nil(t, err)
+}
+
+func TestNewGitSensorClientWithValidConfigAndGrpcDisabled(t *testing.T) {
+
+	config := &ClientConfig{
+		Url:     "127.0.0.1:7070",
+		UseGrpc: false,
 	}
 
 	logger, err := util.NewSugardLogger()
