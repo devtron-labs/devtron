92850fed0b562c3e7585834a03b7e30c58556424
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2023-03-09 18:44:25 +0530

    Wrote the sql down command.

--- scripts/sql/115_jobs.down.sql
+++ scripts/sql/115_jobs.down.sql
@@ -1,32 +1,32 @@
-ALTER TABLE app_workflow  DROP CONSTRAINT app_workflow_app_id_fkey,  ADD CONSTRAINT app_workflow_app_id_fkey FOREIGN KEY (app_id) REFERENCES app(id) ON DELETE CASCADE;
+DELETE FROM ci_pipeline_material WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_store = 2));
 
-ALTER TABLE ci_pipeline DROP CONSTRAINT ci_pipeline_app_id_fkey, ADD CONSTRAINT ci_pipeline_app_id_fkey FOREIGN KEY (app_id) REFERENCES app(id) ON DELETE CASCADE;
+DELETE FROM ci_template_history WHERE git_material_id IN (SELECT id FROM git_material_history WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_store = 2)));
 
-ALTER TABLE ci_template DROP CONSTRAINT ci_template_app_id_fkey, ADD CONSTRAINT ci_template_app_id_fkey FOREIGN KEY (app_id) REFERENCES app(id) ON DELETE CASCADE;
+DELETE FROM ci_pipeline_history WHERE ci_pipeline_id IN (SELECT id FROM ci_pipeline WHERE ci_template_id IN (SELECT id FROM ci_template WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_store = 2))));
 
-ALTER TABLE git_material DROP CONSTRAINT git_material_app_id_fkey, ADD CONSTRAINT git_material_app_id_fkey FOREIGN KEY (app_id) REFERENCES app(id) ON DELETE CASCADE;
+DELETE FROM ci_artifact WHERE ci_workflow_id IN (SELECT id FROM ci_workflow WHERE ci_pipeline_id IN (SELECT id FROM ci_pipeline WHERE ci_template_id IN (SELECT id FROM ci_template WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_store = 2)))));
 
-ALTER TABLE app_label DROP CONSTRAINT app_label_app_id_fkey, ADD CONSTRAINT app_label_app_id_fkey FOREIGN KEY (app_id) REFERENCES app(id) ON DELETE CASCADE;
+DELETE FROM ci_workflow WHERE ci_pipeline_id IN (SELECT id FROM ci_pipeline WHERE ci_template_id IN (SELECT id FROM ci_template WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_store = 2))));
 
-ALTER TABLE ci_template_history DROP CONSTRAINT ci_template_history_app_id_fkey, ADD CONSTRAINT ci_template_history_app_id_fkey FOREIGN KEY (app_id) REFERENCES app(id) ON DELETE CASCADE;
+DELETE FROM pipeline_stage WHERE ci_pipeline_id IN (SELECT id FROM ci_pipeline WHERE ci_template_id IN (SELECT id FROM ci_template WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_store = 2))));
 
-ALTER TABLE app_workflow_mapping DROP CONSTRAINT app_workflow_mapping_app_workflow_id_fkey, ADD CONSTRAINT app_workflow_mapping_app_workflow_id_fkey FOREIGN KEY (app_workflow_id) REFERENCES app_workflow(id) ON DELETE CASCADE;
+DELETE FROM ci_pipeline WHERE ci_template_id IN (SELECT id FROM ci_template WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_store = 2)));
 
-ALTER TABLE ci_pipeline_material DROP CONSTRAINT ci_pipeline_material_ci_pipeline_id_fkey, ADD CONSTRAINT ci_pipeline_material_ci_pipeline_id_fkey FOREIGN KEY (ci_pipeline_id) REFERENCES  ci_pipeline(id) ON DELETE CASCADE;
+DELETE FROM ci_workflow WHERE ci_pipeline_id IN (SELECT id FROM ci_pipeline WHERE ci_template_id IN (SELECT id FROM ci_template WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_store = 2))));
 
-ALTER TABLE ci_pipeline_history DROP CONSTRAINT ci_pipeline_history_ci_pipeline_id_fk, ADD CONSTRAINT ci_pipeline_history_ci_pipeline_id_fk FOREIGN KEY (ci_pipeline_id) REFERENCES  ci_pipeline(id) ON DELETE CASCADE;
+DELETE FROM ci_template_history WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_store = 2));
 
-ALTER TABLE git_material_history DROP CONSTRAINT git_material_history_git_material_id_fkey, ADD CONSTRAINT git_material_history_git_material_id_fkey FOREIGN KEY (git_material_id) REFERENCES   git_material(id) ON DELETE CASCADE;
+DELETE FROM ci_template WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_store = 2));
 
-ALTER TABLE ci_workflow DROP CONSTRAINT ci_workflow_ci_pipeline_id_fkey, ADD CONSTRAINT ci_workflow_ci_pipeline_id_fkey FOREIGN KEY (ci_pipeline_id) REFERENCES   ci_pipeline(id) ON DELETE CASCADE;
+DELETE FROM git_material_history WHERE git_material_id IN(SELECT id FROM git_material WHERE app_id IN(SELECT id FROM app WHERE app_store = 2));
 
-ALTER TABLE ci_pipeline_material DROP CONSTRAINT ci_pipeline_material_git_material_id_fkey, ADD CONSTRAINT ci_pipeline_material_git_material_id_fkey FOREIGN KEY (git_material_id) REFERENCES  git_material(id) ON DELETE CASCADE;
+DELETE FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_store = 2);
 
-ALTER TABLE ci_template DROP CONSTRAINT ci_template_git_material_id_fkey, ADD CONSTRAINT ci_template_git_material_id_fkey FOREIGN KEY (git_material_id) REFERENCES  git_material(id) ON DELETE CASCADE;
+DELETE FROM app_label WHERE app_id IN (SELECT id FROM app WHERE app_store = 2);
 
-ALTER TABLE ci_template_history DROP CONSTRAINT ci_template_git_material_history_id_fkey, ADD CONSTRAINT ci_template_git_material_history_id_fkey FOREIGN KEY (git_material_id) REFERENCES  git_material(id) ON DELETE CASCADE;
+DELETE FROM app_workflow_mapping WHERE app_workflow_id IN(SELECT id FROM app_workflow WHERE app_id IN (SELECT id FROM app WHERE app_store = 2));
 
-ALTER TABLE ci_pipeline DROP CONSTRAINT ci_pipeline_ci_template_id_fkey, ADD CONSTRAINT ci_pipeline_ci_template_id_fkey FOREIGN KEY (ci_template_id) REFERENCES  ci_template(id) ON DELETE CASCADE;
+DELETE FROM app_workflow WHERE app_id IN (SELECT id FROM app WHERE app_store = 2);
 
 DELETE FROM app WHERE app_store = 2;
 
@@ -39,65 +39,5 @@ ALTER TABLE app ALTER COLUMN app_store SET DEFAULT FALSE;
 ALTER TABLE app DROP COLUMN display_name;
 
 ALTER TABLE app DROP COLUMN description;
-
-ALTER TABLE ci_pipeline DROP CONSTRAINT ci_pipeline_ci_template_id_fkey;
-
-ALTER TABLE ci_pipeline ADD CONSTRAINT ci_pipeline_ci_template_id_fkey FOREIGN KEY (ci_template_id) REFERENCES ci_template(id);
-
-ALTER TABLE ci_template_history DROP CONSTRAINT ci_template_git_material_history_id_fkey;
-
-ALTER TABLE ci_template_history ADD CONSTRAINT ci_template_git_material_history_id_fkey FOREIGN KEY (git_material_id) REFERENCES git_material(id);
-
-ALTER TABLE ci_template DROP CONSTRAINT ci_template_git_material_id_fkey;
-
-ALTER TABLE ci_template ADD CONSTRAINT ci_template_git_material_id_fkey FOREIGN KEY (git_material_id) REFERENCES git_material(id);
-
-ALTER TABLE ci_pipeline_material DROP CONSTRAINT ci_pipeline_material_git_material_id_fkey;
-
-ALTER TABLE ci_pipeline_material ADD CONSTRAINT ci_pipeline_material_git_material_id_fkey FOREIGN KEY (git_material_id) REFERENCES git_material(id);
-
-ALTER TABLE ci_workflow DROP CONSTRAINT ci_workflow_ci_pipeline_id_fkey;
-
-ALTER TABLE ci_workflow ADD CONSTRAINT ci_workflow_ci_pipeline_id_fkey FOREIGN KEY (ci_pipeline_id) REFERENCES ci_pipeline(id);
-
-ALTER TABLE git_material_history DROP CONSTRAINT git_material_history_git_material_id_fkey;
-
-ALTER TABLE git_material_history ADD CONSTRAINT git_material_history_git_material_id_fkey FOREIGN KEY (git_material_id) REFERENCES git_material(id);
-
-ALTER TABLE ci_pipeline_history DROP CONSTRAINT ci_pipeline_history_ci_pipeline_id_fk;
-
-ALTER TABLE ci_pipeline_history ADD CONSTRAINT ci_pipeline_history_ci_pipeline_id_fk FOREIGN KEY (ci_pipeline_id) REFERENCES ci_pipeline(id);
-
-ALTER TABLE ci_pipeline_material DROP CONSTRAINT ci_pipeline_material_ci_pipeline_id_fkey;
-
-ALTER TABLE ci_pipeline_material ADD CONSTRAINT ci_pipeline_material_ci_pipeline_id_fkey FOREIGN KEY (ci_pipeline_id) REFERENCES ci_pipeline(id);
-
-ALTER TABLE app_workflow_mapping DROP CONSTRAINT app_workflow_mapping_app_workflow_id_fkey;
-
-ALTER TABLE app_workflow_mapping ADD CONSTRAINT app_workflow_mapping_app_workflow_id_fkey FOREIGN KEY (app_workflow_id) REFERENCES app_workflow(id);
-
-ALTER TABLE ci_template_history DROP CONSTRAINT ci_template_history_app_id_fkey;
-
-ALTER TABLE ci_template_history ADD CONSTRAINT ci_template_history_app_id_fkey FOREIGN KEY (app_id) REFERENCES app(id);
-
-ALTER TABLE app_label DROP CONSTRAINT app_label_app_id_fkey;
-
-ALTER TABLE app_label ADD CONSTRAINT app_label_app_id_fkey FOREIGN KEY (app_id) REFERENCES app(id);
-
-ALTER TABLE git_material DROP CONSTRAINT git_material_app_id_fkey;
-
-ALTER TABLE git_material ADD CONSTRAINT git_material_app_id_fkey FOREIGN KEY (app_id) REFERENCES app(id);
-
-ALTER TABLE ci_template DROP CONSTRAINT ci_template_app_id_fkey;
-
-ALTER TABLE git_material ADD CONSTRAINT ci_template_app_id_fkey FOREIGN KEY (app_id) REFERENCES app(id);
-
-ALTER TABLE ci_pipeline DROP CONSTRAINT ci_pipeline_app_id_fkey;
-
-ALTER TABLE ci_pipeline ADD CONSTRAINT ci_pipeline_app_id_fkey FOREIGN KEY (app_id) REFERENCES app(id);
-
-ALTER TABLE app_workflow DROP CONSTRAINT app_workflow_app_id_fkey;
-
-ALTER TABLE app_workflow ADD CONSTRAINT app_workflow_app_id_fkey FOREIGN KEY (app_id) REFERENCES app(id);
 
 ALTER TABLE ci_artifact DROP COLUMN is_artifact_uploaded;
