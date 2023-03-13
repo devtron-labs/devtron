7714bc1dc8ba68c2a6864523c3d0fea1bcd26dba
Kripansh <kripansh@devtron.ai>
2023-03-13 13:05:07 +0530

    data optimization added

--- internal/sql/repository/AppListingRepository.go
+++ internal/sql/repository/AppListingRepository.go
@@ -432,7 +432,7 @@ func (impl AppListingRepositoryImpl) FetchAppStageStatus(appId int) ([]bean.AppS
 		" LEFT JOIN charts ch on ch.app_id=app.id" +
 		" LEFT JOIN pipeline p on p.app_id=app.id" +
 		" LEFT JOIN chart_env_config_override ceco on ceco.chart_id=ch.id" +
-		" WHERE app.id=? and app.app_type = ?;"
+		" WHERE app.id=? and app.app_type = ? limit 1;"
 
 	impl.Logger.Debugw("last app stages status query:", "query", query)
 	appType := helper.CustomApp
