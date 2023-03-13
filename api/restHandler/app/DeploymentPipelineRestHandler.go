b68e2828df79d77774036a6aeb1282c6ef4e00a5
vishal bihani <vishal@devtron.ai>
2023-03-13 18:04:11 +0530

    enable auto deployment trigger

--- api/restHandler/app/DeploymentPipelineRestHandler.go
+++ api/restHandler/app/DeploymentPipelineRestHandler.go
@@ -336,8 +336,6 @@ func (handler PipelineConfigRestHandlerImpl) HandleChangeDeploymentRequest(w htt
 	}
 	ctx := context.WithValue(r.Context(), "token", acdToken)
 
-	// Disabling auto-trigger until bulk trigger API is fixed
-	deploymentAppTypeChangeRequest.AutoTriggerDeployment = false
 	resp, err := handler.pipelineBuilder.ChangeDeploymentType(ctx, deploymentAppTypeChangeRequest)
 
 	if err != nil {
