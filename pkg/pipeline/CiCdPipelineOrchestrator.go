cba1d0c2c56caa0c47d7f28bb0c801319d1e8b7a
vikramdevtron <vikram@devtron.ai>
2023-03-15 15:00:01 +0530

    optimzation on db queries for app grouping api's

--- pkg/pipeline/CiCdPipelineOrchestrator.go
+++ pkg/pipeline/CiCdPipelineOrchestrator.go
@@ -1418,6 +1418,7 @@ func (impl CiCdPipelineOrchestratorImpl) GetCdPipelinesForEnv(envId int) (cdPipe
 			Id:                            dbPipeline.Id,
 			Name:                          dbPipeline.Name,
 			EnvironmentId:                 dbPipeline.EnvironmentId,
+			EnvironmentName:               dbPipeline.Environment.Name,
 			CiPipelineId:                  dbPipeline.CiPipelineId,
 			TriggerType:                   dbPipeline.TriggerType,
 			PreStage:                      preStage,
