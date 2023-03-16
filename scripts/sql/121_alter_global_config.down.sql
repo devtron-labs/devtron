275198a536d7c18c33fa1b62128b739b94317073
ayushmaheshwari <ayush@devtron.ai>
2023-03-16 15:18:01 +0530

    migration script seq no correction

--- scripts/sql/121_alter_global_config.down.sql
+++ scripts/sql/121_alter_global_config.down.sql
@@ -1,3 +1,1 @@
-ALTER TABLE global_cm_cs ADD secret_ingestion_for VARCHAR(50);
---setting type as CI/CD because secrets until this release should be available in both CI and CD pipelines
-UPDATE global_cm_cs SET secret_ingestion_for='CI/CD';
+ALTER TABLE global_cm_cs DROP COLUMN secret_ingestion_for;
--- scripts/sql/121_alter_global_config.up.sql
+++ scripts/sql/121_alter_global_config.up.sql
@@ -1,1 +1,3 @@
-ALTER TABLE global_cm_cs DROP COLUMN secret_ingestion_for;
+ALTER table global_cm_cs ADD secret_ingestion_for VARCHAR(50);
+--setting type as CI/CD because secrets until this release should be available in both CI and CD pipelines
+UPDATE global_cm_cs SET secret_ingestion_for='CI/CD';
