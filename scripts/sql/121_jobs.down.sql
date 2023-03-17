39a1d028a1cc229cd2c38a7e8b40aa5fb7224c81
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2023-03-17 18:00:08 +0530

    Optimized sql up and down.

--- scripts/sql/121_jobs.down.sql
+++ scripts/sql/121_jobs.down.sql
@@ -1,45 +1,19 @@
-DELETE FROM ci_pipeline_material WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_type = 2));
+UPDATE ci_pipeline_material SET  active = false FROM git_material JOIN app ON app.id = git_material.app_id WHERE app.app_type = 2 AND ci_pipeline_material.git_material_id = git_material.id;
 
-DELETE FROM ci_template_history WHERE git_material_id IN (SELECT id FROM git_material_history WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_type = 2)));
+UPDATE ci_template_history SET active = false WHERE app_id IN (SELECT id FROM app WHERE app_type = 2);
 
-DELETE FROM ci_pipeline_history WHERE ci_pipeline_id IN (SELECT id FROM ci_pipeline WHERE ci_template_id IN (SELECT id FROM ci_template WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_type = 2))));
+UPDATE ci_pipeline SET active = false WHERE app_id IN (SELECT id FROM app WHERE app_type = 2);
 
-DELETE FROM ci_artifact WHERE ci_workflow_id IN (SELECT id FROM ci_workflow WHERE ci_pipeline_id IN (SELECT id FROM ci_pipeline WHERE ci_template_id IN (SELECT id FROM ci_template WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_type = 2)))));
+UPDATE ci_template_history SET active = false WHERE app_id IN (SELECT id FROM app WHERE app_type = 2);
 
-DELETE FROM ci_workflow WHERE ci_pipeline_id IN (SELECT id FROM ci_pipeline WHERE ci_template_id IN (SELECT id FROM ci_template WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_type = 2))));
+UPDATE ci_template SET active = false WHERE app_id IN (SELECT id FROM app WHERE app_type = 2);
 
-DELETE FROM pipeline_stage WHERE ci_pipeline_id IN (SELECT id FROM ci_pipeline WHERE ci_template_id IN (SELECT id FROM ci_template WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_type = 2))));
+UPDATE git_material_history SET active = false WHERE app_id IN (SELECT id FROM app WHERE app_type = 2);
 
-DELETE FROM ci_pipeline WHERE ci_template_id IN (SELECT id FROM ci_template WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_type = 2)));
+UPDATE git_material SET active = false WHERE app_id IN (SELECT id FROM app WHERE app_type = 2);
 
-DELETE FROM ci_workflow WHERE ci_pipeline_id IN (SELECT id FROM ci_pipeline WHERE ci_template_id IN (SELECT id FROM ci_template WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_type = 2))));
+UPDATE app_workflow_mapping SET active = false FROM app_workflow JOIN app ON app_workflow.app_id = app.id AND app.app_type = 2WHERE app_workflow_mapping.app_workflow_id = app_workflow.id;
 
-DELETE FROM ci_template_history WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_type = 2));
+UPDATE app_workflow SET active = false WHERE app_id IN (SELECT id FROM app WHERE app_type = 2);
 
-DELETE FROM ci_template WHERE git_material_id IN (SELECT id FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_type = 2));
-
-DELETE FROM git_material_history WHERE git_material_id IN(SELECT id FROM git_material WHERE app_id IN(SELECT id FROM app WHERE app_type = 2));
-
-DELETE FROM git_material WHERE app_id IN (SELECT id FROM app WHERE app_type = 2) ;
-
-DELETE FROM app_label WHERE app_id IN (SELECT id FROM app WHERE app_type = 2);
-
-DELETE FROM app_workflow_mapping WHERE app_workflow_id IN(SELECT id FROM app_workflow WHERE app_id IN (SELECT id FROM app WHERE app_type = 2));
-
-DELETE FROM app_workflow WHERE app_id IN (SELECT id FROM app WHERE app_type = 2);
-
-DELETE FROM app WHERE app_type = 2;
-
-ALTER TABLE app ALTER COLUMN app_type DROP DEFAULT;
-
-ALTER TABLE app ALTER app_type TYPE boolean USING CASE WHEN app_type=1 THEN true ELSE false end;
-
-ALTER TABLE app ALTER COLUMN app_type SET DEFAULT FALSE;
-
-ALTER TABLE app RENAME COLUMN app_type TO app_store;
-
-ALTER TABLE app DROP COLUMN display_name;
-
-ALTER TABLE app DROP COLUMN description;
-
-ALTER TABLE ci_artifact DROP COLUMN is_artifact_uploaded;
+UPDATE app SET active = false WHERE app_type = 2;
--- scripts/sql/121_jobs.up.sql
+++ scripts/sql/121_jobs.up.sql
@@ -1,10 +1,8 @@
-ALTER TABLE app ALTER COLUMN app_store DROP DEFAULT;
-ALTER TABLE app ALTER app_store TYPE integer USING CASE WHEN app_store=true THEN 1 ELSE 0 end;
-ALTER TABLE app RENAME COLUMN app_store TO app_type;
-ALTER TABLE app ALTER COLUMN app_store SET DEFAULT 0;
-ALTER TABLE app ADD COLUMN display_name varchar(250);
-ALTER TABLE app ADD COLUMN description text;
-ALTER TABLE ci_artifact ADD COLUMN is_artifact_uploaded BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE app ADD COLUMN IF NOT EXISTS app_type integer not null DEFAULT 0;
+UPDATE app SET app_type = CASE WHEN app_store = false THEN 0 WHEN app_store = true THEN 1 ELSE app_type  END;
+ALTER TABLE app ADD COLUMN IF NOT EXISTS description text;
+ALTER TABLE app ADD COLUMN IF NOT EXISTS display_name varchar(250);
+ALTER TABLE ci_artifact ADD COLUMN IF NOT EXISTS is_artifact_uploaded BOOLEAN DEFAULT FALSE;
 UPDATE ci_artifact SET is_artifact_uploaded = true;
 
 
