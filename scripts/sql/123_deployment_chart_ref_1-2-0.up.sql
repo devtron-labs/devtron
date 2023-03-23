3697e05cb099772b6f42db5398bab29d6be98889
jatin-jangir <79351203+jatin-jangir@users.noreply.github.com>
2023-03-23 18:22:25 +0530

    Feat: added new chart for winter-solider ( reference chart 4.18.0) (#3040)
    * added 4.18.0 reference chart
    
    * edited spelling mistakes
    
    * added  winterSoldier description in schema.json
    
    ---------
    
    Co-authored-by: jatin-jangir-0220 <122791251+jatin-jangir-0220@users.noreply.github.com>

---
+++ scripts/sql/123_deployment_chart_ref_1-2-0.up.sql
@@ -0,0 +1,6 @@
+INSERT INTO "public"."chart_ref" ("name","location", "version", "deployment_strategy_path","is_default", "active", "created_on", "created_by", "updated_on", "updated_by") VALUES
+     ('Deployment','deployment-chart_1-2-0', '1.2.0','pipeline-values.yaml','f', 't', 'now()', 1, 'now()', 1);
+
+INSERT INTO global_strategy_metadata_chart_ref_mapping ("global_strategy_metadata_id", "chart_ref_id", "active", "created_on", "created_by", "updated_on", "updated_by")
+VALUES (1,(select id from chart_ref where version='1.2.0' and name='Deployment'), true, now(), 1, now(), 1),
+(4,(select id from chart_ref where version='1.2.0' and name='Deployment'), true, now(), 1, now(), 1);
