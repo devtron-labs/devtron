ff41760a9842e55ce34eb34ca6203aeaf9952428
Badal Kumar Prusty <badalkumar@Badals-MacBook-Pro.local>
2023-04-03 12:45:04 +0530

    modify sqlfile

--- scripts/sql/126-statefulset-chart-ref_1-0-0.up.sql
+++ scripts/sql/126-statefulset-chart-ref_1-0-0.up.sql
@@ -7,4 +7,6 @@ VALUES (nextval("id_seq_global_strategy_metadata"),"ONDELETE","OnDelete strategy
 
 
 INSERT INTO global_strategy_metadata_chart_ref_mapping ("global_strategy_metadata_id", "chart_ref_id", "active", "created_on", "created_by", "updated_on", "updated_by")
-VALUES (1,(select id from chart_ref where location='statefulset-chart_1-0-0' and name is null), true, now(), 1, now(), 1);
+VALUES (1,(select id from chart_ref where location='statefulset-chart_1-0-0' and name is null), true, now(), 1, now(), 1),
+VALUES ((select id from global_strategy_metadata where name="ONDELETE") ,(select id from chart_ref where location='statefulset-chart_1-0-0' and name is null), true, now(), 1, now(), 1);
+
