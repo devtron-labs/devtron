658fe2b57399db0f208e630548e6f0a5aa696317
Badal Kumar Prusty <badalkumar@Badals-MacBook-Pro.local>
2023-04-11 18:42:16 +0530

    modified deployment and sql file

--- scripts/sql/126_statefulset-chart-ref_1-0-0.up.sql
+++ scripts/sql/126_statefulset-chart-ref_1-0-0.up.sql
@@ -3,12 +3,14 @@ INSERT INTO "public"."chart_ref" ("location", "version","deployment_strategy_pat
     ('statefulset-chart_1-0-0', '1.0.0','pipeline-values.yaml', 't', 't', 'now()', 1, 'now()', 1,'StatefulSet');
 
 INSERT INTO global_strategy_metadata("id","name","description","deleted","created_on","created_by","updated_on","updated_by")
+VALUES (nextval('id_seq_global_strategy_metadata'),'ROLLINGUPDATE','Rolling update strategy for statefulset.',false,now(),1,now(),1),
 VALUES (nextval('id_seq_global_strategy_metadata'),'ONDELETE','OnDelete strategy for statefulset.',false,now(),1,now(),1);
 
 
 INSERT INTO global_strategy_metadata_chart_ref_mapping ("global_strategy_metadata_id", "chart_ref_id", "active", "created_on", "created_by", "updated_on", "updated_by")
-VALUES (1,(select id from chart_ref where location='statefulset-chart_1-0-0' and name is null), true, now(), 1, now(), 1),
+VALUES ((select id from global_strategy_metadata where name='ROLLINGUPDATE') ,(select id from chart_ref where location='statefulset-chart_1-0-0' and name is null), true, now(), 1, now(), 1),
 VALUES ((select id from global_strategy_metadata where name='ONDELETE') ,(select id from chart_ref where location='statefulset-chart_1-0-0' and name is null), true, now(), 1, now(), 1);
+
 
 
 INSERT INTO chart_ref_metadata("chart_name","chart_description")
