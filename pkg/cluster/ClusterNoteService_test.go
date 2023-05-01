cc183f3ace4f4a77c3116b4f7729a43e1cb7fbce
Ash-exp <asutosh2000ad@gmail.com>
2023-05-01 12:37:58 +0530

    updated integration test case

--- pkg/cluster/ClusterNoteService_test.go
+++ pkg/cluster/ClusterNoteService_test.go
@@ -17,7 +17,7 @@ type Config struct {
 	Port     string `env:"TEST_PG_PORT" envDefault:"5432"`
 	User     string `env:"TEST_PG_USER" envDefault:"postgres"`
 	Password string `env:"TEST_PG_PASSWORD" envDefault:"postgrespw" secretData:"-"`
-	Database string `env:"TEST_PG_DATABASE" envDefault:"postgres"`
+	Database string `env:"TEST_PG_DATABASE" envDefault:"orchestrator"`
 	LogQuery bool   `env:"TEST_PG_LOG_QUERY" envDefault:"true"`
 }
 
@@ -72,7 +72,7 @@ func TestClusterNoteService_Save(t *testing.T) {
 	cleanDb(t)
 }
 
-func TestClusterNoteServiceImpl_Update_InvalidFields(t *testing.T) {
+func TestClusterNoteServiceImpl_Update(t *testing.T) {
 	if clusterNoteService == nil {
 		InitClusterNoteService()
 	}
@@ -145,13 +145,15 @@ func getDbConn() (*pg.DB, error) {
 		Password: cfg.Password,
 		Database: cfg.Database,
 	}
-	dbConnection := pg.Connect(&options)
-	return dbConnection, nil
+	db = pg.Connect(&options)
+	return db, nil
 }
 
 func cleanDb(tt *testing.T) {
 	DB, _ := getDbConn()
-	query := "truncate cluster cascade;"
+	query := "DELETE FROM cluster_note_history WHERE note_id IN (SELECT id FROM cluster_note);\n" +
+		"DELETE FROM cluster_note WHERE cluster_id IN (SELECT id FROM cluster);\n" +
+		"DELETE FROM cluster WHERE id=10000 OR id=10001;\n"
 	_, err := DB.Exec(query)
 	assert.Nil(tt, err)
 	if err != nil {
@@ -174,68 +176,6 @@ func createClusterData(DB *pg.DB, bean *ClusterBean) error {
 
 func initialiseDb(tt *testing.T) {
 	DB, _ := getDbConn()
-	query := "CREATE TABLE IF NOT EXISTS public.cluster (\n" +
-		"    id integer NOT NULL PRIMARY KEY,\n" +
-		"    cluster_name character varying(250) NOT NULL,\n" +
-		"    active boolean NOT NULL,\n" +
-		"    created_on timestamp with time zone NOT NULL,\n" +
-		"    created_by integer NOT NULL,\n" +
-		"    updated_on timestamp with time zone NOT NULL,\n" +
-		"    updated_by integer NOT NULL,\n" +
-		"    server_url character varying(250),\n" +
-		"    config json,\n" +
-		"    prometheus_endpoint character varying(250),\n" +
-		"    cd_argo_setup boolean DEFAULT false,\n " +
-		"    p_username character varying(250),\n" +
-		"    p_password character varying(250),\n" +
-		"    p_tls_client_cert text,\n" +
-		"    p_tls_client_key text,\n" +
-		"    agent_installation_stage int4 DEFAULT 0,\n" +
-		"    error_in_connecting TEXT,\n" +
-		"    k8s_version varchar(250)\n" +
-		");\n\n\n" +
-		"ALTER TABLE public.cluster OWNER TO postgres;"
-	_, err := DB.Exec(query)
-	assert.Nil(tt, err)
-	if err != nil {
-		return
-	}
-	query = "-- Sequence and defined type for cluster_note\n" +
-		"CREATE SEQUENCE IF NOT EXISTS id_seq_cluster_note;\n\n" +
-		"-- Sequence and defined type for cluster_note_history\n" +
-		"CREATE SEQUENCE IF NOT EXISTS id_seq_cluster_note_history;\n\n" +
-		"-- cluster_note Table Definition\n" +
-		"CREATE TABLE IF NOT EXISTS public.cluster_note\n(\n" +
-		"    \"id\"                        int4         NOT NULL DEFAULT nextval('id_seq_cluster_note'::regclass),\n" +
-		"    \"cluster_id\"                int4         NOT NULL,\n" +
-		"    \"description\"               TEXT         NOT NULL,\n" +
-		"    \"created_on\"                timestamptz  NOT NULL,\n" +
-		"    \"created_by\"                int4         NOT NULL,\n" +
-		"    \"updated_on\"                timestamptz,\n" +
-		"    \"updated_by\"                int4,\n" +
-		"    PRIMARY KEY (\"id\")\n);\n\n" +
-		"-- add foreign key\n" +
-		"ALTER TABLE \"public\".\"cluster_note\"\n" +
-		"    ADD FOREIGN KEY (\"cluster_id\") REFERENCES \"public\".\"cluster\" (\"id\");\n\n" +
-		"-- cluster_note_history Table Definition\n" +
-		"CREATE TABLE IF NOT EXISTS public.cluster_note_history\n" +
-		"(\n" +
-		"    \"id\"                        int4         NOT NULL DEFAULT nextval('id_seq_cluster_note_history'::regclass),\n" +
-		"    \"note_id\"                   int4         NOT NULL,\n" +
-		"    \"description\"               TEXT         NOT NULL,\n" +
-		"    \"created_on\"                timestamptz  NOT NULL,\n" +
-		"    \"created_by\"                int4         NOT NULL,\n" +
-		"    \"updated_on\"                timestamptz,\n" +
-		"    \"updated_by\"                int4,\n" +
-		"    PRIMARY KEY (\"id\")\n);\n\n" +
-		"-- add foreign key\n" +
-		"ALTER TABLE \"public\".\"cluster_note_history\"\n" +
-		"    ADD FOREIGN KEY (\"note_id\") REFERENCES \"public\".\"cluster_note\" (\"id\");"
-	_, err = DB.Exec(query)
-	assert.Nil(tt, err)
-	if err != nil {
-		return
-	}
 	clusters := []ClusterBean{
 		{
 			Id:          10000,
@@ -249,7 +189,7 @@ func initialiseDb(tt *testing.T) {
 		},
 	}
 	for _, cluster := range clusters {
-		err = createClusterData(DB, &cluster)
+		err := createClusterData(DB, &cluster)
 		assert.Nil(tt, err)
 		if err != nil {
 			return
