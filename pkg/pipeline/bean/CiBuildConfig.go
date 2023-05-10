ede6e1a77e6cc3bffc55271ca5e0c065969005ca
Gireesh Naidu <gireesh@devtron.ai>
2023-05-10 12:17:53 +0530

    remove '.' from empty context

--- pkg/pipeline/bean/CiBuildConfig.go
+++ pkg/pipeline/bean/CiBuildConfig.go
@@ -109,9 +109,6 @@ func convertMetadataToBuildPackConfig(buildConfMetadata string) (*BuildPackConfi
 func convertMetadataToDockerBuildConfig(dockerBuildMetadata string) (*DockerBuildConfig, error) {
 	dockerBuildConfig := &DockerBuildConfig{}
 	err := json.Unmarshal([]byte(dockerBuildMetadata), dockerBuildConfig)
-	if dockerBuildConfig.BuildContext == "" {
-		dockerBuildConfig.BuildContext = "."
-	}
 	return dockerBuildConfig, err
 }
 
