ef06cc9747ab6c8332521277fc0aa277bb88951e
Gireesh Naidu <gireesh@devtron.ai>
2023-05-11 11:07:19 +0530

    sql script updated

--- scripts/sql/135_alter_ci_template.down.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE ci_template DROP COLUMN build_context;
-ALTER TABLE ci_template DROP COLUMN build_context_git_material_id;
-ALTER TABLE ci_template_override DROP COLUMN build_context_git_material_id;
