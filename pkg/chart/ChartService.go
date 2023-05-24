1f4785cf3373624081628d159a1c5586e9a8a373
Ash-exp <asutosh2000ad@gmail.com>
2023-05-24 16:14:14 +0530

    fix: env-override save issue

--- pkg/chart/ChartService.go
+++ pkg/chart/ChartService.go
@@ -490,27 +490,27 @@ func (impl ChartServiceImpl) CreateChartFromEnvOverride(templateRequest Template
 		impl.logger.Errorw("chart version parsing", "err", err)
 		return nil, err
 	}
-	var appLevelMetrics *repository3.AppLevelMetrics
-	isAppMetricsSupported, err := impl.CheckIsAppMetricsSupported(templateRequest.ChartRefId)
-	if err != nil {
-		return nil, err
-	}
-	if appMetrics && !(isAppMetricsSupported) {
-		impl.logger.Error("cannot enable app metrics for older chart versions < 3.7.0")
-		appMetricsRequest := AppMetricEnableDisableRequest{UserId: templateRequest.UserId, AppId: templateRequest.AppId, IsAppMetricsEnabled: false}
-		appLevelMetrics, err = impl.updateAppLevelMetrics(&appMetricsRequest)
-		if err != nil {
-			impl.logger.Errorw("err while updating app metrics", "err", err)
-			return nil, err
-		}
-	} else {
-		appMetricsRequest := AppMetricEnableDisableRequest{UserId: templateRequest.UserId, AppId: templateRequest.AppId, IsAppMetricsEnabled: templateRequest.IsAppMetricsEnabled}
-		appLevelMetrics, err = impl.updateAppLevelMetrics(&appMetricsRequest)
-		if err != nil {
-			impl.logger.Errorw("err while updating app metrics", "err", err)
-			return nil, err
-		}
-	}
+	//var appLevelMetrics *repository3.AppLevelMetrics
+	//isAppMetricsSupported, err := impl.CheckIsAppMetricsSupported(templateRequest.ChartRefId)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//if appMetrics && !(isAppMetricsSupported) {
+	//	impl.logger.Error("cannot enable app metrics for older chart versions < 3.7.0")
+	//	appMetricsRequest := AppMetricEnableDisableRequest{UserId: templateRequest.UserId, AppId: templateRequest.AppId, IsAppMetricsEnabled: false}
+	//	appLevelMetrics, err = impl.updateAppLevelMetrics(&appMetricsRequest)
+	//	if err != nil {
+	//		impl.logger.Errorw("err while updating app metrics", "err", err)
+	//		return nil, err
+	//	}
+	//} else {
+	//	appMetricsRequest := AppMetricEnableDisableRequest{UserId: templateRequest.UserId, AppId: templateRequest.AppId, IsAppMetricsEnabled: templateRequest.IsAppMetricsEnabled}
+	//	appLevelMetrics, err = impl.updateAppLevelMetrics(&appMetricsRequest)
+	//	if err != nil {
+	//		impl.logger.Errorw("err while updating app metrics", "err", err)
+	//		return nil, err
+	//	}
+	//}
 
 	impl.logger.Debug("now finally create new chart and make it latest entry in db and previous flag = true")
 	version, err := impl.getNewVersion(chartRepo.Name, chartMeta.Name, refChart)
@@ -586,7 +586,7 @@ func (impl ChartServiceImpl) CreateChartFromEnvOverride(templateRequest Template
 		impl.logger.Errorw("error in creating entry for deployment template history", "err", err, "chart", chart)
 		return nil, err
 	}
-	chartVal, err := impl.chartAdaptor(chart, appLevelMetrics)
+	chartVal, err := impl.chartAdaptor(chart, nil)
 	return chartVal, err
 }
 
