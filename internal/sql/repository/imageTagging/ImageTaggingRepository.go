8d66980eef7ac26a3b2c9a045322c2237b01458e
Gireesh Naidu <gireesh@devtron.ai>
2023-05-26 13:22:58 +0530

    repository layer wip

--- internal/sql/repository/imageTagging/ImageTaggingRepository.go
+++ internal/sql/repository/imageTagging/ImageTaggingRepository.go
@@ -1,7 +1,21 @@
 package repository
 
+import (
+	"github.com/go-pg/pg"
+	"time"
+)
+
+type ImageTaggingAction int
+
+//this action is only allowed for imageComments
+const ActionEdit ImageTaggingAction = 1
+
+const ActionSave ImageTaggingAction = 0
+const ActionHardDelete ImageTaggingAction = 3
+const ActionSoftDelete = 2
+
 type ImageTag struct {
-	tableName  struct{} `sql:"release_tags" json:",omitempty"  pg:",discard_unknown_columns"`
+	TableName  struct{} `sql:"release_tags" json:",omitempty"  pg:",discard_unknown_columns"`
 	Id         int      `sql:"id"`
 	AppId      int      `sql:"app_id"`
 	ArtifactId int      `sql:"artifact_id"`
@@ -9,17 +23,114 @@ type ImageTag struct {
 }
 
 type ImageComment struct {
-	tableName  struct{} `sql:"image_comments" json:",omitempty"  pg:",discard_unknown_columns"`
+	TableName  struct{} `sql:"image_comments" json:",omitempty"  pg:",discard_unknown_columns"`
 	Id         int      `sql:"id"`
 	Comment    int      `sql:"app_id"`
 	ArtifactId int      `sql:"artifact_id"`
+	UserId     int      `sql:"user_id"`
 }
 
-type ImageTagAudit struct {
-	tableName struct{} `sql:"release_tags" json:",omitempty"  pg:",discard_unknown_columns"`
-	Id        int      `sql:"id"`
-	Data      string   `sql:"data"`
+type ImageTaggingAudit struct {
+	TableName  struct{}           `sql:"release_tags" json:",omitempty"  pg:",discard_unknown_columns"`
+	Id         int                `sql:"id"`
+	Data       string             `sql:"data"`
+	DataType   int                `sql:"data_type"`
+	ArtifactId int                `sql:"artifact_id"`
+	UpdatedOn  time.Time          `sql:"updated_on"`
+	UpdatedBy  time.Time          `sql:"updated_by"`
+	Action     ImageTaggingAction `sql:"action"`
+}
 
-	ArtifactId int  `sql:"artifact_id"`
-	Active     bool `sql:"active"`
+type ImageTaggingRepository interface {
+	SaveReleaseTag(tx *pg.Tx, imageTag *ImageTag) error
+	SaveImageComment(tx *pg.Tx, imageComment *ImageComment) error
+	GetTagsByAppId(appId int) ([]ImageTag, error)
+	GetTagsByArtifactId(artifactId int) ([]ImageTag, error)
+	GetImageComment(artifactId int) ([]ImageComment, error)
+	UpdateReleaseTag(tx *pg.Tx, imageTag *ImageTag) error
+	UpdateImageComment(tx *pg.Tx, imageComment *ImageComment) error
+	DeleteReleaseTag(tx *pg.Tx, imageTag *ImageTag) error
+}
+
+type ImageTaggingRepositoryImpl struct {
+	dbConnection *pg.DB
+}
+
+func NewImageTaggingRepositoryImpl(db *pg.DB) *ImageTaggingRepositoryImpl {
+	return &ImageTaggingRepositoryImpl{
+		dbConnection: db,
+	}
+}
+
+func (impl *ImageTaggingRepositoryImpl) SaveReleaseTag(tx *pg.Tx, imageTag *ImageTag) error {
+	err := tx.Insert(imageTag)
+	if err != nil {
+		err = tx.Insert(tx, &ImageTaggingAudit{
+			Action: ActionSave,
+		})
+	}
+	return err
+}
+
+func (impl *ImageTaggingRepositoryImpl) SaveImageComment(tx *pg.Tx, imageComment *ImageComment) error {
+	err := tx.Insert(imageComment)
+	if err != nil {
+		err = tx.Insert(tx, &ImageTaggingAudit{})
+	}
+	return err
+}
+
+func (impl *ImageTaggingRepositoryImpl) GetTagsByAppId(appId int) ([]ImageTag, error) {
+	res := make([]ImageTag, 0)
+	err := impl.dbConnection.Model(&res).
+		Where("app_id=?", appId).
+		Select()
+	return res, err
+}
+
+func (impl *ImageTaggingRepositoryImpl) GetTagsByArtifactId(artifactId int) ([]ImageTag, error) {
+	res := make([]ImageTag, 0)
+	err := impl.dbConnection.Model(&res).
+		Where("artifact_id=?", artifactId).
+		Select()
+	return res, err
+}
+
+func (impl *ImageTaggingRepositoryImpl) GetImageComment(artifactId int) ([]ImageComment, error) {
+	res := make([]ImageComment, 0)
+	err := impl.dbConnection.Model(&res).
+		Where("artifact_id=?", artifactId).
+		Select()
+	return res, err
+}
+
+//this will update the provided release tag
+func (impl *ImageTaggingRepositoryImpl) UpdateReleaseTag(tx *pg.Tx, imageTag *ImageTag) error {
+	//currently tags are not editable, can only be soft deleted or hard delete
+	err := tx.Update(imageTag)
+	if err != nil {
+		err = tx.Insert(tx, &ImageTaggingAudit{
+			Action: ActionSoftDelete,
+		})
+	}
+	return err
+}
+func (impl *ImageTaggingRepositoryImpl) UpdateImageComment(tx *pg.Tx, imageComment *ImageComment) error {
+	err := tx.Update(imageComment)
+	if err != nil {
+		err = tx.Insert(tx, &ImageTaggingAudit{
+			Action: ActionEdit,
+		})
+	}
+	return err
+}
+
+func (impl *ImageTaggingRepositoryImpl) DeleteReleaseTag(tx *pg.Tx, imageTag *ImageTag) error {
+	err := tx.Delete(imageTag)
+	if err != nil {
+		err = tx.Insert(tx, &ImageTaggingAudit{
+			Action: ActionHardDelete,
+		})
+	}
+	return err
 }
