7f5f35cd815d3cd0073335d4550ae8cdc301c88a
ayushmaheshwari <ayush@devtron.ai>
2023-05-26 16:30:58 +0530

    migration correction

--- scripts/sql/143_cd_refactoring.up.sql
+++ scripts/sql/143_cd_refactoring.up.sql
@@ -1,6 +1,6 @@
-ALTER TABLE environment ADD COLUMN virtual_environment BOOLEAN;
+ALTER TABLE environment ADD COLUMN is_virtual_environment BOOLEAN;
 
-update environment set virtual_environment=false;
+update environment set is_virtual_environment=false;
 
 ALTER TABLE cluster ADD COLUMN is_virtual_cluster BOOLEAN;
 
