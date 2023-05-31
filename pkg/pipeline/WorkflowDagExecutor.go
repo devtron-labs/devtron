38b3c35bf5fe78e3eaf265f586bf033df127e5bf
ayushmaheshwari <ayush@devtron.ai>
2023-05-31 13:11:52 +0530

    wip: fix bad gateway

--- pkg/pipeline/WorkflowDagExecutor.go
+++ pkg/pipeline/WorkflowDagExecutor.go
@@ -1700,7 +1700,7 @@ func (impl *WorkflowDagExecutorImpl) ManualCdTrigger(overrideRequest *bean.Value
 
 		var cdWf *pipelineConfig.CdWorkflow
 		if cdWfRunner.CdWorkflowId == 0 {
-			cdWf := &pipelineConfig.CdWorkflow{
+			cdWf = &pipelineConfig.CdWorkflow{
 				CiArtifactId: ciArtifactId,
 				PipelineId:   overrideRequest.PipelineId,
 				AuditLog:     sql.AuditLog{CreatedOn: triggeredAt, CreatedBy: overrideRequest.UserId, UpdatedOn: triggeredAt, UpdatedBy: overrideRequest.UserId},
