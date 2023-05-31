d156459eed8c48aa6dbe7d8ba6c1c7b93f27085d
Shivam-nagar23 <shivam@devtron.ai>
2023-05-31 16:09:02 +0530

    handling latest image scan deploy info

--- pkg/app/AppService.go
+++ pkg/app/AppService.go
@@ -1551,6 +1551,12 @@ func (impl *AppServiceImpl) MarkImageScanDeployed(appId int, envId int, imageDig
 			impl.logger.Errorw("error in creating deploy info", "err", err)
 		}
 	} else {
+		// Updating Execution history for Latest Deployment to fetch out security Vulnerabilities for latest deployed info
+		ot.ImageScanExecutionHistoryId = ids
+		err := impl.imageScanDeployInfoRepository.Update(ot)
+		if err != nil {
+			impl.logger.Errorw("error in updating deploy info for latest deployed image", "err", err)
+		}
 		impl.logger.Debugw("pt", "ot", ot)
 	}
 	return err
