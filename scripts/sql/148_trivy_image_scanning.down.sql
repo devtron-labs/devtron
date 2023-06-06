36b09879d49589a05a69ba2b666d241f9fe9d124
Shivam-nagar23 <shivam@devtron.ai>
2023-06-06 17:49:12 +0530

    sql script change

--- scripts/sql/148_trivy_image_scanning.down.sql
+++ scripts/sql/148_trivy_image_scanning.down.sql
@@ -2,6 +2,7 @@ ALTER TABLE public.module
     DROP COLUMN "enabled" ,
     DROP COLUMN "module_type";
 
-DELETE from public.scan_tool_step CASCADE;
-DELETE from public.registry_index_mapping CASCADE;
-DELETE from public.scan_tool_metadata CASCADE;
+DROP TABLE public.scan_tool_execution_history_mapping CASCADE;
+DROP TABLE public.scan_tool_step CASCADE;
+DROP TABLE public.registry_index_mapping CASCADE;
+DROP TABLE public.scan_tool_metadata CASCADE;
