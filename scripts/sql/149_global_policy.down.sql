505152fef5b25e46fff2dc5f4e53b6b87d4c723a
kartik-579 <kartik@devtron.ai>
2023-06-07 12:46:27 +0530

    updated down script

--- scripts/sql/149_global_policy.down.sql
+++ scripts/sql/149_global_policy.down.sql
@@ -1,11 +1,13 @@
 DROP TABLE IF EXISTS devtron_resource_searchable_key CASCADE;
 DROP SEQUENCE IF EXISTS id_seq_devtron_resource_searchable_key CASCADE;
 
-DROP TABLE IF EXISTS global_policy CASCADE;
-DROP SEQUENCE IF EXISTS id_seq_global_policy CASCADE;
+
+DROP TABLE IF EXISTS global_policy_history;
+DROP SEQUENCE IF EXISTS id_seq_global_policy_history;
 
 DROP TABLE IF EXISTS global_policy_searchable_field;
 DROP SEQUENCE IF EXISTS id_seq_global_policy_searchable_field;
 
-DROP TABLE IF EXISTS global_policy_history;
-DROP SEQUENCE IF EXISTS id_seq_global_policy_history;
+DROP TABLE IF EXISTS global_policy CASCADE;
+DROP SEQUENCE IF EXISTS id_seq_global_policy CASCADE;
+
