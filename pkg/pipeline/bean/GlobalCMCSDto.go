f09dd95f665d1bc54ec23b896ee8ef619d8b43fe
subhashish-devtron <129164905+subhashish-devtron@users.noreply.github.com>
2023-06-08 21:41:38 +0530

    Fix: argo workflow double encoding of secret value in template (#3496)
    * fixing cs issue argo executor
    
    * cleaning
    
    * fix
    
    * addressed PRs

--- pkg/pipeline/bean/GlobalCMCSDto.go
+++ pkg/pipeline/bean/GlobalCMCSDto.go
@@ -3,6 +3,7 @@ package bean
 import (
 	"encoding/json"
 	"github.com/devtron-labs/devtron/api/bean"
+	"github.com/devtron-labs/devtron/internal/sql/repository"
 )
 
 type GlobalCMCSDto struct {
@@ -19,11 +20,24 @@ type GlobalCMCSDto struct {
 }
 
 func (dto GlobalCMCSDto) ConvertToConfigSecretMap() (bean.ConfigSecretMap, error) {
+	var jsonRawMsg []byte
+	var err error
 	configSecretMap := bean.ConfigSecretMap{}
 	configSecretMap.Name = dto.Name
 	configSecretMap.Type = dto.Type
 	configSecretMap.MountPath = dto.MountPath
-	jsonRawMsg, err := json.Marshal(dto.Data)
+
+	// adding handling to get base64 encoded value in map value in case of secrets
+	if dto.ConfigType == repository.CS_TYPE_CONFIG {
+		var csDataMap = make(map[string][]byte)
+		for key, value := range dto.Data {
+			csDataMap[key] = []byte(value)
+		}
+		jsonRawMsg, err = json.Marshal(csDataMap)
+
+	} else {
+		jsonRawMsg, err = json.Marshal(dto.Data)
+	}
 	if err != nil {
 		return configSecretMap, err
 	}
