84eace32430cd35e41bd8d4ad2bbe7582e6ce052
jatin-jangir-0220 <jatin@devtron.ai>
2023-06-09 17:25:06 +0530

    added pipeline-values.yaml

--- scripts/sql/149_cronjob_job_1-5-0.up.sql
+++ scripts/sql/149_cronjob_job_1-5-0.up.sql
@@ -1,6 +1,6 @@
 UPDATE chart_ref SET is_default=false;
-INSERT INTO "public"."chart_ref" ("location", "version", "is_default", "active", "created_on", "created_by", "updated_on", "updated_by", "name") VALUES
-('cronjob-chart_1-5-0', '1.5.0', 'f', 't', 'now()', 1, 'now()', 1, 'Job & CronJob');
+INSERT INTO "public"."chart_ref" ("location", "version", "is_default", "active", "created_on", "created_by", "updated_on", "updated_by", "name","deployment_strategy_path") VALUES
+('cronjob-chart_1-5-0', '1.5.0', 'f', 't', 'now()', 1, 'now()', 1, 'Job & CronJob','pipeline-values.yaml');
 
 UPDATE "public"."chart_ref" SET "is_default" = 't' WHERE "location" = 'reference-chart_4-17-0' AND "version" = '4.17.0';
 
