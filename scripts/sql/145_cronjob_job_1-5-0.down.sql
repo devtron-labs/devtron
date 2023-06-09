adededbbeb70a09f7eabad7970ba29565a435378
jatin-jangir-0220 <jatin@devtron.ai>
2023-06-09 16:37:41 +0530

    updated sql files count

--- scripts/sql/145_cronjob_job_1-5-0.down.sql
+++
@@ -1,1 +0,0 @@
-DELETE FROM "public"."chart_ref" WHERE ("location" = 'cronjob-chart_1-5-0' AND "version" = '1.5.0');
--- scripts/sql/149_cronjob_job_1-5-0.up.sql
+++ scripts/sql/149_cronjob_job_1-5-0.up.sql
@@ -3,3 +3,6 @@ INSERT INTO "public"."chart_ref" ("location", "version", "is_default", "active",
 ('cronjob-chart_1-5-0', '1.5.0', 'f', 't', 'now()', 1, 'now()', 1, 'Job & CronJob');
 
 UPDATE "public"."chart_ref" SET "is_default" = 't' WHERE "location" = 'reference-chart_4-17-0' AND "version" = '4.17.0';
+
+INSERT INTO global_strategy_metadata_chart_ref_mapping ("global_strategy_metadata_id","chart_ref_id", "active","default","created_on", "created_by", "updated_on", "updated_by") VALUES 
+((select id from global_strategy_metadata where name='ROLLING') ,(select id from chart_ref where location='cronjob-chart_1-5-0'), true,true,now(), 1, now(), 1);
