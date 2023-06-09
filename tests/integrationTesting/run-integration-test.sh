8e5602f559974103907242e10943c211ca37be2a
Kripansh <kripansh@devtron.ai>
2023-06-09 19:35:02 +0530

    golang version updated

--- tests/integrationTesting/run-integration-test.sh
+++ tests/integrationTesting/run-integration-test.sh
@@ -1,8 +1,8 @@
 git clone https://github.com/devtron-labs/devtron.git
 cd devtron || exit
 git checkout $TEST_BRANCH
-wget https://go.dev/dl/go1.19.10.linux-amd64.tar.gz -O go1.19.10.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.10.tar.gz
+wget https://go.dev/dl/go1.18.10.linux-amd64.tar.gz -O go1.18.10.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.10.tar.gz
 export GOPATH='/usr/local/go'
 export PATH=$PATH:$GOPATH/bin
 go test ./pkg/pipeline
