7d481f654becf84c66e907f7f156049347739c57
Gireesh Naidu <gireesh@devtron.ai>
2023-06-13 15:45:21 +0530

    optimised query added

--- pkg/cluster/repository/EnvironmentRepository.go
+++ pkg/cluster/repository/EnvironmentRepository.go
@@ -353,3 +353,11 @@ func (repositoryImpl EnvironmentRepositoryImpl) FindEnvLinkedWithCiPipelines(ciP
 	_, err := repositoryImpl.dbConnection.Query(&mappings, query, pg.In(ciPipelineIds))
 	return mappings, err
 }
+
+//query := "SELECT env.* " +
+//" FROM environment env " +
+//" INNER JOIN pipeline ON pipeline.environment_id=env.id and env.active = true " +
+//" INNER JOIN app_workflow_mapping apf ON component_id=pipeline.id AND type='CD_PIPELINE' AND apf.active=true " +
+//" INNER JOIN " +
+//" (SELECT apf2.app_workflow_id FROM app_workflow_mapping apf2 WHERE component_id IN (?) AND type='CI_PIPELINE') sqt " +
+//" ON apf.app_workflow_id = sqt.app_workflow_id;"
