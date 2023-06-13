0e97f1a35b770d0ff1d5e289c2ffc3eacbb81b72
subhashish-devtron <129164905+subhashish-devtron@users.noreply.github.com>
2023-06-13 18:50:10 +0530

    fix: Global secret data bug in CI workflow (#3504)
    * Updated latest image of hyperion in installer
    
    * Updated latest image of devtron in installer
    
    * fix Global secret data bug in CI workflow
    
    * Revert "Updated latest image of devtron in installer"
    
    This reverts commit c5f54cc92e1500133975707b2c1f7017df3a086c.
    
    * Revert "Updated latest image of hyperion in installer"
    
    This reverts commit fb145a1f140b3f97ae6e7388638f73194d10ece1.
    
    ---------
    
    Co-authored-by: ReleaseBot <prakarsh@devtron.ai>

--- pkg/pipeline/bean/GlobalCMCSDto.go
+++ pkg/pipeline/bean/GlobalCMCSDto.go
@@ -20,27 +20,35 @@ type GlobalCMCSDto struct {
 }
 
 func (dto GlobalCMCSDto) ConvertToConfigSecretMap() (bean.ConfigSecretMap, error) {
-	var jsonRawMsg []byte
-	var err error
+
 	configSecretMap := bean.ConfigSecretMap{}
 	configSecretMap.Name = dto.Name
 	configSecretMap.Type = dto.Type
 	configSecretMap.MountPath = dto.MountPath
 
+	var jsonRawMsg []byte
+	var err error
 	// adding handling to get base64 encoded value in map value in case of secrets
 	if dto.ConfigType == repository.CS_TYPE_CONFIG {
-		var csDataMap = make(map[string][]byte)
-		for key, value := range dto.Data {
-			csDataMap[key] = []byte(value)
-		}
-		jsonRawMsg, err = json.Marshal(csDataMap)
+		jsonRawMsg, err = ConvertToEncodedForm(dto.Data)
 
 	} else {
 		jsonRawMsg, err = json.Marshal(dto.Data)
 	}
+
 	if err != nil {
 		return configSecretMap, err
 	}
 	configSecretMap.Data = jsonRawMsg
 	return configSecretMap, nil
+}
+
+// ConvertToEncodedForm Function to encode the values in the input map values
+func ConvertToEncodedForm(data map[string]string) ([]byte, error) {
+	var csDataMap = make(map[string][]byte)
+	for key, value := range data {
+		csDataMap[key] = []byte(value)
+	}
+	jsonRawMsg, err := json.Marshal(csDataMap)
+	return jsonRawMsg, err
 }
