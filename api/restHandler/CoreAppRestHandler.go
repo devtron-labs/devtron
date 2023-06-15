c979263e0cc12ab3c6bdae8d3ea4ac74cdb2a06c
kartik-579 <84493919+kartik-579@users.noreply.github.com>
2023-06-15 11:42:39 +0530

    Oss sync (#253)
    * release: PR for v0.6.18-rc.1 (#3497)
    
    * Created release-notes files
    
    * Update releasenotes.md
    
    * Updated release notes
    
    * Updated latest image of devtron in installer
    
    * Updated latest image of hyperion in installer
    
    * Updated release notes
    
    * Updated latest image of hyperion in installer
    
    * Updated latest image of devtron in installer
    
    * Updated latest image of dashboard in installer
    
    * Updated release notes
    
    * Updated release notes
    
    * Updated latest image of hyperion in installer
    
    * Updated latest image of devtron in installer
    
    * Updated latest image of hyperion in installer
    
    * Updated latest image of devtron in installer
    
    * Updated release-notes files
    
    * Updated latest image of dashboard in installer
    
    * Updated release-notes files
    
    * Updated latest image of ci-runner in installer
    
    * Update Chart.yaml
    
    * Update values.yaml
    
    * Update devtron-bom.yaml
    
    * Update values.yaml
    
    * Update devtron-installer.yaml
    
    * Update installation-script
    
    * Update releasenotes.md
    
    * Updated release-notes files
    
    * Update releasenotes.md
    
    * Update releasenotes.md
    
    * Updated latest image of dashboard in installer
    
    * Updated latest image of ci-runner in installer
    
    * Update Chart.yaml
    
    * Update devtron-bom.yaml
    
    * Update values.yaml
    
    * Update devtron-installer.yaml
    
    * Update installation-script
    
    * Update releasenotes.md
    
    * Updated release-notes files
    
    ---------
    
    Co-authored-by: Pawan Mehta <117346502+pawan-mehta-dt@users.noreply.github.com>
    
    * fix: Global secret data bug in CI workflow (#3504)
    
    * Updated latest image of hyperion in installer
    
    * Updated latest image of devtron in installer
    
    * fix Global secret data bug in CI workflow
    
    * Revert "Updated latest image of devtron in installer"
    
    This reverts commit c5f54cc92e1500133975707b2c1f7017df3a086c.
    
    * Revert "Updated latest image of hyperion in installer"
    
    This reverts commit fb145a1f140b3f97ae6e7388638f73194d10ece1.
    
    ---------
    
    Co-authored-by: ReleaseBot <prakarsh@devtron.ai>
    
    * task: added global policy db scripts (#3486)
    
    * added db scripts
    
    * updated down script
    
    * fix: app workflow creation api panic and app metrics update issue (#3511)
    
    * fix err passing in response writer
    
    * fix for app workflow app mterics issue
    
    * removed redundant comment
    
    ---------
    
    Co-authored-by: Prakarsh <71125043+prakarsh-dt@users.noreply.github.com>
    Co-authored-by: Pawan Mehta <117346502+pawan-mehta-dt@users.noreply.github.com>
    Co-authored-by: subhashish-devtron <129164905+subhashish-devtron@users.noreply.github.com>
    Co-authored-by: ReleaseBot <prakarsh@devtron.ai>

--- api/restHandler/CoreAppRestHandler.go
+++ api/restHandler/CoreAppRestHandler.go
@@ -1764,7 +1764,7 @@ func (handler CoreAppRestHandlerImpl) createEnvDeploymentTemplate(appId int, use
 	}
 
 	// if chart not found for chart_ref then create
-	_, err = handler.chartRepo.FindChartByAppIdAndRefId(appId, chartRefId)
+	chartEntry, err := handler.chartRepo.FindChartByAppIdAndRefId(appId, chartRefId)
 	if err != nil {
 		if pg.ErrNoRows == err {
 			templateRequest := chart.TemplateRequest{
@@ -1786,9 +1786,15 @@ func (handler CoreAppRestHandlerImpl) createEnvDeploymentTemplate(appId int, use
 	}
 
 	// create if required
-	_, err = handler.propertiesConfigService.CreateEnvironmentProperties(appId, envConfigProperties)
+	appMetrics := false
+	if envConfigProperties.AppMetrics != nil {
+		appMetrics = *envConfigProperties.AppMetrics
+	}
+	chartEntry.GlobalOverride = string(envConfigProperties.EnvOverrideValues)
+	_, err = handler.propertiesConfigService.CreateIfRequired(chartEntry, envId, userId, envConfigProperties.ManualReviewed, models.CHARTSTATUS_SUCCESS,
+		true, appMetrics, envConfigProperties.Namespace, envConfigProperties.IsBasicViewLocked, envConfigProperties.CurrentViewEditor, nil)
 	if err != nil {
-		handler.logger.Errorw("service err, CreateEnvironmentProperties", "err", err, "appId", appId, "envId", envId, "chartRefId", chartRefId)
+		handler.logger.Errorw("service err, CreateIfRequired", "err", err, "appId", appId, "envId", envId, "chartRefId", chartRefId)
 		return err
 	}
 
@@ -2090,7 +2096,6 @@ func (handler CoreAppRestHandlerImpl) CreateAppWorkflow(w http.ResponseWriter, r
 	//rbac ends
 
 	handler.logger.Infow("creating app workflow created ", "createAppRequest", createAppRequest)
-	var errResp *multierror.Error
 	var statusCode int
 
 	//creating workflow starts
@@ -2101,7 +2106,7 @@ func (handler CoreAppRestHandlerImpl) CreateAppWorkflow(w http.ResponseWriter, r
 		}
 		err, statusCode = handler.createWorkflows(ctx, createAppRequest.AppId, userId, createAppRequest.AppWorkflows, token, app.AppName)
 		if err != nil {
-			common.WriteJsonResp(w, errResp, nil, statusCode)
+			common.WriteJsonResp(w, err, nil, statusCode)
 			return
 		}
 	}
@@ -2111,7 +2116,7 @@ func (handler CoreAppRestHandlerImpl) CreateAppWorkflow(w http.ResponseWriter, r
 	if createAppRequest.EnvironmentOverrides != nil && len(createAppRequest.EnvironmentOverrides) > 0 {
 		err, statusCode = handler.createEnvOverrides(ctx, createAppRequest.AppId, userId, createAppRequest.EnvironmentOverrides, token)
 		if err != nil {
-			common.WriteJsonResp(w, errResp, nil, statusCode)
+			common.WriteJsonResp(w, err, nil, statusCode)
 			return
 		}
 	}
