11d740d08d8f282d078295b3237bceef968e0083
pawan-59 <pawan@devtron.ai>
2023-06-26 17:43:15 +0530

    added rollout chart 5-0-0

---
+++ scripts/sql/152_rollout-chart-ref_5-0-0.up.sql
@@ -0,0 +1,8 @@
+UPDATE chart_ref SET is_default=false;
+INSERT INTO "public"."chart_ref" ("location", "version","deployment_strategy_path", "is_default", "active", "created_on", "created_by", "updated_on", "updated_by") VALUES
+    ('reference-chart_5-0-0', '5.0.0','pipeline-values.yaml', 't', 't', 'now()', 1, 'now()', 1);
+
+INSERT INTO global_strategy_metadata_chart_ref_mapping ("global_strategy_metadata_id","chart_ref_id", "active","default","created_on", "created_by", "updated_on", "updated_by") VALUES 
+((select id from global_strategy_metadata where name='CANARY') ,(select id from chart_ref where location='reference-chart_5-0-0'), true,false,now(), 1, now(), 1),
+((select id from global_strategy_metadata where name='ROLLING') ,(select id from chart_ref where location='reference-chart_5-0-0'), true, true,now(), 1, now(), 1);
+((select id from global_strategy_metadata where name='BLUE-GREEN') ,(select id from chart_ref where location='reference-chart_5-0-0'), true, false,now(), 1, now(), 1);
