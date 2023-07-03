0cd77c7bbfbc3384f2e2edfe8fb93b5d34e0f27e
adi6859 <aditya.ar1909@gmail.com>
2023-07-03 16:45:57 +0530

    complete migration script added

--- scripts/sql/152_job_ci_env_mapping.down.sql
+++ scripts/sql/152_job_ci_env_mapping.down.sql
@@ -5,3 +5,5 @@ DROP TABLE IF EXISTS "public"."ci_env_mapping_history";
 DROP SEQUENCE IF EXISTS public.id_seq_ci_env_mapping_history;
 
 ALTER TABLE config_map_env_level DROP COLUMN IF EXISTS deleted;
+ALTER TABLE ci_workflow DROP COLUMN IF EXISTS environment_id;
+ALTER TABLE ci_workflow DROP COLUMN IF EXISTS environment_name;
