804932afed264d7b698781bd8782c0705162927f
jatin-jangir-0220 <122791251+jatin-jangir-0220@users.noreply.github.com>
2023-07-03 11:05:27 +0530

    added is_default false

--- scripts/sql/152_deployment_chart_ref_4-18-0.up.sql
+++ scripts/sql/152_deployment_chart_ref_4-18-0.up.sql
@@ -1,5 +1,6 @@
+UPDATE chart_ref SET is_default=false;
 INSERT INTO "public"."chart_ref" ("name","location", "version", "deployment_strategy_path","is_default", "active", "created_on", "created_by", "updated_on", "updated_by") VALUES
-     ('Deployment','deployment-chart_4-18-0', '4.18.0','pipeline-values.yaml','f', 't', 'now()', 1, 'now()', 1);
+     ('Deployment','deployment-chart_4-18-0', '4.18.0','pipeline-values.yaml','t', 't', 'now()', 1, 'now()', 1);
 
 INSERT INTO global_strategy_metadata_chart_ref_mapping ("global_strategy_metadata_id", "chart_ref_id", "active", "created_on", "created_by", "updated_on", "updated_by","default")
 VALUES (1,(select id from chart_ref where version='4.18.0' and name='Deployment'), true, now(), 1, now(), 1,true),
