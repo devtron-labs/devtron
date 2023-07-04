a41583b6222db922120dde00df702708fb5e5118
adi6859 <aditya.ar1909@gmail.com>
2023-07-04 12:19:58 +0530

    test case added for Test_processConfigMapsAndSecrets

--- pkg/pipeline/WorkflowService_test.go
+++ pkg/pipeline/WorkflowService_test.go
@@ -5,9 +5,9 @@ import (
 	"github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
 	bean3 "github.com/devtron-labs/devtron/api/bean"
 	"github.com/devtron-labs/devtron/internal/util"
-	"log"
-
 	"github.com/stretchr/testify/assert"
+	v12 "k8s.io/api/core/v1"
+	"log"
 	"testing"
 )
 
@@ -176,6 +176,70 @@ func Test_getCiTemplateWithConfigMapsAndSecrets(t *testing.T) {
 				return
 			}
 			assert.Equalf(t, tt.want, got, "getCiTemplateWithConfigMapsAndSecrets(%v, %v, %v, %v, %v)", tt.args.configMaps, tt.args.secrets, tt.args.ciTemplate, tt.args.existingConfigMap, tt.args.existingSecrets)
+		})
+	}
+}
+
+func Test_processConfigMapsAndSecrets(t *testing.T) {
+
+	type args struct {
+		impl       *WorkflowServiceImpl
+		configMaps *bean3.ConfigMapJson
+		secrets    *bean3.ConfigSecretJson
+		entryPoint *string
+		steps      *[]v1alpha1.ParallelSteps
+		volumes    *[]v12.Volume
+		templates  *[]v1alpha1.Template
+	}
+	logger, err := util.NewSugardLogger()
+	if err != nil {
+		log.Fatalf("error in logger initialization %s,%s", "err", err)
+	}
+	impl := &WorkflowServiceImpl{Logger: logger}
+	configMaps := &bean3.ConfigMapJson{
+		Enabled: true,
+		Maps: []bean3.ConfigSecretMap{
+			{Name: "job-map",
+				Data:     []byte("{\"abcd\": \"aditya-cm-1-job-test-cm1\"}"),
+				External: false,
+			},
+		},
+	}
+	secrets := &bean3.ConfigSecretJson{
+		Enabled: true,
+		Secrets: []*bean3.ConfigSecretMap{
+			{Name: "job-secret",
+				Data:     []byte("{\"abcd\": \"XCJhZGl0eWEtY20tMS1qb2ItdGVzdC1jbTFcIn0i\"}"),
+				External: false,
+			},
+		},
+	}
+	entryPoint := "\"ci-stages-with-env\""
+	steps := &[]v1alpha1.ParallelSteps{}
+	volumes := &[]v12.Volume{}
+	templates := &[]v1alpha1.Template{}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "TEST 1",
+			args: args{
+				impl:       impl,
+				configMaps: configMaps,
+				secrets:    secrets,
+				entryPoint: &entryPoint,
+				steps:      steps,
+				volumes:    volumes,
+				templates:  templates,
+			},
+			wantErr: assert.NoError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.wantErr(t, processConfigMapsAndSecrets(tt.args.impl, tt.args.configMaps, tt.args.secrets, tt.args.entryPoint, tt.args.steps, tt.args.volumes, tt.args.templates), fmt.Sprintf("processConfigMapsAndSecrets(%v, %v, %v, %v, %v, %v, %v)", tt.args.impl, tt.args.configMaps, tt.args.secrets, tt.args.entryPoint, tt.args.steps, tt.args.volumes, tt.args.templates))
 		})
 	}
 }
