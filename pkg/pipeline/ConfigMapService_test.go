c83da961fbf0593890c94929bcc20c07c1b6415a
adi6859 <aditya.ar1909@gmail.com>
2023-07-05 19:28:47 +0530

    configmap test cases added for get

--- pkg/pipeline/ConfigMapService_test.go
+++ pkg/pipeline/ConfigMapService_test.go
@@ -2,21 +2,16 @@ package pipeline
 
 import (
 	"fmt"
-	"github.com/devtron-labs/devtron/internal/sql/repository/app"
 	mocks4 "github.com/devtron-labs/devtron/internal/sql/repository/app/mocks"
 	"github.com/devtron-labs/devtron/internal/sql/repository/chartConfig"
 	mocks2 "github.com/devtron-labs/devtron/internal/sql/repository/chartConfig/mocks"
 	"github.com/devtron-labs/devtron/internal/util"
-	chartRepoRepository "github.com/devtron-labs/devtron/pkg/chartRepo/repository"
 	"github.com/devtron-labs/devtron/pkg/chartRepo/repository/mocks"
-	"github.com/devtron-labs/devtron/pkg/commonService"
 	mocks3 "github.com/devtron-labs/devtron/pkg/commonService/mocks"
-	"github.com/devtron-labs/devtron/pkg/pipeline/history"
 	mocks5 "github.com/devtron-labs/devtron/pkg/pipeline/history/mocks"
 	"github.com/go-pg/pg"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
-	"go.uber.org/zap"
 	"testing"
 )
 
@@ -181,50 +176,61 @@ func TestConfigMapServiceImpl_ConfigSecretEnvironmentDelete(t *testing.T) {
 }
 
 func TestConfigMapServiceImpl_ConfigSecretEnvironmentGet(t *testing.T) {
-	t.SkipNow()
-	type fields struct {
-		chartRepository             chartRepoRepository.ChartRepository
-		logger                      *zap.SugaredLogger
-		repoRepository              chartRepoRepository.ChartRepoRepository
-		mergeUtil                   util.MergeUtil
-		pipelineConfigRepository    chartConfig.PipelineConfigRepository
-		configMapRepository         chartConfig.ConfigMapRepository
-		environmentConfigRepository chartConfig.EnvConfigOverrideRepository
-		commonService               commonService.CommonService
-		appRepository               app.AppRepository
-		configMapHistoryService     history.ConfigMapHistoryService
+	//t.SkipNow()
+	chartRepository := mocks.NewChartRepository(t)
+	sugaredLogger, _ := util.NewSugardLogger()
+	repoRepository := mocks.NewChartRepoRepository(t)
+	pipelineConfigRepository := mocks2.NewPipelineConfigRepository(t)
+	configMapRepository := mocks2.NewConfigMapRepository(t)
+	environmentConfigRepository := mocks2.NewEnvConfigOverrideRepository(t)
+	commonService := mocks3.NewCommonService(t)
+	appRepository := mocks4.NewAppRepository(t)
+	configMapHistoryService := mocks5.NewConfigMapHistoryService(t)
+	configMap := []*chartConfig.ConfigMapEnvModel{
+		{
+			AppId:         1,
+			EnvironmentId: 1,
+		},
 	}
 	type args struct {
 		appId int
 	}
 	tests := []struct {
-		name    string
-		fields  fields
+		name string
+
 		args    args
 		want    []*chartConfig.ConfigMapEnvModel
 		wantErr assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{
+				appId: 1,
+			},
+			want:    configMap,
+			wantErr: assert.NoError,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			impl := ConfigMapServiceImpl{
-				chartRepository:             tt.fields.chartRepository,
-				logger:                      tt.fields.logger,
-				repoRepository:              tt.fields.repoRepository,
-				mergeUtil:                   tt.fields.mergeUtil,
-				pipelineConfigRepository:    tt.fields.pipelineConfigRepository,
-				configMapRepository:         tt.fields.configMapRepository,
-				environmentConfigRepository: tt.fields.environmentConfigRepository,
-				commonService:               tt.fields.commonService,
-				appRepository:               tt.fields.appRepository,
-				configMapHistoryService:     tt.fields.configMapHistoryService,
+				chartRepository:             chartRepository,
+				logger:                      sugaredLogger,
+				repoRepository:              repoRepository,
+				mergeUtil:                   util.MergeUtil{},
+				pipelineConfigRepository:    pipelineConfigRepository,
+				configMapRepository:         configMapRepository,
+				environmentConfigRepository: environmentConfigRepository,
+				commonService:               commonService,
+				appRepository:               appRepository,
+				configMapHistoryService:     configMapHistoryService,
 			}
+			configMapRepository.On("GetEnvLevelByAppId", 1).Return(configMap, nil)
 			got, err := impl.ConfigSecretEnvironmentGet(tt.args.appId)
 			if !tt.wantErr(t, err, fmt.Sprintf("ConfigSecretEnvironmentGet(%v)", tt.args.appId)) {
 				return
 			}
-			assert.Equalf(t, tt.want, got, "ConfigSecretEnvironmentGet(%v)", tt.args.appId)
+			assert.Equal(t, tt.want[0].EnvironmentId, got[0].EnvironmentId, "ConfigSecretEnvironmentGet(%v)", tt.args.appId)
 		})
 	}
 }
