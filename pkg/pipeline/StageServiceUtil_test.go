57e5f445c94de47a676c472638eace5ecd82c6c7
Prakash Kumar <prakash.kumar@devtron.ai>
2023-07-06 14:55:12 +0530

    improving code duplicacy in stageServiceUtil test file

--- pkg/pipeline/StageServiceUtil_test.go
+++ pkg/pipeline/StageServiceUtil_test.go
@@ -8,6 +8,34 @@ import (
 	"testing"
 )
 
+var cdPipelineConfigObjectWithPreAndPostStage = &bean.CDPipelineConfigObject{
+	Id:                 4,
+	EnvironmentId:      4,
+	EnvironmentName:    "env3",
+	Description:        "",
+	CiPipelineId:       4,
+	TriggerType:        "AUTOMATIC",
+	Name:               "cd-1-e7l1",
+	Namespace:          "ns3",
+	AppWorkflowId:      4,
+	DeploymentTemplate: "ROLLING",
+	PreStage: bean.CdStage{
+		TriggerType: "AUTOMATIC",
+		Name:        "Pre-Deployment",
+		Config:      "version: \"\"\ncdPipelineConf:\n- beforeStages:\n  - id: 0\n    index: 0\n    name: test-1\n    script: |\n      date > test.report\n    outputLocation: ./test.report\n    runstatus: false\n  - id: 0\n    index: 0\n    name: test-2\n    script: |\n      date > test2.report\n    outputLocation: ./test2.report\n    runstatus: false\n  afterStages: []\n",
+	},
+	PostStage: bean.CdStage{
+		TriggerType: "AUTOMATIC",
+		Name:        "Post-Deployment",
+		Config:      "version: \"\"\ncdPipelineConf:\n- beforeStages: []\n  afterStages:\n  - id: 0\n    index: 0\n    name: test-1\n    script: |\n      date > test.report\n    outputLocation: ./test.report\n    runstatus: false\n  - id: 0\n    index: 0\n    name: test-2\n    script: |\n      date > test2.report\n    outputLocation: ./test2.report\n    runstatus: false\n",
+	},
+	ParentPipelineId:   4,
+	ParentPipelineType: "CI_PIPELINE",
+	DeploymentAppType:  "helm",
+	PreDeployStage:     nil,
+	PostDeployStage:    nil,
+}
+
 func TestStageServiceUtil_CreatePreAndPostStageResponse(t *testing.T) {
 	type args struct {
 		cdPipelineConfigObject *bean.CDPipelineConfigObject
@@ -20,61 +48,9 @@ func TestStageServiceUtil_CreatePreAndPostStageResponse(t *testing.T) {
 		wantErr bool
 	}{
 		{name: "Test1_v1_preDeploy_nil_preStage_present", payload: args{
-			cdPipelineConfigObject: &bean.CDPipelineConfigObject{
-				Id:                 4,
-				EnvironmentId:      4,
-				EnvironmentName:    "env3",
-				Description:        "",
-				CiPipelineId:       4,
-				TriggerType:        "AUTOMATIC",
-				Name:               "cd-1-e7l1",
-				Namespace:          "ns3",
-				AppWorkflowId:      4,
-				DeploymentTemplate: "ROLLING",
-				PreStage: bean.CdStage{
-					TriggerType: "AUTOMATIC",
-					Name:        "Pre-Deployment",
-					Config:      "version: 0.0.1\ncdPipelineConf:\n  - beforeStages:\n      - name: test-1\n        script: |\n          date > test.report\n          echo 'hello from inside pre-cd'\n        outputLocation: ./test.report\n      - name: test-2\n        script: |\n          date > test2.report\n        outputLocation: ./test2.report",
-				},
-				PostStage: bean.CdStage{
-					TriggerType: "AUTOMATIC",
-					Name:        "Post-Deployment",
-					Config:      "version: 0.0.1\ncdPipelineConf:\n  - afterStages:\n      - name: test-1\n        script: |\n          date > test.report\n          echo 'hello from inside post-cd'\n        outputLocation: ./test.report\n      - name: test-2\n        script: |\n          date > test2.report\n        outputLocation: ./test2.report",
-				},
-				ParentPipelineId:   4,
-				ParentPipelineType: "CI_PIPELINE",
-				DeploymentAppType:  "helm",
-				PreDeployStage:     nil,
-				PostDeployStage:    nil,
-			},
-			version: "v1",
-		}, want: &bean.CDPipelineConfigObject{
-			Id:                 4,
-			EnvironmentId:      4,
-			EnvironmentName:    "env3",
-			Description:        "",
-			CiPipelineId:       4,
-			TriggerType:        "AUTOMATIC",
-			Name:               "cd-1-e7l1",
-			Namespace:          "ns3",
-			AppWorkflowId:      4,
-			DeploymentTemplate: "ROLLING",
-			PreStage: bean.CdStage{
-				TriggerType: "AUTOMATIC",
-				Name:        "Pre-Deployment",
-				Config:      "version: 0.0.1\ncdPipelineConf:\n  - beforeStages:\n      - name: test-1\n        script: |\n          date > test.report\n          echo 'hello from inside pre-cd'\n        outputLocation: ./test.report\n      - name: test-2\n        script: |\n          date > test2.report\n        outputLocation: ./test2.report",
-			},
-			PostStage: bean.CdStage{
-				TriggerType: "AUTOMATIC",
-				Name:        "Post-Deployment",
-				Config:      "version: 0.0.1\ncdPipelineConf:\n  - afterStages:\n      - name: test-1\n        script: |\n          date > test.report\n          echo 'hello from inside post-cd'\n        outputLocation: ./test.report\n      - name: test-2\n        script: |\n          date > test2.report\n        outputLocation: ./test2.report",
-			},
-			ParentPipelineId:   4,
-			ParentPipelineType: "CI_PIPELINE",
-			DeploymentAppType:  "helm",
-			PreDeployStage:     nil,
-			PostDeployStage:    nil,
-		}, wantErr: false},
+			cdPipelineConfigObject: cdPipelineConfigObjectWithPreAndPostStage,
+			version:                "v1",
+		}, want: cdPipelineConfigObjectWithPreAndPostStage, wantErr: false},
 
 		//it will convert preDeploy into preStageYaml
 		{name: "Test2_v1_preDeploy_present_preStage_nil", payload: args{
@@ -170,64 +146,12 @@ func TestStageServiceUtil_CreatePreAndPostStageResponse(t *testing.T) {
 				},
 			},
 			version: "v1",
-		}, want: &bean.CDPipelineConfigObject{
-			Id:                 4,
-			EnvironmentId:      4,
-			EnvironmentName:    "env3",
-			Description:        "",
-			CiPipelineId:       4,
-			TriggerType:        "AUTOMATIC",
-			Name:               "cd-1-e7l1",
-			Namespace:          "ns3",
-			AppWorkflowId:      4,
-			DeploymentTemplate: "ROLLING",
-			PreStage: bean.CdStage{
-				TriggerType: "AUTOMATIC",
-				Name:        "Pre-Deployment",
-				Config:      "version: \"\"\ncdPipelineConf:\n- beforeStages:\n  - id: 0\n    index: 0\n    name: test-1\n    script: |\n      date > test.report\n    outputLocation: ./test.report\n    runstatus: false\n  - id: 0\n    index: 0\n    name: test-2\n    script: |\n      date > test2.report\n    outputLocation: ./test2.report\n    runstatus: false\n  afterStages: []\n",
-			},
-			PostStage: bean.CdStage{
-				TriggerType: "AUTOMATIC",
-				Name:        "Post-Deployment",
-				Config:      "version: \"\"\ncdPipelineConf:\n- beforeStages: []\n  afterStages:\n  - id: 0\n    index: 0\n    name: test-1\n    script: |\n      date > test.report\n    outputLocation: ./test.report\n    runstatus: false\n  - id: 0\n    index: 0\n    name: test-2\n    script: |\n      date > test2.report\n    outputLocation: ./test2.report\n    runstatus: false\n",
-			},
-			ParentPipelineId:   4,
-			ParentPipelineType: "CI_PIPELINE",
-			DeploymentAppType:  "helm",
-			PreDeployStage:     nil,
-			PostDeployStage:    nil,
-		}, wantErr: false},
+		}, want: cdPipelineConfigObjectWithPreAndPostStage, wantErr: false},
 
 		//v2 :- will convert preStageYaml(if present) to preDeployStageSteps
 		{name: "Test3_v2_preDeploy_nil_preStage_present", payload: args{
-			cdPipelineConfigObject: &bean.CDPipelineConfigObject{
-				Id:                 4,
-				EnvironmentId:      4,
-				EnvironmentName:    "env3",
-				Description:        "",
-				CiPipelineId:       4,
-				TriggerType:        "AUTOMATIC",
-				Name:               "cd-1-e7l1",
-				Namespace:          "ns3",
-				AppWorkflowId:      4,
-				DeploymentTemplate: "ROLLING",
-				PreStage: bean.CdStage{
-					TriggerType: "AUTOMATIC",
-					Name:        "Pre-Deployment",
-					Config:      "version: \"\"\ncdPipelineConf:\n- beforeStages:\n  - id: 0\n    index: 0\n    name: test-1\n    script: |\n      date > test.report\n    outputLocation: ./test.report\n    runstatus: false\n  - id: 0\n    index: 0\n    name: test-2\n    script: |\n      date > test2.report\n    outputLocation: ./test2.report\n    runstatus: false\n  afterStages: []\n",
-				},
-				PostStage: bean.CdStage{
-					TriggerType: "AUTOMATIC",
-					Name:        "Post-Deployment",
-					Config:      "version: \"\"\ncdPipelineConf:\n- beforeStages: []\n  afterStages:\n  - id: 0\n    index: 0\n    name: test-1\n    script: |\n      date > test.report\n    outputLocation: ./test.report\n    runstatus: false\n  - id: 0\n    index: 0\n    name: test-2\n    script: |\n      date > test2.report\n    outputLocation: ./test2.report\n    runstatus: false\n",
-				},
-				ParentPipelineId:   4,
-				ParentPipelineType: "CI_PIPELINE",
-				DeploymentAppType:  "helm",
-				PreDeployStage:     nil,
-				PostDeployStage:    nil,
-			},
-			version: "v2",
+			cdPipelineConfigObject: cdPipelineConfigObjectWithPreAndPostStage,
+			version:                "v2",
 		}, want: &bean.CDPipelineConfigObject{
 			Id:                 4,
 			EnvironmentId:      4,
