feaff3477e5077411cca33e7be09600aafd555f3
Prakash Kumar <prakash.kumar@devtron.ai>
2023-07-07 19:46:47 +0530

    sql script fix

--- scripts/sql/157_create_plugin_stage_mapping.up.sql
+++ scripts/sql/157_create_plugin_stage_mapping.up.sql
@@ -6,7 +6,7 @@ CREATE TABLE  "public"."plugin_stage_mapping"
 (
     "id"             int4    NOT NULL DEFAULT nextval('id_seq_plugin_stage_mapping'::regclass),
     "plugin_id"      int4    NOT NULL,
-    "stage_type"    int4 NOT NULL,
+    "stage_type"     int4 ,
     "created_on"     timestamptz,
     "created_by"     int4,
     "updated_on"     timestamptz,
@@ -14,32 +14,8 @@ CREATE TABLE  "public"."plugin_stage_mapping"
     PRIMARY KEY ("id")
 );
 
-INSERT INTO "public"."plugin_stage_mapping" ("plugin_id","stage_type", "created_on", "created_by", "updated_on", "updated_by") VALUES
-    (1,0,'now()', 1, 'now()', 1);
+INSERT INTO "public"."plugin_stage_mapping" ("plugin_id", "created_on","created_by","updated_on","updated_by")
+SELECT "id", "created_on","created_by","updated_on","updated_by"
+FROM "public"."plugin_metadata";
 
-INSERT INTO "public"."plugin_stage_mapping" ("plugin_id","stage_type", "created_on", "created_by", "updated_on", "updated_by") VALUES
-    (2,0,'now()', 1, 'now()', 1);
-
-INSERT INTO "public"."plugin_stage_mapping" ("plugin_id","stage_type", "created_on", "created_by", "updated_on", "updated_by") VALUES
-    (3,0,'now()', 1, 'now()', 1);
-
-INSERT INTO "public"."plugin_stage_mapping" ("plugin_id","stage_type", "created_on", "created_by", "updated_on", "updated_by") VALUES
-    (4,0,'now()', 1, 'now()', 1);
-
-INSERT INTO "public"."plugin_stage_mapping" ("plugin_id","stage_type", "created_on", "created_by", "updated_on", "updated_by") VALUES
-    (5,0,'now()', 1, 'now()', 1);
-
-INSERT INTO "public"."plugin_stage_mapping" ("plugin_id","stage_type", "created_on", "created_by", "updated_on", "updated_by") VALUES
-    (6,0,'now()', 1, 'now()', 1);
-
-INSERT INTO "public"."plugin_stage_mapping" ("plugin_id","stage_type", "created_on", "created_by", "updated_on", "updated_by") VALUES
-    (7,0,'now()', 1, 'now()', 1);
-
-INSERT INTO "public"."plugin_stage_mapping" ("plugin_id","stage_type", "created_on", "created_by", "updated_on", "updated_by") VALUES
-    (8,0,'now()', 1, 'now()', 1);
-
-INSERT INTO "public"."plugin_stage_mapping" ("plugin_id","stage_type", "created_on", "created_by", "updated_on", "updated_by") VALUES
-    (9,0,'now()', 1, 'now()', 1);
-
-INSERT INTO "public"."plugin_stage_mapping" ("plugin_id","stage_type", "created_on", "created_by", "updated_on", "updated_by") VALUES
-    (10,0,'now()', 1, 'now()', 1);
+UPDATE "public"."plugin_stage_mapping" set "stage_type" = 0;
