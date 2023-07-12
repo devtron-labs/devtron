b974d844fc442ba0b025ea8dd7254292c9b8964d
ayushmaheshwari <ayush@devtron.ai>
2023-07-12 17:20:16 +0530

    helm pull error fix

--- pkg/app/AppService.go
+++ pkg/app/AppService.go
@@ -1879,7 +1879,7 @@ func (impl *AppServiceImpl) BuildManifestPushTemplate(overrideRequest *bean.Valu
 			repoPath, chartName := GetRepoPathAndChartNameFromRepoName(credentialsConfig.RepositoryName)
 			manifestPushTemplate.RepoUrl = path.Join(dockerArtifactStore.RegistryURL, repoPath)
 			manifestPushTemplate.ChartName = chartName
-			manifestPushTemplate.ChartVersion = fmt.Sprintf("%s%d.%d.%d", 1, 0, overrideRequest.WfrId, "DEPLOY")
+			manifestPushTemplate.ChartVersion = fmt.Sprintf("%d.%d.%d-%s", 1, 0, overrideRequest.WfrId, "DEPLOY")
 			containerRegistryConfig := &bean3.ContainerRegistryConfig{
 				RegistryUrl: dockerArtifactStore.RegistryURL,
 				Username:    dockerArtifactStore.Username,
@@ -3196,7 +3196,7 @@ func (impl *AppServiceImpl) BuildManifestPushTemplateForPrePostCd(pipeline *pipe
 		UserId:                triggeredBy,
 		AppName:               pipeline.App.AppName,
 		TargetEnvironmentName: pipeline.Environment.Id,
-		ChartVersion:          fmt.Sprintf("%s%d.%d.%d", 1, 1, cdWorklowRunnerId, deployType),
+		ChartVersion:          fmt.Sprintf("%d.%d.%d-%s", 1, 1, cdWorklowRunnerId, deployType),
 		BuiltChartBytes:       manifest,
 	}
 
