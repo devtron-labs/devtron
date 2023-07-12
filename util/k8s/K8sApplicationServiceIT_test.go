318312b5dec04a865599ef388a4a494dbb5fa2a0
Gireesh Naidu <gireesh@devtron.ai>
2023-07-12 11:50:37 +0530

    test cases skeleton added

--- util/k8s/K8sApplicationServiceIT_test.go
+++ util/k8s/K8sApplicationServiceIT_test.go
@@ -1,29 +1,48 @@
 package k8s
 
 import (
+	"github.com/devtron-labs/authenticator/client"
 	"github.com/devtron-labs/devtron/internal/util"
-	"github.com/devtron-labs/devtron/pkg/cluster/mocks"
+	"github.com/devtron-labs/devtron/pkg/cluster"
 	"github.com/devtron-labs/devtron/pkg/cluster/repository"
 	"github.com/devtron-labs/devtron/pkg/sql"
+	"github.com/devtron-labs/devtron/pkg/terminal"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestGetPodContainersList(t *testing.T) {
+
+	//testClusterId := 1
+	k8sApplicationService := InitK8sApplicationService(t)
+	t.Run("Create Ephemeral Container with valid Data,container status will be running", func(t *testing.T) {
+
+	})
+
+	t.Run("Create Ephemeral Container with inValid Data, container status will be terminated", func(t *testing.T) {
+
+	})
+
+	t.Run("Terminate Ephemeral Container with valid Data, container status will be terminated", func(t *testing.T) {
+
+	})
+
+	t.Run("Terminate Ephemeral Container with InValid Data,Invalid Pod Name payload, resource not found error", func(t *testing.T) {
+
+	})
+
+}
+
+func InitK8sApplicationService(t *testing.T) *K8sApplicationServiceImpl {
 	sugaredLogger, _ := util.InitLogger()
-	cfg, err := sql.GetConfig()
-	dbConn, err := sql.NewDbConnection(cfg, sugaredLogger)
-	if err != nil {
-		assert.Fail(t, "error in loading config")
-	}
-	clusterRepo := repository.NewClusterRepositoryImpl(dbConn, sugaredLogger)
-	testClusterId := 1
-	clusterBean, err := clusterRepo.FindById(testClusterId)
+	config, _ := sql.GetConfig()
+	runtimeConfig, err := client.GetRuntimeConfig()
+	k8sUtil := util.NewK8sUtil(sugaredLogger, runtimeConfig)
 	assert.Nil(t, err)
-	clusterCfg := clusterBean.GetClusterConfig()
-
-	mockedClusterService := mocks.NewClusterService(t)
-	mockedClusterService.On("FindById", testClusterId).Return(clusterBean, nil)
-	mockedClusterService.On("GetClusterConfig", clusterBean).Return(clusterCfg, nil)
-
+	db, _ := sql.NewDbConnection(config, sugaredLogger)
+	clusterRepositoryImpl := repository.NewClusterRepositoryImpl(db, sugaredLogger)
+	clusterServiceImpl := cluster.NewClusterServiceImpl(clusterRepositoryImpl, sugaredLogger, nil, nil, nil, nil, nil)
+	terminalSessionHandlerImpl := terminal.NewTerminalSessionHandlerImpl(nil, clusterServiceImpl, sugaredLogger, k8sUtil)
+	k8sApplicationService := NewK8sApplicationServiceImpl(sugaredLogger, clusterServiceImpl, nil, nil, nil, k8sUtil, nil, nil, terminalSessionHandlerImpl, nil)
+	return k8sApplicationService
 }
