b76b0c78c4648b9f3f38b3191ed2a633a2109aec
Ash-exp <asutosh2000ad@gmail.com>
2023-07-13 12:54:56 +0530

    fixed: error message minor fix

--- pkg/app/HelmRepoPushService.go
+++ pkg/app/HelmRepoPushService.go
@@ -41,7 +41,7 @@ func (impl *HelmRepoPushServiceImpl) PushChart(manifestPushTemplate *bean.Manife
 	if err != nil {
 		impl.logger.Errorw("error in pushing chart to helm oci repo", "err", err)
 		manifestPushResponse.Error = fmt.Errorf("Could not push helm package to \"%v\"", ociHelmRepoPushRequest.RepoURL)
-		timeline := getTimelineObject(manifestPushTemplate, pipelineConfig.TIMELINE_STATUS_MANIFEST_PUSHED_TO_HELM_REPO_FAILED, fmt.Sprintf("Could not push helm package to- \"%v\"", ociHelmRepoPushRequest.RepoURL))
+		timeline := getTimelineObject(manifestPushTemplate, pipelineConfig.TIMELINE_STATUS_MANIFEST_PUSHED_TO_HELM_REPO_FAILED, fmt.Sprintf("Could not push helm package to \"%v\"", ociHelmRepoPushRequest.RepoURL))
 		timelineErr := impl.pipelineStatusTimelineService.SaveTimeline(timeline, nil, false)
 		if timelineErr != nil {
 			impl.logger.Errorw("error in creating timeline status for git commit", "err", timelineErr, "timeline", timeline)
