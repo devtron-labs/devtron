35197c0e07f64db8d302bf9df2bbe2d0873be35b
Badal Kumar Prusty <badalkumar@Badals-MacBook-Pro.local>
2023-07-21 12:19:27 +0530

    modified sql files

--- scripts/sql/161_statefulset-chart_5-0-0.down.sql
+++ scripts/sql/161_statefulset-chart_5-0-0.down.sql
@@ -2,4 +2,4 @@ DELETE FROM global_strategy_metadata_chart_ref_mapping WHERE chart_ref_id=(selec
 
 DELETE FROM "public"."chart_ref" WHERE ("location" = 'statefulset-chart_5-0-0' AND "version" = '5.0.0');
 
-UPDATE "public"."chart_ref" SET "is_default" = 't' WHERE "location" = 'reference-chart_4-17-0' AND "version" = '4.18.0';
+UPDATE "public"."chart_ref" SET "is_default" = 't' WHERE "location" = 'statefulset-chart_4-18-0' AND "version" = '4.18.0';
