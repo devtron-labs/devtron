ae67b01d2f2f17c75ccd0030d3cb07ea03f68187
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2023-07-25 13:13:44 +0530

    sql down's code fixed

--- scripts/sql/161_generic_note.down.sql
+++ scripts/sql/161_generic_note.down.sql
@@ -1,10 +1,10 @@
 ALTER SEQUENCE IF EXISTS id_seq_generic_note RENAME to id_seq_cluster_note;
 ALTER SEQUENCE IF EXISTS id_seq_generic_note_history RENAME to id_seq_cluster_note_history;
 
-ALTER TABLE generic_note RENAME to cluster_note;
-ALTER TABLE cluster_note RENAME COLUMN identifier to cluster_id;
-DELETE FROM cluster_note WHERE identifier_type = 1;
+ALTER TABLE generic_note RENAME COLUMN identifier to cluster_id;
+DELETE FROM generic_note WHERE identifier_type = 1;
 ALTER TABLE generic_note DROP COLUMN  identifier_type;
+ALTER TABLE generic_note RENAME to cluster_note;
 
 AlTER TABLE cluster_note ADD CONSTRAINT cluster_note_cluster_id_fkey
     FOREIGN KEY(cluster_id)
