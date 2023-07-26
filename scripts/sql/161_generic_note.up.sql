07c48eb4870458b7766eaa4962e21220f2add503
Gireesh Naidu <gireesh@devtron.ai>
2023-07-26 16:16:15 +0530

    test script

--- scripts/sql/161_generic_note.up.sql
+++ scripts/sql/161_generic_note.up.sql
@@ -3,7 +3,7 @@ ALTER SEQUENCE IF EXISTS id_seq_cluster_note_history RENAME to id_seq_generic_no
 AlTER TABLE cluster_note DROP CONSTRAINT cluster_note_cluster_id_fkey;
 ALTER TABLE cluster_note RENAME to generic_note;
 ALTER TABLE generic_note RENAME COLUMN cluster_id to identifier;
-ALTER TABLE generic_note ADD COLUMN  identifier_type int4 DEFAULT 0 NOT NULL;
+ALTER TABLE generic_note ADD COLUMN  identifier_type int4;
 
 AlTER TABLE cluster_note_history DROP CONSTRAINT cluster_note_history_cluster_note_id_fkey;
 ALTER TABLE cluster_note_history RENAME to generic_note_history;
