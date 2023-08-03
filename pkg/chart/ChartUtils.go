4be7b3b9a1c3004e6fc8cc3da13b591ad4569492
avdhesh-devtron <138586469+avdhesh-devtron@users.noreply.github.com>
2023-08-03 16:19:49 +0530

    feat: Allow changing chart ref (#3706)
    * Add API to change chart ref id
    
    * Find app metrics
    
    * Fix issues
    
    * Patch winter soldier config
    
    * Add flaggerCanary Check
    
    * Add error message when env.Ovverride is not set
    
    * Refactor and add spec

---
+++ pkg/chart/ChartUtils.go
@@ -0,0 +1,55 @@
+package chart
+
+import (
+	"encoding/json"
+)
+
+func PatchWinterSoldierConfig(override json.RawMessage, newChartType string) (json.RawMessage, error) {
+	var jsonMap map[string]json.RawMessage
+	if err := json.Unmarshal([]byte(override), &jsonMap); err != nil {
+		return override, err
+	}
+	updatedJson, err := PatchWinterSoldierIfExists(newChartType, jsonMap)
+	if err != nil {
+		return override, err
+	}
+	updatedOverride, err := json.Marshal(updatedJson)
+
+	if err != nil {
+		return override, err
+	}
+	return updatedOverride, nil
+}
+
+func PatchWinterSoldierIfExists(newChartType string, jsonMap map[string]json.RawMessage) (map[string]json.RawMessage, error) {
+	winterSoldierConfig, found := jsonMap["winterSoldier"]
+	if !found {
+		return jsonMap, nil
+	}
+	var winterSoldierUnmarshalled map[string]json.RawMessage
+	if err := json.Unmarshal([]byte(winterSoldierConfig), &winterSoldierUnmarshalled); err != nil {
+		return jsonMap, err
+	}
+
+	_, found = winterSoldierUnmarshalled["type"]
+	if !found {
+		return jsonMap, nil
+	}
+	switch newChartType {
+	case DeploymentChartType:
+		winterSoldierUnmarshalled["type"] = json.RawMessage("\"Deployment\"")
+	case RolloutChartType:
+		winterSoldierUnmarshalled["type"] = json.RawMessage("\"Rollout\"")
+	}
+
+	winterSoldierMarshalled, err := json.Marshal(winterSoldierUnmarshalled)
+	if err != nil {
+		return jsonMap, err
+	}
+	jsonMap["winterSoldier"] = winterSoldierMarshalled
+	return jsonMap, nil
+}
+
+//func IsFlaggerCanaryEnabled(override json.RawMessage) (bool, error) {
+//
+//}
