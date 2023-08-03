6497cb0a639e009ba52c0a45fe203c2d94acd976
adi6859 <aditya.ar1909@gmail.com>
2023-08-03 16:16:33 +0530

    sql script file removed from here

--- scripts/sql/162_notification_template_for_approval.down.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE ONLY public.notifier_event_log DROP CONSTRAINT notifier_event_log_event_type_id_fkey;
-ALTER TABLE ONLY public.notifier_event_log
-    ADD CONSTRAINT notifier_event_log_event_type_id_fkey FOREIGN KEY (event_type_id) REFERENCES public.event(id) ON DELETE CASCADE;
-delete from "public"."notification_templates" where event_type_id=4;
-delete from public.event where event_type='APPROVAL';
