7ad9ed006c2d4be2ba5e22978a80caed4bd3d2dc
Badal Kumar Prusty <badalkumar@Badals-MacBook-Pro.local>
2023-08-09 20:56:04 +0530

    changing headless service

---
+++ scripts/sql/164_statefulset-chart_5-0-0.down.sql
@@ -0,0 +1,5 @@
+DELETE FROM global_strategy_metadata_chart_ref_mapping WHERE chart_ref_id=(select id from chart_ref where version='5.0.0' and name is null);
+
+DELETE FROM "public"."chart_ref" WHERE ("location" = 'statefulset-chart_5-0-0' AND "version" = '5.0.0');
+
+UPDATE "public"."chart_ref" SET "is_default" = 't' WHERE "location" = 'statefulset-chart_4-18-0' AND "version" = '4.18.0';
