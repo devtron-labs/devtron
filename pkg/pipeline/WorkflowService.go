fbf7ac0624a8f3ccca586570c6f90def729dce89
Gireesh Naidu <gireesh@devtron.ai>
2023-08-13 23:41:45 +0530

    added prestop lifecycle hook to clean up buildx k8s driver nodes

--- pkg/pipeline/WorkflowService.go
+++ pkg/pipeline/WorkflowService.go
@@ -368,6 +368,14 @@ func (impl *WorkflowServiceImpl) SubmitWorkflow(workflowRequest *WorkflowRequest
 				Name:          "app-data",
 				ContainerPort: 9102,
 			}},
+			Lifecycle: &v12.Lifecycle{
+				PreStop: &v12.LifecycleHandler{
+					HTTPGet: &v12.HTTPGetAction{
+						Path: "/cleanK8sDriver",
+						Port: intstr.FromInt(8080),
+					},
+				},
+			},
 		},
 		ActiveDeadlineSeconds: &intstr.IntOrString{
 			IntVal: int32(workflowRequest.ActiveDeadlineSeconds),
