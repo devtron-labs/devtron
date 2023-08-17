02baca9d7222d2ef6d8b4ec253222e13df852694
dhananjay0601 <dhananjay@devtron.ai>
2023-08-17 10:17:16 +0530

    pr comments

--- api/logger/bean.go
+++ api/logger/bean.go
@@ -3,9 +3,9 @@ package logger
 import "time"
 
 type UserAuthDTO struct {
-	action          string
-	UserID          int
-	updatedBy       int
-	updatedOn       time.Time
-	apiResponseCode int
+	action          string    `json:"action"`
+	UserID          int       `json:"userID"`
+	updatedBy       int       `json:"updatedBy"`
+	updatedOn       time.Time `json:"updatedOn"`
+	apiResponseCode int       `json:"apiResponseCode"`
 }
--- api/logger/loggerService.go
+++ api/logger/loggerService.go
@@ -6,8 +6,5 @@ import (
 )
 
 func NewUserAuthService(action string, updatedBy int32, updatedOn time.Duration, apiResponseCode int) {
-	log.Println(action)
-	log.Println(updatedBy)
-	log.Println(updatedOn)
-	log.Println(apiResponseCode)
+	log.Printf("Action: %s, UpdatedBy: %d, UpdatedOn: %s, APIResponseCode: %d", action, updatedBy, updatedOn, apiResponseCode)
 }
