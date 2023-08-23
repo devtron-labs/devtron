95c69aa7df6c68ccb484c0fb8436000583889326
Ash-exp <asutosh2000ad@gmail.com>
2023-08-24 01:19:45 +0530

    updated chart download

--- pkg/chart/ChartService.go
+++ pkg/chart/ChartService.go
@@ -1661,20 +1661,23 @@ func (impl ChartServiceImpl) GetCustomChartInBytes(chartRefId int) ([]byte, erro
 		impl.logger.Errorw("error getting chart data", "chartRefId", chartRefId, "err", err)
 		return nil, err
 	}
-	refChartPath := filepath.Join(string(impl.refChartDir), chartRef.Location)
-	if _, err := os.Stat(refChartPath); os.IsNotExist(err) && chartRef.ChartData != nil {
-		chartInfo, err := impl.ExtractChartIfMissing(chartRef.ChartData, string(impl.refChartDir), chartRef.Location)
-		if chartInfo != nil && chartInfo.TemporaryFolder != "" {
-			err1 := os.RemoveAll(chartInfo.TemporaryFolder)
-			if err1 != nil {
-				impl.logger.Errorw("error in deleting temp dir ", "err", err)
-			}
-		}
-		if err != nil {
-			impl.logger.Errorw("error chart extraction for download", "err", err)
-			return nil, err
-		}
+	if chartRef.ChartData != nil {
+		return chartRef.ChartData, nil
 	}
+	refChartPath := filepath.Join(string(impl.refChartDir), chartRef.Location)
+	//if _, err := os.Stat(refChartPath); os.IsNotExist(err) && chartRef.ChartData != nil {
+	//	chartInfo, err := impl.ExtractChartIfMissing(chartRef.ChartData, string(impl.refChartDir), chartRef.Location)
+	//	if chartInfo != nil && chartInfo.TemporaryFolder != "" {
+	//		err1 := os.RemoveAll(chartInfo.TemporaryFolder)
+	//		if err1 != nil {
+	//			impl.logger.Errorw("error in deleting temp dir ", "err", err)
+	//		}
+	//	}
+	//	if err != nil {
+	//		impl.logger.Errorw("error chart extraction for download", "err", err)
+	//		return nil, err
+	//	}
+	//}
 	manifestByteArr, err := impl.chartTemplateService.LoadChartInBytes(refChartPath, false)
 	if err != nil {
 		impl.logger.Errorw("error in converting chart to bytes", "err", err)
