757130ffa4600fdaa23441fe724480b7be8a9e40
Ashish-devtron <ashish.kumar@devtron.ai>
2023-08-30 16:46:52 +0530

    ci-config for integration testing

--- pkg/pipeline/CiConfig.go
+++ pkg/pipeline/CiConfig.go
@@ -29,7 +29,7 @@ type CiConfig struct {
 	DefaultCacheBucket               string                       `env:"DEFAULT_CACHE_BUCKET" envDefault:"ci-caching"`
 	DefaultCacheBucketRegion         string                       `env:"DEFAULT_CACHE_BUCKET_REGION" envDefault:"us-east-2"`
 	CiLogsKeyPrefix                  string                       `env:"CI_LOGS_KEY_PREFIX" envDefault:"my-artifacts"`
-	DefaultImage                     string                       `env:"DEFAULT_CI_IMAGE" envDefault:"686244538589.dkr.ecr.us-east-2.amazonaws.com/cirunner:47"`
+	DefaultImage                     string                       `env:"DEFAULT_CI_IMAGE" envDefault:"quay.io/devtron/ci-runner:8a9f8b8c-138-15021"`
 	DefaultNamespace                 string                       `env:"DEFAULT_NAMESPACE" envDefault:"devtron-ci"`
 	DefaultTimeout                   int64                        `env:"DEFAULT_TIMEOUT" envDefault:"3600"`
 	DefaultBuildLogsBucket           string                       `env:"DEFAULT_BUILD_LOGS_BUCKET" envDefault:"devtron-pro-ci-logs"`
@@ -38,27 +38,27 @@ type CiConfig struct {
 	LimitMem                         string                       `env:"LIMIT_CI_MEM" envDefault:"3G"`
 	ReqCpu                           string                       `env:"REQ_CI_CPU" envDefault:"0.5"`
 	ReqMem                           string                       `env:"REQ_CI_MEM" envDefault:"3G"`
-	TaintKey                         string                       `env:"CI_NODE_TAINTS_KEY" envDefault:""`
-	TaintValue                       string                       `env:"CI_NODE_TAINTS_VALUE" envDefault:""`
-	NodeLabelSelector                []string                     `env:"CI_NODE_LABEL_SELECTOR"`
+	TaintKey                         string                       `env:"CI_NODE_TAINTS_KEY" envDefault:"dedicated"`
+	TaintValue                       string                       `env:"CI_NODE_TAINTS_VALUE" envDefault:"ci"`
+	NodeLabelSelector                []string                     `env:"CI_NODE_LABEL_SELECTOR" envDefault:"dedicated=ci"`
 	CacheLimit                       int64                        `env:"CACHE_LIMIT" envDefault:"5000000000"` // TODO: Add to default db config also
-	DefaultBuildLogsKeyPrefix        string                       `env:"DEFAULT_BUILD_LOGS_KEY_PREFIX" envDefault:"arsenal-v1"`
-	DefaultArtifactKeyPrefix         string                       `env:"DEFAULT_ARTIFACT_KEY_LOCATION" envDefault:"arsenal-v1/ci-artifacts"`
+	DefaultBuildLogsKeyPrefix        string                       `env:"DEFAULT_BUILD_LOGS_KEY_PREFIX" envDefault:"devtron"`
+	DefaultArtifactKeyPrefix         string                       `env:"DEFAULT_ARTIFACT_KEY_LOCATION" envDefault:"devtron/ci-artifacts"`
 	WorkflowServiceAccount           string                       `env:"WORKFLOW_SERVICE_ACCOUNT" envDefault:"ci-runner"`
 	ExternalCiApiSecret              string                       `env:"EXTERNAL_CI_API_SECRET" envDefault:"devtroncd-secret"`
 	ExternalCiWebhookUrl             string                       `env:"EXTERNAL_CI_WEB_HOOK_URL" envDefault:""`
 	ExternalCiPayload                string                       `env:"EXTERNAL_CI_PAYLOAD" envDefault:"{\"ciProjectDetails\":[{\"gitRepository\":\"https://github.com/vikram1601/getting-started-nodejs.git\",\"checkoutPath\":\"./abc\",\"commitHash\":\"239077135f8cdeeccb7857e2851348f558cb53d3\",\"commitTime\":\"2022-10-30T20:00:00\",\"branch\":\"master\",\"message\":\"Update README.md\",\"author\":\"User Name \"}],\"dockerImage\":\"445808685819.dkr.ecr.us-east-2.amazonaws.com/orch:23907713-2\"}"`
 	CiArtifactLocationFormat         string                       `env:"CI_ARTIFACT_LOCATION_FORMAT" envDefault:"%d/%d.zip"`
-	ImageScannerEndpoint             string                       `env:"IMAGE_SCANNER_ENDPOINT" envDefault:"http://image-scanner-new-demo-devtroncd-service.devtroncd:80"`
-	CloudProvider                    blob_storage.BlobStorageType `env:"BLOB_STORAGE_PROVIDER" envDefault:"S3"`
-	AzureAccountName                 string                       `env:"AZURE_ACCOUNT_NAME"`
+	ImageScannerEndpoint             string                       `env:"IMAGE_SCANNER_ENDPOINT" envDefault:"http://image-scanner-service.devtroncd:80"`
+	CloudProvider                    blob_storage.BlobStorageType `env:"BLOB_STORAGE_PROVIDER" envDefault:"AZURE"`
+	AzureAccountName                 string                       `env:"AZURE_ACCOUNT_NAME" envDefault:"devtrontestblobstorage"`
 	AzureGatewayUrl                  string                       `env:"AZURE_GATEWAY_URL" envDefault:"http://devtron-minio.devtroncd:9000"`
 	AzureGatewayConnectionInsecure   bool                         `env:"AZURE_GATEWAY_CONNECTION_INSECURE" envDefault:"true"`
-	AzureBlobContainerCiLog          string                       `env:"AZURE_BLOB_CONTAINER_CI_LOG"`
-	AzureBlobContainerCiCache        string                       `env:"AZURE_BLOB_CONTAINER_CI_CACHE"`
+	AzureBlobContainerCiLog          string                       `env:"AZURE_BLOB_CONTAINER_CI_LOG" envDefault:"ci-log-container"`
+	AzureBlobContainerCiCache        string                       `env:"AZURE_BLOB_CONTAINER_CI_CACHE" envDefault:"ci-cache-container"`
 	DefaultAddressPoolBaseCidr       string                       `env:"CI_DEFAULT_ADDRESS_POOL_BASE_CIDR"`
 	DefaultAddressPoolSize           int                          `env:"CI_DEFAULT_ADDRESS_POOL_SIZE"`
-	BlobStorageEnabled               bool                         `env:"BLOB_STORAGE_ENABLED" envDefault:"false"`
+	BlobStorageEnabled               bool                         `env:"BLOB_STORAGE_ENABLED" envDefault:"true"`
 	BlobStorageS3AccessKey           string                       `env:"BLOB_STORAGE_S3_ACCESS_KEY"`
 	BlobStorageS3SecretKey           string                       `env:"BLOB_STORAGE_S3_SECRET_KEY"`
 	BlobStorageS3Endpoint            string                       `env:"BLOB_STORAGE_S3_ENDPOINT"`
@@ -79,7 +79,7 @@ type CiConfig struct {
 	DefaultTargetPlatform            string                       `env:"DEFAULT_TARGET_PLATFORM" envDefault:""`
 	UseBuildx                        bool                         `env:"USE_BUILDX" envDefault:"false"`
 	NodeLabel                        map[string]string
-	EnableBuildContext               bool   `env:"ENABLE_BUILD_CONTEXT" envDefault:"false"`
+	EnableBuildContext               bool   `env:"ENABLE_BUILD_CONTEXT" envDefault:"true"`
 	ImageRetryCount                  int    `env:"IMAGE_RETRY_COUNT" envDefault:"0"`
 	ImageRetryInterval               int    `env:"IMAGE_RETRY_INTERVAL" envDefault:"5"` //image retry interval takes value in seconds
 	OrchestratorHost                 string `env:"ORCH_HOST" envDefault:"http://devtroncd-orchestrator-service-prod.devtroncd/webhook/msg/nats"`
