b2d0686e9c31d5b38a448cb1d0c57f5562dfd166
Shivam-nagar23 <shivam@devtron.ai>
2023-09-04 11:35:19 +0530

    main.go update

--- pkg/sourceController/main.go
+++ pkg/sourceController/main.go
@@ -1,22 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 func main() {
-	//fmt.Println("hello")
-	//app, err := InitializeApp()
-	//if err != nil {
-	//	log.Panic(err)
-	//}
-	////     gracefulStop start
-	//var gracefulStop = make(chan os.Signal)
-	//signal.Notify(gracefulStop, syscall.SIGTERM)
-	//signal.Notify(gracefulStop, syscall.SIGINT)
-	//go func() {
-	//	sig := <-gracefulStop
-	//	fmt.Printf("caught term sig: %+v", sig)
-	//	app.Stop()
-	//	os.Exit(0)
-	//}()
-	////      gracefulStop end
-	//app.Start()
+	fmt.Println("hello")
+	app, err := InitializeApp()
+	if err != nil {
+		log.Panic(err)
+	}
+	//     gracefulStop start
+	var gracefulStop = make(chan os.Signal)
+	signal.Notify(gracefulStop, syscall.SIGTERM)
+	signal.Notify(gracefulStop, syscall.SIGINT)
+	go func() {
+		sig := <-gracefulStop
+		fmt.Printf("caught term sig: %+v", sig)
+		app.Stop()
+		os.Exit(0)
+	}()
+	//      gracefulStop end
+	app.Start()
 
 }
