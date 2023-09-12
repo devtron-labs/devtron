92a074bf9fa9025413c9bd76c94aa847eed5bc35
Subhashish <subhashish@devtron.ai>
2023-09-12 17:48:32 +0530

    fixing last deployed deployment bug introduced after template parser code changes

--- pkg/app/AppService.go
+++ pkg/app/AppService.go
@@ -1294,10 +1294,6 @@ func (impl *AppServiceImpl) GetEnvOverrideByTriggerType(overrideRequest *bean.Va
 		envOverride.IsOverride = true
 		envOverride.EnvOverrideValues = deploymentTemplateHistory.Template
 
-		//impl.variableEntityMappingService.GetAllMappingsForEntities(repository6.Entity{
-		//	EntityType: "",
-		//	EntityId:   0,
-		//})
 		resolvedTemplate, variableMap, err := impl.getResolvedTemplateWithSnapshot(deploymentTemplateHistory.Id, envOverride.EnvOverrideValues)
 		if err != nil {
 			return nil, err
@@ -1464,6 +1460,11 @@ func (impl *AppServiceImpl) extractVariablesAndResolveTemplate(scope models2.Sco
 	if err != nil {
 		return "", nil, err
 	}
+
+	for _, variable := range scopedVariables {
+		variableMap[variable.VariableName] = variable.VariableValue.StringValue()
+	}
+
 	resolvedTemplate := parserResponse.ResolvedTemplate
 	return resolvedTemplate, variableMap, nil
 }
