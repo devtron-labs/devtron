e8d67eddafcd30d96491c7ad19601f7aec089072
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2023-09-13 13:00:20 +0530

    migration script

--- scripts/sql/169_security_scan_list_fix.up.sql
+++ scripts/sql/169_security_scan_list_fix.up.sql
@@ -1,1 +1,1 @@
-ALTER TABLE image_scan_deploy_info ADD COLUMN is_latest_image_scanned BOOLEAN DEFAULT TRUE;
+ALTER TABLE image_scan_deploy_info ADD COLUMN IF NOT EXISTS is_latest_image_scanned BOOLEAN DEFAULT TRUE;
