b75d7b27ab45f1839c1ed931023f8e6dc6961ef9
Badal Kumar Prusty <badalkumar@Badals-MacBook-Pro.local>
2023-09-14 17:54:36 +0530

    test

--- scripts/sql/142_statefulset-chart-ref_4-18-0.down.sql
+++ scripts/sql/142_statefulset-chart-ref_4-18-0.down.sql
@@ -1,5 +1,5 @@
-DELETE FROM global_strategy_metadata_chart_ref_mapping WHERE chart_ref_id=(select id from chart_ref where version='4.18.0' and name is null);
+DELETE FROM global_strategy_metadata_chart_ref_mapping WHERE chart_ref_id=(select id from chart_ref where version='5.0.0' and location="statefulset-chart_5-0-0" and name is null);
 
-DELETE FROM "public"."chart_ref" WHERE ("location" = 'statefulset-chart_4-18-0' AND "version" = '4.18.0');
+DELETE FROM "public"."chart_ref" WHERE ("location" = 'statefulset-chart_5-0-0' AND "version" = '5.0.0');
 
-UPDATE "public"."chart_ref" SET "is_default" = 't' WHERE "location" = 'reference-chart_4-17-0' AND "version" = '4.17.0';
+UPDATE "public"."chart_ref" SET "is_default" = 't' WHERE "location" = 'statefulset-chart_4-18-0' AND "version" = '4.18.0';
--- scripts/sql/169_statefulset-chart_5-0-0.up.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO "public"."chart_ref" ("location", "version","deployment_strategy_path", "is_default", "active", "created_on", "created_by", "updated_on", "updated_by","name") VALUES
-    ('statefulset-chart_5-0-0', '5.0.0','pipeline-values.yaml', 'f', 't', 'now()', 1, 'now()', 1,'StatefulSet');
-
-INSERT INTO global_strategy_metadata_chart_ref_mapping ("global_strategy_metadata_id","chart_ref_id", "active","default","created_on", "created_by", "updated_on", "updated_by") VALUES 
-((select id from global_strategy_metadata where name='ROLLINGUPDATE') ,(select id from chart_ref where location='statefulset-chart_5-0-0'), true,true,now(), 1, now(), 1),
-((select id from global_strategy_metadata where name='ONDELETE') ,(select id from chart_ref where location='statefulset-chart_5-0-0'), true, false,now(), 1, now(), 1);
-
