42309100af8e71c552433302dd19e5c28e49e750
Ashish-devtron <ashish.kumar@devtron.ai>
2023-09-28 16:31:41 +0530

    testing

--- pkg/cluster/EnvironmentService.go
+++ pkg/cluster/EnvironmentService.go
@@ -746,6 +746,7 @@ func (impl EnvironmentServiceImpl) Delete(deleteReq *EnvironmentBean, userId int
 
 	//deleting auth roles entries for this environment
 	err = impl.userAuthService.DeleteRoles(bean.ENV_TYPE, deleteRequest.Name, tx, existingEnv.EnvironmentIdentifier)
+
 	if err != nil {
 		impl.logger.Errorw("error in deleting auth roles", "err", err)
 		return err
