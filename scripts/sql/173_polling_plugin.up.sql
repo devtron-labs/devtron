84ffe612b43ee9beb54aa2e7b93f382602d79771
Shivam-nagar23 <shivam@devtron.ai>
2023-09-28 22:10:18 +0530

    script change

--- scripts/sql/173_polling_plugin.up.sql
+++ scripts/sql/173_polling_plugin.up.sql
@@ -11,7 +11,7 @@ INSERT INTO "plugin_stage_mapping" ("plugin_id","stage_type","created_on", "crea
 VALUES ((SELECT id FROM plugin_metadata WHERE name='Pull images from container repository'),0,'now()', 1, 'now()', 1);
 
 INSERT INTO "plugin_pipeline_script" ("id","type","mount_directory_from_host","container_image_path","deleted","created_on", "created_by", "updated_on", "updated_by")
-VALUES (nextval('id_seq_plugin_pipeline_script'),'CONTAINER_IMAGE','t','quay.io/devtron/poll-container-image:f7309681-545-16560','f','now()',1,'now()',1);
+VALUES (nextval('id_seq_plugin_pipeline_script'),'CONTAINER_IMAGE','t','quay.io/devtron/poll-container-image:f9e56069-545-16577','f','now()',1,'now()',1);
 
 INSERT INTO "script_path_arg_port_mapping" ("id", "type_of_mapping", "file_path_on_disk","file_path_on_container","script_id","deleted","created_on","created_by", "updated_on", "updated_by")
 VALUES (nextval('id_seq_script_path_arg_port_mapping'),'FILE_PATH','/polling-plugin' ,'/output',(SELECT last_value FROM id_seq_plugin_pipeline_script),'f','now()', 1, 'now()', 1);
@@ -20,7 +20,7 @@ INSERT INTO "plugin_step" ("id", "plugin_id","name","description","index","step_
 VALUES (nextval('id_seq_plugin_step'), (SELECT id FROM plugin_metadata WHERE name='Pull images from container repository'),'Step 1','Step 1 - Polling from Container Registry','1','INLINE',(SELECT last_value FROM id_seq_plugin_pipeline_script),'f','now()', 1, 'now()', 1);
 
 INSERT INTO "plugin_step_variable" ("id", "plugin_step_id", "name", "format", "description", "is_exposed", "allow_empty_value", "variable_type", "value_type", "variable_step_index", "deleted", "created_on", "created_by", "updated_on", "updated_by")
-VALUES (nextval('id_seq_plugin_step_variable'), (SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Pull images from container repository' and ps."index"=1 and ps.deleted=false), 'REPOSITORY','STRING','Provide the repository name for polling.',true,false,'INPUT','NEW',1 ,'f','now()', 1, 'now()', 1);
+VALUES (nextval('id_seq_plugin_step_variable'), (SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Pull images from container repository' and ps."index"=1 and ps.deleted=false), 'REPOSITORY','STRING','Provide the repository name for polling, this will override the default repository used in build configuration',true,true,'INPUT','NEW',1 ,'f','now()', 1, 'now()', 1);
 
 INSERT INTO "plugin_step_variable" ("id", "plugin_step_id", "name", "format", "description", "is_exposed", "allow_empty_value","variable_type", "value_type", "variable_step_index",reference_variable_name, "deleted", "created_on", "created_by", "updated_on", "updated_by")
 VALUES (nextval('id_seq_plugin_step_variable'), (SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Pull images from container repository' and ps."index"=1 and ps.deleted=false), 'ACCESS_KEY','STRING','Aws Ecr Access Key',false,true,'INPUT','GLOBAL',1 ,'ACCESS_KEY','f','now()', 1, 'now()', 1);
@@ -36,3 +36,6 @@ VALUES (nextval('id_seq_plugin_step_variable'), (SELECT ps.id FROM plugin_metada
 
 INSERT INTO "plugin_step_variable" ("id", "plugin_step_id", "name", "format", "description", "is_exposed", "allow_empty_value","variable_type", "value_type", "variable_step_index",reference_variable_name, "deleted", "created_on", "created_by", "updated_on", "updated_by")
 VALUES (nextval('id_seq_plugin_step_variable'), (SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Pull images from container repository' and ps."index"=1 and ps.deleted=false), 'AWS_REGION','STRING','CR Aws region',false,true,'INPUT','GLOBAL',1 ,'AWS_REGION','f','now()', 1, 'now()', 1);
+
+INSERT INTO "plugin_step_variable" ("id", "plugin_step_id", "name", "format", "description", "is_exposed", "allow_empty_value","variable_type", "value_type", "variable_step_index",reference_variable_name, "deleted", "created_on", "created_by", "updated_on", "updated_by")
+VALUES (nextval('id_seq_plugin_step_variable'), (SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Pull images from container repository' and ps."index"=1 and ps.deleted=false), 'DOCKER_REPOSITORY','STRING','Provide the repository name for polling.',false,true,'INPUT','GLOBAL',1 ,'DOCKER_REPOSITORY','f','now()', 1, 'now()', 1);
