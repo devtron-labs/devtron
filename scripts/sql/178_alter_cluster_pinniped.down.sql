dacd85b03890e9d3fe453c25ddbb46dbe456bcbc
kartik-579 <kartik@devtron.ai>
2023-10-05 17:43:45 +0530

    updated cluster table name

--- scripts/sql/178_alter_cluster_pinniped.down.sql
+++ scripts/sql/178_alter_cluster_pinniped.down.sql
@@ -2,6 +2,6 @@ ALTER TABLE cluster
     DROP COLUMN to_connect_with_pinniped,
     DROP COLUMN pinniped_concierge_url;
 
-DROP TABLE IF EXISTS public.cluster_user_data;
+DROP TABLE IF EXISTS public.cluster_tool_data;
 
-DROP SEQUENCE IF EXISTS public.id_seq_cluster_user_data;
+DROP SEQUENCE IF EXISTS public.id_seq_cluster_tool_data;
--- scripts/sql/178_alter_cluster_pinniped.up.sql
+++ scripts/sql/178_alter_cluster_pinniped.up.sql
@@ -3,16 +3,16 @@ ALTER TABLE cluster
     ADD COLUMN pinniped_concierge_url   text;
 
 
-CREATE SEQUENCE IF NOT EXISTS id_seq_cluster_user_data;
+CREATE SEQUENCE IF NOT EXISTS id_seq_cluster_tool_data;
 
-CREATE TABLE "public"."cluster_user_data"
+CREATE TABLE "public"."cluster_tool_data"
 (
-    "id"         integer      NOT NULL DEFAULT nextval('id_seq_cluster_user_data'::regclass),
+    "id"         integer      NOT NULL DEFAULT nextval('id_seq_cluster_tool_data'::regclass),
     "cluster_id" integer      NOT NULL,
     "user_id"    integer      NOT NULL,
     "tool_name"  varchar(250) NOT NULL,
     "token"      text         NOT NULL,
-    CONSTRAINT "cluster_user_data_cluster_id_fkey" FOREIGN KEY ("cluster_id") REFERENCES "public"."cluster" ("id"),
-    CONSTRAINT "cluster_user_data_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users" ("id"),
+    CONSTRAINT "cluster_tool_data_cluster_id_fkey" FOREIGN KEY ("cluster_id") REFERENCES "public"."cluster" ("id"),
+    CONSTRAINT "cluster_tool_data_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users" ("id"),
     PRIMARY KEY ("id")
 );
