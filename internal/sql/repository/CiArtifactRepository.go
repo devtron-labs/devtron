cb2038909371992d74725b61769e13d3846162bb
Shivam-nagar23 <shivam@devtron.ai>
2023-10-06 12:25:49 +0530

    query

--- internal/sql/repository/CiArtifactRepository.go
+++ internal/sql/repository/CiArtifactRepository.go
@@ -571,7 +571,7 @@ func (impl CiArtifactRepositoryImpl) GetArtifactByCdWorkflowId(cdWorkflowId int)
 	return artifact, err
 }
 
-// GetArtifactsByParentCiWorkflowId will get all artifacts of child workflow sorted by descing order to fetch latest at top
+// GetArtifactsByParentCiWorkflowId will get all artifacts of child workflow sorted by descending order to fetch latest at top
 func (impl CiArtifactRepositoryImpl) GetArtifactsByParentCiWorkflowId(parentCiWorkflowId int) ([]string, error) {
 	var artifacts []string
 	query := "SELECT cia.image FROM ci_artifact cia where cia.ci_workflow_id in (SELECT wf.id from ci_workflow wf where wf.parent_ci_workflow_id = ? ) ORDER BY cia.created_on DESC ;"
