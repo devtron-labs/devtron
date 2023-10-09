dd54ea51fc87d25bcbd13d1af8c71777bb5e3630
ayushmaheshwari <ayush@devtron.ai>
2023-10-09 12:24:24 +0530

    Revert "Merge remote-tracking branch 'oss/jenkins_integration_migration'"
    This reverts commit efbaa788e8c921fbf2c13ac908e3d9577cf17b31, reversing
    changes made to 0ca1c1ec4a951ca3e26f9fcbee386aa4a3687ba7.

--- scripts/sql/177_jenkins_integration.down.sql
+++
@@ -1,8 +0,0 @@
-DELETE FROM plugin_step_variable WHERE plugin_step_id =(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Jenkins' and ps."index"=1 and ps.deleted=false);
-DELETE from plugin_pipeline_script where id = (SELECT script_id from plugin_step WHERE plugin_id=(SELECT id FROM plugin_metadata WHERE name='Jenkins'));
-DELETE FROM plugin_step WHERE plugin_id = (SELECT id FROM plugin_metadata WHERE name='Jenkins');
-DELETE FROM plugin_stage_mapping WHERE plugin_id =(SELECT id FROM plugin_metadata WHERE name='Jenkins');
-DELETE FROM plugin_tag_relation WHERE plugin_id =(SELECT id FROM plugin_metadata WHERE name='Jenkins');
-DELETE FROM pipeline_stage_step_variable WHERE pipeline_stage_step_id in (SELECT id FROM pipeline_stage_step where ref_plugin_id =(SELECT id from plugin_metadata WHERE name ='Jenkins'));
-DELETE FROM pipeline_stage_step where ref_plugin_id in (SELECT id from plugin_metadata WHERE name ='Jenkins');
-DELETE FROM plugin_metadata WHERE name ='Jenkins';
