a0a1f94fa769aff8b2b23a4a66b221b5407ab109
ayushmaheshwari <ayush@devtron.ai>
2023-10-10 15:12:58 +0530

    fix job support in app api

--- api/restHandler/CoreAppRestHandler.go
+++ api/restHandler/CoreAppRestHandler.go
@@ -722,6 +722,7 @@ func (handler CoreAppRestHandlerImpl) buildCiPipelineResp(appId int, ciPipeline 
 		ParentCiPipeline:         ciPipeline.ParentCiPipeline,
 		ParentAppId:              ciPipeline.ParentAppId,
 		LinkedCount:              ciPipeline.LinkedCount,
+		PipelineType:             string(ciPipeline.PipelineType),
 	}
 
 	//build ciPipelineMaterial resp
