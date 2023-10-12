a4d7d0e4dd7a43ee948663916f9c856bd1ff4e16
Gireesh Naidu <gireesh@devtron.ai>
2023-10-12 15:02:36 +0530

    unit test added for release tag test

--- enterprise/pkg/resourceFilter/CELEvalustorService_test.go
+++ enterprise/pkg/resourceFilter/CELEvalustorService_test.go
@@ -9,17 +9,51 @@ import (
 func TestEvaluateCELRequest(t *testing.T) {
 	logger, _ := util.NewSugardLogger()
 	celService := NewCELServiceImpl(logger)
-	artifact := "devtron/test:v1beta1"
-	releaseTags := []string{"tag1", "latest"}
-	params := celService.GetParamsFromArtifact(artifact, releaseTags)
+	t.Run("valid release tags list", func(tt *testing.T) {
+		artifact := "devtron/test:v1beta1"
+		releaseTags := []string{"tag1", "latest"}
+		params := celService.GetParamsFromArtifact(artifact, releaseTags)
 
-	evalReq := CELRequest{
-		Expression: "'latest' in releaseTags",
-		ExpressionMetadata: ExpressionMetadata{
-			Params: params,
-		},
-	}
-	res, err := celService.EvaluateCELRequest(evalReq)
-	assert.Equal(t, true, res)
-	assert.Equal(t, nil, err)
+		evalReq := CELRequest{
+			Expression: "'latest' in releaseTags",
+			ExpressionMetadata: ExpressionMetadata{
+				Params: params,
+			},
+		}
+		res, err := celService.EvaluateCELRequest(evalReq)
+		assert.Equal(t, true, res)
+		assert.Equal(t, nil, err)
+	})
+
+	t.Run("empty release tags list", func(tt *testing.T) {
+		artifact := "devtron/test:v1beta1"
+		releaseTags := []string{}
+		params := celService.GetParamsFromArtifact(artifact, releaseTags)
+
+		evalReq := CELRequest{
+			Expression: "'latest' in releaseTags",
+			ExpressionMetadata: ExpressionMetadata{
+				Params: params,
+			},
+		}
+		res, err := celService.EvaluateCELRequest(evalReq)
+		assert.Equal(t, false, res)
+		assert.Equal(t, nil, err)
+	})
+
+	t.Run("nil release tags list", func(tt *testing.T) {
+		artifact := "devtron/test:v1beta1"
+		var releaseTags []string
+		params := celService.GetParamsFromArtifact(artifact, releaseTags)
+
+		evalReq := CELRequest{
+			Expression: "'latest' in releaseTags",
+			ExpressionMetadata: ExpressionMetadata{
+				Params: params,
+			},
+		}
+		res, err := celService.EvaluateCELRequest(evalReq)
+		assert.Equal(t, false, res)
+		assert.Equal(t, nil, err)
+	})
 }
