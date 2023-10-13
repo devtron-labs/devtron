93ee84d8d58d3cf3d5e9907f5d40db5d593f2697
Gireesh Naidu <gireesh@devtron.ai>
2023-10-13 12:07:57 +0530

    down script update

--- scripts/sql/179_resource_filter_v2.down.sql
+++ scripts/sql/179_resource_filter_v2.down.sql
@@ -1,9 +1,8 @@
 BEGIN;
--- drop sequences
-DROP SEQUENCE IF EXISTS public.resource_filter_audit_seq;
-DROP SEQUENCE IF EXISTS public.resource_filter_evaluation_audit_seq;
-
 -- drop tables
 DROP TABLE IF EXISTS resource_filter_audit;
 DROP TABLE IF EXISTS resource_filter_evaluation_audit;
+-- drop sequences
+DROP SEQUENCE IF EXISTS public.resource_filter_audit_seq;
+DROP SEQUENCE IF EXISTS public.resource_filter_evaluation_audit_seq;
 COMMIT;
