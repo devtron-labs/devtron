a5e061cb5178ba12742478b4b2f94026ffe5ad01
Subhashish <subhashish@devtron.ai>
2023-10-17 20:01:30 +0530

    commit minor fix

--- util/context-utils.go
+++ util/context-utils.go
@@ -6,15 +6,18 @@ import (
 	"reflect"
 )
 
+const IsSuperAdminFlag = "isSuperAdmin"
+
 func SetSuperAdminInContext(ctx context.Context, isSuperAdmin bool) context.Context {
-	ctx = context.WithValue(ctx, "IsSuperAdmin", isSuperAdmin)
+	ctx = context.WithValue(ctx, IsSuperAdminFlag, isSuperAdmin)
 	return ctx
 }
 
 func GetIsSuperAdminFromContext(ctx context.Context) (bool, error) {
-	flag := ctx.Value("isSuperAdmin")
-	if reflect.TypeOf(flag).Kind() == reflect.Bool {
+	flag := ctx.Value(IsSuperAdminFlag)
+
+	if flag != nil && reflect.TypeOf(flag).Kind() == reflect.Bool {
 		return flag.(bool), nil
 	}
-	return false, fmt.Errorf("context not valid, isSuperAdmin not of type bool %v", flag)
+	return false, fmt.Errorf("context not valid, isSuperAdmin flag not set correctly %v", flag)
 }
