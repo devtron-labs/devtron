d89d12effc4b0bbd97844611b62110e763af22dd
ayushmaheshwari <ayush@devtron.ai>
2023-10-25 11:41:26 +0530

    fixing regex

--- pkg/CustomTagService.go
+++ pkg/CustomTagService.go
@@ -21,7 +21,7 @@ const (
 	ImageTagUnavailableMessage                                    = "Desired image tag already exists"
 	REGEX_PATTERN_FOR_ENSURING_ONLY_ONE_VARIABLE_BETWEEN_BRACKETS = `\{.{2,}\}`
 	REGEX_PATTERN_FOR_CHARACTER_OTHER_THEN_X_OR_x                 = `\{[^xX]|{}\}`
-	REGEX_PATTERN_FOR_IMAGE_TAG                                   = `^[a-zA-Z0-9][a-zA-Z0-9._-]{0,126}[a-zA-Z0-9]$`
+	REGEX_PATTERN_FOR_IMAGE_TAG                                   = `^[a-zA-Z0-9]+[a-zA-Z0-9._-]*$`
 )
 
 var (
