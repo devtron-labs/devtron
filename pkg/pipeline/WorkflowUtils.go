d8894771deedb29b469e5f6dfee50e07e50f5b12
Shivam-nagar23 <shivam@devtron.ai>
2023-10-30 16:53:08 +0530

    pipeline type

--- pkg/pipeline/WorkflowUtils.go
+++ pkg/pipeline/WorkflowUtils.go
@@ -370,7 +370,7 @@ type WorkflowRequest struct {
 	Pipeline                 *pipelineConfig.Pipeline
 	Env                      *repository2.Environment
 	AppLabels                map[string]string
-	ciPipelineType           string `json:"CiPipelineType"`
+	CiPipelineType           string `json:"ciPipelineType"`
 }
 
 type CiCdTriggerEvent struct {
