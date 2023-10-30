8a506d8ef00366b0a5bfa2feb1d41b7a9d16a1a0
kartik-579 <kartik@devtron.ai>
2023-10-30 17:20:00 +0530

    updated migration script

--- scripts/sql/185_create_resource.down.sql
+++ scripts/sql/185_create_resource.down.sql
@@ -1,1 +1,7 @@
-DROP TABLE "public"."devtron_resource" CASCADE;
+DROP TABLE "public"."devtron_resource_object_audit";
+
+DROP TABLE "public"."devtron_resource_object";
+
+DROP TABLE "public"."devtron_resource_schema";
+
+DROP TABLE "public"."devtron_resource";
--- scripts/sql/185_create_resource.up.sql
+++ scripts/sql/185_create_resource.up.sql
@@ -1,7 +1,5 @@
--- Sequence and defined type
 CREATE SEQUENCE IF NOT EXISTS id_seq_devtron_resource;
 
--- Table Definition
 CREATE TABLE "public"."devtron_resource"
 (
     "id"             int          NOT NULL DEFAULT nextval('id_seq_devtron_resource'::regclass),
@@ -14,3 +12,56 @@ CREATE TABLE "public"."devtron_resource"
     "updated_by"     integer,
     PRIMARY KEY ("id")
 );
+
+
+CREATE SEQUENCE IF NOT EXISTS id_seq_devtron_resource_schema;
+
+CREATE TABLE "public"."devtron_resource_schema"
+(
+    "id"                  int        NOT NULL DEFAULT nextval('id_seq_devtron_resource_schema'::regclass),
+    "devtron_resource_id" int,
+    "version"             VARCHAR(5) NOT NULL,
+    "schema"              jsonb,
+    "latest"              boolean,
+    "created_on"          timestamptz,
+    "created_by"          integer,
+    "updated_on"          timestamptz,
+    "updated_by"          integer,
+    PRIMARY KEY ("id")
+);
+
+
+CREATE SEQUENCE IF NOT EXISTS id_seq_devtron_resource_object;
+
+CREATE TABLE "public"."devtron_resource_object"
+(
+    "id"                         int          NOT NULL DEFAULT nextval('id_seq_devtron_resource_object'::regclass),
+    "name"                       VARCHAR(250) NOT NULL,
+    "devtron_resource_id"        int,
+    "devtron_resource_schema_id" int,
+    "schema"                     jsonb,
+    "deleted"                    boolean,
+    "created_on"                 timestamptz,
+    "created_by"                 integer,
+    "updated_on"                 timestamptz,
+    "updated_by"                 integer,
+    PRIMARY KEY ("id")
+);
+
+
+CREATE SEQUENCE IF NOT EXISTS id_seq_devtron_resource_object_audit;
+
+CREATE TABLE "public"."devtron_resource_object_audit"
+(
+    "id"                         int         NOT NULL DEFAULT nextval('id_seq_devtron_resource_object_audit'::regclass),
+    "devtron_resource_object_id" int,
+    "schema"                     jsonb,
+    "audit_operation"            VARCHAR(10) NOT NULL,
+    "created_on"                 timestamptz,
+    "created_by"                 integer,
+    "updated_on"                 timestamptz,
+    "updated_by"                 integer,
+    PRIMARY KEY ("id")
+);
+
+
