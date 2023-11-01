3df7ea500181eee6b15d6b7dbbe556153eb22c3d
Gireesh Naidu <gireesh@devtron.ai>
2023-11-02 02:02:31 +0530

    legacy bug fix

--- internal/sql/repository/pipelineConfig/CiWorkflowRepository.go
+++ internal/sql/repository/pipelineConfig/CiWorkflowRepository.go
@@ -74,9 +74,9 @@ type CiWorkflow struct {
 	CiBuildType            string            `sql:"ci_build_type"`
 	EnvironmentId          int               `sql:"environment_id"`
 	ImagePathReservationId int               `sql:"image_path_reservation_id"`
-	ReferenceCiWorkflowId int               `sql:"ref_ci_workflow_id"`
-	ParentCiWorkFlowId    int               `sql:"parent_ci_workflow_id"`
-	CiPipeline            *CiPipeline
+	ReferenceCiWorkflowId  int               `sql:"ref_ci_workflow_id"`
+	ParentCiWorkFlowId     int               `sql:"parent_ci_workflow_id"`
+	CiPipeline             *CiPipeline
 }
 
 type WorkflowWithArtifact struct {
@@ -103,8 +103,8 @@ type WorkflowWithArtifact struct {
 	EnvironmentId          int               `json:"environmentId"`
 	EnvironmentName        string            `json:"environmentName"`
 	ImagePathReservationId int               `json:"image_path_reservation_id"`
-	RefCiWorkflowId    int               `json:"referenceCiWorkflowId"`
-	ParentCiWorkflowId int               `json:"parent_ci_workflow_id"`
+	RefCiWorkflowId        int               `json:"referenceCiWorkflowId"`
+	ParentCiWorkflowId     int               `json:"parent_ci_workflow_id"`
 }
 
 type GitCommit struct {
@@ -230,7 +230,6 @@ func (impl *CiWorkflowRepositoryImpl) FindCiWorkflowGitTriggersByIds(ids []int) 
 		return workflows, nil
 	}
 	err := impl.dbConnection.Model(&workflows).
-		Column("ci_workflow.git_triggers").
 		Where("ci_workflow.id IN (?)", pg.In(ids)).
 		Select()
 
