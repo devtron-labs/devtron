da33dd4ff572c3d81e0fc1ac83d1f3635fb19f66
ashish sonam <ashishsonam@devtron.ai>
2023-11-03 17:37:56 +0530

    added dummy data for devtron resource

--- scripts/sql/186_create_resource.up.sql
+++ scripts/sql/186_create_resource.up.sql
@@ -67,4 +67,579 @@ CREATE TABLE "public"."devtron_resource_object_audit"
     PRIMARY KEY ("id")
 );
 
+INSERT INTO devtron_resource(kind,display_name,icon,parent_kind_id,deleted,created_on,created_by,updated_on,updated_by)
+    VALUES('application','Applications','',0,false,now(),1,now(),1),
+          ('devtron-application','Devtron applications','',(select id from devtron_resource where kind='application'),false,now(),1,now(),1),
+          ('helm-application','Helm applications','',(select id from devtron_resource where kind='application'),false,now(),1,now(),1),
+          ('job','Jobs','',0,false,now(),1,now(),1),
+          ('cluster','Clusters','',0,false,now(),1,now(),1);
 
+INSERT INTO devtron_resource_schema(devtron_resource_id,version,schema,latest,created_on,created_by,updated_on,updated_by)
+VALUES((select id from devtron_resource where kind='devtron-application'),'v1',
+       '{
+          "$schema": "https://json-schema.org/draft/2020-12/schema",
+          "title": "Devtron Application Schema",
+          "references": {
+            "users": {
+              "type": "object",
+              "properties": {
+                "id": {
+                  "type": "string"
+                },
+                "name": {
+                  "type": "string"
+                }
+              },
+              "required": [
+                "id",
+                "name"
+              ]
+            }
+          },
+          "type": "object",
+          "properties": {
+            "version": {
+              "type": "string"
+            },
+            "kind": {
+              "type": "string"
+            },
+            "overview": {
+              "type": "object",
+              "properties": {
+                "id": {
+                  "type": "string"
+                },
+                "name": {
+                  "type": "string"
+                },
+                "icon": {
+                  "type": "string",
+                  "contentEncoding": "base64"
+                },
+                "Description": {
+                  "type": "string"
+                },
+                "Created On": {
+                  "type": "string",
+                  "format": "date-time"
+                },
+                "Created By": {
+                  "type": "object",
+                  "refType": "#/references/users"
+                },
+                "Tags": {
+                  "additionalProperties": {
+                    "type": "string"
+                  }
+                },
+                "metadata": {
+                  "type": "object",
+                  "properties": {
+                    "Owners & Pager Duty": {
+                      "type": "object",
+                      "properties": {
+                        "On Pager Duty": {
+                          "type": "array",
+                          "items": {
+                            "type": "object",
+                            "refType": "#/references/users"
+                          }
+                        },
+                        "Code Owners": {
+                          "type": "array",
+                          "items": {
+                            "type": "object",
+                            "refType": "#/references/users"
+                          }
+                        }
+                      },
+                      "required": [
+                        "Code Owners"
+                      ]
+                    },
+                    "Service Details": {
+                      "type": "object",
+                      "properties": {
+                        "Framework": {
+                          "type": "string"
+                        },
+                        "Language": {
+                          "type": "array",
+                          "items": {
+                            "type": "string"
+                          }
+                        },
+                        "Map": {
+                          "additionalProperties": {
+                            "type": "string"
+                          }
+                        },
+                        "Communication Method": {
+                          "type": "string"
+                        },
+                        "Internet Facing": {
+                          "type": "boolean"
+                        },
+                        "Cities": {
+                          "type": "array",
+                          "items": {
+                            "type": "string"
+                          }
+                        }
+                      }
+                    },
+                    "Documentation": {
+                      "type": "object",
+                      "properties": {
+                        "Service Doc": {
+                          "type": "string",
+                          "format": "uri"
+                        },
+                        "API Contract": {
+                          "type": "string",
+                          "format": "uri"
+                        },
+                        "Runbook": {
+                          "type": "string",
+                          "format": "uri"
+                        }
+                      }
+                    }
+                  },
+                  "required": [
+                    "Owners & Pager Duty"
+                  ]
+                }
+              },
+              "required": [
+                "id",
+                "Created On",
+                "Created By",
+                "metadata"
+              ]
+            },
+            "actions": {
+              "type": "object"
+            },
+            "dependencies": {
+              "type": "object"
+            }
+          },
+          "required": [
+            "version",
+            "kind",
+            "overview"
+          ]
+        }'
+    ,true,now(),1,now(),1),
+      ((select id from devtron_resource where kind='helm-application'),'v1',
+       '{
+          "$schema": "https://json-schema.org/draft/2020-12/schema",
+          "title": "Helm Application Schema",
+          "references": {
+            "users": {
+              "type": "object",
+              "properties": {
+                "id": {
+                  "type": "string"
+                },
+                "name": {
+                  "type": "string"
+                }
+              },
+              "required": [
+                "id",
+                "name"
+              ]
+            }
+          },
+          "type": "object",
+          "properties": {
+            "version": {
+              "type": "string"
+            },
+            "kind": {
+              "type": "string"
+            },
+            "overview": {
+              "type": "object",
+              "properties": {
+                "id": {
+                  "type": "string"
+                },
+                "name": {
+                  "type": "string"
+                },
+                "icon": {
+                  "type": "string",
+                  "contentEncoding": "base64"
+                },
+                "Description": {
+                  "type": "string"
+                },
+                "Created On": {
+                  "type": "string",
+                  "format": "date-time"
+                },
+                "Created By": {
+                  "type": "object",
+                  "refType": "#/references/users"
+                },
+                "Tags": {
+                  "additionalProperties": {
+                    "type": "string"
+                  }
+                },
+                "metadata": {
+                  "type": "object",
+                  "properties": {
+                    "Owners & Pager Duty": {
+                      "type": "object",
+                      "properties": {
+                        "On Pager Duty": {
+                          "type": "array",
+                          "items": {
+                            "type": "object",
+                            "refType": "#/references/users"
+                          }
+                        },
+                        "Code Owners": {
+                          "type": "array",
+                          "items": {
+                            "type": "object",
+                            "refType": "#/references/users"
+                          }
+                        }
+                      },
+                      "required": [
+                        "Code Owners"
+                      ]
+                    },
+                    "Service Details": {
+                      "type": "object",
+                      "properties": {
+                        "Framework": {
+                          "type": "string"
+                        },
+                        "Language": {
+                          "type": "array",
+                          "items": {
+                            "type": "string"
+                          }
+                        },
+                        "Map": {
+                          "additionalProperties": {
+                            "type": "string"
+                          }
+                        },
+                        "Communication Method": {
+                          "type": "string"
+                        },
+                        "Internet Facing": {
+                          "type": "boolean"
+                        },
+                        "Cities": {
+                          "type": "array",
+                          "items": {
+                            "type": "string"
+                          }
+                        }
+                      }
+                    },
+                    "Documentation": {
+                      "type": "object",
+                      "properties": {
+                        "Service Doc": {
+                          "type": "string",
+                          "format": "uri"
+                        },
+                        "API Contract": {
+                          "type": "string",
+                          "format": "uri"
+                        },
+                        "Runbook": {
+                          "type": "string",
+                          "format": "uri"
+                        }
+                      }
+                    }
+                  },
+                  "required": [
+                    "Owners & Pager Duty"
+                  ]
+                }
+              },
+              "required": [
+                "id",
+                "Created On",
+                "Created By",
+                "metadata"
+              ]
+            },
+            "actions": {
+              "type": "object"
+            },
+            "dependencies": {
+              "type": "object"
+            }
+          },
+          "required": [
+            "version",
+            "kind",
+            "overview"
+          ]
+        }',
+       true,now(),1,now(),1),
+      ((select id from devtron_resource where kind='job'),'v1',
+       '{
+          "$schema": "https://json-schema.org/draft/2020-12/schema",
+          "title": "Job Schema",
+          "references": {
+            "users": {
+              "type": "object",
+              "properties": {
+                "id": {
+                  "type": "string"
+                },
+                "name": {
+                  "type": "string"
+                }
+              },
+              "required": [
+                "id",
+                "name"
+              ]
+            }
+          },
+          "type": "object",
+          "properties": {
+            "version": {
+              "type": "string"
+            },
+            "kind": {
+              "type": "string"
+            },
+            "overview": {
+              "type": "object",
+              "properties": {
+                "id": {
+                  "type": "string"
+                },
+                "name": {
+                  "type": "string"
+                },
+                "icon": {
+                  "type": "string",
+                  "contentEncoding": "base64"
+                },
+                "Description": {
+                  "type": "string"
+                },
+                "Created On": {
+                  "type": "string",
+                  "format": "date-time"
+                },
+                "Created By": {
+                  "type": "object",
+                  "refType": "#/references/users"
+                },
+                "Tags": {
+                  "additionalProperties": {
+                    "type": "string"
+                  }
+                },
+                "metadata": {
+                  "type": "object",
+                  "properties": {
+                    "Owners & Pager Duty": {
+                      "type": "object",
+                      "properties": {
+                        "On Pager Duty": {
+                          "type": "array",
+                          "items": {
+                            "type": "object",
+                            "refType": "#/references/users"
+                          }
+                        },
+                        "Code Owners": {
+                          "type": "array",
+                          "items": {
+                            "type": "object",
+                            "refType": "#/references/users"
+                          }
+                        }
+                      },
+                      "required": [
+                        "Code Owners"
+                      ]
+                    },
+                    "Service Details": {
+                      "type": "object",
+                      "properties": {
+                        "Framework": {
+                          "type": "string"
+                        },
+                        "Language": {
+                          "type": "array",
+                          "items": {
+                            "type": "string"
+                          }
+                        },
+                        "Map": {
+                          "additionalProperties": {
+                            "type": "string"
+                          }
+                        },
+                        "Communication Method": {
+                          "type": "string"
+                        },
+                        "Internet Facing": {
+                          "type": "boolean"
+                        },
+                        "Cities": {
+                          "type": "array",
+                          "items": {
+                            "type": "string"
+                          }
+                        }
+                      }
+                    },
+                    "Documentation": {
+                      "type": "object",
+                      "properties": {
+                        "Service Doc": {
+                          "type": "string",
+                          "format": "uri"
+                        },
+                        "API Contract": {
+                          "type": "string",
+                          "format": "uri"
+                        },
+                        "Runbook": {
+                          "type": "string",
+                          "format": "uri"
+                        }
+                      }
+                    }
+                  },
+                  "required": [
+                    "Owners & Pager Duty"
+                  ]
+                }
+              },
+              "required": [
+                "id",
+                "Created On",
+                "Created By",
+                "metadata"
+              ]
+            },
+            "actions": {
+              "type": "object"
+            },
+            "dependencies": {
+              "type": "object"
+            }
+          },
+          "required": [
+            "version",
+            "kind",
+            "overview"
+          ]
+        }',
+       true,now(),1,now(),1),
+      ((select id from devtron_resource where kind='cluster'),'v1',
+       '{
+          "$schema": "https://json-schema.org/draft/2020-12/schema",
+          "title": "Cluster Schema",
+          "references": {
+            "users": {
+              "type": "object",
+              "properties": {
+                "id": {
+                  "type": "string"
+                },
+                "name": {
+                  "type": "string"
+                }
+              },
+              "required": [
+                "id",
+                "name"
+              ]
+            }
+          },
+          "type": "object",
+          "properties": {
+            "version": {
+              "type": "string"
+            },
+            "kind": {
+              "type": "string"
+            },
+            "overview": {
+              "type": "object",
+              "properties": {
+                "id": {
+                  "type": "string"
+                },
+                "name": {
+                  "type": "string"
+                },
+                "icon": {
+                  "type": "string",
+                  "contentEncoding": "base64"
+                },
+                "Description": {
+                  "type": "string"
+                },
+                "Created On": {
+                  "type": "string",
+                  "format": "date-time"
+                },
+                "Created By": {
+                  "type": "object",
+                  "refType": "#/references/users"
+                },
+                "Tags": {
+                  "additionalProperties": {
+                    "type": "string"
+                  }
+                },
+                "metadata": {
+                  "type": "object",
+                  "properties": {
+                    "POCs": {
+                      "type": "string"
+                    },
+                    "K8s Version": {
+                      "type": "string"
+                    },
+                    "Cluster Provider": {
+                      "type": "string"
+                    }
+                  }
+                }
+              },
+              "required": [
+                "id",
+                "Created On",
+                "Created By",
+                "metadata"
+              ]
+            },
+            "actions": {
+              "type": "object"
+            },
+            "dependencies": {
+              "type": "object"
+            }
+          },
+          "required": [
+            "version",
+            "kind",
+            "overview"
+          ]
+        }',
+       true,now(),1,now(),1),;
