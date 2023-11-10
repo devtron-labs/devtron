47be57792f0cbaf6914a1ca030776134ab6c701f
Gireesh Naidu <gireesh@devtron.ai>
2023-11-10 17:09:04 +0530

    query change

--- internal/sql/repository/CiArtifactsListingQueryBuilder.go
+++ internal/sql/repository/CiArtifactsListingQueryBuilder.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/devtron-labs/devtron/api/bean"
 	"github.com/devtron-labs/devtron/internal/sql/repository/helper"
-	"github.com/go-pg/pg"
 )
 
 func BuildQueryForParentTypeCIOrWebhook(listingFilterOpts bean.ArtifactsListFilterOptions) string {
@@ -73,7 +72,7 @@ func BuildQueryForArtifactsForRollback(listingFilterOptions bean.ArtifactsListFi
 		" WHERE cdw.pipeline_id=%v AND cdwr.workflow_type = '%v' AND cia.image LIKE '%v'"
 	commonQuery = fmt.Sprintf(commonQuery, listingFilterOptions.PipelineId, listingFilterOptions.StageType, listingFilterOptions.SearchString)
 	if len(listingFilterOptions.ExcludeArtifactIds) > 0 {
-		commonQuery = fmt.Sprintf(" %s AND cdwr.id NOT IN (%s)", commonQuery, pg.In(listingFilterOptions.ExcludeWfrIds))
+		commonQuery = fmt.Sprintf(" %s AND cdwr.id NOT IN (%s)", commonQuery, helper.GetCommaSepratedString(listingFilterOptions.ExcludeWfrIds))
 	}
 	totalCountQuery := " SELECT COUNT(cia.id) as total_count " + commonQuery
 	orderByQuery := " ORDER BY cdwr.id DESC "
