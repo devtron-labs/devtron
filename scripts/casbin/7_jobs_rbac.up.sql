019010d50ebe20df207af552425c14e4fad4a051
Shivam-nagar23 <shivam@devtron.ai>
2023-11-10 11:23:06 +0530

    casbin-script

---
+++ scripts/casbin/7_jobs_rbac.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO "public"."casbin_rule" ("p_type", "v0", "v1", "v2", "v3", "v4", "v5") VALUES
+('p','role:super-admin___','jobs','*','*','allow',''),
+('p','role:super-admin___','jobEnv','*','*','allow',''),
+('p','role:super-admin___','workflow','*','*','allow','');
+
