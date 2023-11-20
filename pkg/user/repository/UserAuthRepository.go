4521f20b56302f808ef1f4cc7a11a54b05898d59
Shivam-nagar23 <shivam@devtron.ai>
2023-11-20 13:12:34 +0530

    apps

--- pkg/user/repository/UserAuthRepository.go
+++ pkg/user/repository/UserAuthRepository.go
@@ -951,7 +951,7 @@ func (impl UserAuthRepositoryImpl) GetRolesForProject(teamName string) ([]*RoleM
 
 func (impl UserAuthRepositoryImpl) GetRolesForApp(appName string) ([]*RoleModel, error) {
 	var roles []*RoleModel
-	err := impl.dbConnection.Model(&roles).Where("entity in (?) ", pg.In([]string{"apps", "jobs"})).
+	err := impl.dbConnection.Model(&roles).Where("entity in (?) ", pg.In([]string{bean2.ENTITY_APPS, bean2.EntityJobs})).
 		Where("entity_name = ?", appName).Select()
 	if err != nil {
 		impl.Logger.Errorw("error in getting roles for app", "err", err, "appName", appName)
