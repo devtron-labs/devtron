1acaad4862b6a18054ab1d75477047aa11cbedd7
kartik-579 <kartik@devtron.ai>
2023-11-23 16:33:42 +0530

    added migration script for dependency

---
+++ scripts/sql/196_alter_resource.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO devtron_resource(kind, display_name, icon, parent_kind_id, deleted, created_on, created_by, updated_on,
+                             updated_by)
+VALUES ('cd-pipeline', 'Cd Pipeline', '', 0, false, now(), 1, now(), 1);
