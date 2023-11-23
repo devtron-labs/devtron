95f1bbe58fadd2fcf898deafef7b7bd9d57b32cc
ayushmaheshwari <ayush@devtron.ai>
2023-11-23 14:05:45 +0530

    copy container image plugin image update

--- scripts/sql/192_custom_tag_migration.up.sql
+++ scripts/sql/192_custom_tag_migration.up.sql
@@ -1,1 +1,3 @@
 update custom_tag set enabled=true where active=true;
+update plugin_pipeline_script set container_image_path ='quay.io/devtron/copy-container-images:48bdcbb4-567-19597'
+                              where container_image_path ='quay.io/devtron/copy-container-images:7285439d-567-19519';
