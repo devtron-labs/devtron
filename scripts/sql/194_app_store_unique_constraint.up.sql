4d62d9b7c2a17f8456adc245a774109fa4daa548
ayushmaheshwari <ayush@devtron.ai>
2023-11-30 18:02:57 +0530

    migration script

--- scripts/sql/194_app_store_unique_constraint.up.sql
+++ scripts/sql/194_app_store_unique_constraint.up.sql
@@ -4,5 +4,3 @@ CREATE UNIQUE INDEX IF NOT EXISTS app_store_unique_oci_repo ON public.app_store
     (name, docker_artifact_store_id) where active=true;
 CREATE UNIQUE INDEX IF NOT EXISTS app_store_unique_chart_repo ON public.app_store
     (name, chart_repo_id) where active=true;
-CREATE UNIQUE INDEX IF NOT EXISTS app_store_application_version_unique ON public.app_store_application_version
-      (version,app_store_id);
