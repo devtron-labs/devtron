16512f27d0a52e55f9b73f90a4e005e6ff873164
subs <subhashish@devtron.ai>
2023-12-01 16:50:21 +0530

    type

--- pkg/security/ImageScanService.go
+++ pkg/security/ImageScanService.go
@@ -325,7 +325,7 @@ func (impl ImageScanServiceImpl) FetchExecutionDetailResult(request *ImageScanRe
 			impl.Logger.Errorw("error while fetching scan execution result", "err", err)
 			return nil, err
 		}
-		// TODO: subhashish
+		// TODO: subhashish- done by gireesh
 		ciPipeline, err := impl.ciPipelineRepository.FindById(ciArtifact.PipelineId)
 		if err == pg.ErrNoRows && ciArtifact.PipelineId > 0 {
 			ciPipeline, err = impl.ciPipelineRepository.FindDeletedById(ciArtifact.PipelineId)
