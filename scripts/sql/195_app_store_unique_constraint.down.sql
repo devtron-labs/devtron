95d3d4e4e7ec718f1a8a021cdd77ab7ed9e8e86d
ayushmaheshwari <ayush@devtron.ai>
2023-12-03 21:32:16 +0530

    down script correction

--- scripts/sql/195_app_store_unique_constraint.down.sql
+++ scripts/sql/195_app_store_unique_constraint.down.sql
@@ -1,8 +1,4 @@
-ALTER TABLE public.app_store
-DROP CONSTRAINT IF EXISTS app_store_unique_oci_repo;
+DROP  INDEX IF EXISTS app_store_unique_oci_repo;
+DROP  INDEX IF EXISTS app_store_unique_chart_repo;
 
-ALTER TABLE public.app_store
-DROP CONSTRAINT IF EXISTS app_store_unique_chart_repo;
 
-ALTER TABLE public.app_store
-DROP CONSTRAINT IF EXISTS app_store_application_version_unique;
