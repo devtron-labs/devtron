399c6b88c4cdd6d42352a7f00a4a430497ad61b3
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2023-12-05 15:25:24 +0530

    fix

--- internal/sql/repository/security/CveStoreRepository.go
+++ internal/sql/repository/security/CveStoreRepository.go
@@ -51,7 +51,7 @@ type VulnerabilityExposure struct {
 	AppId   int    `json:"appId"`
 	EnvId   int    `json:"envId"`
 	//ClusterId     int    `json:"clusterId"`
-	AppStore      bool `json:"appStore"`
+	AppType       bool `json:"appType"`
 	Blocked       bool `json:"blocked"`
 	PipelineEnvId int  `json:"-"`
 	ChartEnvId    int  `json:"-"`
