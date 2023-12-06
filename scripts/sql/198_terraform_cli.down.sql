0f4e774c522418683ac09dc3ba54cf17b8cd7961
Badal Kumar Prusty <badalkumar@Badals-MacBook-Pro.local>
2023-12-06 16:30:59 +0530

    update terraform-cli down script

--- scripts/sql/198_terraform_cli.down.sql
+++ scripts/sql/198_terraform_cli.down.sql
@@ -1,4 +1,6 @@
-DELETE from plugin_step where plugin_id=(select id from plugin_metadata where name='Terraform Cli');
-DELETE from plugin_metadata where name='Terraform Cli';
-DELETE from plugin_stage_mapping where plugin_id=(SELECT id from plugin_metadata where name='Terraform Cli');
 DELETE from plugin_step_variable where plugin_step_id=(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Terraform Cli' and ps."index"=1 and ps.deleted=false);
+DELETE from plugin_step where plugin_id=(select id from plugin_metadata where name='Terraform Cli');
+DELETE from plugin_stage_mapping where plugin_id=(SELECT id from plugin_metadata where name='Terraform Cli');
+DELETE from pipeline_stage_step_variable where pipeline_stage_step_id =(select pipeline_stage_id from pipeline_stage_step where name='Terraform Cli');
+DELETE from pipeline_stage_step where name ='Terraform Cli';
+DELETE from plugin_metadata where name='Terraform Cli';
