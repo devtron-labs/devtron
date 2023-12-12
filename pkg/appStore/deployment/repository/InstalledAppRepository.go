aa77cd75aaf2e91325982650c3d1fc286082b186
Ash-exp <asutosh2000ad@gmail.com>
2023-12-12 13:50:30 +0530

    incorporated PR comments

--- pkg/appStore/deployment/repository/InstalledAppRepository.go
+++ pkg/appStore/deployment/repository/InstalledAppRepository.go
@@ -66,7 +66,7 @@ type InstalledAppRepository interface {
 	GetGitOpsInstalledAppsWhereArgoAppDeletedIsTrue(installedAppId int, envId int) (InstalledApps, error)
 	GetInstalledAppByGitHash(gitHash string) (InstallAppDeleteRequest, error)
 	GetInstalledAppByAppName(appName string) (*InstalledApps, error)
-	GetAcdInstalledAppByAppId(appId int) (InstalledApps, error)
+	GetInstalledAppByAppIdAndDeploymentType(appId int, deploymentAppType string) (InstalledApps, error)
 	GetInstalledAppByInstalledAppVersionId(installedAppVersionId int) (InstalledApps, error)
 	GetAllGitOpsDeploymentAppName() ([]string, error)
 	GetAllGitOpsAppNameAndInstalledAppMapping() ([]*GitOpsAppDetails, error)
@@ -732,10 +732,10 @@ func (impl InstalledAppRepositoryImpl) GetInstalledAppByGitHash(gitHash string) 
 	return model, nil
 }
 
-func (impl InstalledAppRepositoryImpl) GetAcdInstalledAppByAppId(appId int) (InstalledApps, error) {
+func (impl InstalledAppRepositoryImpl) GetInstalledAppByAppIdAndDeploymentType(appId int, deploymentAppType string) (InstalledApps, error) {
 	var installedApps InstalledApps
 	queryString := `select * from installed_apps where active=? and app_id=? and deployment_app_type=?;`
-	_, err := impl.dbConnection.Query(&installedApps, queryString, true, appId, util2.PIPELINE_DEPLOYMENT_TYPE_ACD)
+	_, err := impl.dbConnection.Query(&installedApps, queryString, true, appId, deploymentAppType)
 	if err != nil {
 		impl.Logger.Errorw("error in fetching InstalledApp", "err", err)
 		return installedApps, err
