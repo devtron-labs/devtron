f9abf4e85aac8d365362734e3d35e032ac121e1e
Ashish-devtron <ashish.kumar@devtron.ai>
2023-12-15 15:16:26 +0530

    vendor files

---
+++ vendor/github.com/mdaverde/jsonpath/doc.go
@@ -0,0 +1,6 @@
+// Package jsonpath implements getter and setter for generic JSON unmarshaled data structures (i.e. map[string]interface{})
+//
+// Values can be set on nonexistent paths.
+//
+// Path strings are not 100% JSONPath compliant (yet) but will handle most of the common use cases.
+package jsonpath
