3d03af82680a6cb6574a18d6cbcb59ab6cf5f80c
ayushmaheshwari <ayush@devtron.ai>
2023-12-19 12:17:19 +0530

    fix wire

--- Wire.go
+++ Wire.go
@@ -972,8 +972,6 @@ func InitializeApp() (*App, error) {
 
 		pipeline.NewPipelineConfigListenerServiceImpl,
 		wire.Bind(new(pipeline.PipelineConfigListenerService), new(*pipeline.PipelineConfigListenerServiceImpl)),
-
-		argocdServer.GetACDDeploymentConfig,
 	)
 	return &App{}, nil
 }
