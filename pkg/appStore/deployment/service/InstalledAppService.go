7cff6793ee62127d29ebd5888c2cceb6a1369e42
Prakash <prakash.kumar@devtron.ai>
2024-01-22 00:39:42 +0530

    fix: 500 fixes second iteration (#4464)
    * FetchChartNotes return 4xx when installed app or installed app versions not found
    
    * GetDeploymentHistory and GetInstalledAppVersion to return 4xx when installed app or installed app versions not found
    
    * fixes
    
    * adding pg:",discard_unknown_columns" in tables where doesn't exist
    
    * fix for Git Sensor response
    
    * fix for inception
    
    * fix for k8s resource delete
    
    * fix for pipeline already exists
    
    * fix for pipeline already exists
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * go mod version changes
    
    * merged
    
    * common lib main version sync
    
    * main merge
    
    ---------
    
    Co-authored-by: ShashwatDadhich <dadhichshashwat1808@gmail.com>

--- pkg/appStore/deployment/service/InstalledAppService.go
+++ pkg/appStore/deployment/service/InstalledAppService.go
@@ -919,11 +919,14 @@ func (impl *InstalledAppServiceImpl) FetchChartNotes(installedAppId int, envId i
 	installedApp, err := impl.installedAppRepository.FetchNotes(installedAppId)
 	if err != nil && err != pg.ErrNoRows {
 		return "", err
+	} else if err == pg.ErrNoRows {
+		impl.logger.Errorw("installed app not found or may have been deleted", "installedAppId", installedAppId, "envId", envId)
+		return "", &util.ApiError{HttpStatusCode: http.StatusBadRequest, Code: "400", UserMessage: "Installed app not found in database or may have been deleted", InternalMessage: err.Error()}
 	}
 	installedAppVerison, err := impl.installedAppRepository.GetInstalledAppVersionByInstalledAppIdAndEnvId(installedAppId, envId)
 	if err != nil {
 		if err == pg.ErrNoRows {
-			return "", fmt.Errorf("values are outdated. please fetch the latest version and try again")
+			return "", &util.ApiError{HttpStatusCode: http.StatusBadRequest, Code: "400", UserMessage: "values are outdated. please fetch the latest version and try again", InternalMessage: err.Error()}
 		}
 		impl.logger.Errorw("error fetching installed  app version in installed app service", "err", err)
 		return "", err
