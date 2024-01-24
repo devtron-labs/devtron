7a072c2046e0667b0c6578332f81d0c9385c1b46
ashish sonam <ashishsonam@devtron.ai>
2024-01-24 16:54:02 +0530

    changed the ordering of headers and writes in octet stream

--- api/k8s/application/k8sApplicationRestHandler.go
+++ api/k8s/application/k8sApplicationRestHandler.go
@@ -696,8 +696,7 @@ func (handler *K8sApplicationRestHandlerImpl) DownloadPodLogs(w http.ResponseWri
 		}
 	}
 	podLogsFilename := fmt.Sprintf("podlogs-%s-%s.txt", request.K8sRequest.ResourceIdentifier.Name, uuid.New().String())
-	logsBody := k8s.LogsDownloadBean{FileName: podLogsFilename, LogsData: dataBuffer.String()}
-	common.WriteJsonResp(w, nil, logsBody, http.StatusOK)
+	common.WriteOctetStreamResp(w, r, dataBuffer.Bytes(), podLogsFilename)
 }
 
 func (handler *K8sApplicationRestHandlerImpl) requestValidationAndRBAC(w http.ResponseWriter, r *http.Request, token string, request *k8s.ResourceRequestBean) {
