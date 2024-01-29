91e1970291a0a595d725298c8cbf6ba463677ee1
Shivam-nagar23 <shivam@devtron.ai>
2024-01-29 11:10:42 +0530

    removing unused code

--- pkg/auth/user/repository/helper/UserRepositoryQueryBuilder.go
+++ pkg/auth/user/repository/helper/UserRepositoryQueryBuilder.go
@@ -26,21 +26,6 @@ const (
 	TimeFormatForParsing string = "2006-01-02 15:04:05 -0700 MST"
 )
 
-//func (impl UserRepositoryQueryBuilder) GetStatusFromTTL(ttl, recordedTime time.Time) bean.Status {
-//	if ttl.IsZero() || ttl.After(recordedTime) {
-//		return bean.Active
-//	}
-//	return bean.Inactive
-//}
-//
-//func (impl UserRepositoryQueryBuilder) GetStatusFromTimeString(timeString string, recordedTime time.Time) (bean.Status, error) {
-//	formattedTime, err := time.Parse(time.RFC3339, timeString)
-//	if err != nil {
-//		return bean.Unknown, err
-//	}
-//	return impl.GetStatusFromTTL(formattedTime, recordedTime), nil
-//}
-
 func (impl UserRepositoryQueryBuilder) GetQueryForUserListingWithFilters(req *bean.FetchListingRequest) string {
 	whereCondition := fmt.Sprintf("where active = %t AND (user_type is NULL or user_type != '%s') ", true, bean.USER_TYPE_API_TOKEN)
 	orderCondition := ""
@@ -86,24 +71,6 @@ func (impl UserRepositoryQueryBuilder) GetQueryForAllUserWithAudit() string {
 	query := fmt.Sprintf("SELECT \"user_model\".*, \"timeout_window_configuration\".\"id\" AS \"timeout_window_configuration__id\", \"timeout_window_configuration\".\"timeout_window_expression\" AS \"timeout_window_configuration__timeout_window_expression\", \"timeout_window_configuration\".\"timeout_window_expression_format\" AS \"timeout_window_configuration__timeout_window_expression_format\", \"user_audit\".\"id\" AS \"user_audit__id\", \"user_audit\".\"updated_on\" AS \"user_audit__updated_on\",\"user_audit\".\"user_id\" AS \"user_audit__user_id\" ,\"user_audit\".\"created_on\" AS \"user_audit__created_on\" from users As \"user_model\" LEFT JOIN user_audit As \"user_audit\" on \"user_audit\".\"user_id\" = \"user_model\".\"id\" LEFT JOIN timeout_window_configuration AS \"timeout_window_configuration\" ON \"timeout_window_configuration\".\"id\" = \"user_model\".\"timeout_window_configuration_id\" %s %s;", whereCondition, orderCondition)
 	return query
 }
-
-//func (impl UserRepositoryQueryBuilder) GetQueryForBulkUpdate(req *bean.BulkStatusUpdateRequest) string {
-//	ttlTime := req.CurrentTime.Format(QueryTimeFormat)
-//	if req.Status == bean.Active {
-//		if !req.TimeToLive.IsZero() {
-//			ttlTime = fmt.Sprintf("'%s'", req.TimeToLive.Format(QueryTimeFormat))
-//		} else {
-//			ttlTime = "null"
-//		}
-//	} else if req.Status == bean.Inactive {
-//		ttlTime = fmt.Sprintf("CURRENT_TIMESTAMP - INTERVAL '1 day' ")
-//	}
-//
-//	//whereCondition := " where id in (" + helper.GetCommaSepratedString(req.UserIds) + ") "
-//	whereCondition := fmt.Sprintf("AND (user_type is NULL or user_type != '%s');", bean.USER_TYPE_API_TOKEN)
-//	query := fmt.Sprintf("UPDATE USERS set time_to_live= %s %s", ttlTime, whereCondition)
-//	return query
-//}
 
 func (impl UserRepositoryQueryBuilder) GetQueryForGroupListingWithFilters(req *bean.FetchListingRequest) string {
 	whereCondition := fmt.Sprintf("where active = %t ", true)
