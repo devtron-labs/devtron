1febba603f4ae4c384e77d1b18c335df04968faf
Shivam-nagar23 <shivam@devtron.ai>
2024-01-30 18:37:24 +0530

    id for user

--- internal/constants/User.go
+++ internal/constants/User.go
@@ -6,6 +6,6 @@ const (
 )
 
 const (
-	AdminUserId  = 1 // we have established Admin user as 1 while setting up devtron
-	SystemUserId = 2 // we have established System user as 1 while setting up devtron, which are being used for auto-trigger operations
+	AdminUserId  = 2 // we have established Admin user as 2 while setting up devtron
+	SystemUserId = 1 // we have established System user as 1 while setting up devtron, which are being used for auto-trigger operations
 )
