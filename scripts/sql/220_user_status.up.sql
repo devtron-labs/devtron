1cce3af1b6749f5c445ca58463b6afcd8e961d82
Shivam-nagar23 <shivam@devtron.ai>
2024-01-30 18:14:51 +0530

    sql update

--- scripts/sql/220_user_status.up.sql
+++ scripts/sql/220_user_status.up.sql
@@ -6,6 +6,10 @@ CREATE TABLE "public"."timeout_window_configuration" (
     "id" int NOT NULL DEFAULT nextval('id_seq_timeout_window_configuration'::regclass),
     "timeout_window_expression" varchar(255) NOT NULL,
     "timeout_window_expression_format" int NOT NULL,
+    "created_on"                   timestamptz  NOT NULL,
+    "created_by"                   int4         NOT NULL,
+    "updated_on"                   timestamptz  NOT NULL,
+    "updated_by"                   int4         NOT NULL,
      PRIMARY KEY ("id")
 );
 
