594372e459c3754c72cc8e485a58f9e3c27523e3
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2024-02-02 17:37:06 +0530

    bug fix

--- enterprise/pkg/lockConfiguration/LockConfigurationService.go
+++ enterprise/pkg/lockConfiguration/LockConfigurationService.go
@@ -346,6 +346,11 @@ func getDiffJson(savedConfigMap, currentConfigMap map[string]interface{}, path s
 		if !reflect.DeepEqual(savedConfigMap[key], currentConfigMap[key]) {
 			switch reflect.TypeOf(savedConfigMap[key]).Kind() {
 			case reflect.Map:
+				if currentConfigMap[key] != nil {
+					lockedMap[key] = currentConfigMap[key]
+					modifiedMap[key] = currentConfigMap[key]
+					continue
+				}
 				locked, deleted, added, modified, isSaveEligibleChangesDisabled, deletedPaths := getDiffJson(savedConfigMap[key].(map[string]interface{}), currentConfigMap[key].(map[string]interface{}), path+"/"+key)
 				assignMapValueToMap(lockedMap, locked, key)
 				assignMapValueToMap(deletedMap, deleted, key)
