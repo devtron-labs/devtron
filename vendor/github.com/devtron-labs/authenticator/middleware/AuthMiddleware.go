2277651a27a5e45744985ade50b8863181dd3001
Shivam-nagar23 <shivam@devtron.ai>
2024-02-08 17:41:24 +0530

    updated authenticator version

--- vendor/github.com/devtron-labs/authenticator/middleware/AuthMiddleware.go
+++ vendor/github.com/devtron-labs/authenticator/middleware/AuthMiddleware.go
@@ -67,8 +67,8 @@ func Authorizer(sessionManager *SessionManager, whitelistChecker func(url string
 					return
 				}
 				pass = true
-				//TODO - we also can set user info in session (to avoid fetch it for all create n active)
 
+				// checking user status in db
 				isInactive, userId, err := userStatusCheckInDb(token)
 				if err != nil {
 					writeResponse(http.StatusUnauthorized, "Invalid User", w, err)
@@ -77,6 +77,8 @@ func Authorizer(sessionManager *SessionManager, whitelistChecker func(url string
 					writeResponse(http.StatusUnauthorized, "Inactive User", w, fmt.Errorf("inactive User"))
 					return
 				}
+
+				//setting user id in context
 				context.WithValue(r.Context(), "userId", userId)
 			}
 			if pass {
