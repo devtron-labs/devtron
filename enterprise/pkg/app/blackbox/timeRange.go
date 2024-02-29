cb1f2c9b9c3a272b202a653fa2eb131eea0cab87
subhashish-devtron <subhashish@devtron.ai>
2024-02-29 11:48:49 +0530

    cleaning dead code

--- enterprise/pkg/app/blackbox/timeRange.go
+++ enterprise/pkg/app/blackbox/timeRange.go
@@ -4,54 +4,6 @@ import (
 	"time"
 )
 
-//type GlobalPolicyDataManager interface {
-//	// fetch data only from GlobalPolicyRepository
-//	GetPolicyById(policyId int) (*GlobalPolicyBaseModel, error)
-//	GetAllActiveByType() ([]*GlobalPolicyBaseModel, error)
-//	GetPolicyByName(policyName string) (*GlobalPolicyBaseModel, error)
-//	GetPolicyByIds(policyIds []int) ([]*GlobalPolicyBaseModel, error)
-//
-//	// save data using both GlobalPolicyRepository & GlobalPolicySearchableFieldRepository
-//	// but perform operation in single Tx
-//	CreatePolicy(tx *pg.Tx, globalPolicyDataModel *GlobalPolicyDataModel) (*GlobalPolicyDataModel, error)
-//	UpdatePolicy(tx *pg.Tx, globalPolicyDataModel *GlobalPolicyDataModel) (*GlobalPolicyDataModel, error)
-//	DeletePolicyById(tx *pg.Tx, policyId int) error
-//
-//	// fetch data only from GlobalPolicySearchableFieldRepository
-//	GetPolicyMetadataByFields(policyIds []int, fields []*SearchableField) (map[int][]*SearchableField, error)
-//	//
-//	GetPoliciesBySearchableFields(policyIds []int, fields []*SearchableField) ([]*GlobalPolicyBaseModel, error)
-//}
-//
-//type GlobalPolicyBaseModel struct {
-//	Id            int
-//	Name          string
-//	Description   string
-//	Enabled       bool
-//	PolicyOf      bean.GlobalPolicyType
-//	PolicyVersion bean.GlobalPolicyVersion
-//	JsonData      string
-//	Active        bool
-//	UserId        int32
-//}
-//
-//type GlobalPolicyDataModel struct {
-//	GlobalPolicyBaseModel
-//	SearchableFields []SearchableField
-//}
-//
-//type SearchableField struct {
-//	FieldName  string
-//	FieldValue interface{}
-//	FieldType  FieldType
-//}
-//type FieldType int
-//
-//const NumericType FieldType = 1
-//const StringType FieldType = 2
-//const DateTimeType FieldType = 3
-//const BooleanType FieldType = 4
-
 type TimeRange struct {
 	TimeFrom       time.Time
 	TimeTo         time.Time
