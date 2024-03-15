635d2ba9e1083c2e75452c51dafc9dce6ba1fad4
subhashish-devtron <subhashish@devtron.ai>
2024-03-15 13:06:19 +0530

    cleaning up

--- cmd/external-app/wire.go
+++ cmd/external-app/wire.go
@@ -242,13 +242,11 @@ func InitializeApp() (*App, error) {
 		timeoutWindow.NewTimeWindowServiceImpl,
 		wire.Bind(new(timeoutWindow.TimeoutWindowService), new(*timeoutWindow.TimeWindowServiceImpl)),
 
-		repository5.NewTimeoutWindowResourceMappingRepositoryImpl,
-		wire.Bind(new(repository5.TimeoutWindowResourceMappingRepository), new(*repository5.TimeoutWindowResourceMappingRepositoryImpl)),
+		//repository5.NewTimeoutWindowResourceMappingRepositoryImpl,
+		//wire.Bind(new(repository5.TimeoutWindowResourceMappingRepository), new(*repository5.TimeoutWindowResourceMappingRepositoryImpl)),
 
 		repository5.NewTimeWindowRepositoryImpl,
 		wire.Bind(new(repository5.TimeWindowRepository), new(*repository5.TimeWindowRepositoryImpl)),
-		timeoutWindow.NewTimeoutWindowResourceMappingServiceImpl,
-		wire.Bind(new(timeoutWindow.TimeoutWindowResourceMappingService), new(*timeoutWindow.TimeoutWindowResourceMappingServiceImpl)),
 
 		repository5.NewTimeoutWindowResourceMappingRepositoryImpl,
 		wire.Bind(new(repository5.TimeoutWindowResourceMappingRepository), new(*repository5.TimeoutWindowResourceMappingRepositoryImpl)),
