6792be106867e519818496f994ad492fe5dffa81
Prakash Kumar <prakash.kumar@devtron.ai>
2024-03-15 12:58:55 +0530

    revert

--- pkg/auth/sso/SSOLoginService.go
+++ pkg/auth/sso/SSOLoginService.go
@@ -70,21 +70,6 @@ func (r *Config) secureCredentials() {
 	}
 }
 
-func (r *Config) updateCredentialsFromBase(configFromDb *Config) {
-	r.updateSecretFromBase(r, configFromDb, ClientID)
-	r.updateSecretFromBase(r, configFromDb, ClientSecret)
-	if r.IsSsoLdap() {
-		r.updateSecretFromBase(r, configFromDb, LdapBindPW)
-		r.updateSecretFromBase(r, configFromDb, LdapUsernamePrompt)
-	}
-}
-
-func (r *Config) updateSecretFromBase(configData *Config, baseConfigData *Config, key string) {
-	if configData.Config[key] == "" && baseConfigData.Config[key] != nil {
-		configData.Config[key] = baseConfigData.Config[key]
-	}
-}
-
 func (r *Config) secureCredentialValue(configData *Config, credentialKey string) {
 	if configData.Config[credentialKey] != nil {
 		configData.Config[credentialKey] = ""
