9e26d95456643e01f2e40773364ee3a7ae1893de
kirandevtn <kiran.yadav@devtron.ai>
2024-03-21 17:02:51 +0530

    Removed CI trigger plugin

--- scripts/sql/230_ci_trigger_plugin.down.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM plugin_step_variable WHERE plugin_step_id=(SELECT id FROM plugin_metadata WHERE name='Devtron CI Trigger v1.0.0');
-DELETE FROM plugin_step where plugin_id=(SELECT id FROM plugin_metadata WHERE name='Devtron CI Trigger v1.0.0' );
-DELETE FROM plugin_pipeline_script where id=(SELECT id FROM plugin_metadata WHERE name='Devtron CI Trigger v1.0.0');
-DELETE FROM plugin_stage_mapping where plugin_id=(SELECT id from plugin_metadata where name='Devtron CI Trigger v1.0.0');
-DELETE FROM plugin_metadata where name='Devtron CI Trigger v1.0.0';
