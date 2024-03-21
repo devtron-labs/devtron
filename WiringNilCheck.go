ee89c85601a0b806f87113ea64e7707894f2d94a
kartik-579 <kartik@devtron.ai>
2024-03-21 18:15:53 +0530

    refactored nil check

--- WiringNilCheck.go
+++ WiringNilCheck.go
@@ -2,55 +2,84 @@ package main
 
 import (
 	"fmt"
-	"go/ast"
-	"go/parser"
-	"go/token"
 	"log"
+	"reflect"
 	"strings"
+	"unsafe"
 )
 
 func CheckIfNilInWire() {
-	fset := token.NewFileSet()
-	node, err := parser.ParseFile(fset, "test_wire_gen.go", nil, parser.ParseComments)
+	app, err := InitializeApp()
 	if err != nil {
-		log.Fatalf("Error parsing file: %v", err)
+		log.Panic(err)
 	}
-
-	funcName := "InitializeAppTest"
-	var targetFunc *ast.FuncDecl
-	for _, decl := range node.Decls {
-		if fdecl, ok := decl.(*ast.FuncDecl); ok && fdecl.Name.Name == funcName {
-			targetFunc = fdecl
-			break
-		}
-	}
-	if targetFunc == nil {
-		log.Fatalf("Function %s not found in wire_gen.go", funcName)
-	}
-
-	ast.Inspect(targetFunc.Body, func(n ast.Node) bool {
-		if callExpr, ok := n.(*ast.CallExpr); ok {
-			if ident, ok := callExpr.Fun.(*ast.Ident); ok {
-				if strings.HasPrefix(ident.Name, "Provider") {
-					fmt.Println("found call to provider function", ident.Name)
-					response := Provider()
-					if hasNilFields(response) {
-						fmt.Println("error, Response object has nil fields")
-					}
-				}
-			}
-		}
-		return true
-	})
+	nilFieldsMap := make(map[string]bool)
+	checkNilFields(app, nilFieldsMap)
+	fmt.Println(nilFieldsMap)
 }
 
-func hasNilFields(response *Response) bool {
-	if response == nil {
-		return true
+func checkNilFields(obj interface{}, nilObjMap map[string]bool) {
+	val := reflect.ValueOf(obj)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
 	}
-	//to check for interface and add reflect support
-	if response.Field1 == nil || response.Field2 == nil {
+	if val.Kind() != reflect.Struct {
+		return
+	}
+	valName := val.Type().Name()
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fieldName := val.Type().Field(i).Name
+		pkgPath := val.Type().PkgPath()
+		if pkgPath != "main" && !strings.Contains(pkgPath, "devtron-labs/devtron") {
+			//package not from this repo, ignoring
+			continue
+		}
+		if !canFieldTypeBeNil(field) { // field can not be nil, skip
+			continue
+		} else if field.IsNil() { // check if the field is nil
+			mapEntry := fmt.Sprintf("%s.%s", valName, fieldName)
+			nilObjMap[mapEntry] = true
+			continue
+		}
+		if canSkipFieldStructCheck(fieldName) {
+			continue
+		}
+		if !isExported(fieldName) && !field.CanInterface() {
+			unexportedField := GetUnexportedField(field, fieldName)
+			checkNilFields(unexportedField, nilObjMap)
+		} else {
+			// Recurse
+			checkNilFields(field.Interface(), nilObjMap)
+		}
+	}
+}
+
+func canFieldTypeBeNil(field reflect.Value) bool {
+	kind := field.Kind()
+	switch kind {
+	case reflect.Chan, reflect.Func, reflect.Map, reflect.Pointer, reflect.UnsafePointer,
+		reflect.Interface, reflect.Slice:
 		return true
+	default: //other types can not be nil
+		return false
+	}
+}
+
+func canSkipFieldStructCheck(fieldName string) bool {
+	fieldName = strings.ToLower(fieldName)
+	for _, str := range []string{"logger", "dbconnection", "syncedenforcer"} {
+		if fieldName == str {
+			return true
+		}
 	}
 	return false
+}
+
+func GetUnexportedField(field reflect.Value, fieldName string) interface{} {
+	return reflect.NewAt(field.Type(), unsafe.Pointer(field.UnsafeAddr())).Elem().Interface()
+}
+
+func isExported(fieldName string) bool {
+	return strings.ToUpper(fieldName[0:1]) == fieldName[0:1]
 }
