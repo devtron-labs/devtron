d5f01f528c2336bc3400dda5904b7d9e0e8eb18c
Gireesh Naidu <gireesh@devtron.ai>
2024-04-04 20:20:57 +0530

    fix: deprecate latest flag from app_store application_version and increase varchar len for some columns

---
+++ scripts/sql/234_app_atore_application.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE app_store_application_version ALTER COLUMN "icon" TYPE VARCHAR(250);
+ALTER TABLE app_store_application_version ALTER COLUMN "source" TYPE VARCHAR(250);
+ALTER TABLE app_store_application_version ALTER COLUMN "home" TYPE VARCHAR(100);
+ALTER TABLE app_store_application_version ALTER COLUMN "name" TYPE VARCHAR(100);
+ALTER TABLE app_store_application_version ALTER COLUMN "digest" TYPE VARCHAR(250);
