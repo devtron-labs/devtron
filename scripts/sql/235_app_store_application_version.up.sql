1edd8c60f27634b021b478b0f0b2aee5801d792b
Gireesh Naidu <gireesh@devtron.ai>
2024-04-08 12:24:19 +0530

    fix: remove digest column length update

--- scripts/sql/235_app_store_application_version.up.sql
+++ scripts/sql/235_app_store_application_version.up.sql
@@ -2,4 +2,3 @@ ALTER TABLE app_store_application_version ALTER COLUMN "icon" TYPE VARCHAR(512);
 ALTER TABLE app_store_application_version ALTER COLUMN "source" TYPE VARCHAR(512);
 ALTER TABLE app_store_application_version ALTER COLUMN "home" TYPE VARCHAR(256);
 ALTER TABLE app_store_application_version ALTER COLUMN "name" TYPE VARCHAR(256);
-ALTER TABLE app_store_application_version ALTER COLUMN "digest" TYPE VARCHAR(256);
