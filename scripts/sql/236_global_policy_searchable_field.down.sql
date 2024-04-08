680802374a292c446f28cce08fc2ea15ec7f29c3
Shivam-nagar23 <shivam@devtron.ai>
2024-04-08 11:50:35 +0530

    remove unnecessary sql files

--- scripts/sql/236_global_policy_searchable_field.down.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE config_map_env_level DROP COLUMN IF EXISTS field_name;
-ALTER TABLE config_map_env_level DROP COLUMN IF EXISTS value_int;
-ALTER TABLE config_map_env_level DROP COLUMN IF EXISTS value_time_stamp;
