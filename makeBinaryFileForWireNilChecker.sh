432130fd0c04822d71712dc026e1c892adf47acf
Laeeqdev <mo@devtron.ai>
2024-04-09 15:32:49 +0530

    deleted binary file

--- makeBinaryFileForWireNilChecker.sh
+++
@@ -1,8 +0,0 @@
-echo "creating binary file..."
-cp auth_model.conf ./tests/integrationTesting
-chmod 777 WiringNilCheck.go
-go build -o wirenilcheckbinary .
-chmod 557 WiringNilCheck.go
-mv wirenilcheckbinary ./tests/integrationTesting
-cd tests/integrationTesting
-echo "binary file $(ls | grep wirenilcheckbinary) is created"
