49479b5bb86053c1c40ee3b9d3bbb3fc5e75fbdd
subhashish-devtron <subhashish@devtron.ai>
2024-04-10 13:43:31 +0530

    script exists check

--- scripts/sql/235_resource_scan.down.sql
+++ scripts/sql/235_resource_scan.down.sql
@@ -1,4 +1,4 @@
-ALTER TABLE public.scan_tool_execution_history_mapping DROP COLUMN error_message;
+ALTER TABLE public.scan_tool_execution_history_mapping DROP COLUMN IF EXISTS error_message;
 
 DELETE FROM public.scan_tool_step
 WHERE scan_tool_id = 3
