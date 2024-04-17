23538cd8a152e276b1d3a659f558c0083496c7c2
vishu247 <156403229+vishu247@users.noreply.github.com>
2024-04-17 11:16:34 +0530

    Update 239_bitbucket_runner_plugin.down.sql

--- scripts/sql/239_bitbucket_runner_plugin.down.sql
+++ scripts/sql/239_bitbucket_runner_plugin.down.sql
@@ -1,18 +1,8 @@
 DELETE FROM plugin_step_variable WHERE plugin_step_id =(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='BitBucket Runner Trigger v1.0.0' and ps."index"=1 and ps.deleted=false);
-DELETE FROM plugin_step WHERE plugin_id=(SELECT id FROM plugin_metadata WHERE name='BitBucket Runner Trigger v1.0.0');
+DELETE FROM plugin_step_variable where plugin_step_id =(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='BitBucket Runner Trigger v1.0.0' and ps."index"=1 and ps.deleted=false);
+DELETE FROM plugin_stage_mapping where plugin_id=(SELECT id from plugin_metadata where name='BitBucket Runner Trigger v1.0.0');
+DELETE FROM plugin_step where plugin_id=(SELECT id FROM plugin_metadata WHERE name='BitBucket Runner Trigger v1.0.0');
+DELETE FROM plugin_tag_relation WHERE plugin_id=(SELECT id FROM plugin_metadata WHERE name='BitBucket Runner Trigger v1.0.0');
 DELETE FROM pipeline_stage_step_variable where pipeline_stage_step_id in (select id from pipeline_stage_step where name ='BitBucket Runner Trigger v1.0.0');
-DELETE from pipeline_stage_step where name ='BitBucket Runner Trigger v1.0.0';
-DELETE FROM plugin_metadata WHERE name ='BitBucket Runner Trigger v1.0.0';
-
-
-DELETE FROM plugin_step_variable WHERE plugin_step_id IN (SELECT id FROM plugin_step WHERE plugin_id = (SELECT id FROM plugin_metadata WHERE name = 'BitBucket Runner Trigger v1.0.0'));
-
-DELETE FROM plugin_step WHERE plugin_id = (SELECT id FROM plugin_metadata WHERE name = 'BitBucket Runner Trigger v1.0.0');
-
-DELETE FROM plugin_pipeline_script WHERE id = (SELECT script_id FROM plugin_step WHERE plugin_id = (SELECT id FROM plugin_metadata WHERE name = 'BitBucket Runner Trigger v1.0.0'));
-
-DELETE FROM plugin_stage_mapping WHERE plugin_id = (SELECT id FROM plugin_metadata WHERE name = 'BitBucket Runner Trigger v1.0.0');
-
-DELETE FROM plugin_metadata WHERE name = 'BitBucket Runner Trigger v1.0.0';
-
-
+DELETE FROM pipeline_stage_step WHERE name ='BitBucket Runner Trigger v1.0.0';
+DELETE FROM plugin_metadata where name='BitBucket Runner Trigger v1.0.0';
