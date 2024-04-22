0c66e27b4dbeedcd34e24166247584252c9548cd
ashish sonam <ashishsonam@devtron.ai>
2024-04-22 18:46:13 +0530

    refactor

--- pkg/apiToken/ApiTokenService.go
+++ pkg/apiToken/ApiTokenService.go
@@ -180,8 +180,10 @@ func (impl ApiTokenServiceImpl) CreateApiToken(request *openapi.CreateApiTokenRe
 
 	// step-2 - Build email and version
 	email := fmt.Sprintf("%s%s", userBean.API_TOKEN_USER_EMAIL_PREFIX, name)
-	var tokenVersion int
-	var previousVersion int
+	var (
+		tokenVersion    int
+		previousVersion int
+	)
 	if apiTokenExists {
 		tokenVersion = apiToken.Version + 1
 		previousVersion = apiToken.Version
