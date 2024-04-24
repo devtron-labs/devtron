8ba308145dc078553ba98182011d1eeee3549a87
komalreddy3 <koukuntlakomalreddy3@gmail.com>
2024-04-24 17:49:44 +0530

    scripts fix unique

--- scripts/sql/239_auto_remediation.up.sql
+++ scripts/sql/239_auto_remediation.up.sql
@@ -4,13 +4,16 @@ CREATE TABLE "public"."watcher" (
                                     "name" varchar(100) NOT NULL,
                                     "desc" text ,
                                     "filter_expression" text NOT NULL,
-                                    "gvk" int4[],
+                                    "gvks" text[],
                                     "active" bool,
 
-                                    PRIMARY KEY ("id")
-                                        UNIQUE (name),
+                                    PRIMARY KEY ("id"),
+                                        UNIQUE ("name")
 
-)
+);
+CREATE UNIQUE INDEX "idx_unique_watcher_name"
+    ON watcher(name)
+    WHERE (active=true);
 
 CREATE SEQUENCE IF NOT EXISTS id_seq_trigger;
 CREATE TABLE "public"."trigger"(
@@ -20,23 +23,23 @@ CREATE TABLE "public"."trigger"(
                                    "data" JSON,
 
                                    CONSTRAINT trigger_watcher_id_fkey FOREIGN KEY ("watcher_id") REFERENCES "public"."watcher" ("id"),
-                                   PRIMARY KEY ("id"),
+                                   PRIMARY KEY ("id")
 
-)
+);
 
 CREATE SEQUENCE IF NOT EXISTS id_seq_intercepted_events;
 CREATE TABLE "public"."intercepted_events"(
                                               "id" integer NOT NULL default nextval('id_seq_intercepted_events'::regclass),
                                               "cluster_id" integer ,
-                                              "namespace" string,
-                                              "event" string,
-                                              "involved_object" string,
+                                              "namespace" varchar(100),
+                                              "event" text,
+                                              "involved_object" text,
                                               "intercepted_at" timestamptz,
                                               "status" varchar(255) , -- failure,success,inprogress
                                               "trigger_id" integer,
                                               "trigger_execution_id" integer,
 
                                               CONSTRAINT intercepted_events_trigger_id_fkey FOREIGN KEY ("trigger_id") REFERENCES "public"."trigger" ("id"),
-                                              PRIMARY KEY ("id"),
-)
+                                              PRIMARY KEY ("id")
+);
 
