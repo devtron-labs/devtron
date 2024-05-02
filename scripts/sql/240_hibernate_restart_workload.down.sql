0584a80db06176a8109beeb5ca844ed04057fca8
vishu247 <156403229+vishu247@users.noreply.github.com>
2024-05-02 17:47:30 +0530

    hibernate_restart_workload.down.sql

--- scripts/sql/240_hibernate_restart_workload.down.sql
+++ scripts/sql/240_hibernate_restart_workload.down.sql
@@ -1,4 +1,4 @@
-DELETE FROM plugin_step_variable where plugin_step_id =(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Devtron Hibernate/Restart Workload v1.0.0' and ps."index"=1 and ps.deleted=false);
+DELETE FROM plugin_step_variable where plugin_step_id in (SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Devtron Hibernate/Restart Workload v1.0.0' and ps."index"=1 and ps.deleted=false);
 DELETE FROM plugin_stage_mapping where plugin_id=(SELECT id from plugin_metadata where name='Devtron Hibernate/Restart Workload v1.0.0');
 DELETE FROM plugin_step where plugin_id=(SELECT id FROM plugin_metadata WHERE name='Devtron Hibernate/Restart Workload v1.0.0');
 DELETE FROM plugin_tag_relation WHERE plugin_id=(SELECT id FROM plugin_metadata WHERE name='Devtron Hibernate/Restart Workload v1.0.0');
