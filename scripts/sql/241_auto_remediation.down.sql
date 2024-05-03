663fe9a1d5eb5fa73639d1ab60c9d2f5de93b742
Gireesh Naidu <gireesh@devtron.ai>
2024-05-03 18:20:04 +0530

    feat: plugin script update

--- scripts/sql/241_auto_remediation.down.sql
+++ scripts/sql/241_auto_remediation.down.sql
@@ -18,8 +18,9 @@ DROP SEQUENCE IF EXISTS id_seq_k8s_event_watcher;
 
 
 -- PLUGIN DOWN SCRIPT
-DELETE FROM plugin_step_variable WHERE plugin_step_id=(SELECT id FROM plugin_metadata WHERE name='Custom Notification v1.0.0');
-DELETE FROM plugin_step where plugin_id=(SELECT id FROM plugin_metadata WHERE name='Custom Notification v1.0.0' );
-DELETE FROM plugin_pipeline_script where id=(SELECT id FROM plugin_metadata WHERE name='Custom Notification v1.0.0');
-DELETE FROM plugin_stage_mapping where plugin_id=(SELECT id from plugin_metadata where name='Custom Notification v1.0.0');
-DELETE FROM plugin_metadata where name='Custom Notification v1.0.0';
+DELETE FROM plugin_step_variable WHERE plugin_step_id =(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Custom Notifier v1.0.0' and ps."index"=1 and ps.deleted=false);
+DELETE FROM plugin_step WHERE plugin_id=(SELECT id FROM plugin_metadata WHERE name='Custom Notifier v1.0.0');
+DELETE FROM plugin_stage_mapping WHERE plugin_id =(SELECT id FROM plugin_metadata WHERE name='Custom Notifier v1.0.0');
+DELETE FROM pipeline_stage_step_variable WHERE pipeline_stage_step_id in (SELECT id FROM pipeline_stage_step where ref_plugin_id =(SELECT id from plugin_metadata WHERE name ='Custom Notifier v1.0.0'));
+DELETE FROM pipeline_stage_step where ref_plugin_id in (SELECT id from plugin_metadata WHERE name ='Custom Notifier v1.0.0');
+DELETE FROM plugin_metadata WHERE name ='Custom Notifier v1.0.0';
--- scripts/sql/241_auto_remediation.up.sql
+++ scripts/sql/241_auto_remediation.up.sql
@@ -1,4 +1,3 @@
-BEGIN;
 CREATE SEQUENCE IF NOT EXISTS id_seq_k8s_event_watcher;
 CREATE TABLE "public"."k8s_event_watcher" (
                                     "id" integer NOT NULL default nextval('id_seq_k8s_event_watcher'::regclass),
@@ -65,32 +64,32 @@ CREATE TABLE "public"."intercepted_event_execution"(
 
 -- Insert Plugin Metadata
 INSERT INTO "plugin_metadata" ("id", "name", "description", "type", "icon", "deleted", "created_on", "created_by", "updated_on", "updated_by")
-VALUES (nextval('id_seq_plugin_metadata'), 'Custom Notification v1.0.0', 'Send notifications', 'PRESET', 'https://static.vecteezy.com/system/resources/previews/009/394/760/non_2x/bell-icon-transparent-notification-free-png.png', 'f', 'now()', 1, 'now()', 1);
+VALUES (nextval('id_seq_plugin_metadata'), 'Custom Notifier v1.0.0', 'Send notifications', 'PRESET', 'https://static.vecteezy.com/system/resources/previews/009/394/760/non_2x/bell-icon-transparent-notification-free-png.png', 'f', 'now()', 1, 'now()', 1);
 
 -- Insert Plugin Stage Mapping
 INSERT INTO "plugin_stage_mapping" ("plugin_id", "stage_type", "created_on", "created_by", "updated_on", "updated_by")
-VALUES ((SELECT id FROM plugin_metadata WHERE name = 'Custom Notification v1.0.0'), 0, 'now()', 1, 'now()', 1);
+VALUES ((SELECT id FROM plugin_metadata WHERE name = 'Custom Notifier v1.0.0'), 0, 'now()', 1, 'now()', 1);
 
 -- Insert Plugin Script
 INSERT INTO "plugin_pipeline_script" ("id", "script", "type", "deleted", "created_on", "created_by", "updated_on", "updated_by")
 VALUES (
            nextval('id_seq_plugin_pipeline_script'),
-           E'#!/bin/bash
-    set -eo pipefail
+           E'#!/bin/sh
 
     # URL and token from environment variables
-    url="NOTIFICATION_URL"
-    token="NOTIFICATION_TOKEN"
-
+    echo "------------STARTING PLUGIN CUSTOM NOTIFIER------------"
+    echo "CONFIG_TYPE: $CONFIG_TYPE"
+    echo "CONFIG_NAME: $CONFIG_NAME"
+    echo "EMAIL_IDS: $EMAIL_IDS"
+    url = $NOTIFICATION_URL
+    token = $NOTIFICATION_TOKEN
+    echo "NOTIFICATION_URL: $NOTIFICATION_URL"
+    echo "NOTIFICATION_TOKEN: $NOTIFICATION_TOKEN"
+    echo "{"configType": \'"${CONFIG_TYPE}"\', "configName": \'"${CONFIG_NAME}"\', "emailIds": \'"$EMAIL_IDS"\'}"
+    echo \'{"configType":\'${CONFIG_TYPE}\',"configName":\'${CONFIG_NAME}\',"emailIds":\'${EMAIL_IDS}\'}\'
     # Make the API call
-    curl --location "$url" \
-    --header "token: $token" \
-    --header "Content-Type: application/json" \
-    --data-raw '{
-           "configType": "$CONFIG_TYPE",
-           "configName": "$CONFIG_NAME",
-           "emailIds": "$EMAIL_IDS"
-    }'
+    curl -X POST https://devtron-14.devtron.info/orchestrator/scoop/intercept-event/notify?clusterId=1 -H "token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTQ3NDU5NzUsImp0aSI6IjI4Y2Y2YWQzLTY0OGYtNDhjNS04MjIzLTFhZGY3MzExZDQ3YiIsImlhdCI6MTcxNDY1OTU3NSwiaXNzIjoiYXJnb2NkIiwibmJmIjoxNzE0NjU5NTc1LCJzdWIiOiJhZG1pbiJ9.xIWS9sRDYBqbh0GTWtt9nHlSD3GCzyJNjrNaOc1W_W8" -H "Content-Type: application/json" -d ''{"configType": "\'${CONFIG_TYPE}\'","configName":"\'${CONFIG_NAME}\'","emailIds":"\'${EMAIL_IDS}\'"}''
+    echo "------------FINISHING PLUGIN CUSTOM NOTIFIER------------"
     ',
            'SHELL',
            'f',
@@ -104,7 +103,7 @@ VALUES (
 INSERT INTO "plugin_step" ("id", "plugin_id", "name", "description", "index", "step_type", "script_id", "deleted", "created_on", "created_by", "updated_on", "updated_by")
 VALUES (
            nextval('id_seq_plugin_step'),
-           (SELECT id FROM plugin_metadata WHERE name = 'Custom Notification v1.0.0'),
+           (SELECT id FROM plugin_metadata WHERE name = 'Custom Notifier v1.0.0'),
            'Step 1',
            'Custom notifier',
            1,
@@ -118,57 +117,7 @@ VALUES (
        );
 
 -- Insert Plugin Step Variables
-INSERT INTO "plugin_step_variable" ("id", "plugin_step_id", "name", "format", "description", "is_exposed", "allow_empty_value", "variable_type", "value_type", "variable_step_index", "deleted", "created_on", "created_by", "updated_on", "updated_by")
-VALUES
-    (
-        nextval('id_seq_plugin_step_variable'),
-        (SELECT ps.id FROM plugin_metadata p INNER JOIN plugin_step ps ON ps.plugin_id = p.id WHERE p.name = 'Custom Notification v1.0.0' AND ps.index = 1 AND ps.deleted = false),
-        'CONFIG_TYPE',
-        'STRING',
-        'Type of the configuration',
-        true,
-        false,
-        'INPUT',
-        'NEW',
-        1,
-        'f',
-        'now()',
-        1,
-        'now()',
-        1
-    ),
-    (
-        nextval('id_seq_plugin_step_variable'),
-        (SELECT ps.id FROM plugin_metadata p INNER JOIN plugin_step ps ON ps.plugin_id = p.id WHERE p.name = 'Custom Notification v1.0.0' AND ps.index = 1 AND ps.deleted = false),
-        'CONFIG_NAME',
-        'STRING',
-        'Name of the configuration',
-        true,
-        false,
-        'INPUT',
-        'NEW',
-        1,
-        'f',
-        'now()',
-        1,
-        'now()',
-        1
-    ),
-    (
-        nextval('id_seq_plugin_step_variable'),
-        (SELECT ps.id FROM plugin_metadata p INNER JOIN plugin_step ps ON ps.plugin_id = p.id WHERE p.name = 'Custom Notification v1.0.0' AND ps.index = 1 AND ps.deleted = false),
-        'EMAIL_IDS',
-        'STRING',
-        'Email IDs to notify',
-        true,
-        true,
-        'INPUT',
-        'NEW',
-        1,
-        'f',
-        'now()',
-        1,
-        'now()',
-        1
-    );
-COMMIT;
+INSERT INTO plugin_step_variable (id,plugin_step_id,name,format, description,is_exposed,allow_empty_value,default_value,value,variable_type,value_type,previous_step_index,variable_step_index,variable_step_index_in_plugin,reference_variable_name,deleted,created_on,created_by,updated_on,updated_by)VALUES
+                                                                                                                                                                                                                                                                                                             (nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Custom Notifier v1.0.0' and ps."index"=1 and ps.deleted=false),'CONFIG_TYPE','STRING','Type of the configuration','t','f',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1),
+                                                                                                                                                                                                                                                                                                          (nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Custom Notifier v1.0.0' and ps."index"=1 and ps.deleted=false),'CONFIG_NAME','STRING','Name of the configuration','t','f',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1),
+                                                                                                                                                                                                                                                                                                             (nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Custom Notifier v1.0.0' and ps."index"=1 and ps.deleted=false),'EMAIL_IDS','STRING','Email IDs to notify','t','t',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1);
