07fa0338218d2d9ad66e4abeb87f77ea1ad6086c
komalreddy3 <koukuntlakomalreddy3@gmail.com>
2024-05-03 16:04:22 +0530

    plugin custom notifier

--- scripts/sql/241_custom_notification_plugin_down.sql
+++ scripts/sql/241_custom_notification_plugin_down.sql
@@ -1,5 +1,8 @@
-DELETE FROM plugin_step_variable WHERE plugin_step_id=(SELECT id FROM plugin_metadata WHERE name='Custom Notification v1.0.0');
-DELETE FROM plugin_step where plugin_id=(SELECT id FROM plugin_metadata WHERE name='Custom Notification v1.0.0' );
-DELETE FROM plugin_pipeline_script where id=(SELECT id FROM plugin_metadata WHERE name='Custom Notification v1.0.0');
-DELETE FROM plugin_stage_mapping where plugin_id=(SELECT id from plugin_metadata where name='Custom Notification v1.0.0');
-DELETE FROM plugin_metadata where name='Custom Notification v1.0.0';
+
+
+DELETE FROM plugin_step_variable WHERE plugin_step_id =(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Custom Notifier v1.0.0' and ps."index"=1 and ps.deleted=false);
+DELETE FROM plugin_step WHERE plugin_id=(SELECT id FROM plugin_metadata WHERE name='Custom Notifier v1.0.0');
+DELETE FROM plugin_stage_mapping WHERE plugin_id =(SELECT id FROM plugin_metadata WHERE name='Custom Notifier v1.0.0');
+DELETE FROM pipeline_stage_step_variable WHERE pipeline_stage_step_id in (SELECT id FROM pipeline_stage_step where ref_plugin_id =(SELECT id from plugin_metadata WHERE name ='Custom Notifier v1.0.0'));
+DELETE FROM pipeline_stage_step where ref_plugin_id in (SELECT id from plugin_metadata WHERE name ='Custom Notifier v1.0.0');
+DELETE FROM plugin_metadata WHERE name ='Custom Notifier v1.0.0';
