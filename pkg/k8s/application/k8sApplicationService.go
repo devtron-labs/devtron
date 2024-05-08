93aa4e22591cd33ce6063755e6e4af2bcb7b0435
Raunit Verma <raunit.verma@devtron.ai>
2024-05-08 15:06:53 +0530

    sync with main

--- pkg/k8s/application/k8sApplicationService.go
+++ pkg/k8s/application/k8sApplicationService.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 	"github.com/devtron-labs/common-lib/utils"
 	"github.com/devtron-labs/devtron/api/helm-app/gRPC"
-	"github.com/devtron-labs/devtron/enterprise/pkg/resourceFilter"
+	client "github.com/devtron-labs/devtron/api/helm-app/service"
 	util4 "github.com/devtron-labs/devtron/api/util"
 	"github.com/devtron-labs/devtron/enterprise/pkg/deploymentWindow"
 	"github.com/devtron-labs/devtron/enterprise/pkg/resourceFilter"
