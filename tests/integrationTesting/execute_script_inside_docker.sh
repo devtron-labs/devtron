0b04b3a515938a3ed0681756864f568d7a577f06
Laeeqdev <mo@devtron.ai>
2024-05-10 19:44:56 +0530

    resolved error

--- tests/integrationTesting/execute_script_inside_docker.sh
+++ tests/integrationTesting/execute_script_inside_docker.sh
@@ -5,5 +5,6 @@ touch output.env
 export NODE_IP_ADDRESS=$(kubectl get node  --no-headers  -o custom-columns=INTERNAL-IP:status.addresses[0].address)
 export PG_ADDR=$NODE_IP_ADDRESS
 export NATS_SERVER_HOST=nats://$NODE_IP_ADDRESS:30236
+go mod tidy
 go run .
 cp ./test/output.env ./tempfile
