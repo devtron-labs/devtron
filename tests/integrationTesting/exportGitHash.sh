fac18ab378189bbb4a2fdcef1a4f800d24c9a344
Laeeqdev <mo@devtron.ai>
2024-05-13 18:15:45 +0530

    move docker exec cmd into other folder

--- tests/integrationTesting/exportGitHash.sh
+++ tests/integrationTesting/exportGitHash.sh
@@ -1,9 +1,8 @@
 GITHASH=$GIT_HASH
 if [ -z "$GITHASH" ]; then
     echo "-------laeeeeeeeeeeeeq-------------"
-    git log
+    git log --pretty=format:'%h' -n 1
     echo "------laeeqenccddddd----------------"
-		echo "laeeq" $GITHASH
 fi;
 echo "GIT_HASH is" $GITHASH
 docker exec dind-test sh -c "export GIT_HASH=$GITHASH && mkdir test && cp -r wirenil/* test/ && ./test/tests/integrationTesting/exportEnvsExecuteWireNilChecker.sh"
