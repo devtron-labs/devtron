882270d800cfa38cc78d13b5416f48991ab156d1
Laeeqdev <mo@devtron.ai>
2024-05-13 18:33:02 +0530

    create a new file

--- tests/integrationTesting/exportGitHash.sh
+++ tests/integrationTesting/exportGitHash.sh
@@ -1,9 +1,5 @@
-GITHASH=123
 GITHASH=$GIT_HASH
 if [ -z "$GITHASH" ]; then
-    echo "-------laeeeeeeeeeeeeq-------------"
     export GITHASH=$(git log --pretty=format:'%h' -n 1)
-    echo "------laeeqenccddddd----------------"
 fi;
-echo "GIT_HASH is" $GITHASH
 docker exec dind-test sh -c "export GIT_HASH=$GITHASH && mkdir test && cp -r wirenil/* test/ && ./test/tests/integrationTesting/exportEnvsExecuteWireNilChecker.sh"
