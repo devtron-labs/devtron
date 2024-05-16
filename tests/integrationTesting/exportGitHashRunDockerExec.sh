61cf41aa997ce6edf59a834fc9dbd655946afe2c
Laeeqdev <mo@devtron.ai>
2024-05-16 12:54:28 +0530

    testing migrator

--- tests/integrationTesting/exportGitHashRunDockerExec.sh
+++ tests/integrationTesting/exportGitHashRunDockerExec.sh
@@ -1,5 +1,1 @@
-GITHASH=$GIT_HASH
-if [ -z "$GITHASH" ]; then
-    export GITHASH=$(git log --pretty=format:'%h' -n 1)
-fi;
 docker exec dind-test sh -c "export GIT_HASH=$GITHASH && mkdir test && cp -r wirenil/* test/ && ./test/tests/integrationTesting/exportEnvsExecuteWireNilChecker.sh"
