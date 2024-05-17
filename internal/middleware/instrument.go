02ac1abaef862fc762a383b8d0723ef39c291185
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2024-05-17 16:53:10 +0530

    print removed

--- internal/middleware/instrument.go
+++ internal/middleware/instrument.go
@@ -249,10 +249,6 @@ func PrometheusMiddleware(next http.Handler) http.Handler {
 		defer g.Dec()
 		d := NewDelegator(w, nil)
 		next.ServeHTTP(d, r)
-		fmt.Println(UniqueKeys)
-		fmt.Println("************")
-		fmt.Println(UrlLabelsMapping)
-		fmt.Println("************")
 		strArr := []string{path, method, strconv.Itoa(d.Status())}
 		if key, ok := UrlLabelsMapping[path]; !ok {
 			for _, labelKeys := range UniqueKeys {
