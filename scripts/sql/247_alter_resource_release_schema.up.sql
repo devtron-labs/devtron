b015bc98a8182d40aa7c1f62c10d7f35f9dc3293
kartik-579 <kartik@devtron.ai>
2024-05-17 16:58:09 +0530

    updated release action check policy migration

--- scripts/sql/247_alter_resource_release_schema.up.sql
+++ scripts/sql/247_alter_resource_release_schema.up.sql
@@ -227,3 +227,138 @@ UPDATE devtron_resource_schema set schema = '{
         "status"
     ]
 }' where devtron_resource_id=(select id from devtron_resource where kind = 'release');
+
+
+UPDATE global_policy set policy_json='{
+    "definitions":
+    [
+        {
+            "operationType": "patch",
+            "operationPaths":
+            [
+                "overview.description",
+                "overview.releaseNote",
+                "overview.tags",
+                "overview.name",
+                "overview.metadata",
+                "status.config.lock",
+                "status.config"
+            ],
+            "possibleFromStates":
+            [
+                {
+                    "configStatus": "draft",
+                    "dependencyArtifactStatus": "*",
+                    "rolloutStatus": "*",
+                    "lockStatus": "*"
+                },
+                {
+                    "configStatus": "readyForRelease",
+                    "dependencyArtifactStatus": "*",
+                    "rolloutStatus": "*",
+                    "lockStatus": "*"
+                },
+                {
+                    "configStatus": "hold",
+                    "dependencyArtifactStatus": "*",
+                    "rolloutStatus": "*",
+                    "lockStatus": "*"
+                }
+            ]
+        },
+        {
+            "operationType": "patch",
+            "operationPaths":
+            [
+                "dependency.applications",
+                "dependency.applications.image"
+            ],
+            "possibleFromStates":
+            [
+                {
+                    "configStatus": "draft",
+                    "dependencyArtifactStatus": "*",
+                    "rolloutStatus": "notDeployed",
+                    "lockStatus": "unLocked"
+                }
+            ]
+        },
+        {
+            "operationType": "patch",
+            "operationPaths":
+            [
+                "dependency.applications.instruction"
+            ],
+            "possibleFromStates":
+            [
+                {
+                    "configStatus": "draft",
+                    "dependencyArtifactStatus": "*",
+                    "rolloutStatus": "notDeployed",
+                    "lockStatus": "unLocked"
+                },
+                {
+                    "configStatus": "readyForRelease",
+                    "dependencyArtifactStatus": "*",
+                    "rolloutStatus": "notDeployed",
+                    "lockStatus": "unLocked"
+                },
+                {
+                    "configStatus": "hold",
+                    "dependencyArtifactStatus": "*",
+                    "rolloutStatus": "notDeployed",
+                    "lockStatus": "unLocked"
+                }
+            ]
+        },
+        {
+            "operationType": "deploymentTrigger",
+            "possibleFromStates":
+            [
+                {
+                    "configStatus": "readyForRelease",
+                    "dependencyArtifactStatus": "*",
+                    "rolloutStatus": "*",
+                    "lockStatus": "locked"
+                }
+            ]
+        },
+        {
+            "operationType": "delete",
+            "possibleFromStates":
+            [
+                {
+                    "configStatus": "draft",
+                    "dependencyArtifactStatus": "*",
+                    "rolloutStatus": "notDeployed",
+                    "lockStatus": "*"
+                },
+                {
+                    "configStatus": "readyForRelease",
+                    "dependencyArtifactStatus": "*",
+                    "rolloutStatus": "notDeployed",
+                    "lockStatus": "*"
+                },
+                {
+                    "configStatus": "hold",
+                    "dependencyArtifactStatus": "*",
+                    "rolloutStatus": "notDeployed",
+                    "lockStatus": "*"
+                },
+                {
+                    "configStatus": "rescind",
+                    "dependencyArtifactStatus": "*",
+                    "rolloutStatus": "*",
+                    "lockStatus": "*"
+                },
+                {
+                    "configStatus": "corrupted",
+                    "dependencyArtifactStatus": "*",
+                    "rolloutStatus": "*",
+                    "lockStatus": "*"
+                }
+            ]
+        }
+    ],
+    "consequence": "BLOCK"
+}' where policy_of='RELEASE_ACTION_CHECK';
