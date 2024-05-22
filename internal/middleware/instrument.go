2ff0a1eca12816183eab13b961c31c228e530e83
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2024-05-22 16:28:33 +0530

    code review comments incorporated

--- internal/middleware/instrument.go
+++ internal/middleware/instrument.go
@@ -21,6 +21,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/caarlos0/env"
+
 	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -54,53 +55,11 @@ func GetHttpLabels() (*HttpLabels, error) {
 	return cfg, err
 }
 
-/*
-getLabels processes URL and label information, storing mappings and unique keys.
+type UrlPaths struct {
+	Url   []string          `json:"url"`
+	Label map[string]string `json:"label"`
+}
 
-Example:
-Suppose the `GetHttpLabels` function returns the following data:
-
-	{
-	    "UrlPaths": `[
-	        {
-	            "url": ["http://example.com", "http://example.org"],
-	            "label": {"name": "Example", "type": "Website", "key1": "value1"}
-	        },
-	        {
-	            "url": ["http://example.com, http://example.net"],
-	            "label": {"name": "Example2", "type": "Site"}
-	        }
-	    ]`
-	}
-
-Output:
-Unique Keys:
-[
-
-	"path",
-	"method",
-	"status",
-	"name",
-	"type",
-	"key1"
-
-]
-URL Labels Mapping:
-
-	{
-	    "http://example.com": {"name": "Example2", "type": "Site", "key1": "value1"},
-	    "http://example.org": {"name": "Example", "type": "Website", "key1": "value1"},
-	    "http://example.net": {"name": "Example2", "type": "Site"}
-	}
-
-Explanation:
-1. The function calls `GetHttpLabels` to retrieve JSON data containing URL and label information.
-2. It unmarshals the JSON data into a slice of maps.
-3. It initializes maps to store URL to label mappings and unique label keys.
-4. It iterates over the data to process URLs and labels, merging them into `urlMappings`.
-5. It also collects unique keys while processing labels.
-6. The global variables `UrlLabelsMapping` and `UniqueKeys` are updated with the mappings and unique keys, respectively.
-*/
 func getLabels() []string {
 	httpLabels, err := GetHttpLabels()
 	if err != nil {
@@ -108,7 +67,7 @@ func getLabels() []string {
 		return nil
 	}
 
-	var data []map[string]interface{}
+	var data []UrlPaths
 	// Unmarshal JSON into the defined struct
 	if err := json.Unmarshal([]byte(httpLabels.UrlPaths), &data); err != nil {
 		fmt.Println("Error:", err)
@@ -120,27 +79,10 @@ func getLabels() []string {
 	// Define a map to store unique keys (labels)
 	keys := make(map[string]bool)
 
-	// Iterate through each object in the array
+	// Iterate through each UrlPaths object in the array
 	for _, obj := range data {
-		// Get the "url" array from each object
-		urls, ok := obj["url"].([]interface{})
-		if !ok {
-			continue // Skip if "url" is not an array
-		}
-
-		// Get the "label" object from each object
-		labelObj, ok := obj["label"].(map[string]interface{})
-		if !ok {
-			continue // Skip if "label" is not a map
-		}
-
 		// Iterate through each URL and add the mappings
-		for _, url := range urls {
-			urlStr, ok := url.(string)
-			if !ok {
-				continue // Skip if URL is not a string
-			}
-
+		for _, urlStr := range obj.Url {
 			// Handle URLs separated by commas and trim whitespace
 			urlArray := strings.Split(urlStr, ",")
 			for _, sUrl := range urlArray {
@@ -148,23 +90,15 @@ func getLabels() []string {
 
 				// If the URL is already in the map, merge the labels
 				if existingLabels, exists := urlMappings[sUrl]; exists {
-					for key, value := range labelObj {
-						strValue, ok := value.(string)
-						if !ok {
-							continue // Skip if label value is not a string
-						}
-						strValue = strings.TrimSpace(strValue)
+					for key, value := range obj.Label {
+						strValue := strings.TrimSpace(value)
 						existingLabels[key] = strValue
 					}
 				} else {
 					// Create a new map for each URL to store its labels
 					labels := make(map[string]string)
-					for key, value := range labelObj {
-						strValue, ok := value.(string)
-						if !ok {
-							continue // Skip if label value is not a string
-						}
-						strValue = strings.TrimSpace(strValue)
+					for key, value := range obj.Label {
+						strValue := strings.TrimSpace(value)
 						labels[key] = strValue
 					}
 					urlMappings[sUrl] = labels
@@ -173,22 +107,16 @@ func getLabels() []string {
 		}
 
 		// Add keys to the unique keys map
-		for key := range labelObj {
-			// Split the key by ":" to get the key part
-			parts := strings.Split(key, ":")
-			if len(parts) > 0 {
-				keys[parts[0]] = true
-			}
+		for key := range obj.Label {
+			keys[key] = true
 		}
 	}
 
 	UrlLabelsMapping = urlMappings
 
 	// Extract the unique keys
-	var uniqueKeys []string
-	uniqueKeys = append(uniqueKeys, path, method, status)
+	uniqueKeys := []string{path, method, status}
 	for key := range keys {
-		key = strings.TrimSpace(key)
 		uniqueKeys = append(uniqueKeys, key)
 	}
 	UniqueKeys = uniqueKeys
@@ -297,48 +225,6 @@ var TerminalSessionDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
 	Help: "duration of each terminal session",
 }, []string{"podName", "namespace", "clusterId"})
 
-/*
-PrometheusMiddleware
-Example:
-Suppose the incoming HTTP request has the following details:
-- Path: "/api/data"
-- Method: "GET"
-
-And the `UrlLabelsMapping` contains:
-
-	{
-	    "/api/data": {"name": "DataEndpoint", "type": "API"}
-	}
-
-Output:
-The Prometheus metrics for the request would be recorded with the following labels:
-- path: "/api/data"
-- method: "GET"
-- status: "200"
-- name: "DataEndpoint"
-- type: "API"
-
-Explanation:
-1. The middleware function PrometheusMiddleware takes an `http.Handler` and returns a new handler that includes Prometheus metrics.
-2. Inside the handler function:
-  - It captures the start time of the request.
-  - It retrieves the current route and path template using `mux.CurrentRoute`.
-  - It initializes a gauge `g` to track the current number of requests.
-  - It uses `NewDelegator` to wrap the response writer, allowing the status code to be captured.
-  - It serves the request using the next handler in the chain.
-  - It constructs a `strArr` slice to hold label values for Prometheus metrics.
-  - It checks if the path exists in `UrlLabelsMapping`:
-  - If it does, it appends the label values from the mapping.
-  - If it doesn't, it appends empty strings for missing labels.
-  - It records the duration of the request using `httpDuration` metric.
-  - It increments the request counter `requestCounter` with the path, method, and status code.
-
-Metrics:
-- `httpDuration`: Observes the duration of the HTTP request.
-- `requestCounter`: Counts the number of requests processed.
-
-Note: The global variables `UrlLabelsMapping` and `UniqueKeys` are used for label extraction.
-*/
 func PrometheusMiddleware(next http.Handler) http.Handler {
 	// prometheus.MustRegister(requestCounter)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
