9488b96deb0bf509875f0423006b00c9098764da
ayu-devtron <ayush.singh@devtron.ai>
2024-06-04 13:14:56 +0530

    using memoization in array diff

--- enterprise/pkg/lockConfiguration/LockConfigurationService.go
+++ enterprise/pkg/lockConfiguration/LockConfigurationService.go
@@ -7,7 +7,9 @@ package lockConfiguration
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/caarlos0/env/v6"
 	"github.com/devtron-labs/devtron/enterprise/pkg/lockConfiguration/bean"
+	"github.com/devtron-labs/devtron/internal/sql/models"
 	"github.com/devtron-labs/devtron/internal/util"
 	"github.com/devtron-labs/devtron/pkg/auth/user"
 	"github.com/devtron-labs/devtron/pkg/sql"
@@ -32,17 +34,24 @@ type LockConfigurationServiceImpl struct {
 	lockConfigurationRepository LockConfigurationRepository
 	userService                 user.UserService
 	mergeUtil                   util.MergeUtil
+	userTerminalSessionConfig   *models.UserTerminalSessionConfig
 }
 
 func NewLockConfigurationServiceImpl(logger *zap.SugaredLogger,
 	lockConfigurationRepository LockConfigurationRepository,
 	userService user.UserService,
 	mergeUtil util.MergeUtil) *LockConfigurationServiceImpl {
+	config := &models.UserTerminalSessionConfig{}
+	err := env.Parse(config)
+	if err != nil {
+		logger.Warnw("error in initialising UserTerminalSessionConfig but continuing with rest of initialisation", err)
+	}
 	return &LockConfigurationServiceImpl{
 		logger:                      logger,
 		lockConfigurationRepository: lockConfigurationRepository,
 		userService:                 userService,
 		mergeUtil:                   mergeUtil,
+		userTerminalSessionConfig:   config,
 	}
 }
 
@@ -158,7 +167,8 @@ func (impl LockConfigurationServiceImpl) HandleLockConfiguration(currentConfig, 
 		impl.logger.Errorw("Error in umMarshal data", "err", err, "currentConfig", currentConfig)
 		return nil, err
 	}
-	allChanges, deletedMap, addedMap, modifiedMap, containChangesInArray, deletedPaths := getDiffJson(savedConfigMap, currentConfigMap, "")
+	impl.logger.Infow("env var ARRAY_DIFF_MEMOIZATION=", impl.userTerminalSessionConfig.ArrayDiffMemoization)
+	allChanges, deletedMap, addedMap, modifiedMap, containChangesInArray, deletedPaths := impl.getDiffJson(savedConfigMap, currentConfigMap, "")
 	var isLockConfigError bool
 	if lockConfig.ContainAllowedPaths {
 		// Will add in v2 of this feature
@@ -208,45 +218,10 @@ func checkLockedChanges(currentConfig, savedConfig string, lockedConfigJsonPaths
 	return false
 }
 
-func checkForLockedArray(savedConfigArray, currentConfigArray []interface{}) ([]interface{}, []interface{}, []interface{}, []interface{}) {
-	var lockedArray, deletedArray, addedArray, modifiedArray []interface{}
-	var key int
-	for key, _ = range savedConfigArray {
-		if key >= len(currentConfigArray) {
-			deletedArray = append(deletedArray, savedConfigArray[key])
-			continue
-		}
-		if !reflect.DeepEqual(savedConfigArray[key], currentConfigArray[key]) {
-			switch reflect.TypeOf(savedConfigArray[key]).Kind() {
-			case reflect.Map:
-				locked, deleted, added, modified, _, _ := getDiffJson(savedConfigArray[key].(map[string]interface{}), currentConfigArray[key].(map[string]interface{}), "/")
-				appendMapValueToArray(lockedArray, locked)
-				appendMapValueToArray(deletedArray, deleted)
-				appendMapValueToArray(addedArray, added)
-				appendMapValueToArray(modifiedArray, modified)
-			case reflect.Array, reflect.Slice:
-				locked, deleted, added, modified := checkForLockedArray(savedConfigArray[key].([]interface{}), currentConfigArray[key].([]interface{}))
-				appendArrayValueToArray(lockedArray, locked)
-				appendArrayValueToArray(deletedArray, deleted)
-				appendArrayValueToArray(addedArray, added)
-				appendArrayValueToArray(modifiedArray, modified)
-			default:
-				lockedArray = append(lockedArray, currentConfigArray[key])
-				modifiedArray = append(modifiedArray, currentConfigArray[key])
-			}
-		}
-	}
-	for key1, _ := range currentConfigArray {
-		if key1 <= key {
-			continue
-		}
-		lockedArray = append(lockedArray, currentConfigArray[key1])
-		addedArray = append(addedArray, currentConfigArray[key1])
-	}
-	return lockedArray, deletedArray, addedArray, modifiedArray
-}
-
-func getMinOperationsToChangeArray(word1 []interface{}, word2 []interface{}, i int, j int) (int, []interface{}, []interface{}, []interface{}) {
+func (impl LockConfigurationServiceImpl) getMinOperationsToChangeArrayWithMemoization(word1 []interface{}, word2 []interface{}, i int, j int,
+	memoizedArrayI *[][][][]interface{}, memoizedIntArrayI *[][]int,
+	memoizedArrayM *[][][][]interface{}, memoizedIntArrayM *[][]int,
+	memoizedArrayD *[][][][]interface{}, memoizedIntArrayD *[][]int) (int, []interface{}, []interface{}, []interface{}) {
 	var added, modified, deleted []interface{}
 	if i < 0 {
 		return j + 1, word2[0 : j+1], modified, deleted
@@ -255,21 +230,77 @@ func getMinOperationsToChangeArray(word1 []interface{}, word2 []interface{}, i i
 		return i + 1, added, modified, word1[0 : i+1]
 	}
 	if reflect.DeepEqual(word1[i], word2[j]) {
-		val, added, modified, deleted := getMinOperationsToChangeArray(word1, word2, i-1, j-1)
+		val, added, modified, deleted := impl.getMinOperationsToChangeArrayWithMemoization(word1, word2, i-1, j-1, memoizedArrayI, memoizedIntArrayI, memoizedArrayM, memoizedIntArrayM, memoizedArrayD, memoizedIntArrayD)
 		return val, added, modified, deleted
 	}
 	const MaxUint = ^uint(0)
 
 	ans := int(MaxUint >> 1)
-	insert, addedI, modifiedI, deletedI := getMinOperationsToChangeArray(word1, word2, i, j-1) // insert
+
+	// insert
+	insert := (*memoizedIntArrayI)[i][j]
+	addedI := (*memoizedArrayI)[i][j][0]
+	modifiedI := (*memoizedArrayI)[i][j][1]
+	deletedI := (*memoizedArrayI)[i][j][2]
+	if insert == -1 {
+
+		insert, addedI, modifiedI, deletedI = impl.getMinOperationsToChangeArrayWithMemoization(word1, word2, i, j-1,
+			memoizedArrayI, memoizedIntArrayI,
+			memoizedArrayM, memoizedIntArrayM,
+			memoizedArrayD, memoizedIntArrayD) // insert
+
+		//insert = tmpInsert
+		(*memoizedIntArrayI)[i][j] = insert
+		(*memoizedArrayI)[i][j][0] = addedI
+		(*memoizedArrayI)[i][j][1] = modifiedI
+		(*memoizedArrayI)[i][j][2] = deletedI
+
+	}
 	if 1+insert < ans {
 		ans = 1 + insert
 	}
-	deletedV, addedD, modifiedD, deletedD := getMinOperationsToChangeArray(word1, word2, i-1, j) //delete
+
+	//delete
+	deletedV := (*memoizedIntArrayD)[i][j]
+	addedD := (*memoizedArrayD)[i][j][0]
+	modifiedD := (*memoizedArrayD)[i][j][1]
+	deletedD := (*memoizedArrayD)[i][j][2]
+	if deletedV == -1 {
+		deletedV, addedD, modifiedD, deletedD = impl.getMinOperationsToChangeArrayWithMemoization(word1, word2, i-1, j,
+			memoizedArrayI, memoizedIntArrayI,
+			memoizedArrayM, memoizedIntArrayM,
+			memoizedArrayD, memoizedIntArrayD) //delete
+
+		//deletedV = tmpDeletedV
+		(*memoizedIntArrayD)[i][j] = deletedV
+		(*memoizedArrayD)[i][j][0] = addedD
+		(*memoizedArrayD)[i][j][1] = modifiedD
+		(*memoizedArrayD)[i][j][2] = deletedD
+	}
 	if 1+deletedV < ans {
 		ans = 1 + deletedV
 	}
-	modifiedV, addedM, modifiedM, deletedM := getMinOperationsToChangeArray(word1, word2, i-1, j-1) //replace
+
+	//replace
+	modifiedV := (*memoizedIntArrayM)[i][j]
+	addedM := (*memoizedArrayM)[i][j][0]
+	modifiedM := (*memoizedArrayM)[i][j][1]
+	deletedM := (*memoizedArrayM)[i][j][2]
+	if modifiedV == -1 {
+
+		modifiedV, addedM, modifiedM, deletedM = impl.getMinOperationsToChangeArrayWithMemoization(word1, word2, i-1, j-1,
+			memoizedArrayI, memoizedIntArrayI,
+			memoizedArrayM, memoizedIntArrayM,
+			memoizedArrayD, memoizedIntArrayD) //replace
+
+		//modifiedV = tmpModifiedV
+		(*memoizedIntArrayM)[i][j] = modifiedV
+		(*memoizedArrayM)[i][j][0] = addedM
+		(*memoizedArrayM)[i][j][1] = modifiedM
+		(*memoizedArrayM)[i][j][2] = deletedM
+
+	}
+
 	if 1+modifiedV < ans {
 		ans = 1 + modifiedV
 	}
@@ -280,7 +311,7 @@ func getMinOperationsToChangeArray(word1 []interface{}, word2 []interface{}, i i
 			modified = append(modified, modifiedI...)
 			deleted = append(deleted, deletedI...)
 		} else {
-			val := getModifiedValue(word1, word2, i, j)
+			val := impl.getModifiedValue(word1, word2, i, j)
 			modified = append(modified, val)
 			added = append(added, addedM...)
 			modified = append(modified, modifiedM...)
@@ -293,7 +324,7 @@ func getMinOperationsToChangeArray(word1 []interface{}, word2 []interface{}, i i
 			modified = append(modified, modifiedD...)
 			deleted = append(deleted, deletedD...)
 		} else {
-			val := getModifiedValue(word1, word2, i, j)
+			val := impl.getModifiedValue(word1, word2, i, j)
 			modified = append(modified, val)
 			added = append(added, addedM...)
 			modified = append(modified, modifiedM...)
@@ -303,25 +334,109 @@ func getMinOperationsToChangeArray(word1 []interface{}, word2 []interface{}, i i
 	return ans, added, modified, deleted
 }
 
-func getModifiedValue(word1 []interface{}, word2 []interface{}, i int, j int) interface{} {
+func (impl LockConfigurationServiceImpl) getMinOperationsToChangeArray(word1 []interface{}, word2 []interface{}, i int, j int) (int, []interface{}, []interface{}, []interface{}) {
+	var added, modified, deleted []interface{}
+	if i < 0 {
+		return j + 1, word2[0 : j+1], modified, deleted
+	}
+	if j < 0 {
+		return i + 1, added, modified, word1[0 : i+1]
+	}
+	if reflect.DeepEqual(word1[i], word2[j]) {
+		val, added, modified, deleted := impl.getMinOperationsToChangeArray(word1, word2, i-1, j-1)
+		return val, added, modified, deleted
+	}
+	const MaxUint = ^uint(0)
+
+	ans := int(MaxUint >> 1)
+	insert, addedI, modifiedI, deletedI := impl.getMinOperationsToChangeArray(word1, word2, i, j-1) // insert
+	if 1+insert < ans {
+		ans = 1 + insert
+	}
+	deletedV, addedD, modifiedD, deletedD := impl.getMinOperationsToChangeArray(word1, word2, i-1, j) //delete
+	if 1+deletedV < ans {
+		ans = 1 + deletedV
+	}
+	modifiedV, addedM, modifiedM, deletedM := impl.getMinOperationsToChangeArray(word1, word2, i-1, j-1) //replace
+	if 1+modifiedV < ans {
+		ans = 1 + modifiedV
+	}
+	if insert < deletedV {
+		if insert < modifiedV {
+			added = append(added, word2[j])
+			added = append(added, addedI...)
+			modified = append(modified, modifiedI...)
+			deleted = append(deleted, deletedI...)
+		} else {
+			val := impl.getModifiedValue(word1, word2, i, j)
+			modified = append(modified, val)
+			added = append(added, addedM...)
+			modified = append(modified, modifiedM...)
+			deleted = append(deleted, deletedM...)
+		}
+	} else {
+		if deletedV < modifiedV {
+			deleted = append(deleted, word1[i])
+			added = append(added, addedD...)
+			modified = append(modified, modifiedD...)
+			deleted = append(deleted, deletedD...)
+		} else {
+			val := impl.getModifiedValue(word1, word2, i, j)
+			modified = append(modified, val)
+			added = append(added, addedM...)
+			modified = append(modified, modifiedM...)
+			deleted = append(deleted, deletedM...)
+		}
+	}
+	return ans, added, modified, deleted
+}
+
+func (impl LockConfigurationServiceImpl) getModifiedValue(word1 []interface{}, word2 []interface{}, i int, j int) interface{} {
 	switch reflect.TypeOf(word1[i]).Kind() {
 	case reflect.Map:
 		savedConfig := copyMap(word1[i].(map[string]interface{}))
 		currentConfig := copyMap(word2[j].(map[string]interface{}))
-		locked, _, _, _, _, _ := getDiffJson(savedConfig, currentConfig, "/")
+		locked, _, _, _, _, _ := impl.getDiffJson(savedConfig, currentConfig, "/")
 		return locked
 	case reflect.Array, reflect.Slice:
-		locked, _, _, _ := getArrayDiff(word1[i].([]interface{}), word2[j].([]interface{}))
+		locked, _, _, _ := impl.getArrayDiff(word1[i].([]interface{}), word2[j].([]interface{}))
 		return locked
 	default:
 		return word2[j]
 	}
 }
 
-func getArrayDiff(word1 []interface{}, word2 []interface{}) ([]interface{}, []interface{}, []interface{}, []interface{}) {
+func (impl LockConfigurationServiceImpl) getArrayDiff(word1 []interface{}, word2 []interface{}) ([]interface{}, []interface{}, []interface{}, []interface{}) {
+	if impl.userTerminalSessionConfig.ArrayDiffMemoization {
+		return impl.getArrayDiffWithMemoization(word1, word2)
+	} else {
+		return impl.getArrayDiffWithoutMemoization(word1, word2)
+	}
+}
+
+func (impl LockConfigurationServiceImpl) getArrayDiffWithMemoization(word1 []interface{}, word2 []interface{}) ([]interface{}, []interface{}, []interface{}, []interface{}) {
 	l1 := len(word1)
 	l2 := len(word2)
-	_, added, modified, deleted := getMinOperationsToChangeArray(word1, word2, l1-1, l2-1)
+	memoizedArrayI := initializeAndGetArray(l1, l2)
+	memoizedIntArrayI := initializeAndGetIntArray(l1, l2)
+	memoizedArrayM := initializeAndGetArray(l1, l2)
+	memoizedIntArrayM := initializeAndGetIntArray(l1, l2)
+	memoizedArrayD := initializeAndGetArray(l1, l2)
+	memoizedIntArrayD := initializeAndGetIntArray(l1, l2)
+	_, added, modified, deleted := impl.getMinOperationsToChangeArrayWithMemoization(word1, word2, l1-1, l2-1, &memoizedArrayI, &memoizedIntArrayI,
+		&memoizedArrayM, &memoizedIntArrayM,
+		&memoizedArrayD, &memoizedIntArrayD)
+	var lockedArray []interface{}
+	lockedArray = append(lockedArray, added...)
+	lockedArray = append(lockedArray, modified...)
+	lockedArray = append(lockedArray, deleted...)
+	return lockedArray, added, modified, deleted
+}
+
+func (impl LockConfigurationServiceImpl) getArrayDiffWithoutMemoization(word1 []interface{}, word2 []interface{}) ([]interface{}, []interface{}, []interface{}, []interface{}) {
+	l1 := len(word1)
+	l2 := len(word2)
+	_, added, modified, deleted := impl.getMinOperationsToChangeArray(word1, word2, l1-1, l2-1)
 	var lockedArray []interface{}
 	lockedArray = append(lockedArray, added...)
 	lockedArray = append(lockedArray, modified...)
@@ -342,7 +457,7 @@ func copyMap(map1 map[string]interface{}) map[string]interface{} {
 // The second contains the deleted values
 // The third contains the added values
 // The fourth contains the modified values
-func getDiffJson(savedConfigMap, currentConfigMap map[string]interface{}, path string) (map[string]interface{}, map[string]interface{}, map[string]interface{}, map[string]interface{}, bool, []string) {
+func (impl LockConfigurationServiceImpl) getDiffJson(savedConfigMap, currentConfigMap map[string]interface{}, path string) (map[string]interface{}, map[string]interface{}, map[string]interface{}, map[string]interface{}, bool, []string) {
 	// Store all the changes
 	lockedMap := make(map[string]interface{})
 	deletedMap := make(map[string]interface{})
@@ -376,7 +491,7 @@ func getDiffJson(savedConfigMap, currentConfigMap map[string]interface{}, path s
 					modifiedMap[key] = currentConfigMap[key]
 					continue
 				}
-				locked, deleted, added, modified, isSaveEligibleChangesDisabled, deletedPaths := getDiffJson(savedConfigMap[key].(map[string]interface{}), currentConfigMap[key].(map[string]interface{}), path+"/"+key)
+				locked, deleted, added, modified, isSaveEligibleChangesDisabled, deletedPaths := impl.getDiffJson(savedConfigMap[key].(map[string]interface{}), currentConfigMap[key].(map[string]interface{}), path+"/"+key)
 				assignMapValueToMap(lockedMap, locked, key)
 				assignMapValueToMap(deletedMap, deleted, key)
 				assignMapValueToMap(addedMap, added, key)
@@ -386,7 +501,7 @@ func getDiffJson(savedConfigMap, currentConfigMap map[string]interface{}, path s
 					disableSaveEligibleChanges = true
 				}
 			case reflect.Array, reflect.Slice:
-				locked, added, modified, deleted := getArrayDiff(savedConfigMap[key].([]interface{}), currentConfigMap[key].([]interface{}))
+				locked, added, modified, deleted := impl.getArrayDiff(savedConfigMap[key].([]interface{}), currentConfigMap[key].([]interface{}))
 				assignArrayValueToMap(lockedMap, locked, key)
 				assignArrayValueToMap(deletedMap, deleted, key)
 				if len(deleted) != 0 {
@@ -445,4 +560,33 @@ func assignMapValueToMap(mp map[string]interface{}, val map[string]interface{}, 
 	if len(val) != 0 {
 		mp[key] = val
 	}
+}
+
+func initializeAndGetArray(l1 int, l2 int) [][][][]interface{} {
+	memoizedArray := make([][][][]interface{}, l1)
+
+	for i := 0; i < l1; i++ {
+		memoizedArray[i] = make([][][]interface{}, l2)
+		for j := 0; j < l2; j++ {
+			memoizedArray[i][j] = make([][]interface{}, 3)
+			for k := 0; k < 3; k++ {
+				memoizedArray[i][j][k] = []interface{}{}
+			}
+		}
+	}
+
+	return memoizedArray
+}
+
+func initializeAndGetIntArray(l1 int, l2 int) [][]int {
+	memoizedArray := make([][]int, l1)
+
+	for i := 0; i < l1; i++ {
+		memoizedArray[i] = make([]int, l2)
+		for k := 0; k < l2; k++ {
+			memoizedArray[i][k] = -1
+		}
+	}
+
+	return memoizedArray
 }
