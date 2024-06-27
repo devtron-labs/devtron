5349126b7d5f1483789f90a7e00ba97838780b76
Prakash Kumar <prakash.kumar@devtron.ai>
2024-06-27 21:47:46 +0530

    wip: route change

--- api/router/GlobalPluginRouter.go
+++ api/router/GlobalPluginRouter.go
@@ -60,7 +60,7 @@ func (impl *GlobalPluginRouterImpl) initGlobalPluginRouter(globalPluginRouter *m
 	globalPluginRouter.Path("/list/v2").
 		HandlerFunc(impl.globalPluginRestHandler.ListAllPluginsV2).Methods("GET")
 
-	globalPluginRouter.Path("/detail/v2").
+	globalPluginRouter.Path("/list/detail/v2").
 		HandlerFunc(impl.globalPluginRestHandler.GetPluginDetailByIds).Methods("GET")
 
 	globalPluginRouter.Path("/list/tags").
