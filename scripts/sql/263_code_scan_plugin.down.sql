c1486af64d685bfbd5c83be219c7f0b209fbf932
Prakash Kumar <prakash.kumar@devtron.ai>
2024-07-04 12:21:45 +0530

    empty the code and image scan script

--- scripts/sql/263_code_scan_plugin.down.sql
+++ scripts/sql/263_code_scan_plugin.down.sql
@@ -1,7 +1,1 @@
-DELETE FROM plugin_step_variable WHERE plugin_step_id =(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Code Scan' and ps."index"=1 and ps.deleted=false);
-DELETE FROM plugin_step WHERE plugin_id = (SELECT id FROM plugin_metadata WHERE name='Code Scan');
-DELETE FROM plugin_stage_mapping WHERE plugin_id =(SELECT id FROM plugin_metadata WHERE name='Code Scan');
-DELETE FROM plugin_tag_relation WHERE plugin_id =(SELECT id FROM plugin_metadata WHERE name='Code Scan');
-DELETE FROM pipeline_stage_step where ref_plugin_id in (SELECT id from plugin_metadata WHERE name ='Code Scan');
-DELETE from plugin_pipeline_script where id = (SELECT script_id from plugin_step WHERE plugin_id=(SELECT id FROM plugin_metadata WHERE name='Code Scan'));
-DELETE FROM plugin_metadata WHERE name ='Code Scan';
+--empty script to keep common script number
