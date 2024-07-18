8cc591d308e25e5bbfd1ad93bfb245c5032679e6
Gireesh Naidu <gireesh@devtron.ai>
2024-07-18 18:54:35 +0530

    fix: ci pipeline material updates w.r.t linked ci

--- pkg/pipeline/CiCdPipelineOrchestrator.go
+++ pkg/pipeline/CiCdPipelineOrchestrator.go
@@ -641,60 +641,11 @@ func (impl CiCdPipelineOrchestratorImpl) PatchMaterialValue(createRequest *bean.
 		}
 	}
 
-	//if len(childrenCiPipelineIds) > 0 {
-	//
-	//	ciPipelineMaterials, err := impl.ciPipelineMaterialRepository.FindByCiPipelineIdsIn(childrenCiPipelineIds)
-	//	if err != nil {
-	//		impl.logger.Errorw("error in fetching  ciPipelineMaterials", "err", err)
-	//		return nil, err
-	//	}
-	//	parentMaterialsMap := make(map[int]*bean.CiMaterial)
-	//	for _, material := range createRequest.CiMaterial {
-	//		parentMaterialsMap[material.GitMaterialId] = material
-	//	}
-	//	var linkedMaterials []*pipelineConfig.CiPipelineMaterial
-	//	for _, ciPipelineMaterial := range ciPipelineMaterials {
-	//
-	//		// context: linked-ci
-	//		// we never come here for child
-	//
-	//		// for parent, three cases are possible
-	//
-	//		// case-1: new git-material is added
-	//		//	-> we cannot find this in child, but can be found at parent
-	//		//     this should be added at child
-	//
-	//		// case-2: existing material is updated
-	//		//  -> we can find this in child and parent
-	//		//     this should be updated at child
-	//
-	//		// case-3: material was deleted
-	//		//	-> we can find at child but cannot find at parent
-	//		//     this should be deleted at child
-	//
-	//		if parentMaterial, ok := parentMaterialsMap[ciPipelineMaterial.GitMaterialId]; ok {
-	//			pipelineMaterial := &pipelineConfig.CiPipelineMaterial{
-	//				Id:            ciPipelineMaterial.Id,
-	//				Value:         parentMaterial.Source.Value,
-	//				Active:        createRequest.Active,
-	//				Regex:         parentMaterial.Source.Regex,
-	//				AuditLog:      sql.AuditLog{UpdatedBy: userId, UpdatedOn: time.Now(), CreatedOn: time.Now(), CreatedBy: userId},
-	//				Type:          parentMaterial.Source.Type,
-	//				GitMaterialId: parentMaterial.GitMaterialId,
-	//				CiPipelineId:  ciPipelineMaterial.CiPipelineId,
-	//			}
-	//			linkedMaterials = append(linkedMaterials, pipelineMaterial)
-	//		} else {
-	//			//impl.logger.Errorw("material not fount in patent", "gitMaterialId", ciPipelineMaterial.GitMaterialId)
-	//			//return nil, fmt.Errorf("error while updating linked pipeline")
-	//		}
-	//	}
-	//	err = impl.ciPipelineMaterialRepository.Update(tx, linkedMaterials...)
-	//	if err != nil {
-	//		return nil, err
-	//	}
-	//}
-
+	err = impl.syncChildrenCiPipelineMaterialsWithParent(tx, childrenCiPipelineIds, createRequest.CiMaterial, userId)
+	if err != nil {
+		impl.logger.Errorw("error in syncing ci pipleine materials for linked child pipelines", "childrenCiPipelineIds", childrenCiPipelineIds, "err", err)
+		return nil, err
+	}
 	err = tx.Commit()
 	if err != nil {
 		return nil, err
@@ -703,7 +654,119 @@ func (impl CiCdPipelineOrchestratorImpl) PatchMaterialValue(createRequest *bean.
 	return createRequest, nil
 }
 
-// todo: extract common logic into separate func and use that here and in switchService
+func (impl CiCdPipelineOrchestratorImpl) syncChildrenCiPipelineMaterialsWithParent(tx *pg.Tx, childrenCiPipelineIds []int, parentCiMaterials []*bean.CiMaterial, userId int32) error {
+	if len(childrenCiPipelineIds) == 0 {
+		return nil
+	}
+
+	childrenCiPipelineMaterials, err := impl.ciPipelineMaterialRepository.FindByCiPipelineIdsIn(childrenCiPipelineIds)
+	if err != nil {
+		impl.logger.Errorw("error in fetching  ciPipelineMaterials", "err", err)
+		return err
+	}
+
+	childrenMaterialsMap := make(map[int][]*pipelineConfig.CiPipelineMaterial)
+	for _, material := range childrenCiPipelineMaterials {
+		if _, ok := childrenMaterialsMap[material.CiPipelineId]; !ok {
+			childrenMaterialsMap[material.CiPipelineId] = make([]*pipelineConfig.CiPipelineMaterial, 0)
+		}
+		childrenMaterialsMap[material.CiPipelineId] = append(childrenMaterialsMap[material.CiPipelineId], material)
+	}
+
+	// context: linked-ci
+	// we never come here for child
+
+	var creatablePipelineMaterials, updatablePipelineMaterials []*pipelineConfig.CiPipelineMaterial
+	for childPipelineId, childPipelineMaterials := range childrenMaterialsMap {
+		creatableMaterials, updateMaterials := computeCreatableAndUpdatableMaterials(parentCiMaterials, childPipelineMaterials, childPipelineId, userId)
+		creatablePipelineMaterials = append(creatablePipelineMaterials, creatableMaterials...)
+		updatablePipelineMaterials = append(updatablePipelineMaterials, updateMaterials...)
+	}
+
+	if len(updatablePipelineMaterials) > 0 {
+		err = impl.ciPipelineMaterialRepository.Update(tx, updatablePipelineMaterials...)
+		if err != nil {
+			impl.logger.Errorw("error in updating ci pipeline materials for linked child pipelines", "err", err, "updatablePipelineMaterials", updatablePipelineMaterials)
+			return err
+		}
+	}
+
+	if len(creatablePipelineMaterials) > 0 {
+		err = impl.ciPipelineMaterialRepository.Save(tx, creatablePipelineMaterials...)
+		if err != nil {
+			impl.logger.Errorw("error in updating ci pipeline materials for linked child pipelines", "err", err, "updatablePipelineMaterials", updatablePipelineMaterials)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func computeCreatableAndUpdatableMaterials(parentCiMaterials []*bean.CiMaterial, childMaterials []*pipelineConfig.CiPipelineMaterial, childPipelineId int, userId int32) ([]*pipelineConfig.CiPipelineMaterial, []*pipelineConfig.CiPipelineMaterial) {
+	// for parent, three cases are possible
+
+	// case-1: new git-material is added
+	//	-> we cannot find this in child, but can be found at parent
+	//     this should be added at child
+
+	// case-2: existing material is updated
+	//  -> we can find this in child and parent
+	//     this should be updated at child
+
+	// case-3: material was deleted
+	//	-> we can find at child but cannot find at parent
+	//     this should be deleted at child
+
+	parentMaterialsMap := make(map[int]*bean.CiMaterial)
+	for _, material := range parentCiMaterials {
+		parentMaterialsMap[material.GitMaterialId] = material
+	}
+
+	childrenMaterialsMap := make(map[int]*pipelineConfig.CiPipelineMaterial)
+	for _, material := range childMaterials {
+		childrenMaterialsMap[material.GitMaterialId] = material
+	}
+
+	// compute updatable and deletable materials
+	var updatableMaterials []*pipelineConfig.CiPipelineMaterial
+	for _, ciPipelineMaterial := range childMaterials {
+		// this material is found at parent , which means we can update this ciPipelineMaterial
+		if parentMaterial, ok := parentMaterialsMap[ciPipelineMaterial.GitMaterialId]; ok {
+			ciPipelineMaterial.Value = parentMaterial.Source.Value
+			ciPipelineMaterial.Regex = parentMaterial.Source.Regex
+			ciPipelineMaterial.Type = parentMaterial.Source.Type
+			ciPipelineMaterial.GitMaterialId = parentMaterial.GitMaterialId
+		} else {
+			// this material is found at parent , which means we can delete this ciPipelineMaterial
+			ciPipelineMaterial.Active = false
+		}
+		ciPipelineMaterial.UpdatedOn = time.Now()
+		ciPipelineMaterial.UpdatedBy = userId
+		updatableMaterials = append(updatableMaterials, ciPipelineMaterial)
+	}
+
+	// compute creatable materials
+	var creatableMaterials []*pipelineConfig.CiPipelineMaterial
+	for _, ciPipelineMaterial := range parentCiMaterials {
+		// this material is not found at child , which means we should create this ciPipelineMaterial for child
+		if _, ok := childrenMaterialsMap[ciPipelineMaterial.GitMaterialId]; !ok {
+			creatableMaterial := &pipelineConfig.CiPipelineMaterial{
+				Value:         ciPipelineMaterial.Source.Value,
+				Active:        true,
+				Regex:         ciPipelineMaterial.Source.Regex,
+				AuditLog:      sql.NewDefaultAuditLog(userId),
+				Type:          ciPipelineMaterial.Source.Type,
+				GitMaterialId: ciPipelineMaterial.GitMaterialId,
+				CiPipelineId:  childPipelineId,
+			}
+			creatableMaterials = append(creatableMaterials, creatableMaterial)
+		}
+	}
+
+	return creatableMaterials, updatableMaterials
+
+}
+
 func (impl CiCdPipelineOrchestratorImpl) DeleteCiPipeline(pipeline *pipelineConfig.CiPipeline, request *bean.CiPatchRequest, tx *pg.Tx) error {
 
 	userId := request.UserId
