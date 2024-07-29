bbabdd6b33fff894415bd2ea636e44d40d76e033
Prakash Kumar <prakash.kumar@devtron.ai>
2024-07-29 13:52:07 +0530

    269 sql script fix

--- scripts/sql/269_alter_image_scan_execution_result_.down.sql
+++ scripts/sql/269_alter_image_scan_execution_result_.down.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "image_scan_execution_result" DROP COLUMN "version";
-ALTER TABLE "image_scan_execution_result" DROP COLUMN "fixed_version";
+ALTER TABLE "image_scan_execution_result" DROP COLUMN IF EXISTS "version";
+ALTER TABLE "image_scan_execution_result" DROP COLUMN IF EXISTS "fixed_version";
--- scripts/sql/269_alter_image_scan_execution_result_.up.sql
+++ scripts/sql/269_alter_image_scan_execution_result_.up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "image_scan_execution_result" ADD COLUMN "version" text;
-ALTER TABLE "image_scan_execution_result" ADD COLUMN "fixed_version" text;
+ALTER TABLE "image_scan_execution_result" ADD COLUMN IF NOT EXISTS "version" text;
+ALTER TABLE "image_scan_execution_result" ADD COLUMN IF NOT EXISTS "fixed_version" text;
