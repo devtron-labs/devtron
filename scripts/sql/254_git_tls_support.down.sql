6981e3beaef07a807587e23627f600659eb50b84
ayushmaheshwari <ayush@devtron.ai>
2024-08-02 17:08:45 +0530

    removing extra migration

--- scripts/sql/254_git_tls_support.down.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE gitops_config DROP COLUMN IF EXISTS tls_cert,
-ALTER TABLE gitops_config DROP COLUMN IF EXISTS tls_key ;
-ALTER TABLE gitops_config DROP COLUMN IF EXISTS ca_cert;
-ALTER TABLE gitops_config DROP COLUMN IF EXISTS enable_tls_verification;
-
-ALTER TABLE git_provider DROP COLUMN IF EXISTS tls_cert,
-ALTER TABLE git_provider DROP COLUMN IF EXISTS tls_key;
-ALTER TABLE git_provider DROP COLUMN IF EXISTS ca_cert;
-ALTER TABLE git_provider DROP COLUMN IF EXISTS enable_tls_verification;
--- scripts/sql/254_git_tls_support.up.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE gitops_config ADD COLUMN tls_cert TEXT,
-ALTER TABLE gitops_config ADD COLUMN tls_key TEXT;
-ALTER TABLE gitops_config ADD COLUMN ca_cert TEXT;
-ALTER TABLE gitops_config ADD COLUMN enable_tls_verification TEXT;
-
-ALTER TABLE git_provider ADD COLUMN tls_cert TEXT,
-ALTER TABLE git_provider ADD COLUMN tls_cert TEXT;
-ALTER TABLE git_provider ADD COLUMN ca_cert TEXT;
-ALTER TABLE gitops_config ADD COLUMN enable_tls_verification TEXT;
