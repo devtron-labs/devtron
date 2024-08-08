b1316a12bf7327799492559cfe5bdfc60ec58b15
ayushmaheshwari <ayush@devtron.ai>
2024-08-08 13:51:17 +0530

    wip

--- scripts/sql/273_link_external_release.up.sql
+++ scripts/sql/273_link_external_release.up.sql
@@ -1,8 +1,5 @@
 CREATE UNIQUE INDEX "unique_deployment_app_name"
     ON pipeline(deployment_app_name,environment_id,deleted);
 
-ALTER TABLE pipeline
-    ADD COLUMN release_mode VARCHAR(256) DEFAULT 'create';
-
 ALTER TABLE deployment_config
     ADD COLUMN release_mode VARCHAR(256) DEFAULT 'create';
