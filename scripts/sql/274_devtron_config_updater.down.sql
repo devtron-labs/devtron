7b0ae99956a75760405942534522311d97e56ddc
Kiran Devtron <kirandevtron@Kirans-MacBook-Air.local>
2024-08-09 14:58:03 +0530

    Chnages in down script

--- scripts/sql/274_devtron_config_updater.down.sql
+++ scripts/sql/274_devtron_config_updater.down.sql
@@ -1,5 +1,5 @@
-DELETE FROM plugin_step_variable WHERE plugin_step_id =(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Devtron Config Updater v1.0.0' and ps."index"=1 and ps.deleted=false);
-DELETE FROM plugin_step WHERE plugin_id=(SELECT id FROM plugin_metadata WHERE name='Devtron Config Updater v1.0.0');
-DELETE from plugin_pipeline_script where id = (SELECT script_id from plugin_step WHERE plugin_id=(SELECT id FROM plugin_metadata WHERE name='Devtron Config Updater v1.0.0'));
-DELETE FROM plugin_stage_mapping WHERE plugin_id =(SELECT id FROM plugin_metadata WHERE name='Devtron Config Updater v1.0.0');
-DELETE FROM plugin_metadata WHERE name ='Devtron Config Updater v1.0.0';
+DELETE FROM plugin_step_variable WHERE plugin_step_id =(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Devtron Config Updater' and ps."index"=1 and ps.deleted=false);
+DELETE FROM plugin_step WHERE plugin_id=(SELECT id FROM plugin_metadata WHERE name='Devtron Config Updater');
+DELETE from plugin_pipeline_script where id = (SELECT script_id from plugin_step WHERE plugin_id=(SELECT id FROM plugin_metadata WHERE name='Devtron Config Updater'));
+DELETE FROM plugin_stage_mapping WHERE plugin_id =(SELECT id FROM plugin_metadata WHERE name='Devtron Config Updater');
+DELETE FROM plugin_metadata WHERE name ='Devtron Config Updater';
