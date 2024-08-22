09ee9f5e404aed058b4804b80de1c5784b92f561
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2024-08-22 12:03:35 +0530

    Revert "fix: ea fixes for helm app (#5708)"
    This reverts commit 3e31f49f95d373f92b13afbe1806606ac4a39d85.

--- pkg/app/helper.go
+++ pkg/app/helper.go
@@ -16,10 +16,7 @@
 
 package app
 
-import (
-	appRepository "github.com/devtron-labs/devtron/internal/sql/repository/app"
-	"strings"
-)
+import "strings"
 
 // LabelMatchingRegex is the official k8s label matching regex, pls refer https://github.com/kubernetes/apimachinery/blob/bfd2aff97e594f6aad77acbe2cbbe190acc93cbc/pkg/util/validation/validation.go#L167
 const LabelMatchingRegex = "^(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])?$"
@@ -45,22 +42,4 @@ func sanitizeLabels(extraAppLabels map[string]string) map[string]string {
 		}
 	}
 	return extraAppLabels
-}
-
-// identifyDuplicateApps identifies the earliest created app and the most recent duplicate app.
-func identifyDuplicateApps(apps []*appRepository.App) (earliestApp *appRepository.App, duplicatedApp *appRepository.App) {
-	if len(apps) == 0 {
-		return nil, nil
-	}
-	earliestApp = apps[0]
-	duplicatedApp = apps[0]
-	for _, app := range apps[1:] {
-		if app.AuditLog.CreatedOn.Before(earliestApp.AuditLog.CreatedOn) {
-			earliestApp = app
-		}
-		if app.AuditLog.CreatedOn.After(duplicatedApp.AuditLog.CreatedOn) {
-			duplicatedApp = app
-		}
-	}
-	return earliestApp, duplicatedApp
 }
