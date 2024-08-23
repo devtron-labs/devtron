7f5db2365c28cfd2374bbb8d6e8a3b27f0847c84
Gireesh Naidu <111440205+gireesh-naidu@users.noreply.github.com>
2024-08-23 12:36:40 +0530

    fix: remove sql script (#5727)

--- scripts/sql/276_scan_policies.up.sql
+++
@@ -1,6 +0,0 @@
-
--- severity 3 is for high and 5 is for unknown
-INSERT INTO "public"."cve_policy_control" ("global", "cluster_id", "env_id", "app_id", "cve_store_id", "action", "severity", "deleted", "created_on", "created_by", "updated_on", "updated_by") VALUES
-                                                     ('t', NULL, NULL, NULL, NULL, '1', '3', 'f', 'now()', '1', 'now()', '1'),
-                                                     ('t', NULL, NULL, NULL, NULL, '1', '5', 'f', 'now()', '1', 'now()', '1');
-
