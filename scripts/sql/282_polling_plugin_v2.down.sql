d735996b36e3519a40f594d68ab0739ce774faf0
Ash-exp <asutosh2000ad@gmail.com>
2024-08-25 16:02:52 +0530

    updated polling plugin migration script

--- scripts/sql/282_polling_plugin_v2.down.sql
+++ scripts/sql/282_polling_plugin_v2.down.sql
@@ -1,1 +1,5 @@
-UPDATE plugin_pipeline_script SET container_image_path ='quay.io/devtron/poll-container-image:97a996a5-545-16654' WHERE container_image_path ='quay.io/devtron/test:bc62315c-2864-32569';
+-- revert the container image path of the polling plugin version 1.0.0
+UPDATE plugin_pipeline_script
+SET container_image_path ='quay.io/devtron/poll-container-image:97a996a5-545-16654'
+WHERE container_image_path ='ashexp/polling-plugin:v1.0.0-beta2'
+AND deleted = false;
--- scripts/sql/282_polling_plugin_v2.up.sql
+++ scripts/sql/282_polling_plugin_v2.up.sql
@@ -1,1 +1,5 @@
-UPDATE plugin_pipeline_script SET container_image_path ='quay.io/devtron/test:bc62315c-2864-32569' WHERE container_image_path ='quay.io/devtron/poll-container-image:97a996a5-545-16654';
+-- update the container image path for the polling plugin version 1.0.0
+UPDATE plugin_pipeline_script
+SET container_image_path ='ashexp/polling-plugin:v1.0.0-beta2'
+WHERE container_image_path ='quay.io/devtron/poll-container-image:97a996a5-545-16654'
+AND deleted = false;
