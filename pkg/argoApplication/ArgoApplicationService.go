b78f4d1df1e7624e4111568b04f2f24682812723
kartik-579 <kartik@devtron.ai>
2024-08-30 13:35:19 +0530

    wip

--- pkg/argoApplication/ArgoApplicationService.go
+++ pkg/argoApplication/ArgoApplicationService.go
@@ -251,11 +251,8 @@ func (impl *ArgoApplicationServiceImpl) getResourceTreeForExternalCluster(cluste
 
 func getApplicationListDtos(resp2 *k8s.ClusterResourceListMap, clusterName string, clusterId int) []*bean.ArgoApplicationListDto {
 	appLists := make([]*bean.ArgoApplicationListDto, 0)
-	// map of keys and index in row cells, initially set as 0 will be updated by object
-	keysToBeFetchedFromColumnDefinitions := map[string]int{k8sCommonBean.K8sResourceColumnDefinitionName: 0,
-		k8sCommonBean.K8sResourceColumnDefinitionHealthStatus: 0, k8sCommonBean.K8sResourceColumnDefinitionSyncStatus: 0}
-	keysToBeFetchedFromRawObject := []string{k8sCommonBean.K8sClusterResourceNamespaceKey}
 	if resp2 != nil {
+		appLists = make([]*bean.ArgoApplicationListDto, 0, len(resp2.Data))
 		for _, rowData := range resp2.Data {
 			if rowData == nil {
 				continue
@@ -268,52 +265,6 @@ func getApplicationListDtos(resp2 *k8s.ClusterResourceListMap, clusterName strin
 				HealthStatus: rowData[k8sCommonBean.K8sResourceColumnDefinitionHealthStatus].(string),
 				Namespace:    rowData[k8sCommonBean.K8sClusterResourceNamespaceKey].(string),
 			}
-			appLists = append(appLists, appListDto)
-		}
-	}
-	columnsDataRaw := manifestObj[k8sCommonBean.K8sClusterResourceColumnDefinitionKey]
-	if columnsDataRaw != nil {
-		columnsData := columnsDataRaw.([]interface{})
-		for i, columnData := range columnsData {
-			columnDataMap := columnData.(map[string]interface{})
-			for key := range keysToBeFetchedFromColumnDefinitions {
-				if columnDataMap[k8sCommonBean.K8sClusterResourceNameKey] == key {
-					keysToBeFetchedFromColumnDefinitions[key] = i
-				}
-			}
-		}
-	}
-
-	rowsDataRaw := manifestObj[k8sCommonBean.K8sClusterResourceRowsKey]
-	if rowsDataRaw != nil {
-		rowsData := rowsDataRaw.([]interface{})
-		for _, rowData := range rowsData {
-			appListDto := &bean.ArgoApplicationListDto{
-				ClusterId:   clusterId,
-				ClusterName: clusterName,
-			}
-			rowDataMap := rowData.(map[string]interface{})
-			rowCells := rowDataMap[k8sCommonBean.K8sClusterResourceCellKey].([]interface{})
-			for key, value := range keysToBeFetchedFromColumnDefinitions {
-				resolvedValueFromRowCell := rowCells[value].(string)
-				switch key {
-				case k8sCommonBean.K8sResourceColumnDefinitionName:
-					appListDto.Name = resolvedValueFromRowCell
-				case k8sCommonBean.K8sResourceColumnDefinitionSyncStatus:
-					appListDto.SyncStatus = resolvedValueFromRowCell
-				case k8sCommonBean.K8sResourceColumnDefinitionHealthStatus:
-					appListDto.HealthStatus = resolvedValueFromRowCell
-				}
-			}
-			rowObject := rowDataMap[k8sCommonBean.K8sClusterResourceObjectKey].(map[string]interface{})
-			for _, key := range keysToBeFetchedFromRawObject {
-				switch key {
-				case k8sCommonBean.K8sClusterResourceNamespaceKey:
-					metadata := rowObject[k8sCommonBean.K8sClusterResourceMetadataKey].(map[string]interface{})
-					appListDto.Namespace = metadata[k8sCommonBean.K8sClusterResourceNamespaceKey].(string)
-				}
-			}
-
 			appLists = append(appLists, appListDto)
 		}
 	}
