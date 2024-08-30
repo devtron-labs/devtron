ff89a26f9368a0e1059c7b3ce1edaa75d11645c9
kripanshdevtron <107392309+kripanshdevtron@users.noreply.github.com>
2024-08-30 18:52:23 +0530

    fix: scan tool active check removed (#5771)
    * scan tool active check removed
    
    * query fix

--- scripts/sql/281_update_scan_tool_metadata.up.sql
+++ scripts/sql/281_update_scan_tool_metadata.up.sql
@@ -19,9 +19,7 @@ UPDATE scan_tool_metadata SET result_descriptor_template = '[
 
 WHERE name = 'TRIVY'
     AND version = 'V1'
-    AND scan_target = 'IMAGE'
-    AND active = true
-    AND deleted = false;
+    AND scan_target = 'IMAGE';
 
 ALTER TABLE image_scan_execution_result
     ADD COLUMN class TEXT,
