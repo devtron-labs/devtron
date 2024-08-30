edbd5d67e2dafe55543a4b64b884e388b12928dd
Ash-exp <asutosh2000ad@gmail.com>
2024-08-30 19:04:11 +0530

    fix: for GetConfigForHelmApps err: pg no row

--- pkg/appStore/installedApp/repository/InstalledAppRepository.go
+++ pkg/appStore/installedApp/repository/InstalledAppRepository.go
@@ -714,7 +714,7 @@ func (impl InstalledAppRepositoryImpl) GetDeploymentSuccessfulStatusCountForTele
 func (impl InstalledAppRepositoryImpl) GetGitOpsInstalledAppsWhereArgoAppDeletedIsTrue(installedAppId int, envId int) (InstalledApps, error) {
 	var installedApps InstalledApps
 	err := impl.dbConnection.Model(&installedApps).
-		Column("installed_apps.*", "App.app_name", "Environment.namespace", "Environment.cluster_id", "Environment.environment_name").
+		Column("installed_apps.*", "App.id", "App.app_name", "Environment.namespace", "Environment.cluster_id", "Environment.environment_name").
 		Where("deployment_app_delete_request = ?", true).
 		Where("installed_apps.active = ?", true).
 		Where("installed_apps.id = ?", installedAppId).
