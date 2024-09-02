b92fc314d3193f696cb15f59b547f8d0a157806a
Shivam-nagar23 <shivam@devtron.ai>
2024-09-02 15:27:48 +0530

    is exposed

--- scripts/sql/283_release_channel.up.sql
+++ scripts/sql/283_release_channel.up.sql
@@ -1,6 +1,6 @@
 INSERT INTO devtron_resource(kind, display_name, icon,is_exposed, parent_kind_id, deleted, created_on, created_by, updated_on,
                              updated_by)
-VALUES ('release-channel', 'Release Channel', '',true, 0, false, now(), 1, now(), 1);
+VALUES ('release-channel', 'Release Channel', '',false, 0, false, now(), 1, now(), 1);
 
 INSERT INTO devtron_resource_schema(devtron_resource_id, version, schema, sample_schema, latest, created_on, created_by, updated_on,
                                     updated_by)
