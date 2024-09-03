d7a960f40fc621406aa5501b70242d4b2c416fe4
kartik-579 <kartik@devtron.ai>
2024-09-03 13:53:17 +0530

    updated ExternalLinkIdentifierMappingRepository

--- pkg/externalLink/ExternalLinkIdentifierMappingRepository.go
+++ pkg/externalLink/ExternalLinkIdentifierMappingRepository.go
@@ -118,10 +118,10 @@ func (impl ExternalLinkIdentifierMappingRepositoryImpl) FindAllActiveByLinkIdent
 
 func (impl ExternalLinkIdentifierMappingRepositoryImpl) FindAllActiveLinkIdentifierData() ([]ExternalLinkIdentifierMappingData, error) {
 	var links []ExternalLinkIdentifierMappingData
-	query := "select el.id,el.external_link_monitoring_tool_id,el.name,el.url,el.is_editable,el.description,el.updated_on," +
-		"elim.id as mapping_id,elim.active,elim.type,elim.identifier,elim.env_id,elim.app_id,elim.cluster_id" +
-		" FROM external_link el" +
-		" LEFT JOIN external_link_identifier_mapping elim ON el.id = elim.external_link_id Where el.active=true and elim.active = true;"
+	query := `select el.id,el.external_link_monitoring_tool_id,el.name,el.url,el.is_editable,el.description,el.updated_on, 
+			  elim.id as mapping_id,elim.active,elim.type,elim.identifier,elim.env_id,elim.app_id,elim.cluster_id
+			  FROM external_link el
+			  LEFT JOIN external_link_identifier_mapping elim ON el.id = elim.external_link_id Where el.active=true and elim.active = true;`
 	_, err := impl.dbConnection.Query(&links, query)
 	return links, err
 }
