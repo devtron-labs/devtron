3b0deda93f4660104b8d892ea95e8038df6a2c26
kartik-579 <kartik@devtron.ai>
2024-09-04 12:57:23 +0530

    added comment

--- util/SQLUtil.go
+++ util/SQLUtil.go
@@ -3,8 +3,8 @@ package util
 import "fmt"
 
 // GetLIKEClauseQueryParam converts string "abc" into "%abc%".
-//This is used for SQL queries and we have taken this approach instead of ex- .Where("name = %s%", "abc") because
-//it will result into query : where name = %'abc'% since string params are added with quotes.
+// This is used for SQL queries and we have taken this approach instead of ex- .Where("name = %s%", "abc") because
+// it will result into query : where name = %'abc'% since string params are added with quotes.
 func GetLIKEClauseQueryParam(s string) string {
 	return fmt.Sprintf("%%%s%%", s)
 }
