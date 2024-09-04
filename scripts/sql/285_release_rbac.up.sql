51ec1d43e0bfd79ba7ad4260f78e61ead77523cb
Shivam-nagar23 <shivam@devtron.ai>
2024-09-04 17:56:41 +0530

    script chnage

--- scripts/sql/285_release_rbac.up.sql
+++ scripts/sql/285_release_rbac.up.sql
@@ -2,22 +2,22 @@
 INSERT INTO rbac_policy_resource_detail ("resource", "policy_resource_value", "allowed_actions",
                                          "resource_object", "eligible_entity_access_types", "deleted", "created_on",
                                          "created_by", "updated_on", "updated_by")
-VALUES ('release', '{"value": "release", "indexKeyMap": {}}', ARRAY['get','update','create','delete','patch'],'{"value": "%", "indexKeyMap": {"0": "ReleaseObj"}', ARRAY['release'],'f','now()', 1, 'now()', 1);
+VALUES ('release', '{"value": "release", "indexKeyMap": {}}', ARRAY['get','update','create','delete','patch'],'{"value": "%/%", "indexKeyMap": {"0": "ReleaseTrackObj", "2": "ReleaseObj"}}', ARRAY['release'],'f','now()', 1, 'now()', 1);
 
 INSERT INTO rbac_policy_resource_detail ("resource", "policy_resource_value", "allowed_actions",
                                          "resource_object", "eligible_entity_access_types", "deleted", "created_on",
                                          "created_by", "updated_on", "updated_by")
-VALUES ('release-requirement', '{"value": "release-requirement", "indexKeyMap": {}}', ARRAY['get','update','create','delete','patch'],'{"value": "%", "indexKeyMap": {"0": "ReleaseObj"}', ARRAY['release'],'f','now()', 1, 'now()', 1);
+VALUES ('release-requirement', '{"value": "release-requirement", "indexKeyMap": {}}', ARRAY['get','update','create','delete','patch'],'{"value": "%/%", "indexKeyMap": {"0": "ReleaseTrackObj", "2": "ReleaseObj"}}', ARRAY['release'],'f','now()', 1, 'now()', 1);
 
 INSERT INTO rbac_policy_resource_detail ("resource", "policy_resource_value", "allowed_actions",
                                          "resource_object", "eligible_entity_access_types", "deleted", "created_on",
                                          "created_by", "updated_on", "updated_by")
-VALUES ('release-track', '{"value": "release-track", "indexKeyMap": {}}', ARRAY['get','update','create','delete','patch'],'{"value": "%", "indexKeyMap": {"0": "ReleaseTrackObj"}', ARRAY['release'],'f','now()', 1, 'now()', 1);
+VALUES ('release-track', '{"value": "release-track", "indexKeyMap": {}}', ARRAY['get','update','create','delete','patch'],'{"value": "%", "indexKeyMap": {"0": "ReleaseTrackObj"}}', ARRAY['release'],'f','now()', 1, 'now()', 1);
 
 INSERT INTO rbac_policy_resource_detail ("resource", "policy_resource_value", "allowed_actions",
                                          "resource_object", "eligible_entity_access_types", "deleted", "created_on",
                                          "created_by", "updated_on", "updated_by")
-VALUES ('release-track-requirement', '{"value": "release-track-requirement", "indexKeyMap": {}}', ARRAY['get','update','create','delete','patch'],'{"value": "%", "indexKeyMap": {"0": "ReleaseTrackObj"}', ARRAY['release'],'f','now()', 1, 'now()', 1);
+VALUES ('release-track-requirement', '{"value": "release-track-requirement", "indexKeyMap": {}}', ARRAY['get','update','create','delete','patch'],'{"value": "%", "indexKeyMap": {"0": "ReleaseTrackObj"}}', ARRAY['release'],'f','now()', 1, 'now()', 1);
 
 
 
