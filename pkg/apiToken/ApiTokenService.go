18431d04363f3447094a2330b6b24a37aceff26f
kartik-579 <84493919+kartik-579@users.noreply.github.com>
2024-09-18 11:28:47 +0530

    fix: fixed user rbac flows (#5804)
    * update CreateService rbac flow
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * prod bug fixes
    
    * updated logic for chartGroup
    
    * updated comments
    
    * role group delete prod bug fix
    
    * role group update prod bug fix

--- pkg/apiToken/ApiTokenService.go
+++ pkg/apiToken/ApiTokenService.go
@@ -205,7 +205,7 @@ func (impl ApiTokenServiceImpl) CreateApiToken(request *openapi.CreateApiTokenRe
 		EmailId:  email,
 		UserType: bean.USER_TYPE_API_TOKEN,
 	}
-	createUserResponse, _, err := impl.userService.CreateUser(&createUserRequest, token, managerAuth)
+	createUserResponse, err := impl.userService.CreateUser(&createUserRequest)
 	if err != nil {
 		impl.logger.Errorw("error while creating user for api-token", "email", email, "error", err)
 		return nil, err
