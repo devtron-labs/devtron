412619a4c52be8cc605808d7dbca85b56904853f
ayushmaheshwari <ayush@devtron.ai>
2024-09-25 13:35:21 +0530

    wip: adding app name check

--- internal/sql/repository/app/AppRepository.go
+++ internal/sql/repository/app/AppRepository.go
@@ -390,7 +390,7 @@ func (repo AppRepositoryImpl) fixMultipleHelmAppsWithSameName(appName string) er
 	// deleting all apps other than app with max id
 	_, err = repo.dbConnection.Model((*App)(nil)).
 		Set("active = ?", false).Set("updated_by = ?", SYSTEM_USER_ID).Set("updated_on = ?", time.Now()).
-		Where("id not in (?) ", maxAppIdQuery).Update()
+		Where("id not in (?) and app_name = ? ", maxAppIdQuery, appName).Update()
 
 	return nil
 }
