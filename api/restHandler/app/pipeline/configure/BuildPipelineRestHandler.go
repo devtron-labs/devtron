555b6276e133f7ec092993dd3ae5af841e78c37f
kartik-579 <kartik@devtron.ai>
2024-10-01 13:17:46 +0530

    fixed job trigger bug

--- api/restHandler/app/pipeline/configure/BuildPipelineRestHandler.go
+++ api/restHandler/app/pipeline/configure/BuildPipelineRestHandler.go
@@ -721,21 +721,25 @@ func (handler *PipelineConfigRestHandlerImpl) TriggerCiPipeline(w http.ResponseW
 		rbacObjectToCdPipeline[envObject] = cdPipeline
 	}
 
-	hasAnyEnvTriggerAccess := false
-	envRbacResultMap := handler.enforcer.EnforceInBatch(token, casbin.ResourceEnvironment, casbin.ActionTrigger, cdPipelineRbacObjects)
-	for rbacObject, rbacResultOk := range envRbacResultMap {
-		cdPipeline := rbacObjectToCdPipeline[rbacObject]
-		if cdPipeline.TriggerType == pipelineConfig.TRIGGER_TYPE_AUTOMATIC && !rbacResultOk {
+	hasAnyEnvTriggerAccess := len(cdPipelines) == 0 //if no pipelines then appAccess is enough. For jobs also, this will be true
+	if !hasAnyEnvTriggerAccess {
+		//cdPipelines present, to check access for cd trigger
+		envRbacResultMap := handler.enforcer.EnforceInBatch(token, casbin.ResourceEnvironment, casbin.ActionTrigger, cdPipelineRbacObjects)
+		for rbacObject, rbacResultOk := range envRbacResultMap {
+			cdPipeline := rbacObjectToCdPipeline[rbacObject]
+			if cdPipeline.TriggerType == pipelineConfig.TRIGGER_TYPE_AUTOMATIC && !rbacResultOk {
+				common.WriteJsonResp(w, err, "Unauthorized User", http.StatusForbidden)
+				return
+			}
+			if rbacResultOk { //this flow will come if pipeline is automatic and has access or if pipeline is manual,
+				// by which we can ensure if there are no automatic pipelines then atleast access on one manual is present
+				hasAnyEnvTriggerAccess = true
+			}
+		}
+		if !hasAnyEnvTriggerAccess {
 			common.WriteJsonResp(w, err, "Unauthorized User", http.StatusForbidden)
 			return
 		}
-		if rbacResultOk {
-			hasAnyEnvTriggerAccess = true
-		}
-	}
-	if !hasAnyEnvTriggerAccess {
-		common.WriteJsonResp(w, err, "Unauthorized User", http.StatusForbidden)
-		return
 	}
 
 	//RBAC ENDS
