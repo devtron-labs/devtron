0078d8040ffb23b815386fc96f985fac7f806390
Prakash Kumar <prakash.kumar@devtron.ai>
2024-10-17 15:09:53 +0530

    code review incorporation :- kartik 2

--- pkg/generateManifest/adapter.go
+++ pkg/generateManifest/adapter.go
@@ -2,13 +2,7 @@ package generateManifest
 
 func ConvertPointerDeploymentTemplateResponseToNonPointer(r *DeploymentTemplateResponse) DeploymentTemplateResponse {
 	if r != nil {
-		return DeploymentTemplateResponse{
-			Data:                r.Data,
-			ResolvedData:        r.ResolvedData,
-			VariableSnapshot:    r.VariableSnapshot,
-			TemplateVersion:     r.TemplateVersion,
-			IsAppMetricsEnabled: r.IsAppMetricsEnabled,
-		}
+		return *r
 	}
 	return DeploymentTemplateResponse{}
 }
