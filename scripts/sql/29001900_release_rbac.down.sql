85279c8127453c6b83e5db8cafa3b451ed5bb5df
Vikram <73224103+vikramdevtron@users.noreply.github.com>
2024-10-24 18:49:39 +0530

    delete unwanted files (#6025)

--- scripts/sql/29001900_release_rbac.down.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE roles DROP COLUMN "release";
-ALTER TABLE roles DROP COLUMN "release_track";
-DELETE from rbac_role_resource_detail where resource in ('release','release-track');
-DELETE from rbac_policy_resource_detail where resource in ('release','release-track');
--- scripts/sql/29201900_helm_app_deployment_message.down.sql
+++
@@ -1,3 +0,0 @@
--- Drop the message column from the "public"."installed_app_version_history" table
-ALTER TABLE "public"."installed_app_version_history"
-    DROP COLUMN IF EXISTS message;
