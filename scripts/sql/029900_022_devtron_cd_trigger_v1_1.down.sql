125d95532768b12acf84542e06cdb289edd904ee
Neha Sharma <nehasharma@devtron.ai>
2024-10-24 12:49:34 +0530

    add

--- scripts/sql/029900_022_devtron_cd_trigger_v1_1.down.sql
+++ scripts/sql/029900_022_devtron_cd_trigger_v1_1.down.sql
@@ -1,4 +1,4 @@
-iDELETE FROM plugin_step_variable WHERE plugin_step_id=(SELECT id FROM plugin_metadata WHERE name='Devtron CD Trigger');
+DELETE FROM plugin_step_variable WHERE plugin_step_id=(SELECT id FROM plugin_metadata WHERE name='Devtron CD Trigger');
 DELETE FROM plugin_step where plugin_id=(SELECT id FROM plugin_metadata WHERE name='Devtron CD Trigger');
 DELETE FROM plugin_pipeline_script where id=(SELECT id FROM plugin_metadata WHERE name='Devtron CD Trigger');
 DELETE FROM plugin_stage_mapping where plugin_id=(SELECT id from plugin_metadata where name='Devtron CD Trigger');
