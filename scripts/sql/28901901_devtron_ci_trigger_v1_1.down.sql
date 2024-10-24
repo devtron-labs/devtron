282a5745af94e12c7e0e5b9671cd1f69f33a2dd8
Vikram Singh <vikram@devtron.ai>
2024-10-24 18:47:10 +0530

    delete unwanted files

--- scripts/sql/28901901_devtron_ci_trigger_v1_1.down.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM plugin_step_variable WHERE plugin_step_id=(SELECT id FROM plugin_metadata WHERE name='Devtron CI Trigger');
-DELETE FROM plugin_step where plugin_id=(SELECT id FROM plugin_metadata WHERE name='Devtron CI Trigger');
-DELETE FROM plugin_pipeline_script where id=(SELECT id FROM plugin_metadata WHERE name='Devtron CI Trigger');
-DELETE FROM plugin_stage_mapping where plugin_id=(SELECT id from plugin_metadata where name='Devtron CI Trigger');
-DELETE FROM plugin_metadata where name='Devtron CI Trigger';
-UPDATE plugin_metadata SET is_latest = true WHERE id = (SELECT id FROM plugin_metadata WHERE name= 'Devtron CI Trigger v1.0.0' and is_latest= false);
