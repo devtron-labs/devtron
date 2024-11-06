685bb2a06a57ccfefd8fea434d77a043e758e807
ayushmaheshwari <ayush@devtron.ai>
2024-11-06 12:22:54 +0530

    migration fixes

--- scripts/sql/029900_022_config_patch_strategy.down.sql
+++ scripts/sql/029900_022_config_patch_strategy.down.sql
@@ -1,1 +1,3 @@
 ALTER TABLE chart_env_config_override DROP COLUMN merge_strategy;
+ALTER TABLE deployment_template_history DROP COLUMN merge_strategy;
+ALTER TABLE deployment_template_history DROP COLUMN  template_patch_data;
--- scripts/sql/029900_022_config_patch_strategy.up.sql
+++ scripts/sql/029900_022_config_patch_strategy.up.sql
@@ -1,1 +1,3 @@
 ALTER TABLE chart_env_config_override ADD COLUMN merge_strategy VARCHAR(100);
+ALTER TABLE deployment_template_history ADD COLUMN merge_strategy VARCHAR(100);
+ALTER TABLE deployment_template_history ADD COLUMN  template_patch_data TEXT ;
