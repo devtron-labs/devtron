a3da0a758f4b098aae7c746ddcafef0841856cd8
Shivam-nagar23 <shivam@devtron.ai>
2024-12-16 12:51:03 +0530

    added logs for deployment trigger

--- pkg/workflow/dag/WorkflowDagExecutor.go
+++ pkg/workflow/dag/WorkflowDagExecutor.go
@@ -377,6 +377,7 @@ func (impl *WorkflowDagExecutorImpl) ProcessDevtronAsyncInstallRequest(cdAsyncIn
 		return err
 	}
 	if util.IsHelmApp(overrideRequest.DeploymentAppType) {
+		impl.logger.Debugw("processing async install request for helm app", "cdWfrId", cdWfr.Id)
 		// update workflow runner status, used in app workflow view
 		err = impl.cdWorkflowCommonService.UpdateNonTerminalStatusInRunner(newCtx, overrideRequest.WfrId, overrideRequest.UserId, cdWorkflow2.WorkflowStarting)
 		if err != nil {
@@ -391,6 +392,7 @@ func (impl *WorkflowDagExecutorImpl) ProcessDevtronAsyncInstallRequest(cdAsyncIn
 	}
 	releaseId, releaseErr := impl.cdTriggerService.TriggerRelease(overrideRequest, envDeploymentConfig, newCtx, cdAsyncInstallReq.TriggeredAt, cdAsyncInstallReq.TriggeredBy)
 	if releaseErr != nil {
+		impl.logger.Errorw("error encountered in ProcessDevtronAsyncInstallRequest", "err", releaseErr, "cdWfrId", cdWfr.Id)
 		impl.handleAsyncTriggerReleaseError(newCtx, releaseErr, cdWfr, overrideRequest)
 		return releaseErr
 	} else {
