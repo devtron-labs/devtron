2f9394e2246d443a62e67451c798d7a71319cc76
prakhar katiyar <prkhrkat@gmail.com>
2024-12-20 18:07:00 +0530

    migration order changes

--- scripts/sql/32002500_global-tag-v2.up.sql
+++ scripts/sql/32002500_global-tag-v2.up.sql
@@ -5,4 +5,9 @@ ALTER TABLE global_tag
     ADD COLUMN IF NOT EXISTS deployment_policy VARCHAR(50) DEFAULT 'allow' NOT NULL,
     ADD COLUMN IF NOT EXISTS value_constraint_id INT;
 
+-- Change column type to TEXT if it is not already TEXT
+ALTER TABLE global_tag
+ALTER COLUMN mandatory_project_ids_csv TYPE TEXT
+    USING mandatory_project_ids_csv::TEXT;
+
 COMMIT;
