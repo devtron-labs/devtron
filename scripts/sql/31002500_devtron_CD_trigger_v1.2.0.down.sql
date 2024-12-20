7242c7070a08a8dbcf62293519b484afa4577639
kamal-devtron <kamal@devtron.ai>
2024-12-20 16:28:44 +0530

    changes in down script

--- scripts/sql/31002500_devtron_CD_trigger_v1.2.0.down.sql
+++ scripts/sql/31002500_devtron_CD_trigger_v1.2.0.down.sql
@@ -3,4 +3,4 @@ DELETE FROM plugin_step where plugin_id=(SELECT id FROM plugin_metadata WHERE na
 DELETE FROM plugin_pipeline_script where id=(SELECT id FROM plugin_metadata WHERE name='Devtron CD Trigger');
 DELETE FROM plugin_stage_mapping where plugin_id=(SELECT id from plugin_metadata where name='Devtron CD Trigger');
 DELETE FROM plugin_metadata where name='Devtron CD Trigger';
-UPDATE plugin_metadata SET is_latest = true WHERE id = (SELECT id FROM plugin_metadata WHERE name= 'Devtron CD Trigger' and is_latest= false);
+UPDATE plugin_metadata SET is_latest = true WHERE id = (SELECT id FROM plugin_metadata WHERE name= 'Devtron CD Trigger' and is_latest= false and plugin_version='1.1.0');
