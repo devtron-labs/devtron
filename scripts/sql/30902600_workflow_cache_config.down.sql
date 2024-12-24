1fad5b74dc81aee7d161a58b322da32a643bfeb5
ayu-devtron <ayush.singh@devtron.ai>
2024-12-24 14:57:22 +0530

    remove older script file

--- scripts/sql/30902600_workflow_cache_config.down.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "public"."pipeline" DROP COLUMN IF EXISTS "pre_stage_cache_config";
-ALTER TABLE "public"."pipeline" DROP COLUMN IF EXISTS "post_stage_cache_config";
-ALTER TABLE "public"."ci_pipeline" DROP COLUMN IF EXISTS "workflow_cache_config";
--- scripts/sql/30902600_workflow_cache_config.up.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "public"."pipeline" ADD COLUMN IF NOT EXISTS "pre_stage_cache_config" varchar(50);
-ALTER TABLE "public"."pipeline" ADD COLUMN IF NOT EXISTS "post_stage_cache_config" varchar(50);
-ALTER TABLE "public"."ci_pipeline" ADD COLUMN IF NOT EXISTS "workflow_cache_config" varchar(50);
