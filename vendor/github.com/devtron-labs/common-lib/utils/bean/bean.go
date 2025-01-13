902d1c45ed1b1466794828e41ecb281b254472f1
ayu-devtron <ayush.singh@devtron.ai>
2025-01-13 20:40:43 +0530

    remove unsued pg_query_duration redeclaration

--- vendor/github.com/devtron-labs/common-lib/utils/bean/bean.go
+++ vendor/github.com/devtron-labs/common-lib/utils/bean/bean.go
@@ -68,7 +68,7 @@ type PgQueryMonitoringConfig struct {
 	LogSlowQuery           bool  `env:"PG_LOG_SLOW_QUERY" envDefault:"true"`
 	LogAllQuery            bool  `env:"PG_LOG_ALL_QUERY" envDefault:"false"`
 	LogAllFailureQueries   bool  `env:"PG_LOG_ALL_FAILURE_QUERIES" envDefault:"true"`
-	ExportPromMetrics      bool  `env:"PG_EXPORT_PROM_METRICS" envDefault:"false"`
+	ExportPromMetrics      bool  `env:"PG_EXPORT_PROM_METRICS" envDefault:"true"`
 	QueryDurationThreshold int64 `env:"PG_QUERY_DUR_THRESHOLD" envDefault:"5000"`
 	ServiceName            string
 }
