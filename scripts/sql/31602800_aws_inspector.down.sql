1d71caf21aaf28aeac321dec9b842b39039fb548
Shivam-nagar23 <shivam@devtron.ai>
2025-01-20 16:02:06 +0530

    plugin

--- scripts/sql/31602800_aws_inspector.down.sql
+++ scripts/sql/31602800_aws_inspector.down.sql
@@ -6,7 +6,7 @@ ALTER TABLE public.plugin_metadata DROP COLUMN IF EXISTS is_exposed ;
 ALTER TABLE public.scan_tool_metadata DROP COLUMN IF EXISTS is_preset ;
 ALTER TABLE public.scan_tool_metadata DROP COLUMN IF EXISTS plugin_id;
 ALTER TABLE public.scan_tool_metadata DROP CONSTRAINT IF EXISTS scan_tool_metadata_name_version_unique;
-ALTER TABLE public.scan_tool_metadata ADD COLUMN IF EXISTS url;
+ALTER TABLE public.scan_tool_metadata DROP COLUMN IF EXISTS url;
 
 -- ---------------------------------------------------
 COMMIT;
