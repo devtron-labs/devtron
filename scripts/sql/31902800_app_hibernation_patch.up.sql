a43337b9c6bed9ec4f2107eeccd83a1740e33f27
Rajeev <rajeev.ranjan@devtron.ai>
2025-01-28 00:29:45 +0530

    added the script to migrate the existing data into new Column

--- scripts/sql/31902800_app_hibernation_patch.up.sql
+++ scripts/sql/31902800_app_hibernation_patch.up.sql
@@ -9,6 +9,11 @@ ALTER TABLE public.chart_ref_schema
 ALTER TABLE public.chart_ref_schema
     ADD COLUMN IF NOT EXISTS resourceValue TEXT;
 
+-- Migrate data from old column "schema" to "resourceValue"
+UPDATE public.chart_ref_schema
+SET resourceValue = schema
+WHERE schema IS NOT NULL;
+
 CREATE UNIQUE INDEX chart_ref_schema_unique_active_idx
     ON public.chart_ref_schema (name, resourceType)
     WHERE active = TRUE;
