bbec402bcd7050ecfe872d7a5dfe87ab5d450d46
ayu-devtron <ayush.singh@devtron.ai>
2025-01-27 18:34:33 +0530

    update sql script number

--- scripts/sql/31902800_build_worker_status.up.sql
+++ scripts/sql/31902800_build_worker_status.up.sql
@@ -1,6 +1,5 @@
 CREATE SEQUENCE IF NOT EXISTS id_seq_workflow_execution_stage;
 
-
 CREATE TABLE public.workflow_execution_stage (
                                                  id int4 NOT NULL DEFAULT nextval('id_seq_workflow_execution_stage'::regclass),
                                                  stage_name varchar(50) NULL,
@@ -10,8 +9,8 @@ CREATE TABLE public.workflow_execution_stage (
                                                  metadata text NULL,
                                                  workflow_id int4 NOT NULL,
                                                  workflow_type varchar(50) NOT NULL,
-                                                 start_time timestamptz,
-                                                 end_time timestamptz,
+                                                 start_time text,
+                                                 end_time text,
                                                  created_on timestamptz NOT NULL,
                                                  created_by int4 NOT NULL,
                                                  updated_on timestamptz NOT NULL,
