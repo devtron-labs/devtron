ef60bc434aad244b611cdf1104ecd7c5fb91d7fa
ayu-devtron <ayush.singh@devtron.ai>
2025-01-30 00:28:47 +0530

    review comment to check wfr terminal status

--- pkg/eventProcessor/in/WorkflowEventProcessorService.go
+++ pkg/eventProcessor/in/WorkflowEventProcessorService.go
@@ -170,7 +170,7 @@ func (impl *WorkflowEventProcessorImpl) SubscribeCDStageCompleteEvent() error {
 			return
 		}
 		wfr.IsArtifactUploaded = cdStageCompleteEvent.IsArtifactUploaded
-		if wfr.Status != string(v1alpha1.NodeSucceeded) && wfr.Status != string(v1alpha1.NodeFailed) {
+		if slices.Contains(cdWorkflowModelBean.WfrTerminalStatusList, wfr.Status) {
 			impl.logger.Debugw("event received from ci runner, updating workflow runner status as succeeded", "savedWorkflowRunnerId", wfr.Id, "oldStatus", wfr.Status, "podStatus", wfr.PodStatus)
 			if cdStageCompleteEvent.IsSuccess {
 				wfr.Status = string(v1alpha1.NodeSucceeded)
