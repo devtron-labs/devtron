ef1cc8e8222b3124c0069690ab04562e83506f40
Ash-exp <asutosh2000ad@gmail.com>
2025-02-17 17:30:02 +0530

    updated migration script

--- scripts/sql/32102900_release_config.down.sql
+++ scripts/sql/32102900_release_config.down.sql
@@ -1,1 +1,7 @@
-ALTER TABLE deployment_config DROP COLUMN IF EXISTS release_config;
+BEGIN;
+
+-- Drop the column release_config from deployment_config
+ALTER TABLE deployment_config
+    DROP COLUMN IF EXISTS release_config;
+
+END;
--- scripts/sql/32102900_release_config.up.sql
+++ scripts/sql/32102900_release_config.up.sql
@@ -1,1 +1,15 @@
-ALTER TABLE deployment_config ADD COLUMN IF NOT EXISTS release_config jsonb;
+BEGIN;
+
+-- Add release_config column to deployment_config
+ALTER TABLE deployment_config
+    ADD COLUMN IF NOT EXISTS release_config jsonb;
+
+-- Set active to false for all deployment_configs that have an app_id that is inactive
+UPDATE deployment_config
+    SET active = false
+    WHERE active = true
+    AND app_id IN (
+        SELECT id FROM app WHERE active = false
+    );
+
+END;
