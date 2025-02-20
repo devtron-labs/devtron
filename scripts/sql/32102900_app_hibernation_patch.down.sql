8194a87cc791125432cb09fb81798a2106c34737
Rajeev <rajeev.ranjan@devtron.ai>
2025-02-20 14:32:20 +0530

    added the script for deleting the created extra entries of hibernation patch

--- scripts/sql/32102900_app_hibernation_patch.down.sql
+++ scripts/sql/32102900_app_hibernation_patch.down.sql
@@ -5,3 +5,5 @@ DROP INDEX IF EXISTS public.chart_ref_schema_unique_active_idx;
 ALTER TABLE public.chart_ref_schema
 DROP COLUMN IF EXISTS resource_type,
 DROP COLUMN IF EXISTS resource_value;
+--- hard Deleting the added resourceQualifier entries for the hibernationPatch
+DELETE FROM public.resource_qualifier_mapping where qualifier_id=9 and resource_type=10;
