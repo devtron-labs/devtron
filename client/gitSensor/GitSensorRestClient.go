619006e0b8b492e281aceae4b1e901eda9a02798
Ash-exp <asutosh2000ad@gmail.com>
2025-03-20 18:32:26 +0530

    added supoort for preserve mode in update repo api

--- client/gitSensor/GitSensorRestClient.go
+++ client/gitSensor/GitSensorRestClient.go
@@ -88,6 +88,7 @@ type GitMaterial struct {
 	FetchSubmodules  bool
 	FilterPattern    []string
 	CloningMode      string
+	PreserveMode     bool
 }
 type GitProvider struct {
 	Id                    int
