22726690e145e0aa07e7d43c396cd3d1057abdaf
prakhar katiyar <prkhrkat@gmail.com>
2025-04-07 13:02:54 +0530

    fix: get data Source API

--- pkg/cluster/ClusterServiceExtended.go
+++ pkg/cluster/ClusterServiceExtended.go
@@ -268,7 +268,7 @@ func (impl *ClusterServiceImplExtended) CreateGrafanaDataSource(clusterBean *bea
 		//starts grafana creation
 		// appending envId to ensure unique datasource name for each environment (ex- env got deleted and created with same name)
 		// reverting to old name will be done in next release
-		DataSourceName := "Prometheus-" + env.Name
+		DataSourceName := "Prometheus-" + env.Name + "-EnvId-" + fmt.Sprint(env.Id)
 		createDatasourceReq := grafana.CreateDatasourceRequest{
 			Name:      DataSourceName,
 			Type:      "prometheus",
