4a6a38996627027b75c6e9aef83a10984862265d
kartik-579 <kartik@devtron.ai>
2025-04-28 11:50:02 +0530

    sql files

---
+++ scripts/sql/32903500_alter_pipeline_timeline.up.sql
@@ -0,0 +1,3 @@
+CREATE INDEX "idx_pipeline_status_timeline_cd_workflow_runner_id" ON pipeline_status_timeline USING BTREE ("cd_workflow_runner_id");
+
+CREATE INDEX "idx_pipeline_status_timeline_installed_app_version_history_id" ON pipeline_status_timeline USING BTREE ("installed_app_version_history_id");
