bcf51254b7ba40683c698e9a42c676ed3416fcb8
Rupin Solanki <rupin@devtron.ai>
2025-05-15 15:18:32 +0530

    updated sql script

--- scripts/sql/32603301_reference-5-1-0.down.sql
+++ scripts/sql/32603301_reference-5-1-0.down.sql
@@ -1,5 +1,3 @@
 DELETE FROM global_strategy_metadata_chart_ref_mapping WHERE chart_ref_id=(select id from chart_ref where version='5.1.0' and name is null);
 
 DELETE FROM "public"."chart_ref" WHERE ("location" = 'reference-chart_5-1-0' AND "version" = '5.1.0');
-
-UPDATE "public"."chart_ref" SET "is_default" = 't' WHERE "location" = 'deployment-chart_4-20-0' AND "version" = '4.20.0';
