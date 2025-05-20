0a29c22c199dd898dc4dea51287f6c6146217645
prakhar katiyar <prkhrkat@gmail.com>
2025-05-20 18:47:01 +0530

    panic recovered go-routines

--- pkg/workflow/dag/WorkflowDagExecutor.go
+++ pkg/workflow/dag/WorkflowDagExecutor.go
@@ -1075,7 +1075,7 @@ func (impl *WorkflowDagExecutorImpl) HandleCiSuccessEvent(triggerContext trigger
 		for j := 0; j < batchSize; j++ {
 			wg.Add(1)
 			index := i + j
-			go func(index int) {
+			runnableFunc := func(index int) {
 				defer wg.Done()
 				ciArtifact := ciArtifactArr[index]
 				// handle individual CiArtifact success event
@@ -1083,7 +1083,8 @@ func (impl *WorkflowDagExecutorImpl) HandleCiSuccessEvent(triggerContext trigger
 				if err != nil {
 					impl.logger.Errorw("error on handle  ci success event", "ciArtifactId", ciArtifact.Id, "err", err)
 				}
-			}(index)
+			}
+			impl.asyncRunnable.Execute(func() { runnableFunc(index) })
 		}
 		wg.Wait()
 		i += batchSize
