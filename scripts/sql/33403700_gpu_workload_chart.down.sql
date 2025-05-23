59c8ff86ea0d6db3cbe90af0cf6d14b6f13b255d
Rupin Solanki <rupin@devtron.ai>
2025-05-23 13:56:49 +0530

    migration for gpu-workloads

---
+++ scripts/sql/33403700_gpu_workload_chart.down.sql
@@ -0,0 +1,3 @@
+DELETE FROM global_strategy_metadata_chart_ref_mapping WHERE chart_ref_id=(select id from chart_ref where version='4.21.0' and name='GPU-Workload');
+
+DELETE FROM "public"."chart_ref" WHERE ("location" = 'gpu-workload_4-21-0' AND "version" = '4.21.0');
