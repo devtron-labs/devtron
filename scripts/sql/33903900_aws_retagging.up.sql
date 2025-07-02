36959b6cde744534c1af8837c9cc151a10779503
kamal-devtron <kamal@devtron.ai>
2025-07-02 16:04:49 +0530

    changes in script

--- scripts/sql/33903900_aws_retagging.up.sql
+++ scripts/sql/33903900_aws_retagging.up.sql
@@ -14,6 +14,16 @@ INSERT INTO "plugin_pipeline_script" ("id", "script","type","deleted","created_o
         #!/bin/sh 
         set -eo pipefail 
         #set -v  ## uncomment this to debug the script 
+        if [[ "$AwsEcrFixedTag" &&  "$AwsEcrTagPrefix"  ]];then 
+            echo "Please provide only one input"
+            exit 1;
+        elif [[ $AwsEcrFixedTag ]]; then
+            Tag=$AwsEcrFixedTag
+        elif [[ $AwsEcrTagPrefix ]]; then
+            Tag=$AwsEcrTagPrefix-$DOCKER_IMAGE_TAG
+        else
+            echo "No Tags Provided for retagging"
+        fi  
         AwsAccessKey="${AwsAccessEcrKey:-$(echo "$CI_CD_EVENT" | jq -r \'.commonWorkflowRequest.accessKey\')}"
         AwsSecretKey="${AwsSecretEcrKey:-$(echo "$CI_CD_EVENT" | jq -r \'.commonWorkflowRequest.secretKey\')}"
         mkdir -p ~/.aws
@@ -23,17 +33,18 @@ INSERT INTO "plugin_pipeline_script" ("id", "script","type","deleted","created_o
         fi 
         pipeline_type=$(echo $CI_CD_EVENT | jq -r \'.type\')
         Region=$(echo $CI_CD_EVENT | jq -r .commonWorkflowRequest.dockerRegistryURL | sed \'s|https://||\'|  awk -F. \'{print $4}\')
+
         echo "************* Starting the AWS ECR Retag plugin  *******************"
         if [[ "$pipeline_type" == "CI" ]]; then
             image_repo=$(echo $CI_CD_EVENT | jq -r  .commonWorkflowRequest.dockerRepository)
             image_tag=$(echo $CI_CD_EVENT | jq -r .commonWorkflowRequest.dockerImageTag)
             MANIFEST=$(aws ecr batch-get-image --repository-name $image_repo --image-ids imageTag=$image_tag --region $Region --output text --query \'images[].imageManifest\')
-            aws ecr put-image --repository-name $image_repo --image-tag=$AwsEcrCustomTag --image-manifest "$MANIFEST" --region $Region
+            aws ecr put-image --repository-name $image_repo --image-tag=$AwsEcrFixedTag --image-manifest "$MANIFEST" --region $Region
         elif [[ "$pipeline_type" == "CD" ]]; then
             image_repo=$(echo $CI_CD_EVENT | jq -r .commonWorkflowRequest.ciArtifactDTO.image | cut -d\'/\' -f2 | cut -d\':\' -f1)
             image_tag=$(echo $CI_CD_EVENT | jq -r .commonWorkflowRequest.ciArtifactDTO.image | cut -d \':\' -f2)
             MANIFEST=$(aws ecr batch-get-image --repository-name $image_repo --image-ids imageTag=$image_tag --region $Region --output text --query \'images[].imageManifest\')
-            aws ecr put-image --repository-name $image_repo --image-tag=$AwsEcrCustomTag --image-manifest "$MANIFEST" --region $Region
+            aws ecr put-image --repository-name $image_repo --image-tag=$AwsEcrFixedTag --image-manifest "$MANIFEST" --region $Region
         fi  
         if [ $? -ne 0 ]; then
             echo "*************Failed to Retag the image****************"
@@ -47,6 +58,7 @@ INSERT INTO "plugin_step" ("id", "plugin_id","name","description","index","step_
 VALUES (nextval('id_seq_plugin_step'), (SELECT id FROM plugin_metadata WHERE name='AWS ECR Retag' AND plugin_version='1.0.0' AND deleted= false),'Step 1','Runnig the plugin','1','INLINE',(SELECT last_value FROM id_seq_plugin_pipeline_script),'f','now()', 1, 'now()', 1);
 
 INSERT INTO plugin_step_variable (id,plugin_step_id,name,format,description,is_exposed,allow_empty_value,default_value,value,variable_type,value_type,previous_step_index,variable_step_index,variable_step_index_in_plugin,reference_variable_name,deleted,created_on,created_by,updated_on,updated_by) 
-VALUES (nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='AWS ECR Retag' and p.plugin_version='1.0.0' and ps."index"=1 and ps.deleted=false),'AwsEcrCustomTag','STRING','Provide the  tag for retagging','t','f',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1),
+VALUES (nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='AWS ECR Retag' and p.plugin_version='1.0.0' and ps."index"=1 and ps.deleted=false),'AwsEcrFixedTag','STRING','Provide the fixed tag for retagging','t','t',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1),
+(nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='AWS ECR Retag' and p.plugin_version='1.0.0' and ps."index"=1 and ps.deleted=false),'AwsEcrTagPrefix','STRING','Provide the tag prefix','t','t',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1),
 (nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='AWS ECR Retag' and p.plugin_version='1.0.0' and ps."index"=1 and ps.deleted=false),'AwsAccessEcrKey','STRING','Provide the access key with ECR permission','t','t',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1),
 (nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='AWS ECR Retag' and p.plugin_version='1.0.0' and ps."index"=1 and ps.deleted=false),'AwsSecretEcrKey','STRING','Provide the secret key with ECR permission','t','t',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1);
