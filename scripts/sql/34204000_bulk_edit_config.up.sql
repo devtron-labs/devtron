b392a66d2320e758018abc0fcc74386c66577762
Ash-exp <asutosh2000ad@gmail.com>
2025-07-16 03:52:51 +0530

    updated migration script

--- scripts/sql/34204000_bulk_edit_config.up.sql
+++ scripts/sql/34204000_bulk_edit_config.up.sql
@@ -5,7 +5,7 @@ CREATE SEQUENCE IF NOT EXISTS id_seq_bulk_edit_config;
 
 -- Create a new table
 CREATE TABLE IF NOT EXISTS "public"."bulk_edit_config" (
-    "id"            INTEGER             NOT NULL DEFAULT nextval('id_seq_workflow_config_snapshot'::regclass),
+                                                           "id"            INTEGER             NOT NULL DEFAULT nextval('id_seq_workflow_config_snapshot'::regclass),
     "api_version"   VARCHAR(255) NOT NULL,
     "kind"          VARCHAR(255) NOT NULL,
     "readme"        TEXT,
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS "public"."bulk_edit_config" (
     "updated_by"    INTEGER,
     PRIMARY KEY ("id"),
     UNIQUE ("api_version", "kind")
-);
+    );
 
 -- Insert data from bulk_update_readme (assuming only one row for 'v1beta1/application')
 INSERT INTO "public"."bulk_edit_config" ("api_version", "kind", "readme", "schema", "created_on", "created_by")
@@ -205,9 +205,386 @@ The following tables list the configurable parameters of the Payload component i
     }
   }
 }', NOW(), 1
-WHERE NOT EXISTS (
+    WHERE NOT EXISTS (
     SELECT 1 FROM "public"."bulk_edit_config"
     WHERE api_version = 'v1beta1' AND kind = 'application'
+);
+
+-- Insert data from bulk_update_readme (assuming only one row for 'v1beta1/application')
+INSERT INTO "public"."bulk_edit_config" ("api_version", "kind", "readme", "schema", "created_on", "created_by")
+SELECT 'v1beta2', 'application','# Bulk Update - Application
+
+This feature helps you to update Deployment Template, ConfigMaps & Secrets for multiple apps in one go! You can filter the apps on the basis of environments, global flag, and app names(we provide support for both substrings included and excluded in the app name).
+
+## Example
+
+Example below will select all applications having `abc and xyz` present in their name and out of those will exclude applications having `abcd and xyza` in their name. Since global flag is false and envId 23 is provided, it will make changes in envId 23 and not in global deployment template for this application.
+
+If you want to update globally then please set `global: true`. If you have provided envId by deployment template, configMap or secret is not overridden for that particular environment then it will not apply the changes.
+Also, of all the provided names of configMaps/secrets, for every app & environment override only the name that are present in them will be considered.
+
+```
+apiVersion: batch/v1beta2
+kind: Application
+spec:
+  selectors:
+    match:
+      type: criteria|global
+      project:
+        includes:
+          names:
+            - "%abc%"
+            - "%xyz%"
+        excludes:
+          names:
+            - "%abcd%"
+            - "%xyza%"
+      app:
+        includes:
+          names:
+            - "%abc%"
+            - "%xyz%"
+        excludes:
+          names:
+            - "%abcd%"
+            - "%xyza%"
+      env:
+        type: prod|non-prod
+        includes:
+          names:
+            - "%abc%"
+            - "%xyz%"
+        excludes:
+          names:
+            - "%abcd%"
+            - "%xyza%"
+  deploymentTemplate:
+    match:
+      include-base-config: true|false
+      chart:
+        name: RolloutDeployment
+        version:
+          value: 4.20.0
+          operator: EQUAL|GREATER|LESS|GREATER_EQUAL|LESS_EQUAL
+        custom: true|false
+    operation:
+      action: update
+      field: values|version
+      patchJson: ''[{ "op": "add", "path": "/{key}", "value": "{value}" },{"op": "replace","path":"/{key}","value": "{value}"}]''
+      chartVersion: "4.20.0"
+  configMap:
+    spec:
+      match:
+        type: environment|volume
+        include-base-config: true|false
+        includes:
+          names:
+            - "%abc%"
+            - "%xyz%"
+        excludes:
+          names:
+            - "%abcd%"
+            - "%xyza%"
+      operation:
+        action: create|update|delete
+        field: data
+        patchJson: ''[{ "op": "add", "path": "/{key}", "value": "{value}" },{"op": "replace","path":"/{key}","value": "{value}"}]''
+        value: {{ .Value }}
+  secret:
+    spec:
+      match:
+        type: environment|volume
+        include-base-config: true|false
+        includes:
+          names:
+            - "%abc%"
+            - "%xyz%"
+        excludes:
+          names:
+            - "%abcd%"
+            - "%xyza%"
+      operation:
+        action: create|update|delete
+        field: data
+        patchJson: ''[{ "op": "add", "path": "/{key}", "value": "{value}" },{"op": "replace","path":"/{key}","value": "{value}"}]''
+        value: {{ .Value }}
+```
+
+## Payload Configuration
+
+
+The following tables list the configurable parameters of the Payload component in the Script and their description along with example. Also, if you do not need to apply updates on all the tasks, i.e. Deployment Template, ConfigMaps & Secrets, leave the Spec object empty for that respective task.
+
+| Parameter                      | Description                        | Example                                                    |
+| -------------------------- | ---------------------------------- | ---------------------------------------------------------- |
+| `includes.names `        | Will filter apps having exact string or similar substrings                 | `["app%","%abc", "xyz"]` (will include all apps having `"app%"` **OR** `"%abc"` as one of their substring, example - app1, app-test, test-abc etc. **OR** application with name xyz)    |
+| `excludes.names`          | Will filter apps not having exact string or similar substrings.              | `["%z","%y", "abc"]`       (will filter out all apps having `"%z"` **OR** `"%y"` as one of their substring, example - appz, test-app-y etc. **OR** application with name abc)                                        |
+| `envIds`       | List of envIds to be updated for the selected applications.           | `[1,2,3]`                                                   |
+| `global`       | Flag to update global deployment template of applications.            | `true`,`false`                                                        |
+| `deploymentTemplate.spec.patchJson`       | String having the update operation(you can apply more than one changes at a time). It supports [JSON patch ](http://jsonpatch.com/) specifications for update. | `''[ { "op": "add", "path": "/MaxSurge", "value": 1 }, { "op": "replace", "path": "/GracePeriod", "value": "30" }]''` |
+| `configMap.spec.names`      | Names of all ConfigMaps to be updated. | `configmap1`,`configmap2`,`configmap3` |
+| `secret.spec.names`      | Names of all Secrets to be updated. | `secret1`,`secret2`|
+| `configMap.spec.patchJson` / `secret.spec.patchJson`       | String having the update operation for ConfigMaps/Secrets(you can apply more than one changes at a time). It supports [JSON patch ](http://jsonpatch.com/) specifications for update. | `''[{ "op": "add", "path": "/{key}", "value": "{value}" },{"op": "replace","path":"/{key}","value": "{value}"}]''`(Replace the `{key}` part to the key you want to perform operation on & the `{value}`is the key''s corresponding value |
+','{
+  "$schema": "https://json-schema.org/draft/2020-12/schema",
+  "title": "Bulk Edit Application batch/v1beta2 Schema",
+  "type": "object",
+  "description": "Input Script for bulk edit",
+  "required": [
+    "apiVersion",
+    "kind",
+    "spec"
+  ],
+  "properties": {
+    "apiVersion": {
+      "const": "batch/v1beta2"
+    },
+    "kind": {
+      "const": "Application"
+    },
+    "spec": {
+      "type": "object",
+      "properties": {
+        "anyOf": [
+          {
+            "required": [
+              "deploymentTemplate"
+            ]
+          },
+          {
+            "required": [
+              "configMap"
+            ]
+          },
+          {
+            "required": [
+              "secret"
+            ]
+          }
+        ],
+        "selectors": {
+          "type": "object",
+          "properties": {
+            "match": {
+              "type": "object",
+              "properties": {
+                "type": {
+                  "type": "string",
+                  "enum": ["criteria", "global"]
+                },
+                "project": {
+                  "type": "object",
+                  "properties": {
+                    "includes": {
+                      "$ref": "#/definitions/NameIncludesExcludes"
+                    },
+                    "excludes": {
+                      "$ref": "#/definitions/NameIncludesExcludes"
+                    }
+                  }
+                },
+                "app": {
+                  "type": "object",
+                  "properties": {
+                    "includes": {
+                      "$ref": "#/definitions/NameIncludesExcludes"
+                    },
+                    "excludes": {
+                      "$ref": "#/definitions/NameIncludesExcludes"
+                    }
+                  }
+                },
+                "env": {
+                  "type": "object",
+                  "properties": {
+                    "type": {
+                      "type": "string",
+                      "enum": ["prod", "non-prod"]
+                    },
+                    "includes": {
+                      "$ref": "#/definitions/NameIncludesExcludes"
+                    },
+                    "excludes": {
+                      "$ref": "#/definitions/NameIncludesExcludes"
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "deploymentTemplate": {
+          "type": "object",
+          "properties": {
+            "match": {
+              "type": "object",
+              "properties": {
+                "include-base-config": {
+                  "type": "boolean"
+                },
+                "chart": {
+                  "type": "object",
+                  "properties": {
+                    "name": {
+                      "type": "string"
+                    },
+                    "version": {
+                      "type": "object",
+                      "properties": {
+                        "value": {
+                          "type": "string",
+                          "pattern": "^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$"
+                        },
+                        "operator": {
+                          "type": "string",
+                          "enum": ["EQUAL", "GREATER", "LESS", "GREATER_EQUAL", "LESS_EQUAL"]
+                        }
+                      }
+                    },
+                    "custom": {
+                      "type": "boolean"
+                    }
+                  }
+                }
+              }
+            },
+            "operation": {
+              "type": "object",
+              "properties": {
+                "action": {
+                  "type": "string",
+                  "enum": ["update"]
+                },
+                "field": {
+                  "type": "string",
+                  "enum": ["values", "version"]
+                },
+                "patchJson": {
+                  "type": "string"
+                },
+                "chartVersion": {
+                  "type": "string",
+                  "pattern": "^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$"
+                }
+              }
+            }
+          }
+        },
+        "configMap": {
+          "type": "object",
+          "properties": {
+            "spec": {
+              "type": "object",
+              "properties": {
+                "match": {
+                  "type": "object",
+                  "properties": {
+                    "type": {
+                      "type": "string",
+                      "enum": ["environment", "volume"]
+                    },
+                    "include-base-config": {
+                      "type": "boolean"
+                    },
+                    "includes": {
+                      "$ref": "#/definitions/NameIncludesExcludes"
+                    },
+                    "excludes": {
+                      "$ref": "#/definitions/NameIncludesExcludes"
+                    }
+                  }
+                },
+                "operation": {
+                  "type": "object",
+                  "properties": {
+                    "action": {
+                      "type": "string",
+                      "enum": ["create", "update", "delete"]
+                    },
+                    "field": {
+                      "type": "string",
+                      "enum": ["data"]
+                    },
+                    "patchJson": {
+                      "type": "string"
+                    },
+                    "value": {
+                      "type": "string"
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "secret": {
+          "type": "object",
+          "properties": {
+            "spec": {
+              "type": "object",
+              "properties": {
+                "match": {
+                  "type": "object",
+                  "properties": {
+                    "type": {
+                      "type": "string",
+                      "enum": ["environment", "volume"]
+                    },
+                    "include-base-config": {
+                      "type": "boolean"
+                    },
+                    "includes": {
+                      "$ref": "#/definitions/NameIncludesExcludes"
+                    },
+                    "excludes": {
+                      "$ref": "#/definitions/NameIncludesExcludes"
+                    }
+                  }
+                },
+                "operation": {
+                  "type": "object",
+                  "properties": {
+                    "action": {
+                      "type": "string",
+                      "enum": ["create", "update", "delete"]
+                    },
+                    "field": {
+                      "type": "string",
+                      "enum": ["data"]
+                    },
+                    "patchJson": {
+                      "type": "string"
+                    },
+                    "value": {
+                      "type": "string"
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+  "definitions": {
+    "NameIncludesExcludes": {
+      "type": "object",
+      "properties": {
+        "names": {
+          "type": "array",
+          "items": {
+            "type": "string",
+            "pattern": "^[a-z%]+[a-z0-9%\\-\\?]*[a-z0-9%]+$"
+          }
+        }
+      }
+    }
+  }
+}', NOW(), 1
+    WHERE NOT EXISTS (
+    SELECT 1 FROM "public"."bulk_edit_config"
+    WHERE api_version = 'v1beta2' AND kind = 'application'
 );
 
 COMMIT;
