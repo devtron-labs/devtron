bbb3ec7aa7d12620adb4f3c390f59aa2af98fdbf
vivekanandan-devtron <vivekanandan.gm@devtron.ai>
2026-09-16 12:19:24 +0530

    feat: RBAC support for external argo/flux apps (#7014)
    * chore: create a makefile to setup and clean resources for development
    
    * feat: add flag for frontend to show argo/flux tabs
    
    - add migration files
    - append flags in the /orchestrator/user/check/roles API to show/hide argo/flux tabs
    
    * feat: changes for dropdown API in user permissions page
    
    * fix: missing dependencies in constructor method
    
    * fix: improper filtering logic
    
    - earlier filter was only applied with the cluster ID
    - add changes to filter based on cluster ID's and namespaces
    
    * fix: prevent connecting to kubelink for failed clusters
    
    * wip: policy enforcements
    
    * wip: policy enforcements
    
    * chore: remove test folder
    
    * fix: code review fixes
    
    * fix: code review fixes
    
    * fix: code review fixes
    
    * chore: rename migration scripts to maintain sync with enterprise
    
    * chore: change migration script number
    
    * fix: roles missing while fetching user roles
    
    * fix: deleted roles being listed
    
    * fix: deleted roles being listed
    
    * fix: review fixes

--- pkg/userResource/logicRouteService.go
+++ pkg/userResource/logicRouteService.go
@@ -3,6 +3,7 @@ package userResource
 import (
 	"context"
 	"fmt"
+
 	apiBean "github.com/devtron-labs/devtron/api/userResource/bean"
 	bean2 "github.com/devtron-labs/devtron/pkg/auth/user/bean"
 	"github.com/devtron-labs/devtron/pkg/userResource/bean"
@@ -17,12 +18,16 @@ func getUserResourceKindWithVersionKey(kind bean.UserResourceKind, version bean.
 
 var mapOfUserResourceKindToAllResourceOptionsFunc = map[string]func(impl *UserResourceServiceImpl, context context.Context, token string, reqBean *apiBean.ResourceOptionsReqDto, params *apiBean.PathParams) (*bean.ResourceOptionsDto, error){
 	getUserResourceKindWithVersionKey(bean.KindTeam, bean.Alpha1Version):            (*UserResourceServiceImpl).getTeamResourceOptions,
-	getUserResourceKindWithVersionKey(bean.KindHelmEnvironment, bean.Alpha1Version): (*UserResourceServiceImpl).getHelmEnvResourceOptions,
+	getUserResourceKindWithVersionKey(bean.KindHelmEnvironment, bean.Alpha1Version): (*UserResourceServiceImpl).getCombinedEnvResourceOptions,
 	getUserResourceKindWithVersionKey(bean.KindHelmApplication, bean.Alpha1Version): (*UserResourceServiceImpl).getHelmAppResourceOptions,
 	getUserResourceKindWithVersionKey(bean.KindCluster, bean.Alpha1Version):         (*UserResourceServiceImpl).getClusterResourceOptions,
 	getUserResourceKindWithVersionKey(bean.ClusterApiResources, bean.Alpha1Version): (*UserResourceServiceImpl).getClusterApiResourceOptions,
 	getUserResourceKindWithVersionKey(bean.ClusterNamespaces, bean.Alpha1Version):   (*UserResourceServiceImpl).getClusterNamespacesResourceOptions,
 	getUserResourceKindWithVersionKey(bean.ClusterResources, bean.Alpha1Version):    (*UserResourceServiceImpl).getClusterResourceListOptions,
+	getUserResourceKindWithVersionKey(bean.KindArgoEnvironment, bean.Alpha1Version): (*UserResourceServiceImpl).getCombinedEnvResourceOptions,
+	getUserResourceKindWithVersionKey(bean.KindFluxEnvironment, bean.Alpha1Version): (*UserResourceServiceImpl).getCombinedEnvResourceOptions,
+	getUserResourceKindWithVersionKey(bean.KindArgoApplication, bean.Alpha1Version): (*UserResourceServiceImpl).getArgoAppResourceOptions,
+	getUserResourceKindWithVersionKey(bean.KindFluxApplication, bean.Alpha1Version): (*UserResourceServiceImpl).getFluxAppResourceOptions,
 }
 
 func getAllResourceOptionsFunc(kind bean.UserResourceKind, version bean.Version) func(impl *UserResourceServiceImpl, context context.Context, token string, reqBean *apiBean.ResourceOptionsReqDto, params *apiBean.PathParams) (*bean.ResourceOptionsDto, error) {
@@ -34,7 +39,7 @@ func getAllResourceOptionsFunc(kind bean.UserResourceKind, version bean.Version)
 
 var mapOfUserResourceKindToAllResourceOptionsExtendedFunc = map[string]func(impl *UserResourceExtendedServiceImpl, context context.Context, token string, reqBean *apiBean.ResourceOptionsReqDto, params *apiBean.PathParams) (*bean.ResourceOptionsDto, error){
 	getUserResourceKindWithVersionKey(bean.KindTeam, bean.Alpha1Version):               (*UserResourceExtendedServiceImpl).getTeamResourceOptions,
-	getUserResourceKindWithVersionKey(bean.KindHelmEnvironment, bean.Alpha1Version):    (*UserResourceExtendedServiceImpl).getHelmEnvResourceOptions,
+	getUserResourceKindWithVersionKey(bean.KindHelmEnvironment, bean.Alpha1Version):    (*UserResourceExtendedServiceImpl).getCombinedEnvResourceOptions,
 	getUserResourceKindWithVersionKey(bean.KindHelmApplication, bean.Alpha1Version):    (*UserResourceExtendedServiceImpl).getHelmAppResourceOptions,
 	getUserResourceKindWithVersionKey(bean.KindCluster, bean.Alpha1Version):            (*UserResourceExtendedServiceImpl).getClusterResourceOptions,
 	getUserResourceKindWithVersionKey(bean.ClusterApiResources, bean.Alpha1Version):    (*UserResourceExtendedServiceImpl).getClusterApiResourceOptions,
@@ -45,6 +50,10 @@ var mapOfUserResourceKindToAllResourceOptionsExtendedFunc = map[string]func(impl
 	getUserResourceKindWithVersionKey(bean.KindChartGroup, bean.Alpha1Version):         (*UserResourceExtendedServiceImpl).getChartGroupResourceOptions,
 	getUserResourceKindWithVersionKey(bean.KindJobs, bean.Alpha1Version):               (*UserResourceExtendedServiceImpl).getJobsResourceOptions,
 	getUserResourceKindWithVersionKey(bean.KindWorkflow, bean.Alpha1Version):           (*UserResourceExtendedServiceImpl).getAppWfsResourceOptions,
+	getUserResourceKindWithVersionKey(bean.KindArgoEnvironment, bean.Alpha1Version):    (*UserResourceExtendedServiceImpl).getCombinedEnvResourceOptions,
+	getUserResourceKindWithVersionKey(bean.KindFluxEnvironment, bean.Alpha1Version):    (*UserResourceExtendedServiceImpl).getCombinedEnvResourceOptions,
+	getUserResourceKindWithVersionKey(bean.KindArgoApplication, bean.Alpha1Version):    (*UserResourceExtendedServiceImpl).getArgoAppResourceOptions,
+	getUserResourceKindWithVersionKey(bean.KindFluxApplication, bean.Alpha1Version):    (*UserResourceExtendedServiceImpl).getFluxAppResourceOptions,
 }
 
 func getAllResourceOptionsExtendedFunc(kind bean.UserResourceKind, version bean.Version) func(impl *UserResourceExtendedServiceImpl, context context.Context, token string, reqBean *apiBean.ResourceOptionsReqDto, params *apiBean.PathParams) (*bean.ResourceOptionsDto, error) {
@@ -57,11 +66,15 @@ func getAllResourceOptionsExtendedFunc(kind bean.UserResourceKind, version bean.
 var mapOfKindWithEntityAccessTypeKeyToResourceOptionRbacFunc = map[string]func(impl *UserResourceServiceImpl, token string, params *apiBean.PathParams, resourceOptions *bean.ResourceOptionsDto) (*bean.UserResourceResponseDto, error){
 	getUserResourceKindWithEntityAccessKey(bean.KindTeam, bean.Alpha1Version, bean2.ENTITY_APPS, bean2.APP_ACCESS_TYPE_HELM):            (*UserResourceServiceImpl).enforceRbacForTeamForHelmApp,
 	getUserResourceKindWithEntityAccessKey(bean.KindHelmApplication, bean.Alpha1Version, bean2.ENTITY_APPS, bean2.APP_ACCESS_TYPE_HELM): (*UserResourceServiceImpl).enforceRbacForHelmAppsListing,
-	getUserResourceKindWithEntityAccessKey(bean.KindHelmEnvironment, bean.Alpha1Version, bean2.ENTITY_APPS, bean2.APP_ACCESS_TYPE_HELM): (*UserResourceServiceImpl).enforceRbacForEnvForHelmApp,
+	getUserResourceKindWithEntityAccessKey(bean.KindHelmEnvironment, bean.Alpha1Version, bean2.ENTITY_APPS, bean2.APP_ACCESS_TYPE_HELM): (*UserResourceServiceImpl).enforceRbacForEnvOptions,
 	getUserResourceKindWithEntityAccessKey(bean.KindCluster, bean.Alpha1Version, bean2.CLUSTER_ENTITIY, bean2.EmptyAccessType):          (*UserResourceServiceImpl).enforceRbacForClusterList,
 	getUserResourceKindWithEntityAccessKey(bean.ClusterApiResources, bean.Alpha1Version, bean2.CLUSTER_ENTITIY, bean2.EmptyAccessType):  (*UserResourceServiceImpl).enforceRbacForClusterApiResource,
 	getUserResourceKindWithEntityAccessKey(bean.ClusterNamespaces, bean.Alpha1Version, bean2.CLUSTER_ENTITIY, bean2.EmptyAccessType):    (*UserResourceServiceImpl).enforceRbacForClusterNamespaces,
 	getUserResourceKindWithEntityAccessKey(bean.ClusterResources, bean.Alpha1Version, bean2.CLUSTER_ENTITIY, bean2.EmptyAccessType):     (*UserResourceServiceImpl).enforceRbacForClusterResourceList,
+	getUserResourceKindWithEntityAccessKey(bean.KindArgoEnvironment, bean.Alpha1Version, bean2.ENTITY_APPS, bean2.APP_ACCESS_TYPE_ARGO): (*UserResourceServiceImpl).enforceRbacForEnvOptions,
+	getUserResourceKindWithEntityAccessKey(bean.KindFluxEnvironment, bean.Alpha1Version, bean2.ENTITY_APPS, bean2.APP_ACCESS_TYPE_FLUX): (*UserResourceServiceImpl).enforceRbacForEnvOptions,
+	getUserResourceKindWithEntityAccessKey(bean.KindArgoApplication, bean.Alpha1Version, bean2.ENTITY_APPS, bean2.APP_ACCESS_TYPE_ARGO): (*UserResourceServiceImpl).enforceRbacForArgoAppsListing,
+	getUserResourceKindWithEntityAccessKey(bean.KindFluxApplication, bean.Alpha1Version, bean2.ENTITY_APPS, bean2.APP_ACCESS_TYPE_FLUX): (*UserResourceServiceImpl).enforceRbacForFluxAppsListing,
 }
 
 func getResourceOptionRbacFunc(kind bean.UserResourceKind, version bean.Version, entity string, accessType string) func(impl *UserResourceServiceImpl, token string, params *apiBean.PathParams, resourceOptions *bean.ResourceOptionsDto) (*bean.UserResourceResponseDto, error) {
@@ -74,7 +87,7 @@ func getResourceOptionRbacFunc(kind bean.UserResourceKind, version bean.Version,
 var mapOfKindWithEntityAccessTypeKeyToResourceOptionRbacExtendedFunc = map[string]func(impl *UserResourceExtendedServiceImpl, token string, params *apiBean.PathParams, resourceOptions *bean.ResourceOptionsDto) (*bean.UserResourceResponseDto, error){
 	getUserResourceKindWithEntityAccessKey(bean.KindTeam, bean.Alpha1Version, bean2.ENTITY_APPS, bean2.APP_ACCESS_TYPE_HELM):            (*UserResourceExtendedServiceImpl).enforceRbacForTeamForHelmApp,
 	getUserResourceKindWithEntityAccessKey(bean.KindHelmApplication, bean.Alpha1Version, bean2.ENTITY_APPS, bean2.APP_ACCESS_TYPE_HELM): (*UserResourceExtendedServiceImpl).enforceRbacForHelmAppsListing,
-	getUserResourceKindWithEntityAccessKey(bean.KindHelmEnvironment, bean.Alpha1Version, bean2.ENTITY_APPS, bean2.APP_ACCESS_TYPE_HELM): (*UserResourceExtendedServiceImpl).enforceRbacForEnvForHelmApp,
+	getUserResourceKindWithEntityAccessKey(bean.KindHelmEnvironment, bean.Alpha1Version, bean2.ENTITY_APPS, bean2.APP_ACCESS_TYPE_HELM): (*UserResourceExtendedServiceImpl).enforceRbacForEnvOptions,
 	getUserResourceKindWithEntityAccessKey(bean.KindCluster, bean.Alpha1Version, bean2.CLUSTER_ENTITIY, bean2.EmptyAccessType):          (*UserResourceExtendedServiceImpl).enforceRbacForClusterList,
 	getUserResourceKindWithEntityAccessKey(bean.ClusterApiResources, bean.Alpha1Version, bean2.CLUSTER_ENTITIY, bean2.EmptyAccessType):  (*UserResourceExtendedServiceImpl).enforceRbacForClusterApiResource,
 	getUserResourceKindWithEntityAccessKey(bean.ClusterNamespaces, bean.Alpha1Version, bean2.CLUSTER_ENTITIY, bean2.EmptyAccessType):    (*UserResourceExtendedServiceImpl).enforceRbacForClusterNamespaces,
@@ -87,6 +100,10 @@ var mapOfKindWithEntityAccessTypeKeyToResourceOptionRbacExtendedFunc = map[strin
 	getUserResourceKindWithEntityAccessKey(bean.KindChartGroup, bean.Alpha1Version, bean2.CHART_GROUP_ENTITY, bean2.EmptyAccessType):    (*UserResourceExtendedServiceImpl).enforceRbacForChartGroup,
 	getUserResourceKindWithEntityAccessKey(bean.KindJobs, bean.Alpha1Version, bean2.EntityJobs, bean2.EmptyAccessType):                  (*UserResourceExtendedServiceImpl).enforceRbacForJobs,
 	getUserResourceKindWithEntityAccessKey(bean.KindWorkflow, bean.Alpha1Version, bean2.EntityJobs, bean2.EmptyAccessType):              (*UserResourceExtendedServiceImpl).enforceRbacForJobsWfs,
+	getUserResourceKindWithEntityAccessKey(bean.KindArgoEnvironment, bean.Alpha1Version, bean2.ENTITY_APPS, bean2.APP_ACCESS_TYPE_ARGO): (*UserResourceExtendedServiceImpl).enforceRbacForEnvOptions,
+	getUserResourceKindWithEntityAccessKey(bean.KindFluxEnvironment, bean.Alpha1Version, bean2.ENTITY_APPS, bean2.APP_ACCESS_TYPE_FLUX): (*UserResourceExtendedServiceImpl).enforceRbacForEnvOptions,
+	getUserResourceKindWithEntityAccessKey(bean.KindArgoApplication, bean.Alpha1Version, bean2.ENTITY_APPS, bean2.APP_ACCESS_TYPE_ARGO): (*UserResourceExtendedServiceImpl).enforceRbacForArgoAppsListing,
+	getUserResourceKindWithEntityAccessKey(bean.KindFluxApplication, bean.Alpha1Version, bean2.ENTITY_APPS, bean2.APP_ACCESS_TYPE_FLUX): (*UserResourceExtendedServiceImpl).enforceRbacForFluxAppsListing,
 }
 
 func getResourceOptionRbacExtendedFunc(kind bean.UserResourceKind, version bean.Version, entity string, accessType string) func(impl *UserResourceExtendedServiceImpl, token string, params *apiBean.PathParams, resourceOptions *bean.ResourceOptionsDto) (*bean.UserResourceResponseDto, error) {
